211d627a96a7ab62ef479452c86ae176edadecd5
Alex Tran <alex.tran1502@gmail.com>
2022-02-05 23:55:05 -0600

    Video player working correctly

--- mobile/lib/modules/home/ui/image_grid.dart
+++ mobile/lib/modules/home/ui/image_grid.dart
@@ -30,7 +30,7 @@ class ImageGrid extends ConsumerWidget {
                           child: Row(
                             children: [
                               Text(
-                                assetGroup[index].duration.toString().substring(0, 7),
+                                assetGroup[index].duration.toString(),
                                 style: const TextStyle(
                                   color: Colors.white,
                                   fontSize: 10,
