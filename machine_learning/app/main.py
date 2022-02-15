b7b86151e8733ebc3e662621c655844cd4e4bee2
Alex Tran <alex.tran1502@gmail.com>
2022-02-14 21:56:09 -0600

    Added support for gpu in docker file

--- machine_learning/app/main.py
+++ machine_learning/app/main.py
@@ -1,17 +1,31 @@
 from typing import Optional
 
 from fastapi import FastAPI
-import tensorflow as tf
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
+import numpy as np
+import os
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    print(tf.__version__)
-    return {"Hello": "World 312"}
+
+    return os.getcwd()
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/predict")
+def predict():
+    model = VGG16(weights='imagenet', include_top=False)
+
+    img_path = './app/upload/db6e94e1-ab1d-4ff0-a3b7-ba7d9e7b9d84/thumb/76f0308a9a53080e/zMGrPdjRoOU.jpeg'
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+
+    features = model.predict(x)
+
+    return "ok"
