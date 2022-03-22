31c18ff34ce47442404a076feaad89c73f83789e
Alex Tran <alex.tran1502@gmail.com>
2022-03-22 01:57:40 -0500

    Fixed Docker and Fastlane build issue

--- server/src/modules/image-optimize/image-optimize.processor.ts
+++ server/src/modules/image-optimize/image-optimize.processor.ts
@@ -1,15 +1,7 @@
-import { InjectQueue, Process, Processor } from '@nestjs/bull';
+import { Processor } from '@nestjs/bull';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Job, Queue } from 'bull';
 import { Repository } from 'typeorm';
 import { AssetEntity } from '../../api-v1/asset/entities/asset.entity';
-import sharp from 'sharp';
-import { existsSync, mkdirSync, readFile } from 'fs';
-import { ConfigService } from '@nestjs/config';
-import ffmpeg from 'fluent-ffmpeg';
-import { APP_UPLOAD_LOCATION } from '../../constants/upload_location.constant';
-import { WebSocketServer } from '@nestjs/websockets';
-import { Socket, Server as SocketIoServer } from 'socket.io';
 import { CommunicationGateway } from '../../api-v1/communication/communication.gateway';
 import { BackgroundTaskService } from '../background-task/background-task.service';
 
