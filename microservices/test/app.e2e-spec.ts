195c28006a78e88ac827d367befc1bdf4dec7ea3
Alex Tran <alex.tran1502@gmail.com>
2022-03-25 15:44:38 -0500

    Added tRPC

--- microservices/test/app.e2e-spec.ts
+++ microservices/test/app.e2e-spec.ts
@@ -1,8 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
+// End to End test
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
