5c276dce4f038a0ef0e3d240773464d6ea72bcf0
Alex <alex.tran1502@gmail.com>
2022-04-13 19:55:48 -0500

    Added selected user list

--- mobile/lib/modules/sharing/views/select_user_for_sharing_page.dart
+++ mobile/lib/modules/sharing/views/select_user_for_sharing_page.dart
@@ -1,36 +1,87 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:immich_mobile/modules/sharing/providers/suggested_shared_users.provider.dart';
 import 'package:immich_mobile/shared/models/user_info.model.dart';
-import 'package:immich_mobile/shared/services/user.service.dart';
+import 'package:immich_mobile/shared/ui/immich_loading_indicator.dart';
 
 class SelectUserForSharingPage extends HookConsumerWidget {
   const SelectUserForSharingPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final ValueNotifier<List<UserInfo>> userList = useState([]);
+    final sharedUsersList = useState<List<UserInfo>>([]);
+    AsyncValue<List<UserInfo>> suggestedShareUsers = ref.watch(suggestedSharedUsersProvider);
 
-    _getAllUsers() async {
-      UserService userService = UserService();
-      userList.value = await userService.getAllUsersInfo();
+    _buildUserList(List<UserInfo> users) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 50,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              shrinkWrap: true,
+              itemBuilder: ((context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Chip(label: Text(sharedUsersList.value[index].email)),
+                );
+              }),
+              itemCount: sharedUsersList.value.length,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'Suggestions',
+              style: TextStyle(fontSize: 14, color: Colors.grey, fontWeight: FontWeight.bold),
+            ),
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemBuilder: ((context, index) {
+              return ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: const AssetImage('assets/immich-logo-no-outline.png'),
+                  backgroundColor: Theme.of(context).primaryColor.withAlpha(50),
+                ),
+                title: Text(
+                  users[index].email,
+                  style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                ),
+                onTap: () {
+                  sharedUsersList.value = [...sharedUsersList.value, users[index]];
+                },
+              );
+            }),
+            itemCount: users.length,
+          ),
+        ],
+      );
     }
 
-    useEffect(() {
-      _getAllUsers();
-      return null;
-    }, []);
-
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Invite to album'),
-        ),
-        body: ListView.builder(
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(userList.value[index].email),
-            );
+      appBar: AppBar(
+        title: const Text('Invite to album'),
+        elevation: 0,
+        centerTitle: false,
+        leading: IconButton(
+          icon: const Icon(Icons.close_rounded),
+          onPressed: () async {
+            AutoRouter.of(context).pop();
           },
-          itemCount: userList.value.length,
-        ));
+        ),
+      ),
+      body: suggestedShareUsers.when(
+        data: (users) {
+          return _buildUserList(users);
+        },
+        error: (e, _) => Text("Error loading suggested users $e"),
+        loading: () => const Center(
+          child: ImmichLoadingIndicator(),
+        ),
+      ),
+    );
   }
 }
