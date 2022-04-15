1b90f3e4f5a7c2f08f3a479e78db7df902319cea
Alex <alex.tran1502@gmail.com>
2022-04-15 12:05:18 -0500

    Added header for album

--- mobile/lib/modules/sharing/views/album_viewer_page.dart
+++ mobile/lib/modules/sharing/views/album_viewer_page.dart
@@ -1,41 +1,11 @@
-import 'dart:math';
-
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/modules/sharing/models/shared_album.model.dart';
 import 'package:immich_mobile/modules/sharing/providers/shared_album.provider.dart';
 import 'package:immich_mobile/shared/ui/immich_loading_indicator.dart';
-
-// 1
-class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
-  final double minHeight;
-  final double maxHeight;
-  final Widget child;
-
-  _SliverAppBarDelegate({
-    required this.minHeight,
-    required this.maxHeight,
-    required this.child,
-  });
-
-  @override
-  double get minExtent => minHeight;
-
-  @override
-  double get maxExtent => max(maxHeight, minHeight);
-
-  // 2
-  @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return SizedBox.expand(child: child);
-  }
-
-  // 3
-  @override
-  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
-    return maxHeight != oldDelegate.maxHeight || minHeight != oldDelegate.minHeight || child != oldDelegate.child;
-  }
-}
+import 'package:immich_mobile/shared/ui/immich_sliver_persistent_app_bar_delegate.dart';
+import 'package:intl/intl.dart';
 
 class AlbumViewerPage extends HookConsumerWidget {
   final String albumId;
@@ -46,28 +16,83 @@ class AlbumViewerPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     AsyncValue<SharedAlbum> _albumInfo = ref.watch(sharedAlbumDetailProvider(albumId));
 
+    Widget _buildTitle(String title) {
+      return Padding(
+        padding: const EdgeInsets.only(left: 16.0, top: 16),
+        child: Text(
+          title,
+          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      );
+    }
+
+    Widget _buildAlbumDateRange(SharedAlbum albumInfo) {
+      if (albumInfo.sharedAssets != null && albumInfo.sharedAssets!.isNotEmpty) {
+        String startDate = "";
+        DateTime parsedStartDate = DateTime.parse(albumInfo.sharedAssets!.first.assetInfo.createdAt);
+        DateTime parsedEndDate = DateTime.parse(albumInfo.sharedAssets!.last.assetInfo.createdAt);
+
+        if (parsedStartDate.year == parsedEndDate.year) {
+          startDate = DateFormat('LLL d').format(parsedStartDate);
+        } else {
+          startDate = DateFormat('LLL d, y').format(parsedStartDate);
+        }
+
+        String endDate = DateFormat('LLL d, y').format(parsedEndDate);
+
+        return Padding(
+          padding: const EdgeInsets.only(left: 16.0, top: 8),
+          child: Text(
+            "$startDate-$endDate",
+            style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.grey),
+          ),
+        );
+      } else {
+        return Container();
+      }
+    }
+
+    Widget _buildHeader(SharedAlbum albumInfo) {
+      return SliverToBoxAdapter(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildTitle(albumInfo.albumName),
+            _buildAlbumDateRange(albumInfo),
+            SizedBox(
+              height: 60,
+              child: ListView.builder(
+                padding: const EdgeInsets.only(left: 16),
+                scrollDirection: Axis.horizontal,
+                itemBuilder: ((context, index) {
+                  return const Padding(
+                    padding: EdgeInsets.only(right: 8.0),
+                    child: CircleAvatar(
+                      radius: 15,
+                      child: Text(
+                        'TU',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    ),
+                  );
+                }),
+                itemCount: albumInfo.sharedUsers.length,
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
     _buildBody(SharedAlbum albumInfo) {
       return CustomScrollView(
         slivers: [
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Container(
-                  alignment: Alignment.center,
-                  color: Colors.lightBlue[100 * (index % 9)],
-                  child: Text('list item $index'),
-                );
-              },
-              childCount: 10,
-            ),
-          ),
+          _buildHeader(albumInfo),
           SliverPersistentHeader(
             pinned: true,
-            delegate: _SliverAppBarDelegate(
-              // 2
+            delegate: ImmichSliverPersistentAppBarDelegate(
               minHeight: 40,
               maxHeight: 70,
-              // 3
               child: Container(
                 color: Colors.indigo,
                 child: const Center(
@@ -95,8 +120,13 @@ class AlbumViewerPage extends HookConsumerWidget {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
+        leading: IconButton(
+          onPressed: () => AutoRouter.of(context).pop(),
+          icon: const Icon(Icons.arrow_back_ios_rounded),
+        ),
         actions: [
           IconButton(
+            splashRadius: 25,
             onPressed: () {},
             icon: const Icon(Icons.more_horiz_rounded),
           ),
