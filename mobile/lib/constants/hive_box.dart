da9eb615320bb47ca8fb49c982ec9e521556ff2b
Alex <alex.tran1502@gmail.com>
2022-04-24 21:33:10 -0500

    Implemented remembering login data with radio button (#126)

--- mobile/lib/constants/hive_box.dart
+++ mobile/lib/constants/hive_box.dart
@@ -3,9 +3,9 @@ const String userInfoBox = "immichBoxUserInfo"; // Box
 const String accessTokenKey = "immichBoxAccessTokenKey"; // Key 1
 const String deviceIdKey = 'immichBoxDeviceIdKey'; // Key 2
 
-// SERVER ENDPOINT
+// Server endpoint
 const String serverEndpointKey = 'immichBoxServerEndpoint';
 
-// KEY
-const String hiveAllAsssetKey = "allAssets";
-const String hiveBackupProgressKey = "backupProgressAssets";
+// Login Info
+const String hiveLoginInfoBox = "immichLoginInfoBox";
+const String savedLoginInfoKey = "immichSavedLoginInfoKey";
