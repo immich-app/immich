ce06af0c9be0e7fc46fffe8f947dc33eca0a55d6
Alex <alex.tran1502@gmail.com>
2022-05-22 04:48:38 -0500

    Fixed lodash library not invoking in production build (#171)
    * Added staging docker-compose file
    * Use lodash-es and remove hydration option on photos page fixed the problem

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -3,8 +3,6 @@ import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
