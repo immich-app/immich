4a25e7dc229931c9a6f962e5e67f5509ff46a02b
Alex Tran <alex.tran1502@gmail.com>
2022-05-21 23:36:58 -0500

    remove footer to fix error when reloading the page

--- web/src/routes/__layout.svelte
+++ web/src/routes/__layout.svelte
@@ -1,28 +1,28 @@
 <script lang="ts">
-	import { getRequest } from '$lib/api';
-	import { onDestroy } from 'svelte';
-	import { fade } from 'svelte/transition';
+	// import { getRequest } from '$lib/api';
+	// import { onDestroy } from 'svelte';
+	// import { fade } from 'svelte/transition';
 	import '../app.css';
-	import { serverEndpoint } from '../lib/constants';
+	// import { serverEndpoint } from '../lib/constants';
 
-	let endpoint = serverEndpoint;
-	let isServerOk = true;
+	// let endpoint = serverEndpoint;
+	// let isServerOk = true;
 
-	const pingServerInterval = setInterval(async () => {
-		const response = await getRequest('server-info/ping', '');
+	// const pingServerInterval = setInterval(async () => {
+	// 	const response = await getRequest('server-info/ping', '');
 
-		if (response.res === 'pong') isServerOk = true;
-		if (response.statusCode === 404) isServerOk = false;
-	}, 10000);
+	// 	if (response.res === 'pong') isServerOk = true;
+	// 	if (response.statusCode === 404) isServerOk = false;
+	// }, 10000);
 
-	onDestroy(() => clearInterval(pingServerInterval));
+	// onDestroy(() => clearInterval(pingServerInterval));
 </script>
 
 <main>
 	<slot />
 </main>
 
-<footer
+<!-- <footer
 	class="text-sm fixed bottom-0 h-8 flex place-items-center place-content-center bg-gray-50 w-screen font-mono gap-8 px-4 font-medium"
 >
 	<p class="">
@@ -36,4 +36,4 @@
 			<span class="text-red-500 font-bold">OFFLINE</span>
 		{/if}
 	</p>
-</footer>
+</footer> -->
