7937e1657e934fdca1090206d08e6a1b5792c879
Alex Tran <alex.tran1502@gmail.com>
2022-06-06 16:53:59 -0500

    added user dto

--- server/apps/immich/src/api-v1/user/response-dto/user.ts
+++ server/apps/immich/src/api-v1/user/response-dto/user.ts
@@ -1,4 +1,4 @@
-import { UserEntity } from '../entities/user.entity';
+import { UserEntity } from '../../../../../../libs/database/src/entities/user.entity';
 
 export interface User {
   id: string;
