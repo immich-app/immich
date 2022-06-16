e7f53cd8c35a37dd098a50306364d01b37744fcf
Matthias <matthias.rupp@posteo.de>
2022-06-16 20:17:30 +0200

    Dart format

--- mobile/lib/modules/asset_viewer/views/image_viewer_page.dart
+++ mobile/lib/modules/asset_viewer/views/image_viewer_page.dart
@@ -1,8 +1,6 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_swipe_detector/flutter_swipe_detector.dart';
 import 'package:hive/hive.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/constants/hive_box.dart';
