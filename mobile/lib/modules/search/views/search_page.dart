07a4fde7c9e8cb384f046f25db6e9a03af0c9b83
Matthias <matthias.rupp@posteo.de>
2022-06-27 15:27:16 +0200

    Translation of search page

--- mobile/lib/modules/search/views/search_page.dart
+++ mobile/lib/modules/search/views/search_page.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -83,7 +84,7 @@ class SearchPage extends HookConsumerWidget {
                       return ThumbnailWithInfo(
                         imageUrl:
                             'https://images.unsplash.com/photo-1612178537253-bccd437b730e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Ymxhbmt8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60',
-                        textInfo: 'No Places Info Available',
+                        textInfo: 'search_no_places'.tr(),
                         onTap: () {},
                       );
                     }),
@@ -135,7 +136,7 @@ class SearchPage extends HookConsumerWidget {
                       return ThumbnailWithInfo(
                         imageUrl:
                             'https://images.unsplash.com/photo-1612178537253-bccd437b730e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Ymxhbmt8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60',
-                        textInfo: 'No Object Info Available',
+                        textInfo: 'sarch_no_objects'.tr(),
                         onTap: () {},
                       );
                     }),
@@ -159,20 +160,20 @@ class SearchPage extends HookConsumerWidget {
           children: [
             ListView(
               children: [
-                const Padding(
+                Padding(
                   padding: EdgeInsets.all(16.0),
-                  child: Text(
-                    "Places",
+                  child: const Text(
+                    "search_places",
                     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
+                  ).tr(),
                 ),
                 _buildPlaces(),
-                const Padding(
+                Padding(
                   padding: EdgeInsets.all(16.0),
-                  child: Text(
-                    "Things",
+                  child: const  Text(
+                    "search_things",
                     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
+                  ).tr(),
                 ),
                 _buildThings()
               ],
