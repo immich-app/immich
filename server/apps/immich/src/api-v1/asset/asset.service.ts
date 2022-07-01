0759423f18d570cb83d1c43576c6fd029421718f
Alex Tran <alex.tran1502@gmail.com>
2022-06-30 20:31:07 -0500

    Fixed filename duplication when upload from web

--- server/apps/immich/src/api-v1/asset/asset.service.ts
+++ server/apps/immich/src/api-v1/asset/asset.service.ts
@@ -279,7 +279,6 @@ export class AssetService {
           }
 
           /** Sending Partial Content With HTTP Code 206 */
-          console.log('Send Range', range);
           res.status(206).set({
             'Content-Range': `bytes ${start}-${end}/${size}`,
             'Accept-Ranges': 'bytes',
