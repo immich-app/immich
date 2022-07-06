5cd9d31e5e3256043acb156149c3a5899f6986de
Alex <alex.tran1502@gmail.com>
2022-07-06 14:15:51 -0500

    Display error card with thumbnail

--- mobile/lib/modules/backup/views/failed_backup_status_page.dart
+++ mobile/lib/modules/backup/views/failed_backup_status_page.dart
@@ -2,6 +2,8 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/modules/backup/providers/error_backup_list.provider.dart';
+import 'package:intl/intl.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 class FailedBackupStatusPage extends HookConsumerWidget {
   const FailedBackupStatusPage({Key? key}) : super(key: key);
@@ -12,9 +14,9 @@ class FailedBackupStatusPage extends HookConsumerWidget {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
-        title: const Text(
-          "Failed Backup",
-          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        title: Text(
+          "Failed Backup (${errorBackupList.length})",
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
         ),
         leading: IconButton(
             onPressed: () {
@@ -25,8 +27,98 @@ class FailedBackupStatusPage extends HookConsumerWidget {
               Icons.arrow_back_ios_rounded,
             )),
       ),
-      body: Center(
-        child: Text('Failed Backup Status ${errorBackupList.length}'),
+      body: ListView.builder(
+        shrinkWrap: true,
+        itemCount: errorBackupList.length,
+        itemBuilder: ((context, index) {
+          var errorAsset = errorBackupList.elementAt(index);
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 12.0,
+              vertical: 4,
+            ),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15), // if you need this
+                side: const BorderSide(
+                  color: Colors.black12,
+                  width: 1,
+                ),
+              ),
+              elevation: 0,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(
+                      minWidth: 100,
+                      minHeight: 150,
+                      maxWidth: 100,
+                      maxHeight: 200,
+                    ),
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(15),
+                        topLeft: Radius.circular(15),
+                      ),
+                      clipBehavior: Clip.hardEdge,
+                      child: Image(
+                        fit: BoxFit.cover,
+                        image: AssetEntityImageProvider(
+                          errorAsset.asset,
+                          isOriginal: false,
+                          thumbnailSize: const ThumbnailSize.square(200),
+                          thumbnailFormat: ThumbnailFormat.jpeg,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            errorAsset.fileName,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                              color: Colors.red[300],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 4),
+                            child: Text(
+                              DateFormat.yMMMMd('en_US').format(
+                                DateTime.parse(
+                                  errorAsset.createdAt.toString(),
+                                ),
+                              ),
+                              style: const TextStyle(
+                                  fontSize: 12, fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                          Text(
+                            errorAsset.errorMessage,
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.grey[600],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        }),
       ),
     );
   }
