f53f909e000e981a5af9a8fa324f28cef10c1c3c
Alex <alex.tran1502@gmail.com>
2022-07-12 07:48:20 -0500

    Update Readme

--- mobile/lib/modules/home/services/asset.service.dart
+++ mobile/lib/modules/home/services/asset.service.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/modules/home/models/delete_asset_response.model.dart';
-import 'package:immich_mobile/modules/home/models/get_all_asset_response.model.dart';
 import 'package:immich_mobile/shared/models/immich_asset.model.dart';
 import 'package:immich_mobile/shared/models/immich_asset_with_exif.model.dart';
 import 'package:immich_mobile/shared/services/network.service.dart';
@@ -27,58 +26,6 @@ class AssetService {
       debugPrint("Error getAllAsset  ${e.toString()}");
     }
     return null;
-  }
-
-  Future<GetAllAssetResponse?> getAllAssetWithPagination() async {
-    var res = await _networkService.getRequest(url: "asset/all");
-    try {
-      Map<String, dynamic> decodedData = jsonDecode(res.toString());
-
-      GetAllAssetResponse result = GetAllAssetResponse.fromMap(decodedData);
-      return result;
-    } catch (e) {
-      debugPrint("Error getAllAsset  ${e.toString()}");
-    }
-    return null;
-  }
-
-  Future<GetAllAssetResponse?> getOlderAsset(String? nextPageKey) async {
-    try {
-      var res = await _networkService.getRequest(
-        url: "asset/all?nextPageKey=$nextPageKey",
-      );
-
-      Map<String, dynamic> decodedData = jsonDecode(res.toString());
-
-      GetAllAssetResponse result = GetAllAssetResponse.fromMap(decodedData);
-      if (result.count != 0) {
-        return result;
-      }
-    } catch (e) {
-      debugPrint("Error getAllAsset  ${e.toString()}");
-    }
-    return null;
-  }
-
-  Future<List<ImmichAsset>> getNewAsset(String latestDate) async {
-    try {
-      var res = await _networkService.getRequest(
-        url: "asset/new?latestDate=$latestDate",
-      );
-
-      List<dynamic> decodedData = jsonDecode(res.toString());
-
-      List<ImmichAsset> result =
-          List.from(decodedData.map((a) => ImmichAsset.fromMap(a)));
-      if (result.isNotEmpty) {
-        return result;
-      }
-
-      return [];
-    } catch (e) {
-      debugPrint("Error getAllAsset  ${e.toString()}");
-      return [];
-    }
   }
 
   Future<ImmichAssetWithExif?> getAssetById(String assetId) async {
