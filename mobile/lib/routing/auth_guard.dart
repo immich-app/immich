3b46d33ff33076ac174fd6fb92f2dfc588b3f6b8
Alex <alex.tran1502@gmail.com>
2022-07-13 07:22:59 -0500

    PR feedback

--- mobile/lib/routing/auth_guard.dart
+++ mobile/lib/routing/auth_guard.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/foundation.dart';
+import 'package:immich_mobile/routing/router.dart';
 import 'package:immich_mobile/shared/services/api.service.dart';
 
 class AuthGuard extends AutoRouteGuard {
@@ -8,20 +9,16 @@ class AuthGuard extends AutoRouteGuard {
   @override
   void onNavigation(NavigationResolver resolver, StackRouter router) async {
     try {
-      var test = await _apiService.authenticationApi.validateAccessToken();
+      var res = await _apiService.authenticationApi.validateAccessToken();
 
-      if (test == null) {
-        router.removeUntil((route) => route.name == "LoginRoute");
+      if (res != null && res.authStatus) {
+        resolver.next(true);
       } else {
-        if (test.authStatus) {
-          resolver.next(true);
-        } else {
-          router.removeUntil((route) => route.name == "LoginRoute");
-        }
+        router.replaceAll([const LoginRoute()]);
       }
     } catch (e) {
       debugPrint("Error [onNavigation] ${e.toString()}");
-      router.removeUntil((route) => route.name == "LoginRoute");
+      router.replaceAll([const LoginRoute()]);
       return;
     }
   }
