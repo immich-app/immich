bece6253d5a068490ccd11b1820adb167faaa4ed
Fynn Petersen-Frey <zoodyy@users.noreply.github.com>
2022-07-13 14:42:00 +0200

    Improve Docker setup and reduce memory usage of production containers (#338)

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,1 +1,6 @@
-npm run build && node /usr/src/app/build/index.js
+npm run build
+if [ `id -u` -eq 0 ] && [ -n "$PUID" ] && [ -n "$PGID" ]; then
+    exec setpriv --reuid $PUID --regid $PGID --clear-groups node /usr/src/app/build/index.js
+else
+    node /usr/src/app/build/index.js
+fi
