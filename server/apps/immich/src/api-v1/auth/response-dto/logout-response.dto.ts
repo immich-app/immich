9e78c3d508da40c42fafc132c68408676715a706
Matthias <matthias.rupp@posteo.de>
2022-07-19 20:41:32 +0200

    Add logout route that deletes http only cookies

---
+++ server/apps/immich/src/api-v1/auth/response-dto/logout-response.dto.ts
@@ -0,0 +1,10 @@
+import { ApiResponseProperty } from '@nestjs/swagger';
+
+export class LogoutResponseDto {
+    constructor (successful: boolean) {
+        this.successful = successful;
+    }
+
+    @ApiResponseProperty()
+    successful!: boolean;
+};
