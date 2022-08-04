fdb1a7bc250369bc0ddbea53d291026ba17b2bf8
Alex Tran <alex.tran1502@gmail.com>
2022-08-04 00:51:52 -0500

    Added friendly debug message

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@ echo "Starting Immich installation..."
 
 ip_address=$(hostname -I | awk '{print $1}')
 
-immich_has() {
+RED='\033[0;31m'
+GREEN='\032[0;31m'
+NC='\033[0m' # No Color
+
+machine_has() {
   type "$1" >/dev/null 2>&1
 }
 
@@ -23,10 +27,12 @@ download_dot_env_file() {
 
 populate_upload_location() {
   echo "Populating default UPLOAD_LOCATION value..."
-  cd ./immich-app/immich-data
-  upload_location=$(pwd)
-  # Replace value of UPLOAD_LOCATION in .env with upload_location path
 
+  cd ./immich-app/immich-data
+
+  upload_location=$(pwd)
+
+  # Replace value of UPLOAD_LOCATION in .env with upload_location path
   if [[ "$OSTYPE" == "darwin"* ]]; then
     sed -i '' "s|UPLOAD_LOCATION=.*|UPLOAD_LOCATION=$upload_location|" ../.env
   else
@@ -41,13 +47,39 @@ populate_server_endpoint() {
 
   if [[ "$OSTYPE" == "darwin"* ]]; then
     ip_address=$(ipconfig getifaddr en0)
-    sed -i '' "s|VITE_SERVER_ENDPOINT=.*|VITE_SERVER_ENDPOINT=http://$ip_address/api|" ./.env
+    sed -i '' "s|VITE_SERVER_ENDPOINT=.*|VITE_SERVER_ENDPOINT=http://$ip_address:2283/api|" ./.env
   else
     ip_address=$(hostname -I | awk '{print $1}')
-    sed -i "s|VITE_SERVER_ENDPOINT=.*|VITE_SERVER_ENDPOINT=http://$ip_address/api|" ./.env
+    sed -i "s|VITE_SERVER_ENDPOINT=.*|VITE_SERVER_ENDPOINT=http://$ip_address:2283/api|" ./.env
   fi
+}
 
-  # Replace value of VITE_SERVER_ENDPOINT
+start_docker_compose() {
+  echo "Starting Immich's docker containers"
+
+  if machine_has "docker compose"; then {
+    docker compose up --remove-orphans -d
+
+    show_friendly_message
+    exit 0
+  }; fi
+
+  if machine_has "docker-compose"; then
+    docker-compose up --remove-orphans -d
+
+    show_friendly_message
+    exit 0
+  fi
+}
+
+show_friendly_message() {
+  echo "Succesfully deployed Immich!"
+  echo "You can access the website at http://$ip_address:2283 and the server URL for the mobile app is http://$ip_address:2283/api"
+  echo "The backup (or upload) location is $upload_location"
+  echo "---------------------------------------------------"
+  echo "If you want to confgure custom information of the server, including the database, Redis information, or the backup (or upload) location, etc."
+
+  echo "Before updating the content of the configuration file, please bring down the containers with the command 'docker-compose down' in the immich-app directory, then change the information that fits your needs in the '.env' file, and then bring the containers back up with the command 'docker-compose up --remove-orphans -d' in the immich-app directory"
 }
 
 # MAIN
@@ -56,3 +88,4 @@ download_docker_compose_file
 download_dot_env_file
 populate_upload_location
 populate_server_endpoint
+start_docker_compose
