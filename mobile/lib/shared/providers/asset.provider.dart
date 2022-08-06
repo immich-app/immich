8270abdb5cd4e863125c8bd58baebc329ae3e9e5
Matthias Rupp <matthias.rupp@posteo.de>
2022-08-06 14:38:39 +0200

    Cleanup

--- mobile/lib/shared/providers/asset.provider.dart
+++ mobile/lib/shared/providers/asset.provider.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/modules/home/services/asset.service.dart';
