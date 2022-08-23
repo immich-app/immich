261f98ec4420793bccf8f11bacd06dae9737eef9
Thanh Pham <pano.tjoan@yahoo.com>
2022-08-23 22:48:15 +0700

    feat(server): calc sha1 of uploaded file

--- server/apps/immich/src/global.d.ts
+++ server/apps/immich/src/global.d.ts
@@ -4,5 +4,9 @@ declare global {
   namespace Express {
     // eslint-disable-next-line @typescript-eslint/no-empty-interface
     interface User extends UserResponseDto {}
+
+    interface File {
+      checksum?: string;
+    }
   }
 }
--- server/apps/immich/src/utils/disk-storage.ts
+++ server/apps/immich/src/utils/disk-storage.ts
@@ -5,18 +5,22 @@ import * as crypto from 'node:crypto';
 import * as fs from 'node:fs';
 import * as os from 'node:os';
 import * as path from 'node:path';
-import { ParsedQs } from 'qs';
+import type { ParsedQs } from 'qs';
 import * as mkdirp from 'mkdirp';
 
-type GetFileNameFn = (req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) => void;
-type GetDestFn = (req: Request, file: Express.Multer.File, cb: (error: Error | null, destination: string) => void) => void;
-
-interface ExtendedDiskStorageOptions extends DiskStorageOptions {
+export interface ExtendedFile extends Express.Multer.File {
+  checksum?: string;
 }
+
+export interface ExtendedDiskStorageOptions extends DiskStorageOptions {
+}
+
+type GetFileNameFn = (req: Request, file: ExtendedFile, cb: (error: Error | null, filename: string) => void) => void;
+type GetDestFn = (req: Request, file: ExtendedFile, cb: (error: Error | null, destination: string) => void) => void;
 
 function getFilename(
   req: Request,
-  file: Express.Multer.File,
+  file: ExtendedFile,
   cb: (error: Error | null, filename: string) => void
 ) {
   crypto.randomBytes(16, function (err, raw) {
@@ -26,7 +30,7 @@ function getFilename(
 
 function getDestination(
   req: Request,
-  file: Express.Multer.File,
+  file: ExtendedFile,
   cb: (error: Error | null, destination: string) => void
 ) {
   cb(null, os.tmpdir())
@@ -49,7 +53,7 @@ class ExtendedDiskStorage implements StorageEngine {
     }
   }
 
-  public _handleFile(req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, file: Express.Multer.File, cb: (error?: any, info?: Partial<Express.Multer.File> | undefined) => void): void {
+  public _handleFile(req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, file: ExtendedFile, cb: (error?: any, info?: Partial<ExtendedFile> | undefined) => void): void {
     const that = this;
 
     that._getDestinationFn(req, file, (err, destination) => {
@@ -58,9 +62,13 @@ class ExtendedDiskStorage implements StorageEngine {
       that._getFilenameFn(req, file, (err, filename) => {
         if (err) return cb(err);
 
-        const finalPath = path.join(destination, filename)
+        const finalPath = path.join(destination, filename);
         const outStream = fs.createWriteStream(finalPath);
+        const sha1Hash = crypto.createHash('sha1');
 
+        file.stream.on('data', (chunk) => {
+          sha1Hash.update(chunk);
+        });
         file.stream.pipe(outStream);
         outStream.on('error', cb);
         outStream.on('finish', () => {
@@ -68,14 +76,15 @@ class ExtendedDiskStorage implements StorageEngine {
             destination: destination,
             filename: filename,
             path: finalPath,
-            size: outStream.bytesWritten
+            size: outStream.bytesWritten,
+            checksum: sha1Hash.digest('hex'),
           });
         });
       })
     })
   }
 
-  public _removeFile(req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, file: Express.Multer.File, cb: (error: Error | null) => void): void {
+  public _removeFile(req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, file: ExtendedFile, cb: (error: Error | null) => void): void {
     const path = file.path;
     const refFile = file as any; // make intellisense happy
 
