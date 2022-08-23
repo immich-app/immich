02c151fed31fb3fa32153274745c6645329b91ff
Matthias <matthias.rupp@posteo.de>
2022-08-23 12:45:39 +0200

    Use custom caches in all modules

--- mobile/lib/shared/services/cache.service.dart
+++ mobile/lib/shared/services/cache.service.dart
@@ -2,20 +2,31 @@ import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 enum CacheType {
-  albumThumbnail,
-  sharedAlbumThumbnail;
+  // Shared cache for asset thumbnails in various modules
+  thumbnail(cacheSize: 10000),
+
+  imageViewerPreview(cacheSize: 800),
+  imageViewerFull(cacheSize: 400),
+  albumThumbnail(cacheSize: 1000),
+  sharedAlbumThumbnail(cacheSize: 1000);
+
+  const CacheType({this.cacheSize = 200});
+  final int cacheSize;
 }
 
 final cacheServiceProvider = Provider((_) => CacheService());
 
 class CacheService {
-
   BaseCacheManager getCache(CacheType type) {
-    return _getDefaultCache(type.name);
+    return _getDefaultCache(type.name, type.cacheSize);
   }
 
-  BaseCacheManager _getDefaultCache(String cacheName) {
-    return CacheManager(Config(cacheName));
+  BaseCacheManager _getDefaultCache(String cacheName, int size) {
+    return CacheManager(
+      Config(
+        cacheName,
+        maxNrOfCacheObjects: size,
+      ),
+    );
   }
-
 }
