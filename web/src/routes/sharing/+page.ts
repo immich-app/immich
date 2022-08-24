2b3484294a4abac4f3d4524cdab4499e91a95cb2
Alex <alex.tran1502@gmail.com>
2022-08-24 09:50:30 -0700

    refactor sharing page

--- web/src/routes/sharing/+page.ts
+++ web/src/routes/sharing/+page.ts
@@ -4,14 +4,15 @@ export const prerender = false;
 import { AlbumResponseDto, api, UserResponseDto } from '@api';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ fetch }) => {
+export const load: PageLoad = async ({ fetch, parent }) => {
 	try {
-		const [user, sharedAlbums] = await Promise.all([
-			fetch('/data/user/get-my-user-info').then((r) => r.json() as Promise<UserResponseDto>),
-			fetch('/data/album/get-all-albums?isShared=true').then(
-				(r) => r.json() as Promise<AlbumResponseDto[]>
-			)
-		]);
+		const { user } = await parent();
+		if (!user) {
+			throw redirect(302, '/auth/login');
+		}
+		const sharedAlbums = await fetch('/data/album/get-all-albums?isShared=true').then(
+			(r) => r.json() as Promise<AlbumResponseDto[]>
+		);
 
 		return {
 			user: user,
