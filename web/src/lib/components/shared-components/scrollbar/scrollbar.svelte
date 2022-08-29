c1249dd60da8294191ab65f94c7f7168254438f6
Alex Tran <alex.tran1502@gmail.com>
2022-08-29 16:52:40 -0500

    Render datetime segment

--- web/src/lib/components/shared-components/scrollbar/scrollbar.svelte
+++ web/src/lib/components/shared-components/scrollbar/scrollbar.svelte
@@ -3,62 +3,86 @@
 	import { AssetCountByTimeGroupResponseDto } from '@api';
 
 	import { onMount, onDestroy } from 'svelte';
+	import { SegmentScrollbarLayout } from './segment-scrollbar-layout';
 	export let viewportHeight = 0;
 	export let segmentData: AssetCountByTimeGroupResponseDto;
 
 	let scrollbarHeight: number;
 	let scrollbarHeightLeft: number;
+	let segmentScrollbarLayout: SegmentScrollbarLayout[] = [];
+	let isHover = false;
+	let hoveredDate: Date;
 
 	const getSegmentHeight = (groupCount: number) => {
-		const percentage = (groupCount * 100) / segmentData.totalAssets;
-		return (percentage * scrollbarHeightLeft) / 100;
+		if (segmentData.groups.length > 0) {
+			const percentage = (groupCount * 100) / segmentData.totalAssets;
+			return Math.round((percentage * scrollbarHeightLeft) / 100);
+		} else {
+			return 0;
+		}
 	};
 
 	const getLayoutDistance = () => {
-		let result = [];
-		/**
-		 * {
-		 *  group_name: 'dec-2020',
-		 *  margin_top: '40'
-		 * }
-		 */
-		for (const [i, segment] of segmentData.groups.entries()) {
-			if (i == 0) {
-				result.push(segment);
-				continue;
-			}
+		let result: SegmentScrollbarLayout[] = [];
+		for (const segment of segmentData.groups) {
+			let segmentLayout = new SegmentScrollbarLayout();
 
-			result.push('ok');
-			console.log('ok');
+			segmentLayout.count = segment.count;
+			segmentLayout.height = getSegmentHeight(segment.count);
+			segmentLayout.timeGroup = segment.timeGroup;
+
+			result.push(segmentLayout);
 		}
-		// viewport height in pixel
-		// scrollbar height in pixel
-
-		// total asset in count is equivalent to total scrollbar height -> interpolation
-
-		// each group asset count is used to calculate the margin from the top
 
 		console.log(result);
+		return result;
 	};
 
 	onMount(() => {
 		scrollbarHeightLeft = scrollbarHeight;
-		getLayoutDistance();
+		segmentScrollbarLayout = getLayoutDistance();
 	});
 </script>
 
 <div
 	id="immich-scubbable-scrollbar"
-	class="fixed right-0 w-[60px] h-full bg-white z-[99999999]"
+	class="fixed right-0 w-[60px] h-full bg-immich-bg z-[9999]"
 	bind:clientHeight={scrollbarHeight}
+	on:mouseenter={() => (isHover = true)}
+	on:mouseleave={() => (isHover = false)}
 >
-	{#each segmentData.groups as segment (segment.timeGroup)}
+	<div class="text-xs">
+		{hoveredDate?.toLocaleString('default', { month: 'short' })}
+		{hoveredDate?.getFullYear()}
+	</div>
+
+	{#each segmentScrollbarLayout as segment, index (segment.timeGroup)}
 		{@const groupDate = new Date(segment.timeGroup)}
-		<div class="text-xs">
-			{groupDate.toLocaleString('default', { month: 'short' })}
-			{groupDate.getFullYear()}
+
+		<div
+			class="relative "
+			style:height={segment.height + 'px'}
+			aria-label={segment.timeGroup + ' ' + segment.count}
+			on:mouseenter={() => (hoveredDate = groupDate)}
+		>
+			<!-- {groupDate.toLocaleString('default', { month: 'short' })} -->
+			{#if new Date(segmentScrollbarLayout[index - 1]?.timeGroup).getFullYear() !== groupDate.getFullYear()}
+				<div
+					aria-label={segment.timeGroup + ' ' + segment.count}
+					class="absolute right-0 pr-3 z-[99999] text-xs font-medium bg-immich-bg"
+				>
+					{groupDate.getFullYear()}
+				</div>
+			{:else if segment.height > 3}
+				<div
+					aria-label={segment.timeGroup + ' ' + segment.count}
+					class="absolute right-0 rounded-full h-[4px] w-[4px] mr-3 bg-gray-300 block"
+				/>
+			{/if}
 		</div>
 	{/each}
+
+	<!-- on hovered show info box with day and month at mouse location -->
 </div>
 
 <style>
