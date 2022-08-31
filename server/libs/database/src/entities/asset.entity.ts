f53b6489843b748bde3c04a26c6f1adca1baacdf
Thanh Pham <pano.tjoan@yahoo.com>
2022-08-31 22:21:21 +0700

    feat(server): remove un-used deviceAssetId cols.

--- server/libs/database/src/entities/asset.entity.ts
+++ server/libs/database/src/entities/asset.entity.ts
@@ -3,13 +3,13 @@ import { ExifEntity } from './exif.entity';
 import { SmartInfoEntity } from './smart-info.entity';
 
 @Entity('assets')
-@Unique(['deviceAssetId', 'userId', 'deviceId'])
+@Unique(['userId', 'checksum'])
 export class AssetEntity {
   @PrimaryGeneratedColumn('uuid')
   id!: string;
 
-  @Column()
-  deviceAssetId!: string;
+  // @Column()
+  // deviceAssetId!: string;
 
   @Column()
   userId!: string;
