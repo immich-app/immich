8dbf884ddb79431a960566e22374a2f1fe152bc7
Thanh Pham <pano.tjoan@yahoo.com>
2022-08-31 23:16:35 +0700

    feat(server): generate checksum for previous uploaded assets

---
+++ server/apps/microservices/src/processors/generate-checksum.processor.ts
@@ -0,0 +1,65 @@
+import { AssetEntity } from '@app/database/entities/asset.entity';
+import { generateChecksumQueueName } from '@app/job';
+import { Process, Processor } from '@nestjs/bull';
+import { Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { createHash } from 'node:crypto';
+import fs from 'node:fs';
+import { IsNull, Repository } from 'typeorm';
+
+// TODO: just temporary task to generate previous uploaded assets.
+@Processor(generateChecksumQueueName)
+export class GenerateChecksumProcessor {
+  constructor(
+    @InjectRepository(AssetEntity)
+    private assetRepository: Repository<AssetEntity>,
+  ) {}
+
+  @Process()
+  async generateChecksum() {
+    let hashNext = true;
+    let pageSize = 200;
+    let offset = 0;
+
+    while (hashNext) {
+      const assets = await this.assetRepository.find({
+        where: {
+          checksum: IsNull()
+        },
+        skip: offset,
+        take: pageSize,
+      });
+
+      if (!assets?.length) {
+        hashNext = false; // avoid using break
+      } else {
+        for (const asset of assets) {
+          try {
+            await this.generateAssetChecksum(asset);
+          } catch (err: any) {
+            Logger.error(`Error generate checksum ${err}`);
+          }
+        }
+
+        offset += pageSize;
+      }
+    }
+  }
+
+  private async generateAssetChecksum(asset: AssetEntity) {
+    if (!asset.originalPath) return;
+    if (!fs.existsSync(asset.originalPath)) return;
+
+    const fileReadStream = fs.createReadStream(asset.originalPath);
+    const sha1Hash = createHash('sha1');
+    const deferred = new Promise<Buffer>((resolve, reject) => {
+      sha1Hash.once('error', (err) => reject(err));
+      sha1Hash.once('finish', () => resolve(sha1Hash.read()));
+    });
+
+    fileReadStream.pipe(sha1Hash);
+    const checksum = await deferred;
+
+    await this.assetRepository.update(asset.id, { checksum });
+  }
+}
--- server/libs/job/src/constants/queue-name.constant.ts
+++ server/libs/job/src/constants/queue-name.constant.ts
@@ -2,3 +2,4 @@ export const thumbnailGeneratorQueueName = 'thumbnail-generator-queue';
 export const assetUploadedQueueName = 'asset-uploaded-queue';
 export const metadataExtractionQueueName = 'metadata-extraction-queue';
 export const videoConversionQueueName = 'video-conversion-queue';
+export const generateChecksumQueueName = 'generate-checksum-queue';
