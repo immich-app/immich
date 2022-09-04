344b4b278169b3e09f922bc59fff9bb38c9c5849
Alexandre Bouijoux <alexandre@bouijoux.fr>
2022-09-04 12:17:24 +0200

    parse_args(), help message and global variables
    -h shows a help message with arguments accepted by the script
    --dev executes script with dev=1, portions of code can change their behavior
    while looking at dev's value.

--- installation/alpine.sh
+++ installation/alpine.sh
@@ -2,3 +2,35 @@
 
 # All-in-one installation without Docker
 
+immich_ver=1.26.0_36-dev
+alpine_ver=3.14
+
+tmp_dir=/tmp
+
+# see parse_args()
+# [0|1]
+dev=0
+
+# $@: args from cli
+parse_args()
+{
+    if [ "$1" == "--dev" ]
+    then
+        # Instead of downloading code from a release,
+        # the source code is provided.
+        dev=1
+    elif [ "$1" == "-h" ]
+    then
+        echo "Dockerless installation script."
+        echo "usage: $0 [--dev|-h]"
+        echo
+        echo "  -h      Show this help message"
+        echo "  --dev   Run in dev mode"
+        echo
+        exit 0
+    fi
+}
+
+
+# main()
+parse_args "$@"
