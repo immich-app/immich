65ed7da86e67cbb12c06aa608f5cc93e91927240
Thanh Pham <pano.tjoan@yahoo.com>
2022-09-05 17:32:25 +0700

    feat(server): make sure job has finished before closing

--- server/apps/microservices/src/main.ts
+++ server/apps/microservices/src/main.ts
@@ -3,11 +3,28 @@ import { NestFactory } from '@nestjs/core';
 import { RedisIoAdapter } from '../../immich/src/middlewares/redis-io.adapter.middleware';
 import { MicroservicesModule } from './microservices.module';
 
+interface Closable {
+  close(): Promise<void>;
+}
+
+const closables: Closable[] = [];
+const gracefullHandler = async () => {
+  Logger.log('Immich Microservices is shutting down...');
+  for (const closable of closables) {
+    await closable.close();
+  }
+  process.exit(1);
+};
+
+process.on('SIGINT', gracefullHandler);
+process.on('SIGTERM', gracefullHandler);
+
 async function bootstrap() {
   const app = await NestFactory.create(MicroservicesModule);
 
   app.useWebSocketAdapter(new RedisIoAdapter(app));
 
+  closables.push(app);
   await app.listen(3002, () => {
     if (process.env.NODE_ENV == 'development') {
       Logger.log('Running Immich Microservices in DEVELOPMENT environment', 'ImmichMicroservice');
