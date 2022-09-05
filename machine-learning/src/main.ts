bfb09cb416821830ac34e50e5c485bd7b5883e35
Thanh Pham <pano.tjoan@yahoo.com>
2022-09-05 17:35:46 +0700

    feat(machine-learning): make sure job queues finished before closing

--- machine-learning/src/main.ts
+++ machine-learning/src/main.ts
@@ -2,9 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
+interface Closable {
+  close(): Promise<void>;
+}
+
+const closables: Closable[] = [];
+const gracefullHandler = async () => {
+  console.log('Shutting down...');
+  for (const closable of closables) {
+    await closable.close();
+  }
+  process.exit(1);
+};
+
+process.on('SIGINT', gracefullHandler);
+process.on('SIGTERM', gracefullHandler);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  closables.push(app);
   await app.listen(3003, () => {
     if (process.env.NODE_ENV == 'development') {
       Logger.log(
