0ffc36de830f9d6f81c97994a7951a8d480d69c8
Thanh Pham <pano.tjoan@yahoo.com>
2022-09-06 01:00:54 +0700

    chore(server): add webhook env

--- server/libs/common/src/config/app.config.ts
+++ server/libs/common/src/config/app.config.ts
@@ -16,6 +16,6 @@ export const immichAppConfig: ConfigModuleOptions = {
       then: Joi.string().optional().allow(null, ''),
       otherwise: Joi.string().required(),
     }),
-    WEBHOOK_URL: Joi.string().optional().default('http://immich-machine-learning:3003/webhook')
+    WEBHOOK_URL: Joi.string().optional().allow(null, ''),
   }),
 };
