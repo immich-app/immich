b6d025da09d813bef7a43e0aa3a0a24cf068c5f3
Jaime Baez <j.baezcastillo@gmail.com>
2022-09-09 14:40:35 +0200

    Fix Notification components possible memory leaks (#650)
    Dispose subscriptions and timeouts when
    the components are removed from the DOM

---
+++ web/src/lib/components/shared-components/notification/__tests__/notification-card.spec.ts
@@ -0,0 +1,39 @@
+import { jest, describe, it } from '@jest/globals';
+import { render, cleanup, RenderResult } from '@testing-library/svelte';
+import { NotificationType } from '../notification';
+import NotificationCard from '../notification-card.svelte';
+import '@testing-library/jest-dom';
+
+describe('NotificationCard component', () => {
+	let sut: RenderResult<NotificationCard>;
+
+	it('disposes timeout if already removed from the DOM', () => {
+		jest.spyOn(window, 'clearTimeout');
+
+		sut = render(NotificationCard, {
+			notificationInfo: {
+				id: 1234,
+				message: 'Notification message',
+				timeout: 1000,
+				type: NotificationType.Info
+			}
+		});
+
+		cleanup();
+		expect(window.clearTimeout).toHaveBeenCalledTimes(1);
+	});
+
+	it('shows message and title', () => {
+		sut = render(NotificationCard, {
+			notificationInfo: {
+				id: 1234,
+				message: 'Notification message',
+				timeout: 1000,
+				type: NotificationType.Info
+			}
+		});
+
+		expect(sut.getByTestId('title')).toHaveTextContent('Info');
+		expect(sut.getByTestId('message')).toHaveTextContent('Notification message');
+	});
+});
