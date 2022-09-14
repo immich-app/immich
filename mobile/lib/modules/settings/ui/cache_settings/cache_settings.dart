d7c6c9a1ae1dca04abc20e34ebc05799a9dabdfc
Alex <alex.tran1502@gmail.com>
2022-09-14 09:30:52 -0500

    Update

--- mobile/lib/modules/settings/ui/cache_settings/cache_settings.dart
+++ mobile/lib/modules/settings/ui/cache_settings/cache_settings.dart
@@ -14,6 +14,7 @@ class CacheSettings extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final CacheService cacheService = ref.watch(cacheServiceProvider);
     final clearCacheState = useState(false);
 
     Future<void> clearCache() async {
