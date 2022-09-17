8ee3ae7971c8cbb2eaa61c905b008b18997eb510
Alex Tran <alex.tran1502@gmail.com>
2022-09-16 23:36:18 -0500

    fix(web): Incorrectly display time in detail panel when navigating in gallery view

--- web/src/lib/components/asset-viewer/detail-panel.svelte
+++ web/src/lib/components/asset-viewer/detail-panel.svelte
@@ -4,7 +4,7 @@
 	import ImageOutline from 'svelte-material-icons/ImageOutline.svelte';
 	import CameraIris from 'svelte-material-icons/CameraIris.svelte';
 	import MapMarkerOutline from 'svelte-material-icons/MapMarkerOutline.svelte';
-	import moment from 'moment';
+	import moment, { utc } from 'moment';
 	import { createEventDispatcher, onMount } from 'svelte';
 	import { browser } from '$app/environment';
 	import { env } from '$env/dynamic/public';
@@ -24,19 +24,19 @@
 		drawMap(asset.exifInfo.latitude, asset.exifInfo.longitude);
 	}
 
+	$: {
+		// remove timezone when user not config PUBLIC_TZ var. Etc/UTC is used in default.
+		if (asset.exifInfo?.dateTimeOriginal && !env.PUBLIC_TZ) {
+			const dateTimeOriginal = asset.exifInfo.dateTimeOriginal;
+			asset.exifInfo.dateTimeOriginal = dateTimeOriginal.slice(0, dateTimeOriginal.length - 1);
+		}
+	}
 	export let albums: AlbumResponseDto[];
 
 	onMount(async () => {
 		if (browser) {
 			if (asset.exifInfo?.latitude != null && asset.exifInfo?.longitude != null) {
 				await drawMap(asset.exifInfo.latitude, asset.exifInfo.longitude);
-			}
-
-			// remove timezone when user not config PUBLIC_TZ var. Etc/UTC is used in default.
-			if (asset.exifInfo?.dateTimeOriginal && !env.PUBLIC_TZ) {
-				const dateTimeOriginal = asset.exifInfo.dateTimeOriginal;
-
-				asset.exifInfo.dateTimeOriginal = dateTimeOriginal.slice(0, dateTimeOriginal.length - 1);
 			}
 		}
 	});
