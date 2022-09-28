37b26dd28f556569d7a4537d02a90539e97dafb4
Alex Tran <alex.tran1502@gmail.com>
2022-09-28 11:06:32 -0500

    Added setting tile

--- web/src/lib/components/admin-page/settings/setting-panel.svelte
+++ web/src/lib/components/admin-page/settings/setting-panel.svelte
@@ -1,14 +1,20 @@
-<div class="">
-	<div id="button-setting" class="flex border p-6 rounded-lg bg-white">
-		<div class="w-[70%]">
-			<h1 class="font-medium text-immich-primary">Generate thumbnail (JPEG)</h1>
-			<p class="text-sm mt-1">Regenerate asset that does not have JPEG thumbnail</p>
-		</div>
-		<div class="w-[30%] flex place-items-center place-content-end">
-			<button
-				class="border px-4 py-2 text-sm bg-gray-50 font-medium rounded-lg hover:bg-immich-primary/10 transition-all"
-				>Generate</button
-			>
-		</div>
-	</div>
+<script>
+	import SettingTile from './setting-tile.svelte';
+</script>
+
+<div class="flex flex-col gap-3">
+	<SettingTile
+		title={'Generate thumbnail (JPEG)'}
+		subtitle={'Regenerate asset that does not have JPEG thumbnail'}
+	/>
+
+	<SettingTile
+		title={'Generate thumbnail (JPEG)'}
+		subtitle={'Regenerate asset that does not have JPEG thumbnail'}
+	/>
+
+	<SettingTile
+		title={'Generate thumbnail (JPEG)'}
+		subtitle={'Regenerate asset that does not have JPEG thumbnail'}
+	/>
 </div>
---
+++ web/src/lib/components/admin-page/settings/setting-tile.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	export let title: string;
+	export let subtitle: string;
+
+	const dispatch = createEventDispatcher();
+</script>
+
+<div class="flex border p-6 rounded-lg bg-white">
+	<div class="w-[70%]">
+		<h1 class="font-medium text-immich-primary">{title}</h1>
+		<p class="text-sm mt-1">{subtitle}il</p>
+	</div>
+	<div class="w-[30%] flex place-items-center place-content-end">
+		<button
+			on:click={() => dispatch('click')}
+			class="border px-4 py-2 text-sm bg-gray-50 font-medium rounded-lg hover:bg-immich-primary/10 transition-all"
+			>Generate</button
+		>
+	</div>
+</div>
