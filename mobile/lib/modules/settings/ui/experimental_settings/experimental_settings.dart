6a6aec4ab0bb4ec5be094c6ee29c40774071efe4
Matthias Rupp <matthias.rupp@posteo.de>
2022-09-28 11:55:29 +0200

    Add 'restart to apply setting' toast

--- mobile/lib/modules/settings/ui/experimental_settings/experimental_settings.dart
+++ mobile/lib/modules/settings/ui/experimental_settings/experimental_settings.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/modules/settings/providers/app_settings.provider.dart';
 import 'package:immich_mobile/modules/settings/services/app_settings.service.dart';
@@ -29,6 +30,8 @@ class ExperimentalSettings extends HookConsumerWidget {
       useExperimentalAssetGrid.value = status;
       appSettingService.setSetting(
           AppSettingsEnum.useExperimentalAssetGrid, status);
+
+      Fluttertoast.showToast(msg: "settings_require_restart".tr());
     }
 
     return ExpansionTile(
