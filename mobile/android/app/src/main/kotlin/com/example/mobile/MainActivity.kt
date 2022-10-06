a3aca4acb5f6830c7b43a8cbbf260e6f9d689321
Fynn Petersen-Frey <zoodyy@users.noreply.github.com>
2022-10-06 18:32:45 +0200

    feat(mobile) Run background service after being killed (#789)

--- mobile/android/app/src/main/kotlin/com/example/mobile/MainActivity.kt
+++ mobile/android/app/src/main/kotlin/com/example/mobile/MainActivity.kt
@@ -5,21 +5,11 @@ import io.flutter.embedding.engine.FlutterEngine
 import android.os.Bundle
 import android.content.Intent
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        flutterEngine.getPlugins().add(BackgroundServicePlugin())
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        try {
-            startService(Intent(getBaseContext(), AppClearedService::class.java));
-        } catch (e: Exception) {
-            // startService must not be called when app is in background (crashes app)
-            // there is nothing we can do
-        }
+        flutterEngine.plugins.add(BackgroundServicePlugin())
     }
 
 }
