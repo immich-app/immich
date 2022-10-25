faf296b78d9e60bec95fc85958e80d84836e44b7
Alex Tran <alex.tran1502@gmail.com>
2022-10-25 16:31:51 -0500

    finished design

--- web/src/lib/components/admin-page/jobs/job-tile.svelte
+++ web/src/lib/components/admin-page/jobs/job-tile.svelte
@@ -11,7 +11,7 @@
 	const dispatch = createEventDispatcher();
 </script>
 
-<div class="flex border p-6 rounded-2xl bg-white">
+<div class="flex border p-6 rounded-2xl border-immich-gray bg-immich-gray">
 	<div class="w-[70%]">
 		<h1 class="font-medium text-immich-primary">{title}</h1>
 		<p class="text-sm mt-1 font-medium">{subtitle}</p>
@@ -20,14 +20,16 @@
 		</p>
 		<table class="text-left w-full mt-4">
 			<!-- table header -->
-			<thead class="border rounded-md mb-2 bg-gray-50 flex text-immich-primary w-full h-12">
+			<thead
+				class="border rounded-md mb-2 bg-immich-primary/10 flex text-immich-primary w-full h-12"
+			>
 				<tr class="flex w-full place-items-center">
 					<th class="text-center w-1/3 font-medium text-sm">Status</th>
 					<th class="text-center w-1/3 font-medium text-sm">Active</th>
 					<th class="text-center w-1/3 font-medium text-sm">Waiting</th>
 				</tr>
 			</thead>
-			<tbody class="overflow-y-auto rounded-md w-full max-h-[320px] block border">
+			<tbody class="overflow-y-auto rounded-md w-full max-h-[320px] block border bg-white">
 				<tr class="text-center flex place-items-center w-full h-[40px]">
 					<td class="text-sm px-2 w-1/3 text-ellipsis">{jobStatus ? 'Active' : 'Idle'}</td>
 					<td class="text-sm px-2 w-1/3 text-ellipsis">{activeJobCount}</td>
@@ -39,7 +41,7 @@
 	<div class="w-[30%] flex place-items-center place-content-end">
 		<button
 			on:click={() => dispatch('click')}
-			class="border px-6 py-3 text-sm bg-gray-50 font-medium rounded-2xl hover:bg-immich-primary/10 transition-all hover:cursor-pointer disabled:cursor-not-allowed"
+			class="border px-6 py-3 text-sm bg-gray-50 font-medium rounded-2xl hover:bg-immich-primary/10 transition-all hover:cursor-pointer disabled:cursor-not-allowed shadow-sm"
 			disabled={jobStatus}
 		>
 			{#if jobStatus}
---
+++ web/src/lib/components/admin-page/server-stats/stats-card.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	export let logo: any;
+	export let title: string;
+	export let value: string;
+	export let unit: string | undefined = undefined;
+
+	$: zeros = () => {
+		let result = '';
+		const maxLength = 9;
+		const valueLength = parseInt(value).toString().length;
+		const zeroLength = maxLength - valueLength;
+		for (let i = 0; i < zeroLength; i++) {
+			result += '0';
+		}
+		return result;
+	};
+</script>
+
+<div class="w-[180px] h-[140px] bg-immich-gray rounded-3xl p-5 flex flex-col justify-between">
+	<div class="flex place-items-center gap-4">
+		<svelte:component this={logo} size="40" color={'#4250af'} />
+		<p class="text-immich-primary">{title}</p>
+	</div>
+
+	<div class="text-center font-mono font-semibold text-2xl">
+		<span class="text-[#DCDADA]">{zeros()}</span><span class="text-immich-primary"
+			>{parseInt(value)}</span
+		>
+	</div>
+</div>
