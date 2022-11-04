4531ed2c36cabfaf9e354d1cdd235b4b013d82eb
Alex Tran <alex.tran1502@gmail.com>
2022-11-04 15:40:55 -0500

    Update

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -116,10 +116,10 @@ const config = {
           {
             title: "More",
             items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
+              // {
+              //   label: "Blog",
+              //   to: "/blog",
+              // },
               {
                 label: "GitHub",
                 href: "https://github.com/immich-app/immich",
