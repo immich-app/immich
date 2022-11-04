0b6b18b9c07b0a835cb26f3a30a6a62b79f8664d
Alex Tran <alex.tran1502@gmail.com>
2022-11-04 15:33:39 -0500

    Update

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -27,7 +27,7 @@ function HomepageHeader() {
         <div className={styles.buttonsRow}>
           <div className={styles.buttons}>
             <Link
-              className="button button--secondary button--lg"
+              className={clsx("button button--lg", styles.introButton)}
               to="docs/overview/introduction"
             >
               Introduction
