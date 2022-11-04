cc53989bbec3c62da56abc1449d993ce55efbbc7
Alex Tran <alex.tran1502@gmail.com>
2022-11-04 14:59:22 -0500

    Update

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -12,12 +12,20 @@ function HomepageHeader() {
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <img
+          src="/img/immich-screenshots.png"
+          alt="logo"
+          // style={{ height: "800px" }}
+        />
+        <h1 className="hero__title" style={{ paddingTop: "50px" }}>
+          Immich
+        </h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="docs/installation/requirements"
+            to="docs/overview/introduction"
           >
             Getting Started
           </Link>
