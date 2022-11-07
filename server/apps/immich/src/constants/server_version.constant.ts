d90527a095b0afd0d83a65dbae1443de01405829
Alex Tran <alex.tran1502@gmail.com>
2022-11-06 21:00:32 -0600

    Up version for release

--- server/apps/immich/src/constants/server_version.constant.ts
+++ server/apps/immich/src/constants/server_version.constant.ts
@@ -10,7 +10,7 @@ export interface IServerVersion {
 
 export const serverVersion: IServerVersion = {
   major: 1,
-  minor: 33,
-  patch: 1,
-  build: 52,
+  minor: 34,
+  patch: 0,
+  build: 53,
 };
