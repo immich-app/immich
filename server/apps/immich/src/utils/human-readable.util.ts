1e617a1f8435ae61e99b507363558087affc40aa
Kiel Hurley <kielhurley@gmail.com>
2022-11-23 17:44:53 +1300

    Binary prefixes for human-readable
    Spacing for units

--- server/apps/immich/src/utils/human-readable.util.ts
+++ server/apps/immich/src/utils/human-readable.util.ts
@@ -1,31 +1,28 @@
-const KB = 1000;
-const MB = KB * 1000;
-const GB = MB * 1000;
-const TB = GB * 1000;
-const PB = TB * 1000;
+const KiB = Math.pow(1024, 1);
+const MiB = Math.pow(1024, 2);
+const GiB = Math.pow(1024, 3);
+const TiB = Math.pow(1024, 4);
+const PiB = Math.pow(1024, 5);
 
-export const HumanReadableSize = { KB, MB, GB, TB, PB };
+export const HumanReadableSize = { KiB, MiB, GiB, TiB, PiB };
 
-export function asHumanReadable(bytes: number, precision = 1) {
-  if (bytes >= PB) {
-    return `${(bytes / PB).toFixed(precision)}PB`;
+export function asHumanReadable(bytes: number, precision = 1): string {
+  if (bytes >= PiB) {
+    return `${(bytes / PiB).toFixed(precision)} PiB`;
   }
-
-  if (bytes >= TB) {
-    return `${(bytes / TB).toFixed(precision)}TB`;
+  else if (bytes >= TiB) {
+    return `${(bytes / TiB).toFixed(precision)} TiB`;
   }
-
-  if (bytes >= GB) {
-    return `${(bytes / GB).toFixed(precision)}GB`;
+  else if (bytes >= GiB) {
+    return `${(bytes / GiB).toFixed(precision)} GiB`;
   }
-
-  if (bytes >= MB) {
-    return `${(bytes / MB).toFixed(precision)}MB`;
+  else if (bytes >= MiB) {
+    return `${(bytes / MiB).toFixed(precision)} MiB`;
   }
-
-  if (bytes >= KB) {
-    return `${(bytes / KB).toFixed(precision)}KB`;
+  else if (bytes >= KiB) {
+    return `${(bytes / KiB).toFixed(precision)} KiB`;
   }
-
-  return `${bytes}B`;
+  else {
+    return `${bytes} B`;
+  }
 }
