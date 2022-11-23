95f7ab038fc4e0a3ae2b5aa2891f32cc049b8ad3
Kiel Hurley <kielhurley@gmail.com>
2022-11-23 22:06:10 +1300

    Function for human-readable byte string

--- server/apps/immich/src/utils/human-readable.util.ts
+++ server/apps/immich/src/utils/human-readable.util.ts
@@ -7,22 +7,19 @@ const PiB = Math.pow(1024, 5);
 export const HumanReadableSize = { KiB, MiB, GiB, TiB, PiB };
 
 export function asHumanReadable(bytes: number, precision = 1): string {
-  if (bytes >= PiB) {
-    return `${(bytes / PiB).toFixed(precision)} PiB`;
-  }
-  else if (bytes >= TiB) {
-    return `${(bytes / TiB).toFixed(precision)} TiB`;
-  }
-  else if (bytes >= GiB) {
-    return `${(bytes / GiB).toFixed(precision)} GiB`;
-  }
-  else if (bytes >= MiB) {
-    return `${(bytes / MiB).toFixed(precision)} MiB`;
-  }
-  else if (bytes >= KiB) {
-    return `${(bytes / KiB).toFixed(precision)} KiB`;
-  }
-  else {
-    return `${bytes} B`;
-  }
+  const units = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB'];
+
+	let magnitude = 0;
+	let remainder = bytes;
+	while (remainder >= 1024) {
+		if (magnitude + 1 < units.length) {
+			magnitude++;
+			remainder /= 1024;
+		}
+		else {
+			break;
+		}
+	}
+
+	return `${remainder.toFixed( magnitude == 0 ? 0 : 1 )} ${units[magnitude]}`;
 }
