2acde5337eea56c47af48ca938fa68367ec0c547
Alex Tran <alex.tran1502@gmail.com>
2022-12-06 07:10:13 -0600

    refactor duplicate code

--- server/apps/microservices/src/processors/machine-learning.processor.ts
+++ server/apps/microservices/src/processors/machine-learning.processor.ts
@@ -1,5 +1,4 @@
 import { AssetEntity } from '@app/database/entities/asset.entity';
-import { SmartInfoEntity } from '@app/database/entities/smart-info.entity';
 import { TagEntity, TagType } from '@app/database/entities/tag.entity';
 import { MachineLearningJobNameEnum, QueueNameEnum } from '@app/job';
 import { IMachineLearningJob } from '@app/job/interfaces/machine-learning.interface';
@@ -8,38 +7,61 @@ import { Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import axios from 'axios';
 import { Job } from 'bull';
-import { DataSource, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 
 const immich_machine_learning_url = process.env.IMMICH_MACHINE_LEARNING_URL || 'http://immich-machine-learning:3003';
 
 @Processor(QueueNameEnum.MACHINE_LEARNING)
 export class MachineLearningProcessor {
-  constructor(
-    @InjectRepository(SmartInfoEntity)
-    private smartInfoRepository: Repository<SmartInfoEntity>,
+  readonly logger = new Logger(MachineLearningProcessor.name);
 
+  constructor(
     @InjectRepository(AssetEntity)
     private assetRepository: Repository<AssetEntity>,
 
     @InjectRepository(TagEntity)
     private tagRepository: Repository<TagEntity>,
-
-    private dataSource: DataSource,
   ) {}
 
   @Process({ name: MachineLearningJobNameEnum.IMAGE_TAGGING, concurrency: 2 })
   async tagImage(job: Job<IMachineLearningJob>) {
-    const { asset } = job.data;
+    try {
+      const { asset } = job.data;
 
-    const res = await axios.post(immich_machine_learning_url + '/image-classifier/tag-image', {
-      thumbnailPath: asset.resizePath,
-    });
+      const res = await axios.post(immich_machine_learning_url + '/image-classifier/tag-image', {
+        thumbnailPath: asset.resizePath,
+      });
 
-    if (res.status == 201 && res.data.length > 0) {
-      const tags = res.data;
+      if (res.status == 201 && res.data.length > 0) {
+        this.createAndSaveTagToAsset(asset, res.data);
+      }
+    } catch (e) {
+      this.logger.error(e);
+    }
+  }
+
+  @Process({ name: MachineLearningJobNameEnum.OBJECT_DETECTION, concurrency: 2 })
+  async detectObject(job: Job<IMachineLearningJob>) {
+    try {
+      const { asset }: { asset: AssetEntity } = job.data;
+
+      const res = await axios.post(immich_machine_learning_url + '/object-detection/detect-object', {
+        thumbnailPath: asset.resizePath,
+      });
+
+      if (res.status == 201 && res.data.length > 0) {
+        this.createAndSaveTagToAsset(asset, res.data);
+      }
+    } catch (e) {
+      this.logger.error(e);
+    }
+  }
+
+  private async createAndSaveTagToAsset(asset: AssetEntity, tagNames: string[]) {
+    try {
       const immichTags: TagEntity[] = [];
 
-      for (const tag of tags) {
+      for (const tag of tagNames) {
         let immichTag: TagEntity | null;
 
         immichTag = await this.tagRepository.findOne({ where: { name: tag, userId: asset.userId } });
@@ -62,48 +84,8 @@ export class MachineLearningProcessor {
         assetEntity.tags = assetEntity.tags ? Array.from(new Set([...assetEntity.tags, ...immichTags])) : immichTags;
         await this.assetRepository.save(assetEntity);
       }
-    }
-  }
-
-  @Process({ name: MachineLearningJobNameEnum.OBJECT_DETECTION, concurrency: 2 })
-  async detectObject(job: Job<IMachineLearningJob>) {
-    try {
-      const { asset }: { asset: AssetEntity } = job.data;
-
-      const res = await axios.post(immich_machine_learning_url + '/object-detection/detect-object', {
-        thumbnailPath: asset.resizePath,
-      });
-
-      if (res.status == 201 && res.data.length > 0) {
-        const tags = res.data;
-        const immichTags: TagEntity[] = [];
-
-        for (const tag of tags) {
-          let immichTag: TagEntity | null;
-
-          immichTag = await this.tagRepository.findOne({ where: { name: tag, userId: asset.userId } });
-
-          if (!immichTag) {
-            immichTag = new TagEntity();
-            immichTag.name = tag;
-            immichTag.type = TagType.OBJECT;
-            immichTag.userId = asset.userId;
-
-            immichTag = await this.tagRepository.save(immichTag);
-          }
-
-          immichTags.push(immichTag);
-        }
-
-        const assetEntity = await this.assetRepository.findOne({ where: { id: asset.id } });
-
-        if (assetEntity) {
-          assetEntity.tags = assetEntity.tags ? Array.from(new Set([...assetEntity.tags, ...immichTags])) : immichTags;
-          await this.assetRepository.save(assetEntity);
-        }
-      }
-    } catch (error) {
-      Logger.error(`Failed to trigger object detection pipe line ${String(error)}`);
+    } catch (e) {
+      this.logger.error(e);
     }
   }
 }
