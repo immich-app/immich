4274dc60b2e75ee13cd69b574ddc95ef8c853996
Jason Rasmussen <jrasm91@gmail.com>
2022-12-08 09:30:10 -0500

    fix: return types and dedupe code

--- server/apps/immich/src/api-v1/device-info/device-info.controller.ts
+++ server/apps/immich/src/api-v1/device-info/device-info.controller.ts
@@ -4,7 +4,7 @@ import { AuthUserDto, GetAuthUser } from '../../decorators/auth-user.decorator';
 import { Authenticated } from '../../decorators/authenticated.decorator';
 import { DeviceInfoService } from './device-info.service';
 import { UpsertDeviceInfoDto } from './dto/upsert-device-info.dto';
-import { mapDeviceInfoResponse } from './response-dto/device-info-response.dto';
+import { DeviceInfoResponseDto, mapDeviceInfoResponse } from './response-dto/device-info-response.dto';
 
 @Authenticated()
 @ApiBearerAuth()
@@ -15,20 +15,27 @@ export class DeviceInfoController {
 
   /** @deprecated */
   @Post()
-  public async createDeviceInfo(@GetAuthUser() user: AuthUserDto, @Body(ValidationPipe) dto: UpsertDeviceInfoDto) {
-    const deviceInfo = await this.deviceInfoService.upsert({ ...dto, userId: user.id });
-    return mapDeviceInfoResponse(deviceInfo);
+  public async createDeviceInfo(
+    @GetAuthUser() user: AuthUserDto,
+    @Body(ValidationPipe) dto: UpsertDeviceInfoDto,
+  ): Promise<DeviceInfoResponseDto> {
+    return this.upsertDeviceInfo(user, dto);
   }
 
   /** @deprecated */
   @Patch()
-  public async updateDeviceInfo(@GetAuthUser() user: AuthUserDto, @Body(ValidationPipe) dto: UpsertDeviceInfoDto) {
-    const deviceInfo = await this.deviceInfoService.upsert({ ...dto, userId: user.id });
-    return mapDeviceInfoResponse(deviceInfo);
+  public async updateDeviceInfo(
+    @GetAuthUser() user: AuthUserDto,
+    @Body(ValidationPipe) dto: UpsertDeviceInfoDto,
+  ): Promise<DeviceInfoResponseDto> {
+    return this.upsertDeviceInfo(user, dto);
   }
 
   @Put()
-  public async upsertDeviceInfo(@GetAuthUser() user: AuthUserDto, @Body(ValidationPipe) dto: UpsertDeviceInfoDto) {
+  public async upsertDeviceInfo(
+    @GetAuthUser() user: AuthUserDto,
+    @Body(ValidationPipe) dto: UpsertDeviceInfoDto,
+  ): Promise<DeviceInfoResponseDto> {
     const deviceInfo = await this.deviceInfoService.upsert({ ...dto, userId: user.id });
     return mapDeviceInfoResponse(deviceInfo);
   }
