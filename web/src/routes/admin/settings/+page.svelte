d356b44e19ab60bf97fe496228f6e5790666a05b
Alex Tran <alex.tran1502@gmail.com>
2022-12-09 08:49:57 -0600

    Naming

--- web/src/routes/admin/settings/+page.svelte
+++ web/src/routes/admin/settings/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import FFmpegSettings from '$lib/components/admin-page/settings/ffmpeg/ffmpeg-settings.svelte';
 	import OAuthSettings from '$lib/components/admin-page/settings/oauth/oauth-settings.svelte';
-	import SettingsAccordion from '$lib/components/admin-page/settings/settings-accordion.svelte';
+	import SettingAccordion from '$lib/components/admin-page/settings/setting-accordion.svelte';
 	import LoadingSpinner from '$lib/components/shared-components/loading-spinner.svelte';
 	import { api, SystemConfigDto } from '@api';
 
@@ -19,15 +19,15 @@
 	{#await getConfig()}
 		<LoadingSpinner />
 	{:then configs}
-		<SettingsAccordion
+		<SettingAccordion
 			title="FFmpeg Settings"
 			subtitle="Manage the resolution and encoding information of the video files"
 		>
 			<FFmpegSettings ffmpegConfig={configs.ffmpeg} />
-		</SettingsAccordion>
+		</SettingAccordion>
 
-		<SettingsAccordion title="OAuth Settings" subtitle="Manage the OAuth integration to Immich app">
+		<SettingAccordion title="OAuth Settings" subtitle="Manage the OAuth integration to Immich app">
 			<OAuthSettings oauthConfig={configs.oauth} />
-		</SettingsAccordion>
+		</SettingAccordion>
 	{/await}
 </section>
