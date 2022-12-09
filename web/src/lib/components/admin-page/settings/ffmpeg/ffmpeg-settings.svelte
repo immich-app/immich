fa3e38dc1dfbc9d865089492594249d2c5660339
Alex Tran <alex.tran1502@gmail.com>
2022-12-08 19:32:00 -0600

    Added custom input field

--- web/src/lib/components/admin-page/settings/ffmpeg/ffmpeg-settings.svelte
+++ web/src/lib/components/admin-page/settings/ffmpeg/ffmpeg-settings.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
 	import { SystemConfigFFmpegDto } from '@api';
+	import SettingInputField, { SettingInputFieldType } from '../setting-input-field.svelte';
 
 	export let ffmpegConfig: SystemConfigFFmpegDto;
 </script>
 
 <div>
-	My ffmpeg config is: {ffmpegConfig}
+	{JSON.stringify(ffmpegConfig)}
+
+	<SettingInputField
+		inputType={SettingInputFieldType.NUMBER}
+		label="CRF"
+		bind:value={ffmpegConfig.crf}
+		required={true}
+	/>
+
+	<SettingInputField
+		inputType={SettingInputFieldType.TEXT}
+		label="PRESET"
+		bind:value={ffmpegConfig.preset}
+		required={true}
+	/>
+
+	<button on:click={() => console.log(ffmpegConfig)}>OK</button>
 </div>
---
+++ web/src/lib/components/admin-page/settings/setting-input-field.svelte
@@ -0,0 +1,32 @@
+<script lang="ts" context="module">
+	export enum SettingInputFieldType {
+		TEXT = 'text',
+		NUMBER = 'number',
+		PASSWORD = 'password'
+	}
+</script>
+
+<script lang="ts">
+	export let inputType: SettingInputFieldType;
+	export let value: string;
+	export let label: string;
+	export let required = false;
+
+	const handleInput = (e: Event) => {
+		value = (e.target as HTMLInputElement).value;
+	};
+</script>
+
+<div class="m-4 flex flex-col gap-2">
+	{value}
+	<label class="immich-form-label" for={label}>{label.toUpperCase()}</label>
+	<input
+		class="immich-form-input"
+		id={label}
+		name={label}
+		type={inputType}
+		{required}
+		{value}
+		on:input={handleInput}
+	/>
+</div>
