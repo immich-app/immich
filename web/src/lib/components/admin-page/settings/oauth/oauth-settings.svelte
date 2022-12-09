4e9ccbf27dc5b66e536561726663066c2cc30ff5
Alex Tran <alex.tran1502@gmail.com>
2022-12-09 10:53:29 -0600

    Added save settings

--- web/src/lib/components/admin-page/settings/oauth/oauth-settings.svelte
+++ web/src/lib/components/admin-page/settings/oauth/oauth-settings.svelte
@@ -1,13 +1,48 @@
 <script lang="ts">
-	import { SystemConfigOAuthDto } from '@api';
+	import {
+		notificationController,
+		NotificationType
+	} from '$lib/components/shared-components/notification/notification';
+	import { api, SystemConfigOAuthDto } from '@api';
+	import SettingButtonsRow from '../setting-buttons-row.svelte';
 	import SettingInputField, { SettingInputFieldType } from '../setting-input-field.svelte';
 	import SettingSwitch from '../setting-switch.svelte';
 
 	export let oauthConfig: SystemConfigOAuthDto;
+
+	async function resetToDefault() {
+		const { data: defaultConfig } = await api.systemConfigApi.getConfig();
+
+		oauthConfig = defaultConfig.oauth;
+	}
+
+	async function saveSetting() {
+		try {
+			const { data: currentConfig } = await api.systemConfigApi.getConfig();
+
+			const result = await api.systemConfigApi.updateConfig({
+				ffmpeg: currentConfig.ffmpeg,
+				oauth: oauthConfig
+			});
+
+			oauthConfig = result.data.oauth;
+
+			notificationController.show({
+				message: 'OAuth settings saved',
+				type: NotificationType.Info
+			});
+		} catch (e) {
+			console.error('Error [oauth-settings] [saveSetting]', e);
+			notificationController.show({
+				message: 'Unable to save settings',
+				type: NotificationType.Error
+			});
+		}
+	}
 </script>
 
 <div class="mt-2">
-	<form autocomplete="off">
+	<form autocomplete="off" on:submit|preventDefault>
 		<div class="mt-4">
 			<SettingSwitch title="Enable" bind:checked={oauthConfig.enabled} />
 		</div>
@@ -57,9 +92,12 @@
 		<div class="mt-4">
 			<SettingSwitch
 				title="AUTO REGISTER"
+				subtitle="Automatically register new users after singning in with OAuth"
 				bind:checked={oauthConfig.autoRegister}
 				disabled={!oauthConfig.enabled}
 			/>
 		</div>
+
+		<SettingButtonsRow on:reset={resetToDefault} on:save={saveSetting} />
 	</form>
 </div>
