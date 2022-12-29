4391c9ee6d828f3cdda86f066ef91f9a854d2b09
otbutz <tbutz@optitool.de>
2022-12-29 12:20:46 +0100

    Fix shellcheck issues

--- nginx/start.sh
+++ nginx/start.sh
@@ -2,11 +2,15 @@
 set -e
 
 export IMMICH_WEB_URL=${IMMICH_WEB_URL:-http://immich-web:3000}
-export IMMICH_WEB_SCHEME=$(echo $IMMICH_WEB_URL | grep -Eo 'https?://')
-export IMMICH_WEB_HOST=$(echo $IMMICH_WEB_URL | cut -d '/' -f 3)
+IMMICH_WEB_SCHEME=$(echo "$IMMICH_WEB_URL" | grep -Eo '^https?://' || echo "http://")
+export IMMICH_WEB_SCHEME
+IMMICH_WEB_HOST=$(echo "$IMMICH_WEB_URL" | cut -d '/' -f 3)
+export IMMICH_WEB_HOST
 export IMMICH_SERVER_URL=${IMMICH_SERVER_URL:-http://immich-server:3001}
-export IMMICH_SERVER_SCHEME=$(echo $IMMICH_WEB_URL | grep -Eo 'https?://')
-export IMMICH_SERVER_HOST=$(echo $IMMICH_SERVER_URL | cut -d '/' -f 3)
+IMMICH_SERVER_SCHEME=$(echo "$IMMICH_WEB_URL" | grep -Eo '^https?://' || echo "http://")
+export IMMICH_SERVER_SCHEME
+IMMICH_SERVER_HOST=$(echo "$IMMICH_SERVER_URL" | cut -d '/' -f 3)
+export IMMICH_SERVER_HOST
 
 envsubst '$IMMICH_WEB_SCHEME $IMMICH_WEB_HOST  $IMMICH_SERVER_SCHEME $IMMICH_SERVER_HOST' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
