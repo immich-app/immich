a895510d2c5947540d036e49c807e30c1a5b54cf
bt90 <btom1990@googlemail.com>
2022-12-29 15:13:31 +0000

    Appl shellcheck fixes

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-if [ `id -u` -eq 0 ] && [ -n "$PUID" ] && [ -n "$PGID" ]; then
-    exec setpriv --reuid $PUID --regid $PGID --clear-groups node /usr/src/app/build/index.js
+if [ "$(id -u)" -eq 0 ] && [ -n "$PUID" ] && [ -n "$PGID" ]; then
+    exec setpriv --reuid "$PUID" --regid "$PGID" --clear-groups node /usr/src/app/build/index.js
 else
     exec node /usr/src/app/build/index.js
 fi
