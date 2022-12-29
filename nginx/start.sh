9574190978be55d1791ab8a191e795c38f1884f9
otbutz <tbutz@optitool.de>
2022-12-29 12:11:29 +0100

    Adapt envsubst

--- nginx/start.sh
+++ nginx/start.sh
@@ -2,8 +2,12 @@
 set -e
 
 export IMMICH_WEB_URL=${IMMICH_WEB_URL:-http://immich-web:3000}
+export IMMICH_WEB_SCHEME=$(echo $IMMICH_WEB_URL | grep -Eo 'https?://')
+export IMMICH_WEB_HOST=$(echo $IMMICH_WEB_URL | cut -d '/' -f 3)
 export IMMICH_SERVER_URL=${IMMICH_SERVER_URL:-http://immich-server:3001}
+export IMMICH_SERVER_SCHEME=$(echo $IMMICH_WEB_URL | grep -Eo 'https?://')
+export IMMICH_SERVER_HOST=$(echo $IMMICH_SERVER_URL | cut -d '/' -f 3)
 
-envsubst '$IMMICH_WEB_URL $IMMICH_SERVER_URL' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$IMMICH_WEB_SCHEME $IMMICH_WEB_HOST  $IMMICH_SERVER_SCHEME $IMMICH_SERVER_HOST' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
