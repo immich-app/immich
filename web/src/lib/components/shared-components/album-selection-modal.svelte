bb552c77c14d44eee49641b418e76cc72115bc0b
Alex Tran <alex.tran1502@gmail.com>
2022-12-29 15:31:23 -0600

    styling

--- web/src/lib/components/shared-components/album-selection-modal.svelte
+++ web/src/lib/components/shared-components/album-selection-modal.svelte
@@ -58,51 +58,47 @@
 	<div class=" max-h-[400px] overflow-y-auto immich-scrollba pb-10">
 		<div class="flex flex-col mb-2">
 			{#if loading}
-				<div>
-					{#each { length: 3 } as _}
-						<div class="animate-pulse flex gap-4 px-6 py-2">
-							<div class="h-12 w-12 bg-slate-200 rounded-xl" />
-							<div class="flex flex-col items-start justify-center gap-2">
-								<span class="animate-pulse w-36 h-4 bg-slate-200" />
-								<div class="flex animate-pulse gap-1">
-									<span class="w-8 h-3 bg-slate-200" />
-									<span class="w-20 h-3 bg-slate-200" />
-								</div>
+				{#each { length: 3 } as _}
+					<div class="animate-pulse flex gap-4 px-6 py-2">
+						<div class="h-12 w-12 bg-slate-200 rounded-xl" />
+						<div class="flex flex-col items-start justify-center gap-2">
+							<span class="animate-pulse w-36 h-4 bg-slate-200" />
+							<div class="flex animate-pulse gap-1">
+								<span class="w-8 h-3 bg-slate-200" />
+								<span class="w-20 h-3 bg-slate-200" />
 							</div>
 						</div>
-					{/each}
-				</div>
+					</div>
+				{/each}
 			{:else}
-				<div in:fade={{ duration: 250 }}>
-					<button
-						on:click={handleNew}
-						class="flex gap-4 px-6 py-2 hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors items-center"
-					>
-						<div class="h-12 w-12 flex justify-center items-center">
-							<Plus size="30" />
-						</div>
-						<p class="">
-							New {#if shared}Shared {/if}Album
-						</p>
-					</button>
-					{#if albums.length > 0}
-						{#if !shared}
-							<p class="text-xs px-5 py-3">RECENT</p>
-							{#each recentAlbums as album (album.id)}
-								<AlbumListItem variant="simple" {album} on:album={() => handleSelect(album)} />
-							{/each}
-						{/if}
-
-						{#if !shared}
-							<p class="text-xs px-5 py-3">ALL ALBUMS</p>
-						{/if}
-						{#each albums as album (album.id)}
-							<AlbumListItem {album} on:album={() => handleSelect(album)} />
+				<button
+					on:click={handleNew}
+					class="flex gap-4 px-6 py-2 hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors items-center"
+				>
+					<div class="h-12 w-12 flex justify-center items-center">
+						<Plus size="30" />
+					</div>
+					<p class="">
+						New {#if shared}Shared {/if}Album
+					</p>
+				</button>
+				{#if albums.length > 0}
+					{#if !shared}
+						<p class="text-xs px-5 py-3">RECENT</p>
+						{#each recentAlbums as album (album.id)}
+							<AlbumListItem variant="simple" {album} on:album={() => handleSelect(album)} />
 						{/each}
-					{:else}
-						<p class="text-sm px-5 py-1">It looks like you do not have any albums yet.</p>
 					{/if}
-				</div>
+
+					{#if !shared}
+						<p class="text-xs px-5 py-3">ALL ALBUMS</p>
+					{/if}
+					{#each albums as album (album.id)}
+						<AlbumListItem {album} on:album={() => handleSelect(album)} />
+					{/each}
+				{:else}
+					<p class="text-sm px-5 py-1">It looks like you do not have any albums yet.</p>
+				{/if}
 			{/if}
 		</div>
 	</div>
