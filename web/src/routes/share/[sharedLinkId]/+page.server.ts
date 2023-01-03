dcc389522178604039fc412453b8211524dff2b3
Alex Tran <alex.tran1502@gmail.com>
2023-01-03 12:52:21 -0600

    Added key authentication to server api

--- web/src/routes/share/[sharedLinkId]/+page.server.ts
+++ web/src/routes/share/[sharedLinkId]/+page.server.ts
@@ -12,17 +12,26 @@ export const load: PageServerLoad = async ({ params }) => {
 		const { data: sharedLink } = await serverApi.shareApi.getSharedLink(sharedLinkId);
 
 		for (const assetId of sharedLink.assets) {
-			const { data: asset } = await serverApi.assetApi.getAssetById(assetId);
+			const { data: asset } = await serverApi.assetApi.getAssetById(assetId, {
+				params: {
+					key: sharedLink.key
+				}
+			});
 			assets.push(asset);
 		}
 
 		if (sharedLink.album) {
-			const { data: album } = await serverApi.albumApi.getAlbumInfo(sharedLink.album.id);
+			const { data: album } = await serverApi.albumApi.getAlbumInfo(sharedLink.album.id, {
+				params: {
+					key: sharedLink.key
+				}
+			});
 			sharedLink.album = album;
 		}
 
 		return { sharedLink, assets };
 	} catch (e) {
+		console.log(e);
 		throw error(404, {
 			message: 'Invalid shared link'
 		});
