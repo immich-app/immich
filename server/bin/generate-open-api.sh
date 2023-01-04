a533f059864758ae88f268ecfb21eec3f8b692c8
Jason Rasmussen <jrasm91@gmail.com>
2023-01-04 10:05:22 -0500

    feat: extract to sh script

---
+++ server/bin/generate-open-api.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# mobile
+rm -rf ../mobile/openapi
+
+cd ./openapi-generator/templates/serialization/native
+
+wget -O native_class.mustache https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/modules/openapi-generator/src/main/resources/dart2/serialization/native/native_class.mustache
+
+patch -u native_class.mustache < native_class.mustache.patch 
+
+cd ../../../..
+
+npx openapi-generator-cli generate -g dart -i ./immich-openapi-specs.json -o ../mobile/openapi -t ./openapi-generator/templates
+
+# web
+rm -rf ../web/src/api/open-api 
+npx openapi-generator-cli generate -g typescript-axios -i ./immich-openapi-specs.json -o ../web/src/api/open-api
