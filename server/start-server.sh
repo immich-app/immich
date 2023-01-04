eab78fae48374964899327e6668afbf187a7f25b
James <jdm12989@gmail.com>
2023-01-04 07:23:18 -0500

    Support secrets

--- server/start-server.sh
+++ server/start-server.sh
@@ -1,2 +1,31 @@
-#! /bin/sh
+#! /bin/bash
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		printf >&2 'error: both %s and %s are set (but are exclusive)\n' "$var" "$fileVar"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+file_env 'JWT_SECRET'
+file_env 'DB_HOSTNAME' 'immich_postgres'
+file_env 'DB_DATABASE_NAME' 'immich'
+file_env 'DB_USERNAME' 'postgres'
+file_env 'DB_PASSWORD' 'postgres'
+
 exec node dist/apps/immich/apps/immich/src/main
