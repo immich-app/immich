58f704f6e9e21fe77387f0c1260acd646e22ee9f
Chris Hamer <developer@monotok.org>
2023-01-05 01:01:55 +0000

    Allow the use of SSL connections to the postgres database.

--- server/libs/infra/src/db/config/database.config.ts
+++ server/libs/infra/src/db/config/database.config.ts
@@ -1,7 +1,8 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { DataSource } from 'typeorm';
 
-export const databaseConfig: PostgresConnectionOptions = {
+let additionalSSLDatabaseConfig;
+let baseDatabaseConfig: PostgresConnectionOptions = {
   type: 'postgres',
   host: process.env.DB_HOSTNAME || 'immich_postgres',
   port: parseInt(process.env.DB_PORT || '5432'),
@@ -15,4 +16,17 @@ export const databaseConfig: PostgresConnectionOptions = {
   connectTimeoutMS: 10000, // 10 seconds
 };
 
+if(process.env.DB_SSL) {
+    additionalSSLDatabaseConfig = {
+      ssl: {
+        rejectUnauthorized : false,
+      },
+    };
+}
+else {
+    additionalSSLDatabaseConfig = {
+    };
+}
+
+export const databaseConfig: PostgresConnectionOptions = {...baseDatabaseConfig, ...additionalSSLDatabaseConfig};
 export const dataSource = new DataSource(databaseConfig);
