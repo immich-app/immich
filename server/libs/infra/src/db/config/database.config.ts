62f53235d5df15b36734519e8e7cbef8423820ab
Chris Hamer <developer@monotok.org>
2023-01-07 00:04:16 +0000

    Refactor the database connection to optionally use a URL string instead of the env variables

--- server/libs/infra/src/db/config/database.config.ts
+++ server/libs/infra/src/db/config/database.config.ts
@@ -1,7 +1,21 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { DataSource } from 'typeorm';
 
-export const databaseConfig: PostgresConnectionOptions = {
+let additionalSSLDatabaseConfig;
+let baseDatabaseConfig: PostgresConnectionOptions = {
+  type: 'postgres',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: false,
+  migrations: [__dirname + '/../migrations/*.{js,ts}'],
+  migrationsRun: true,
+};
+
+let urlBasedDatabaseConfig: PostgresConnectionOptions = {
+  type: 'postgres',
+  url: process.env.DB_URL || 'postgres://postgres:postgres@immich_postgres:5432/immich?sslmode=disable',
+};
+
+let envBasedDatabaseConfig: PostgresConnectionOptions = {
   type: 'postgres',
   host: process.env.DB_HOSTNAME || 'immich_postgres',
   port: parseInt(process.env.DB_PORT || '5432'),
@@ -17,5 +31,14 @@ export const databaseConfig: PostgresConnectionOptions = {
     : false,
   connectTimeoutMS: 10000, // 10 seconds
 };
+
+if(process.env.DB_URL) {
+    additionalSSLDatabaseConfig = urlBasedDatabaseConfig;
+}
+else {
+    additionalSSLDatabaseConfig = envBasedDatabaseConfig;
+}
+
+export const databaseConfig: PostgresConnectionOptions = {...baseDatabaseConfig, ...additionalSSLDatabaseConfig};
 
 export const dataSource = new DataSource(databaseConfig);
