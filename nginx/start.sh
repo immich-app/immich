d040f90754b70f8731979d20eb6763efa27be0c9
otbutz <tbutz@optitool.de>
2023-01-10 14:33:56 +0100

    Switch to sh

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,12 +1,12 @@
-#! /bin/bash
+#! /bin/sh
 set -e
 
-export IMMICH_WEB_URL=${IMMICH_WEB_URL:-http://immich-web:3000}
+export IMMICH_WEB_URL="${IMMICH_WEB_URL:-http://immich-web:3000}"
 IMMICH_WEB_SCHEME=$(echo "$IMMICH_WEB_URL" | grep -Eo '^https?://' || echo "http://")
 export IMMICH_WEB_SCHEME
 IMMICH_WEB_HOST=$(echo "$IMMICH_WEB_URL" | cut -d '/' -f 3)
 export IMMICH_WEB_HOST
-export IMMICH_SERVER_URL=${IMMICH_SERVER_URL:-http://immich-server:3001}
+export IMMICH_SERVER_URL="${IMMICH_SERVER_URL:-http://immich-server:3001}"
 IMMICH_SERVER_SCHEME=$(echo "$IMMICH_WEB_URL" | grep -Eo '^https?://' || echo "http://")
 export IMMICH_SERVER_SCHEME
 IMMICH_SERVER_HOST=$(echo "$IMMICH_SERVER_URL" | cut -d '/' -f 3)
