89a6ed2a5bacfd5cf7716c3a1028a7d4470dc601
Matthias Rupp <matthias.rupp@posteo.de>
2023-01-11 21:54:12 +0100

    feat(mobile): Rework of the exif sheet (#1213)
    * Draggable sheet in asset viewer page
    
    * Minor improvements
    
    * Fix display bug
    
    * Fix some styling
    
    Co-authored-by: Alex <alex.tran1502@gmail.com>

--- mobile/lib/modules/home/ui/control_bottom_app_bar.dart
+++ mobile/lib/modules/home/ui/control_bottom_app_bar.dart
@@ -5,6 +5,7 @@ import 'package:hive/hive.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/constants/hive_box.dart';
 import 'package:immich_mobile/modules/home/ui/delete_diaglog.dart';
+import 'package:immich_mobile/shared/ui/drag_sheet.dart';
 import 'package:immich_mobile/utils/image_url_builder.dart';
 import 'package:openapi/api.dart';
 
@@ -194,56 +195,6 @@ class AddToAlbumTitleRow extends StatelessWidget {
                 fontSize: 14,
               ),
             ).tr(),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class CustomDraggingHandle extends StatelessWidget {
-  const CustomDraggingHandle({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 5,
-      width: 30,
-      decoration: BoxDecoration(
-        color: Colors.grey[500],
-        borderRadius: BorderRadius.circular(16),
-      ),
-    );
-  }
-}
-
-class ControlBoxButton extends StatelessWidget {
-  const ControlBoxButton({
-    Key? key,
-    required this.label,
-    required this.iconData,
-    required this.onPressed,
-  }) : super(key: key);
-
-  final String label;
-  final IconData iconData;
-  final Function onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialButton(
-      padding: const EdgeInsets.all(10),
-      shape: const CircleBorder(),
-      onPressed: () => onPressed(),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Icon(iconData, size: 24),
-          const SizedBox(height: 6),
-          Text(
-            label,
-            style: const TextStyle(fontSize: 12.0),
           ),
         ],
       ),
