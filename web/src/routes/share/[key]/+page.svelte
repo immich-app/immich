05cf1edbb9d3d8e54f466ff97a8aff6d69e4d88d
Alex Tran <alex.tran1502@gmail.com>
2023-01-12 21:12:04 -0600

    Added viewer for individual shared link

--- web/src/routes/share/[key]/+page.svelte
+++ web/src/routes/share/[key]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import AlbumViewer from '$lib/components/album-page/album-viewer.svelte';
-	import { AlbumResponseDto } from '@api';
+	import IndividualSharedViewer from '$lib/components/share-page/individual-shared-viewer.svelte';
+	import { AlbumResponseDto, SharedLinkType } from '@api';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -8,13 +9,20 @@
 	const { sharedLink } = data;
 
 	let album: AlbumResponseDto | null = null;
+
 	if (sharedLink.album) {
 		album = { ...sharedLink.album, assets: sharedLink.assets };
 	}
 </script>
 
-{#if album}
+{#if sharedLink.type == SharedLinkType.Album && album}
 	<div class="immich-scrollbar">
 		<AlbumViewer {album} {sharedLink} />
+	</div>
+{/if}
+
+{#if sharedLink.type == SharedLinkType.Individual}
+	<div class="immich-scrollbar">
+		<IndividualSharedViewer {sharedLink} />
 	</div>
 {/if}
