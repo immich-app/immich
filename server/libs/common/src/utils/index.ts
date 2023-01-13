97c2332c1645a5f40da86acf3d6a5d51b26c0691
Jason Rasmussen <jrasm91@gmail.com>
2023-01-13 00:51:32 -0500

    refactor: logging

--- server/libs/common/src/utils/index.ts
+++ server/libs/common/src/utils/index.ts
@@ -1,3 +1,15 @@
+import { LogLevel } from '@nestjs/common';
+
 export * from './time-utils';
 export * from './asset-utils';
 export * from './user-utils';
+
+export function getLogLevels() {
+  const LOG_LEVELS: LogLevel[] = ['verbose', 'debug', 'log', 'warn', 'error'];
+  let logLevel = process.env.LOG_LEVEL || 'log';
+  if (logLevel === 'simple') {
+    logLevel = 'log';
+  }
+  const logLevelIndex = LOG_LEVELS.indexOf(logLevel as LogLevel);
+  return logLevelIndex === -1 ? [] : LOG_LEVELS.slice(logLevelIndex);
+}
