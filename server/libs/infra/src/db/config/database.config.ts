04ca034c6a332d97688707c55818253c2a1d1a33
Chris Hamer <developer@monotok.org>
2023-01-15 01:22:12 +0000

    Formatted file correctly

--- server/libs/infra/src/db/config/database.config.ts
+++ server/libs/infra/src/db/config/database.config.ts
@@ -8,6 +8,7 @@ let baseDatabaseConfig: PostgresConnectionOptions = {
   synchronize: false,
   migrations: [__dirname + '/../migrations/*.{js,ts}'],
   migrationsRun: true,
+  connectTimeoutMS: 10000, // 10 seconds
 };
 
 let envBasedDatabaseConfig = {
@@ -16,12 +17,11 @@ let envBasedDatabaseConfig = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE_NAME,
-  connectTimeoutMS: 10000, // 10 seconds
 };
 
 const url = process.env.DB_URL;
-additionalSSLDatabaseConfig = url ? ({ url }) : (envBasedDatabaseConfig)
+additionalSSLDatabaseConfig = url ? { url } : envBasedDatabaseConfig;
 
-export const databaseConfig: PostgresConnectionOptions = {...baseDatabaseConfig, ...additionalSSLDatabaseConfig};
+export const databaseConfig: PostgresConnectionOptions = { ...baseDatabaseConfig, ...additionalSSLDatabaseConfig };
 
 export const dataSource = new DataSource(databaseConfig);
