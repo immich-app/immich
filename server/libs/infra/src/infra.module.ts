62d62c63b34808417f41df26c87af3810e10a710
Jason Rasmussen <jrasm91@gmail.com>
2023-01-16 15:55:36 -0500

    chore: fix provider

--- server/libs/infra/src/infra.module.ts
+++ server/libs/infra/src/infra.module.ts
@@ -2,12 +2,13 @@ import { ICryptoRepository, IKeyRepository, IUserRepository } from '@app/domain'
 import { databaseConfig, UserEntity } from '@app/infra';
 import { Global, Module, Provider } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { cryptoRepository } from './auth/crypto.repository';
 import { APIKeyEntity, UserRepository } from './db';
 import { APIKeyRepository } from './db/repository';
 
 const providers: Provider[] = [
   //
-  { provide: ICryptoRepository, useValue: ICryptoRepository },
+  { provide: ICryptoRepository, useValue: cryptoRepository },
   { provide: IKeyRepository, useClass: APIKeyRepository },
   { provide: IUserRepository, useClass: UserRepository },
 ];
