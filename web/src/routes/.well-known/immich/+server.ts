753d81adadac23498c302d971b46aeed4ffdcfd1
Alex Tran <alex.tran1502@gmail.com>
2023-01-19 10:51:39 -0600

    Fix incorrect way to access the environment variable in Svelte

--- web/src/routes/.well-known/immich/+server.ts
+++ web/src/routes/.well-known/immich/+server.ts
@@ -1,7 +1,6 @@
-import { env } from '$env/dynamic/public';
 import { json } from '@sveltejs/kit';
 
-const endpoint = env.PUBLIC_IMMICH_API_URL_EXTERNAL || '/api';
+const endpoint = process.env.IMMICH_API_URL_EXTERNAL || '/api';
 
 export const GET = async () => {
 	return json({
