4b92041e7b2407d94a4cc0f179a6e971979f8ea8
Skyler Mäntysaari <samip5@users.noreply.github.com>
2023-01-20 09:02:30 +0200

    feat(server): Support webm without transcoding.
    Transcoding result doesn't appear to be used by anything expect for quicktime.

--- server/apps/immich/src/config/asset-upload.config.ts
+++ server/apps/immich/src/config/asset-upload.config.ts
@@ -28,7 +28,7 @@ function fileFilter(req: Request, file: any, cb: any) {
   }
   if (
     file.mimetype.match(
-      /\/(jpg|jpeg|png|gif|mp4|x-msvideo|quicktime|heic|heif|dng|x-adobe-dng|webp|tiff|3gpp|nef|x-nikon-nef)$/,
+      /\/(jpg|jpeg|png|gif|mp4|webm|x-msvideo|quicktime|heic|heif|dng|x-adobe-dng|webp|tiff|3gpp|nef|x-nikon-nef)$/,
     )
   ) {
     cb(null, true);
