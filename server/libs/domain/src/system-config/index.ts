64bc95d6db886b06ba45aa8be9ad0d148a431afe
Jason Rasmussen <jrasm91@gmail.com>
2023-01-23 22:00:20 -0500

    refactor: constants

--- server/libs/domain/src/system-config/index.ts
+++ server/libs/domain/src/system-config/index.ts
@@ -1,6 +1,5 @@
 export * from './dto';
 export * from './response-dto';
-export * from './system-config.datetime-variables';
-export * from './system-config.initial';
+export * from './system-config.constants';
 export * from './system-config.repository';
 export * from './system-config.service';
