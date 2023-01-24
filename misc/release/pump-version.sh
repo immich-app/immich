50c36068e74631cbdd9d85004a0bc7af92c52e15
bo0tzz <git@bo0tzz.me>
2023-01-24 16:26:58 +0100

    build: Add workflow for creating draft releases (#1402)
    * build: Change pump-version script to use flags
    
    * build: Create initial prepare-release workflow
    
    * build: Fix release script path
    
    * build: Rename .env.example to example.env
    
    * docs: propagate example.env rename
    
    * build: Fix pump-version script patch argument
    
    * build: Final tweaks to release scripts

--- misc/release/pump-version.sh
+++ misc/release/pump-version.sh
@@ -3,16 +3,25 @@
 #
 # Pump one or both of the server/mobile versions in appropriate files
 #
-# usage: './scripts/pump-version.sh <major|minor|patch|fase> <mobile|false'>
+# usage: './scripts/pump-version.sh -s <major|minor|patch> <-m>
 #
 # examples:
-#    ./scripts/pump-version.sh major false        # 1.0.0+50 => 2.0.0+50 
-#    ./scripts/pump-version.sh minor mobile       # 1.0.0+50 => 1.1.0+51
-#    ./scripts/pump-version.sh false mobile       # 1.0.0+50 => 1.0.0+51
+#    ./scripts/pump-version.sh -s major        # 1.0.0+50 => 2.0.0+50 
+#    ./scripts/pump-version.sh -s minor -m     # 1.0.0+50 => 1.1.0+51
+#    ./scripts/pump-version.sh -m              # 1.0.0+50 => 1.0.0+51
 #
 
-SERVER_PUMP=$1
-MOBILE_PUMP=$2
+SERVER_PUMP="false"
+MOBILE_PUMP="false"
+
+while getopts 's:m:' flag; do
+  case "${flag}" in
+    s) SERVER_PUMP=${OPTARG} ;;
+    m) MOBILE_PUMP=${OPTARG} ;;
+    *) echo "Invalid args"
+       exit 1 ;;
+  esac
+done
 
 CURRENT_SERVER=$(cat server/package.json | jq -r '.version')
 MAJOR=$(echo $CURRENT_SERVER | cut -d '.' -f1)
@@ -23,12 +32,12 @@ if [[ $SERVER_PUMP == "major" ]]; then
   MAJOR=$((MAJOR + 1))
 elif [[ $SERVER_PUMP == "minor" ]]; then
   MINOR=$((MINOR + 1))
-elif [[ $1 == "patch" ]]; then
+elif [[ $SERVER_PUMP == "patch" ]]; then
   PATCH=$((PATCH + 1))
 elif [[ $SERVER_PUMP == "false" ]]; then
   echo 'Skipping Server Pump'
 else
-  echo 'Expected <major|minor|patch|false> for the first argument'
+  echo 'Expected <major|minor|patch|false> for the server argument'
   exit 1
 fi
 
@@ -36,12 +45,12 @@ NEXT_SERVER=$MAJOR.$MINOR.$PATCH
 
 CURRENT_MOBILE=$(cat mobile/pubspec.yaml | grep "^version: .*+[0-9]\+$" | cut -d "+" -f2)
 NEXT_MOBILE=$CURRENT_MOBILE
-if [[ $MOBILE_PUMP == "mobile" ]]; then
+if [[ $MOBILE_PUMP == "true" ]]; then
   set $((NEXT_MOBILE++))
 elif [[ $MOBILE_PUMP == "false" ]]; then
   echo 'Skipping Mobile Pump'
 else
-  echo 'Expected <mobile|false> for the second argument'
+  echo "Fatal: MOBILE_PUMP value $MOBILE_PUMP is invalid"
   exit 1
 fi
 
@@ -65,3 +74,5 @@ if [ "$CURRENT_MOBILE" != "$NEXT_MOBILE" ]; then
   sed -i "s/\"android\.injected\.version\.code\" => $CURRENT_MOBILE,/\"android\.injected\.version\.code\" => $NEXT_MOBILE,/" mobile/android/fastlane/Fastfile
   sed -i "s/^version: $CURRENT_SERVER+$CURRENT_MOBILE$/version: $NEXT_SERVER+$NEXT_MOBILE/" mobile/pubspec.yaml
 fi
+
+echo "IMMICH_VERSION=v$NEXT_SERVER" >> $GITHUB_ENV
