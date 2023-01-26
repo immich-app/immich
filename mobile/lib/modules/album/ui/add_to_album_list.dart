296fad5a5610a32fd01694d6fd69611151a053de
Marty Fuhry <martyfuhry@gmail.com>
2023-01-25 22:10:56 -0500

    updates album

--- mobile/lib/modules/album/ui/add_to_album_list.dart
+++ mobile/lib/modules/album/ui/add_to_album_list.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/modules/album/providers/album.provider.dart';
 import 'package:immich_mobile/modules/album/providers/asset_selection.provider.dart';
@@ -27,6 +28,17 @@ class AddToAlbumList extends HookConsumerWidget {
     final albums = ref.watch(albumProvider);
     final albumService = ref.watch(albumServiceProvider);
     final sharedAlbums = ref.watch(sharedAlbumProvider);
+
+    useEffect(
+      () {
+        // Fetch album updates, e.g., cover image
+        ref.read(albumProvider.notifier).getAllAlbums();
+        ref.read(sharedAlbumProvider.notifier).getAllSharedAlbums();
+
+        return null;
+      },
+      [], 
+    );
 
     void addToAlbum(AlbumResponseDto album) async {
       final result = await albumService.addAdditionalAssetToAlbum(
