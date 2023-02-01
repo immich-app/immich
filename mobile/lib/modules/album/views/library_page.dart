9ff07060e5080024fec174c0ea2ab26b2d5d41be
Matthias Rupp <matthias.rupp@posteo.de>
2023-02-01 15:16:00 +0100

    Add album sorting

--- mobile/lib/modules/album/views/library_page.dart
+++ mobile/lib/modules/album/views/library_page.dart
@@ -1,11 +1,14 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:collection/collection.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/modules/album/providers/album.provider.dart';
 import 'package:immich_mobile/modules/album/ui/album_thumbnail_card.dart';
 import 'package:immich_mobile/routing/router.dart';
+import 'package:openapi/api.dart';
 
 class LibraryPage extends HookConsumerWidget {
   const LibraryPage({Key? key}) : super(key: key);
@@ -25,9 +28,6 @@ class LibraryPage extends HookConsumerWidget {
     Widget buildAppBar() {
       return const SliverAppBar(
         centerTitle: true,
-        floating: true,
-        pinned: false,
-        snap: false,
         automaticallyImplyLeading: false,
         title: Text(
           'IMMICH',
@@ -36,6 +36,69 @@ class LibraryPage extends HookConsumerWidget {
             fontWeight: FontWeight.bold,
             fontSize: 22,
           ),
+        ),
+      );
+    }
+
+    final selectedAlbumSortOrder = useState(0);
+
+    List<AlbumResponseDto> sortedAlbums() {
+      if (selectedAlbumSortOrder.value == 0) {
+        return albums.sortedBy((album) => album.createdAt).reversed.toList();
+      }
+      return albums.sortedBy((album) => album.albumName);
+    }
+
+    Widget buildSortButton() {
+      const options = ["Most recently created", "Album title"];
+
+      return PopupMenuButton(
+        position: PopupMenuPosition.over,
+        itemBuilder: (BuildContext context) {
+          return options.mapIndexed<PopupMenuEntry<int>>((index, option) {
+            final selected = selectedAlbumSortOrder.value == index;
+            return PopupMenuItem(
+              value: index,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 12.0),
+                    child: Icon(
+                      Icons.check,
+                      color: selected
+                          ? Theme.of(context).primaryColor
+                          : Colors.transparent,
+                    ),
+                  ),
+                  Text(
+                    option,
+                    style: selected
+                        ? TextStyle(color: Theme.of(context).primaryColor)
+                        : null,
+                  )
+                ],
+              ),
+            );
+          }).toList();
+        },
+        onSelected: (int value) {
+          selectedAlbumSortOrder.value = value;
+        },
+        child: Row(
+          children: [
+            Icon(
+              Icons.swap_vert,
+              size: 28,
+              color: Theme.of(context).primaryColor,
+            ),
+            Text(
+              options[selectedAlbumSortOrder.value],
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ],
         ),
       );
     }
@@ -87,10 +150,16 @@ class LibraryPage extends HookConsumerWidget {
           SliverToBoxAdapter(
             child: Padding(
               padding: const EdgeInsets.all(12.0),
-              child: const Text(
-                'library_page_albums',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ).tr(),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'library_page_albums',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ).tr(),
+                  buildSortButton(),
+                ],
+              ),
             ),
           ),
           SliverPadding(
@@ -100,7 +169,7 @@ class LibraryPage extends HookConsumerWidget {
                 spacing: 12,
                 children: [
                   buildCreateAlbumButton(),
-                  for (var album in albums)
+                  for (var album in sortedAlbums())
                     AlbumThumbnailCard(
                       album: album,
                     ),
