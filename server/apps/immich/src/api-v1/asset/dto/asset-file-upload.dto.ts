2e844696fcb1e4fd68ddf6b26f434155d93ae7cd
Michel Heusschen <59014050+michelheusschen@users.noreply.github.com>
2023-02-10 10:05:25 +0100

    remove unused DTO

--- server/apps/immich/src/api-v1/asset/dto/asset-file-upload.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
-
-export class AssetFileUploadDto {
-  @IsNotEmpty()
-  @ApiProperty({ type: 'string', format: 'binary' })
-  assetData!: any;
-}
