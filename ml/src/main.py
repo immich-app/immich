6ab4e2a3379183e7f72cb8e7f6a152afca2c895a
Alex Tran <alex.tran1502@gmail.com>
2023-02-17 15:37:02 -0600

    dev: Add image classification using mobilenet

--- ml/src/main.py
+++ ml/src/main.py
@@ -1,16 +1,24 @@
 from flask import Flask
 import os
 from sentence_transformers import SentenceTransformer, util
+from transformers import pipeline
 from PIL import Image
 
 model = SentenceTransformer('clip-ViT-B-32')
+classifier = pipeline(task="image-classification",
+                      model="google/mobilenet_v2_1.0_224")
+
 server = Flask(__name__)
 is_dev = os.getenv('NODE_ENV') == 'development'
 
 
 @server.route("/")
 def hello():
-    print("Triggering hello route", flush=True)
+    preds = classifier(
+        'upload/3e54a230-5daa-4983-b356-af4fd65292aa/thumb/WEB/bf7d976f-4cbc-4991-a006-b11c0811781c.jpeg')
+    preds = [{"score": round(pred["score"], 4), "label": pred["label"]}
+             for pred in preds]
+    print(*preds, sep="\n", flush=True)
     return "Immich Machine Learning 12!\n"
 
 
