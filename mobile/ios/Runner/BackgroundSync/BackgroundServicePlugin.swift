cf10f747e715cdad881f0c33df85ffa4d3df4bc6
Marty Fuhry <marty@fuhry.farm>
2023-02-17 13:27:37 -0500

    adds comments, made the AppRefresh task only run while not requiring network or charge

--- mobile/ios/Runner/BackgroundSync/BackgroundServicePlugin.swift
+++ mobile/ios/Runner/BackgroundSync/BackgroundServicePlugin.swift
@@ -17,13 +17,18 @@ class BackgroundServicePlugin: NSObject, FlutterPlugin {
         flutterPluginRegistrantCallback = callback
     }
 
-  //  Pause the application in XCode, then enter
-  // e -l objc -- (void)[[BGTaskScheduler sharedScheduler] _simulateLaunchForTaskWithIdentifier:@"immichBackground"]
-  // Then resume the application see the background code run
-  // Tested on a physical device, not a simulator
-
+    //  Pause the application in XCode, then enter
+    // e -l objc -- (void)[[BGTaskScheduler sharedScheduler] _simulateLaunchForTaskWithIdentifier:@"immichBackground"]
+    // Then resume the application see the background code run
+    // Tested on a physical device, not a simulator
+    // This will simulate running the BGAppRefreshTask, which is the "quick sync" task.
+    // I'm not sure how to simulate running the BGProcessingTask
+    
+    // This is the task ID in Info.plist to register as our background task ID
     public static let backgroundSyncTaskID = "immichBackground"
     
+    // Establish communication with the main isolate and set up the channel call
+    // to this BackgroundServicePlugion()
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(
             name: "immich/foregroundChannel",
@@ -35,10 +40,12 @@ class BackgroundServicePlugin: NSObject, FlutterPlugin {
         registrar.addApplicationDelegate(instance)
     }
     
+    // Registers the Flutter engine with the plugins, used by the other Background Flutter engine
     public static func register(engine: FlutterEngine) {
         GeneratedPluginRegistrant.register(with: engine)
     }
 
+    // Registers the task IDs from the system so that we can process them here in this class
     public static func registerBackgroundProcessing() {
         BGTaskScheduler.shared.register(forTaskWithIdentifier: BackgroundServicePlugin.backgroundSyncTaskID, using: nil) { task in
             if task is BGAppRefreshTask {
@@ -51,6 +58,7 @@ class BackgroundServicePlugin: NSObject, FlutterPlugin {
         }
     }
 
+    // Handles the channel methods from Flutter
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
             case "enable":
@@ -74,71 +82,92 @@ class BackgroundServicePlugin: NSObject, FlutterPlugin {
         }
     }
     
+    // Called by the flutter code when enabled so that we can turn on the backround services
+    // and save the callback information to communicate on this method channel
     public func handleBackgroundEnable(call: FlutterMethodCall, result: FlutterResult) {
-        print("background enable called")
-
+        
+        // Needs to parse the arguments from the method call
         guard let args = call.arguments as? Array<Any> else {
             print("Cannot parse args as array: \(call.arguments)")
             result(FlutterMethodNotImplemented)
             return
         }
         
+        // Requires 3 arguments in the array
         guard args.count == 3 else {
             print("Requires 3 arguments and received \(args.count)")
             result(FlutterMethodNotImplemented)
             return
         }
         
+        // Parses the arguments
         let callbackHandle = args[0] as? Int64
         let notificationTitle = args[1] as? String
         let instant = args[2] as? Bool
         
+        // Store these to user defaults
         let defaults = UserDefaults.standard
+        
+        // The callback handle is an int64 address to communicate with the main isolate's
+        // entry function
         defaults.set(callbackHandle, forKey: "callback_handle")
+        
+        // This is not used yet and will need to be implemented
         defaults.set(notificationTitle, forKey: "notification_title")
         
+        // Schedule the background services
         BackgroundServicePlugin.scheduleBackgroundSync()
+        BackgroundServicePlugin.scheduleBackgroundFetch()
         result(true)
-        
-        print("registered callback handle \(callbackHandle) and notification \(notificationTitle)")
     }
     
+    // Called by the flutter code at launch to let us know to schedule the services
     func handleIsEnabled(call: FlutterMethodCall, result: FlutterResult) {
+        // Schedule the background services
         BackgroundServicePlugin.scheduleBackgroundSync()
-        print("isEnabled called")
+        BackgroundServicePlugin.scheduleBackgroundFetch()
         
         result(true)
     }
     
+    // Called by the Flutter code whenever a change in configuration is set
     func handleConfigure(call: FlutterMethodCall, result: FlutterResult) {
+        
+        // Needs to be able to parse the arguments or else fail
         guard let args = call.arguments as? Array<Any> else {
             print("Cannot parse args as array: \(call.arguments)")
             result(FlutterError())
             return
         }
         
+        // Needs to have 4 arguments in the call or else fail
         guard args.count == 4 else {
             print("Not enough arguments, 4 required: \(args.count) given")
             result(FlutterError())
             return
         }
         
+        // Parse the arguments from the method call
         let requireUnmeteredNetwork = args[0] as? Bool
         let requireCharging = args[1] as? Bool
         let triggerUpdateDelay = args[2] as? Int
         let triggerMaxDelay = args[3] as? Int
         
+        // Store the values from the call in the defaults
         let defaults = UserDefaults.standard
         defaults.set(requireUnmeteredNetwork, forKey: "require_unmetered_network")
         defaults.set(requireCharging, forKey: "require_charging")
         defaults.set(triggerUpdateDelay, forKey: "trigger_update_delay")
         defaults.set(triggerMaxDelay, forKey: "trigger_max_delay")
 
+        // Cancel the background services and reschedule them
         BGTaskScheduler.shared.cancelAllTaskRequests()
         BackgroundServicePlugin.scheduleBackgroundSync()
+        BackgroundServicePlugin.scheduleBackgroundFetch()
         result(true)
     }
     
+    // Disables the service, cancels all the task requests
     func handleDisable(call: FlutterMethodCall, result: FlutterResult) {
         BGTaskScheduler.shared.cancelAllTaskRequests()
         result(true)
@@ -146,9 +175,19 @@ class BackgroundServicePlugin: NSObject, FlutterPlugin {
   
     // Schedules a short-running background sync to sync only a few photos
     static func scheduleBackgroundFetch() {
+        // We will only schedule this task to run if the user has explicitely allowed us to backup while
+        // not using a metered connection and not connected to power
+        let defaults = UserDefaults.standard
+        if defaults.value(forKey: "require_unmetered_network") as? Bool == true ||
+           defaults.value(forKey: "require_charging") as? Bool == true {
+            // We won't run while either of these are required
+            return
+        }
+                
         let backgroundFetch = BGAppRefreshTaskRequest(identifier: BackgroundServicePlugin.backgroundSyncTaskID)
-        // Use 1 minute from now as earliest begin date
-        backgroundFetch.earliestBeginDate = Date(timeIntervalSinceNow: 60)
+        
+        // Use 5 minutes from now as earliest begin date
+        backgroundFetch.earliestBeginDate = Date(timeIntervalSinceNow: 5 * 60)
         
         do {
             try BGTaskScheduler.shared.submit(backgroundFetch)
@@ -160,11 +199,13 @@ class BackgroundServicePlugin: NSObject, FlutterPlugin {
     // Schedules a long-running background sync for syncing all of the photos
     static func scheduleBackgroundSync() {
         let backgroundProcessing = BGProcessingTaskRequest(identifier: BackgroundServicePlugin.backgroundSyncTaskID)
-        print("scheduled background sync")
+        // We need the values for requiring charging or unmetered network usage
         let defaults = UserDefaults.standard
         let requireCharging = defaults.value(forKey: "require_charging") as? Bool
         let requireUnmeteredNetwork = defaults.value(forKey: "require_unmetered_network") as? Bool
         
+        // Add these to the processing task request so the system knows to only run them when
+        // they are both appropriately set
         backgroundProcessing.requiresNetworkConnectivity = requireUnmeteredNetwork ?? true
         backgroundProcessing.requiresExternalPower = requireCharging ?? true
                 
@@ -172,30 +213,37 @@ class BackgroundServicePlugin: NSObject, FlutterPlugin {
         backgroundProcessing.earliestBeginDate = Date(timeIntervalSinceNow: 15 * 60)
         
         do {
+            // Submit the task to the scheduler
             try BGTaskScheduler.shared.submit(backgroundProcessing)
         } catch {
             print("Could not schedule the background task \(error.localizedDescription)")
         }
     }
     
+    // This function runs when the system kicks off the BGAppRefreshTask from the Background Task Scheduler
     static func handleBackgroundFetch(task: BGAppRefreshTask) {
-        print("handling background fetch")
-        // Schedule the next sync task
+        // Schedule the next sync task so we can run this again later
         scheduleBackgroundFetch()
         
+        // The background sync task should only run for 10 seconds at most
         BackgroundServicePlugin.runBackgroundSync(maxSeconds: 10)
         task.setTaskCompleted(success: true)
     }
     
+    // This function runs when the system kicks off the BGProcessingTask from the Background Task Scheduler
     static func handleBackgroundProcessing(task: BGProcessingTask) {
-        print("handling background processing")
-        // Schedule the next sync task
+        // Schedule the next sync task so we run this again later
         scheduleBackgroundSync()
         
+        // We won't specify a max time for the background sync service, so this can run for longer
         BackgroundServicePlugin.runBackgroundSync(maxSeconds: nil)
         task.setTaskCompleted(success: true)
     }
     
+    // This is a synchronous function which uses a semaphore to run the background sync worker's run
+    // function, which will create a background Isolate and communicate with the Flutter code to back
+    // up the assets. When it completes, we signal the semaphore and complete the execution allowing the
+    // control to pass back to the caller synchronously
     static func runBackgroundSync(maxSeconds: Int?) {
         let semaphore = DispatchSemaphore(value: 0)
         DispatchQueue.main.async {
--- mobile/ios/Runner/BackgroundSync/BackgroundSyncWorker.swift
+++ mobile/ios/Runner/BackgroundSync/BackgroundSyncWorker.swift
@@ -10,18 +10,32 @@ import Foundation
 import Flutter
 import BackgroundTasks
 
+// The background worker which creates a new Flutter VM, communicates with it
+// to run the backup job, and then finishes execution and calls back to its callback
+// handler
 class BackgroundSyncWorker {
 
+    // The Flutter engine we create for background execution.
+    // This is not the main Flutter engine which shows the UI,
+    // this is a brand new isolate created and managed in this code
+    // here. It does not share memory with the main
+    // Flutter engine which shows the UI.
+    // It needs to be started up, registered, and torn down here
     let engine: FlutterEngine? = FlutterEngine(
         name: "BackgroundImmich"
     )
     
+    // The background message passing channel
     var channel: FlutterMethodChannel?
     
     var completionHandler: (UIBackgroundFetchResult) -> Void
     let taskSessionStart = Date()
     
+    // We need the completion handler to tell the system when we are done running
     init(_ completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+        // This is the background message passing channel to be used with the background engine
+        // created here in this platform code
         self.channel = FlutterMethodChannel(
             name: "immich/backgroundChannel",
             binaryMessenger: engine!.binaryMessenger
@@ -29,40 +43,46 @@ class BackgroundSyncWorker {
         self.completionHandler = completionHandler
     }
     
+    // Handles all of the messages from the Flutter VM called into this platform code
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
         case "initialized":
+            // Initialize tells us that we can now call into the Flutter VM to tell it to begin the update
             self.channel?.invokeMethod(
                 "backgroundProcessing",
                 arguments: nil,
                 result: { flutterResult in
-                    let taskSessionCompleter = Date()
-                    let result: UIBackgroundFetchResult = (flutterResult as? Bool ?? false) ? .newData : .failed
-                    let taskDuration = taskSessionCompleter.timeIntervalSince(self.taskSessionStart)
-                    print("[\(String(describing: self))] \(#function) -> performBackgroundRequest.\(result) (finished in \(taskDuration)")
                     
+                    // This is the result we send back to the BGTaskScheduler to let it know whether we'll need more time later or
+                    // if this execution failed
+                    let result: UIBackgroundFetchResult = (flutterResult as? Bool ?? false) ? .newData : .failed
+                    
+                    // Show the task duration
+                    let taskSessionCompleter = Date()
+                    let taskDuration = taskSessionCompleter.timeIntervalSince(self.taskSessionStart)
+                    print("[\(String(describing: self))] \(#function) -> performBackgroundRequest.\(result) (finished in \(taskDuration) seconds)")
+                    
+                    // Complete the execution
                     self.complete(result)
                 })
             break
         case "updateNotification":
-            print("update notification called")
             // TODO: implement update notification
             result(true)
             break
         case "showError":
-            print("showError called")
             // TODO: implement show error
             result(true)
             break
         case "clearErrorNotifications":
-            print("clearErrorNotifications")
             // TODO: implement clear error notifications
             result(true)
             break
         case "hasContentChanged":
-            print("hasContentChanged")
-            // TODO: implement has content changed
-            result(true)
+            // This is only called for Android, but we provide an implementation here
+            // telling Flutter that we don't have any information about whether the gallery
+            // contents have changed or not, so we can just say "no, they've not changed"
+            result(false)
             break
         default:
             result(FlutterError())
@@ -70,15 +90,26 @@ class BackgroundSyncWorker {
         }
     }
     
+    // Runs the background sync by starting up a new isolate and handling the calls
+    // until it completes
     public func run(maxSeconds: Int?) {
+        // We need the callback handle to start up the Flutter VM from the entry point
         let defaults = UserDefaults.standard
         guard let callbackHandle = defaults.value(forKey: "callback_handle") as? Int64 else {
+            // Can't find the callback handle, this is fatal
             complete(UIBackgroundFetchResult.failed)
             return
             
         }
-        let callback = FlutterCallbackCache.lookupCallbackInformation(callbackHandle)
         
+        // Use the provided callbackHandle to get the callback function
+        guard let callback = FlutterCallbackCache.lookupCallbackInformation(callbackHandle) else {
+            // We need this callback or else this is fatal
+            complete(UIBackgroundFetchResult.failed)
+            return
+        }
+        
+        // Sanity check for the engine existing
         if engine == nil {
             complete(UIBackgroundFetchResult.failed)
             return
@@ -86,28 +117,39 @@ class BackgroundSyncWorker {
                
         // Run the engine
         let isRunning = engine!.run(
-            withEntrypoint: callback?.callbackName,
-            libraryURI: callback?.callbackLibraryPath
+            withEntrypoint: callback.callbackName,
+            libraryURI: callback.callbackLibraryPath
         )
         
+        // If this engine isn't running, this is fatal
         if !isRunning {
             complete(UIBackgroundFetchResult.failed)
             return
         }
         
+        // If we have a timer, we need to start the timer to cancel ourselves
+        // so that we don't run longer than the provided maxSeconds
+        // After maxSeconds has elapsed, we will invoke "systemStop"
         if maxSeconds != nil {
-            print("starting timer with \(maxSeconds) before running system Stop")
+            // Schedule a non-repeating timer to run after maxSeconds
             let timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(maxSeconds!),
                                                       repeats: false) { timer in
+                // The callback invalidates the timer and stops execution
                 timer.invalidate()
-                if self.channel != nil {
-                    print("running system stop")
-                    self.channel?.invokeMethod(
-                        "systemStop",
-                        arguments: nil,
-                        result: nil)
-                    self.complete(UIBackgroundFetchResult.newData)
+                
+                // If the channel is already deallocated, we don't need to do anything
+                guard case self.channel != nil else {
+                    return
                 }
+                
+                // Tell the Flutter VM to stop backing up now
+                self.channel?.invokeMethod(
+                    "systemStop",
+                    arguments: nil,
+                    result: nil)
+                
+                // Complete the execution
+                self.complete(UIBackgroundFetchResult.newData)
             }
         }
         
@@ -117,11 +159,8 @@ class BackgroundSyncWorker {
         // Register this to get access to the plugins on the platform channel
         BackgroundServicePlugin.flutterPluginRegistrantCallback?(engine!)
     }
-    
-    @objc private func cancel() {
-    
-    }
 
+    // Completes the execution, destroys the engine, and sends a completion to our callback completionHandler
     private func complete(_ fetchResult: UIBackgroundFetchResult) {
         engine?.destroyContext()
         channel = nil
