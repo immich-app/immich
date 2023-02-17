dd5ac2f13f359fda8378c9f5b3bf50ad46ebc14e
Michel Heusschen <59014050+michelheusschen@users.noreply.github.com>
2023-02-17 11:15:06 +0100

    refactor(web): use cookies interface

--- web/src/routes/+layout.server.ts
+++ web/src/routes/+layout.server.ts
@@ -1,29 +1,19 @@
 import { serverApi } from '@api';
-import * as cookieParser from 'cookie';
-
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async ({ request }) => {
+export const load = (async ({ cookies }) => {
 	try {
-		const cookies = cookieParser.parse(request.headers.get('cookie') || '');
-		const accessToken = cookies['immich_access_token'];
-
+		const accessToken = cookies.get('immich_access_token');
 		if (!accessToken) {
-			return {
-				user: undefined
-			};
+			return { user: undefined };
 		}
 
 		serverApi.setAccessToken(accessToken);
-		const { data: userInfo } = await serverApi.userApi.getMyUserInfo();
+		const { data: user } = await serverApi.userApi.getMyUserInfo();
 
-		return {
-			user: userInfo
-		};
+		return { user };
 	} catch (e) {
 		console.error('[ERROR] layout.server.ts [LayoutServerLoad]: ');
-		return {
-			user: undefined
-		};
+		return { user: undefined };
 	}
-};
+}) satisfies LayoutServerLoad;
