e4e488e1e7fa376053fa69ff701dc16a2ad4d136
Fynn Petersen-Frey <zoodyy@users.noreply.github.com>
2023-03-14 23:18:53 +0100

    fix analyzer error

--- mobile/lib/shared/views/splash_screen.dart
+++ mobile/lib/shared/views/splash_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -12,7 +10,6 @@ import 'package:immich_mobile/modules/login/providers/authentication.provider.da
 import 'package:immich_mobile/modules/onboarding/providers/gallery_permission.provider.dart';
 import 'package:immich_mobile/routing/router.dart';
 import 'package:immich_mobile/shared/providers/api.provider.dart';
-import 'package:openapi/api.dart';
 
 class SplashScreenPage extends HookConsumerWidget {
   const SplashScreenPage({Key? key}) : super(key: key);
