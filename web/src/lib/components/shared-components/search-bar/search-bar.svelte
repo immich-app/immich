ed6dc18fed00958a5068d7f3375a092b7e7cf68f
Alex Tran <alex.tran1502@gmail.com>
2023-03-23 15:08:58 -0500

    Added search bar detail box

--- web/src/lib/components/shared-components/search-bar/search-bar.svelte
+++ web/src/lib/components/shared-components/search-bar/search-bar.svelte
@@ -3,7 +3,9 @@
 	import Magnify from 'svelte-material-icons/Magnify.svelte';
 	import Close from 'svelte-material-icons/Close.svelte';
 	import { goto } from '$app/navigation';
-
+	import { enableClip, savedSearchTerms } from '$lib/stores/search.store';
+	import { clickOutside } from '$lib/utils/click-outside';
+	import { fade, fly } from 'svelte/transition';
 	export let value = '';
 	export let grayTheme: boolean;
 
@@ -11,9 +13,12 @@
 	// of having to go through every search query.
 	export let replaceHistoryState = false;
 
+	let showBigSearchBar = false;
 	$: showClearIcon = value.length > 0;
 
 	function onSearch() {
+		saveSearchTerm();
+
 		const params = new URLSearchParams({
 			q: value,
 			clip: 'true'
@@ -21,14 +26,30 @@
 
 		goto(`${AppRoute.SEARCH}?${params}`, { replaceState: replaceHistoryState });
 	}
+
+	const saveSearchTerm = () => {
+		$savedSearchTerms = [value, ...$savedSearchTerms];
+
+		if ($savedSearchTerms.length > 5) {
+			$savedSearchTerms = $savedSearchTerms.slice(0, 5);
+		}
+	};
+
+	const clearSearchTerm = () => {
+		$savedSearchTerms = [];
+	};
 </script>
 
 <form
+	draggable="false"
 	autocomplete="off"
 	class="relative text-sm"
 	action={AppRoute.SEARCH}
 	on:reset={() => (value = '')}
 	on:submit|preventDefault={onSearch}
+	on:focusin={() => (showBigSearchBar = true)}
+	use:clickOutside
+	on:outclick={() => (showBigSearchBar = false)}
 >
 	<label>
 		<div class="absolute inset-y-0 left-0 flex items-center pl-6">
@@ -39,9 +60,11 @@
 		<input
 			type="text"
 			name="q"
-			class="w-full rounded-3xl bg-gray-200 {grayTheme
+			class="w-full transition-all bg-gray-200 {grayTheme
 				? 'dark:bg-immich-dark-gray'
-				: 'dark:bg-immich-dark-bg'} text-immich-fg/75 dark:text-immich-dark-fg px-14 py-4"
+				: 'dark:bg-immich-dark-bg'} text-immich-fg/75 dark:text-immich-dark-fg px-14 py-4 {showBigSearchBar
+				? 'rounded-t-3xl'
+				: 'rounded-3xl'}"
 			placeholder="Search"
 			required
 			bind:value
@@ -57,4 +80,28 @@
 			</button>
 		</div>
 	{/if}
+
+	<!-- {#if showBigSearchBar} -->
+	<div
+		transition:fly={{ y: 25, duration: 250 }}
+		class="w-full pb-5 absolute bg-white transition-all rounded-b-3xl shadow-2xl border border-gray-200"
+	>
+		<div class="p-5 text-xs flex justify-between">
+			<p>RECENT SEARCHES</p>
+			<button type="button" class="text-immich-primary font-semibold" on:click={clearSearchTerm}
+				>Clear all</button
+			>
+		</div>
+
+		{#each $savedSearchTerms as savedSearchTerm, i (i)}
+			<button
+				type="button"
+				class="w-full hover:bg-gray-100 px-5 py-3 cursor-pointer flex gap-3 text-black"
+			>
+				<Magnify size="1.5em" />
+				{savedSearchTerm}
+			</button>
+		{/each}
+	</div>
+	<!-- {/if} -->
 </form>
