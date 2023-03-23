32a065afc7d1041be1d564b66784906d0ddd2dea
martyfuhry <martyfuhry@gmail.com>
2023-03-23 11:08:14 -0400

    feat(mobile): Use new search API and GridView for Places / Locations (#2043)
    * Use new search API and GridView for Places / Locations
    
    * Fixes search service by adding clip: true
    
    * Rebased from master, uses view all explore grid now
    
    * localized view all button
    
    * adds empty
    
    * style text
    
    * Fix issue with horizontal Things not render due to missing height info
    
    ---------
    
    Co-authored-by: Alex Tran <alex.tran1502@gmail.com>

---
+++ mobile/lib/modules/search/views/curated_location_page.dart
@@ -0,0 +1,47 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:immich_mobile/modules/search/models/curated_content.dart';
+import 'package:immich_mobile/modules/search/providers/search_page_state.provider.dart';
+import 'package:immich_mobile/modules/search/ui/explore_grid.dart';
+import 'package:immich_mobile/shared/ui/immich_loading_indicator.dart';
+import 'package:openapi/api.dart';
+
+class CuratedLocationPage extends HookConsumerWidget {
+  const CuratedLocationPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    AsyncValue<List<CuratedLocationsResponseDto>> curatedLocation =
+        ref.watch(getCuratedLocationProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'curated_location_page_title',
+          style: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontWeight: FontWeight.bold,
+            fontSize: 16.0,
+          ),
+        ).tr(),
+      ),
+      body: curatedLocation.when(
+        loading: () => const Center(child: ImmichLoadingIndicator()),
+        error: (err, stack) => Center(
+          child: Text('Error: $err'),
+        ),
+        data: (curatedLocations) => ExploreGrid(
+          curatedContent: curatedLocations
+              .map(
+                (l) => CuratedContent(
+                  label: l.city,
+                  id: l.id,
+                ),
+              )
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
