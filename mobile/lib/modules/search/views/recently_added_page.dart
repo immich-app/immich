b0e8b87d4b440f23c6ba2ae0106f88e3f7c0e8f3
Marty Fuhry <martyfuhry@gmail.com>
2023-03-24 16:51:18 -0400

    theming

--- mobile/lib/modules/search/views/recently_added_page.dart
+++ mobile/lib/modules/search/views/recently_added_page.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/modules/home/ui/asset_grid/immich_asset_grid.dart';
 import 'package:immich_mobile/modules/search/providers/recently_added.provider.dart';
@@ -14,19 +13,6 @@ class RecentlyAddedPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final recents = ref.watch(recentlyAddedProvider);
     
-    // Needs to suppress hero animations 
-    final enableHeroAnimations = useState(false);
-    // Wait for transition to complete, then re-enable
-    ModalRoute.of(context)?.animation?.addListener(() {
-      if (enableHeroAnimations.value) {
-        return;
-      }
-      final animation = ModalRoute.of(context)?.animation;
-      if (animation != null) {
-        enableHeroAnimations.value = animation.isCompleted;
-      }
-    });
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('recently_added_page_title').tr(),
