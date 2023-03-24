b743bb967a8e07e33ff9695869c5b578602060a9
Marty Fuhry <martyfuhry@gmail.com>
2023-03-24 13:02:17 -0400

    refactor immich asset grid to use ref and provider

--- mobile/lib/modules/search/views/search_result_page.dart
+++ mobile/lib/modules/search/views/search_result_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/modules/home/ui/asset_grid/immich_asset_grid.dart';
+import 'package:immich_mobile/modules/home/ui/asset_grid/immich_asset_grid_view.dart';
 import 'package:immich_mobile/modules/search/providers/search_page_state.provider.dart';
 import 'package:immich_mobile/modules/search/providers/search_result_page.provider.dart';
 import 'package:immich_mobile/modules/search/ui/search_suggestion_list.dart';
