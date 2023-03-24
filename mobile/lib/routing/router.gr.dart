7763c42c84f548ef821b7e3842e789ff0c9ccb85
Marty Fuhry <martyfuhry@gmail.com>
2023-03-24 16:22:57 -0400

    Fixed issue with hero animations

--- mobile/lib/routing/router.gr.dart
+++ mobile/lib/routing/router.gr.dart
@@ -144,9 +144,14 @@ class _$AppRouter extends RootStackRouter {
       );
     },
     RecentlyAddedRoute.name: (routeData) {
-      return MaterialPageX<dynamic>(
+      return CustomPage<dynamic>(
         routeData: routeData,
         child: const RecentlyAddedPage(),
+        transitionsBuilder: TransitionsBuilders.noTransition,
+        durationInMilliseconds: 200,
+        reverseDurationInMilliseconds: 200,
+        opaque: true,
+        barrierDismissible: false,
       );
     },
     AssetSelectionRoute.name: (routeData) {
