b1b9992f9ccb82cd4a23361b53297bb179cbe1a0
Michel Heusschen <59014050+michelheusschen@users.noreply.github.com>
2023-03-30 11:40:50 +0200

    feat(web): pause and resume jobs

--- web/src/routes/admin/jobs-status/+page.server.ts
+++ web/src/routes/admin/jobs-status/+page.server.ts
@@ -1,18 +1,25 @@
+import { AppRoute } from '$lib/constants';
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ parent }) => {
-	const { user } = await parent();
-
+export const load = (async ({ locals: { user, api } }) => {
 	if (!user) {
-		throw redirect(302, '/auth/login');
+		throw redirect(302, AppRoute.AUTH_LOGIN);
 	} else if (!user.isAdmin) {
-		throw redirect(302, '/photos');
+		throw redirect(302, AppRoute.PHOTOS);
 	}
 
-	return {
-		meta: {
-			title: 'Job Status'
-		}
-	};
-};
+	try {
+		const { data: jobs } = await api.jobApi.getAllJobsStatus();
+
+		return {
+			jobs,
+			meta: {
+				title: 'Job Status'
+			}
+		};
+	} catch (err) {
+		console.error('[jobs] > getAllJobsStatus', err);
+		throw err;
+	}
+}) satisfies PageServerLoad;
