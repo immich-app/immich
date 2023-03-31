703c2a4bbe785d035f1dd44f278ae87c9071a466
Skyler Mäntysaari <samip5@users.noreply.github.com>
2023-03-31 19:03:14 +0300

    feat(server): redis sentinel initial support

--- server/libs/infra/src/infra.config.ts
+++ server/libs/infra/src/infra.config.ts
@@ -3,17 +3,35 @@ import { BullModuleOptions } from '@nestjs/bull';
 import { RedisOptions } from 'ioredis';
 import { ConfigurationOptions } from 'typesense/lib/Typesense/Configuration';
 
-export const redisConfig: RedisOptions = {
-  host: process.env.REDIS_HOSTNAME || 'immich_redis',
-  port: parseInt(process.env.REDIS_PORT || '6379'),
-  db: parseInt(process.env.REDIS_DBINDEX || '0'),
-  password: process.env.REDIS_PASSWORD || undefined,
-  path: process.env.REDIS_SOCKET || undefined,
-};
+export function redisConfig(): RedisOptions {
+  const redisUrl = process.env.REDIS_URL;
+  if (redisUrl != undefined && redisUrl.startsWith('ioredis://')) {
+    let customOptions = {};
+    try {
+      const decodedString = Buffer.from(redisUrl.slice(10), "base64").toString();
+      customOptions = JSON.parse(decodedString);
+    } catch (error) {
+      throw new Error(`Failed to decode redis options: ${error}`);
+    }
+    return customOptions;
+  }else {
+    {
+      return {
+        host: process.env.REDIS_HOSTNAME || 'immich_redis',
+        port: parseInt(process.env.REDIS_PORT || '6379'),
+        db: parseInt(process.env.REDIS_DBINDEX || '0'),
+        username: process.env.REDIS_USERNAME || undefined,
+        password: process.env.REDIS_PASSWORD || undefined,
+        path: process.env.REDIS_SOCKET || undefined,
+      };
+    }
+  }
+}
+
 
 export const bullConfig: BullModuleOptions = {
   prefix: 'immich_bull',
-  redis: redisConfig,
+  redis: redisConfig(),
   defaultJobOptions: {
     attempts: 3,
     removeOnComplete: true,
