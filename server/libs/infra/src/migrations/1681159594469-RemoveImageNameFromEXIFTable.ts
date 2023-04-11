3484a4cbd80f7fc9de05b4ff254d39fb58faafc5
Alex Tran <alex.tran1502@gmail.com>
2023-04-10 20:51:05 -0500

    change migration order for imagename

--- server/libs/infra/src/migrations/1681159594469-RemoveImageNameFromEXIFTable.ts
+++ server/libs/infra/src/migrations/1681159594469-RemoveImageNameFromEXIFTable.ts
@@ -4,7 +4,6 @@ export class RemoveImageNameFromEXIFTable1681159594469 implements MigrationInter
   name = 'RemoveImageNameFromEXIFTable1681159594469';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "exif" DROP COLUMN "imageName"`);
     await queryRunner.query(`ALTER TABLE "exif" DROP COLUMN IF EXISTS "exifTextSearchableColumn"`);
     await queryRunner.query(
       `DELETE FROM "typeorm_metadata" WHERE "type" = $1 AND "name" = $2 AND "database" = $3 AND "schema" = $4 AND "table" = $5`,
@@ -29,6 +28,7 @@ export class RemoveImageNameFromEXIFTable1681159594469 implements MigrationInter
         "TO_TSVECTOR('english',\n                         COALESCE(make, '') || ' ' ||\n                         COALESCE(model, '') || ' ' ||\n                         COALESCE(orientation, '') || ' ' ||\n                         COALESCE(\"lensModel\", '') || ' ' ||\n                         COALESCE(\"city\", '') || ' ' ||\n                         COALESCE(\"state\", '') || ' ' ||\n                         COALESCE(\"country\", ''))",
       ],
     );
+    await queryRunner.query(`ALTER TABLE "exif" DROP COLUMN "imageName"`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
