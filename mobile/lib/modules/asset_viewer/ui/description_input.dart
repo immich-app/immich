974eea2b40dc0f8811063c5ded19d17229dd0257
Alex <alex.tran1502@gmail.com>
2023-04-12 16:10:41 -0500

    move files

--- mobile/lib/modules/asset_viewer/ui/description_input.dart
+++ mobile/lib/modules/asset_viewer/ui/description_input.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:immich_mobile/modules/asset_viewer/providers/asset_description.provider.dart';
+import 'package:immich_mobile/modules/asset_viewer/services/asset_description.service.dart';
 import 'package:immich_mobile/shared/models/asset.dart';
 import 'package:immich_mobile/shared/ui/immich_toast.dart';
 import 'package:logging/logging.dart';
