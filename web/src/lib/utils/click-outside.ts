e7d12fcb256902b0c36b5e1978328c186faf7e58
bo0tzz <git@bo0tzz.me>
2023-04-15 15:09:28 +0200

    feat(web): Allow closing modals with Escape key

--- web/src/lib/utils/click-outside.ts
+++ web/src/lib/utils/click-outside.ts
@@ -6,11 +6,19 @@ export function clickOutside(node: Node) {
 		}
 	};
 
+	const handleKey = (event: KeyboardEvent) => {
+		if (event.key == 'Escape') {
+			node.dispatchEvent(new CustomEvent('outclick'));
+		}
+	};
+
 	document.addEventListener('click', handleClick, true);
+	document.addEventListener('keydown', handleKey, true);
 
 	return {
 		destroy() {
 			document.removeEventListener('click', handleClick, true);
+			document.removeEventListener('keydown', handleKey, true);
 		}
 	};
 }
