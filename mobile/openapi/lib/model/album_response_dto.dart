a360b835c5e988a9369b8a572575de154cec87b5
Alex Tran <alex.tran1502@gmail.com>
2023-04-16 22:44:51 -0500

    regenerate api

--- mobile/openapi/lib/model/album_response_dto.dart
+++ mobile/openapi/lib/model/album_response_dto.dart
@@ -49,57 +49,54 @@ class AlbumResponseDto {
   UserResponseDto owner;
 
   @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is AlbumResponseDto &&
-          other.assetCount == assetCount &&
-          other.id == id &&
-          other.ownerId == ownerId &&
-          other.albumName == albumName &&
-          other.createdAt == createdAt &&
-          other.updatedAt == updatedAt &&
-          other.albumThumbnailAssetId == albumThumbnailAssetId &&
-          other.shared == shared &&
-          other.sharedUsers == sharedUsers &&
-          other.assets == assets &&
-          other.owner == owner;
+  bool operator ==(Object other) => identical(this, other) || other is AlbumResponseDto &&
+     other.assetCount == assetCount &&
+     other.id == id &&
+     other.ownerId == ownerId &&
+     other.albumName == albumName &&
+     other.createdAt == createdAt &&
+     other.updatedAt == updatedAt &&
+     other.albumThumbnailAssetId == albumThumbnailAssetId &&
+     other.shared == shared &&
+     other.sharedUsers == sharedUsers &&
+     other.assets == assets &&
+     other.owner == owner;
 
   @override
   int get hashCode =>
-      // ignore: unnecessary_parenthesis
-      (assetCount.hashCode) +
-      (id.hashCode) +
-      (ownerId.hashCode) +
-      (albumName.hashCode) +
-      (createdAt.hashCode) +
-      (updatedAt.hashCode) +
-      (albumThumbnailAssetId == null ? 0 : albumThumbnailAssetId!.hashCode) +
-      (shared.hashCode) +
-      (sharedUsers.hashCode) +
-      (assets.hashCode) +
-      (owner.hashCode);
+    // ignore: unnecessary_parenthesis
+    (assetCount.hashCode) +
+    (id.hashCode) +
+    (ownerId.hashCode) +
+    (albumName.hashCode) +
+    (createdAt.hashCode) +
+    (updatedAt.hashCode) +
+    (albumThumbnailAssetId == null ? 0 : albumThumbnailAssetId!.hashCode) +
+    (shared.hashCode) +
+    (sharedUsers.hashCode) +
+    (assets.hashCode) +
+    (owner.hashCode);
 
   @override
-  String toString() =>
-      'AlbumResponseDto[assetCount=$assetCount, id=$id, ownerId=$ownerId, albumName=$albumName, createdAt=$createdAt, updatedAt=$updatedAt, albumThumbnailAssetId=$albumThumbnailAssetId, shared=$shared, sharedUsers=$sharedUsers, assets=$assets, owner=$owner]';
+  String toString() => 'AlbumResponseDto[assetCount=$assetCount, id=$id, ownerId=$ownerId, albumName=$albumName, createdAt=$createdAt, updatedAt=$updatedAt, albumThumbnailAssetId=$albumThumbnailAssetId, shared=$shared, sharedUsers=$sharedUsers, assets=$assets, owner=$owner]';
 
   Map<String, dynamic> toJson() {
     final json = <String, dynamic>{};
-    json[r'assetCount'] = this.assetCount;
-    json[r'id'] = this.id;
-    json[r'ownerId'] = this.ownerId;
-    json[r'albumName'] = this.albumName;
-    json[r'createdAt'] = this.createdAt;
-    json[r'updatedAt'] = this.updatedAt;
+      json[r'assetCount'] = this.assetCount;
+      json[r'id'] = this.id;
+      json[r'ownerId'] = this.ownerId;
+      json[r'albumName'] = this.albumName;
+      json[r'createdAt'] = this.createdAt;
+      json[r'updatedAt'] = this.updatedAt;
     if (this.albumThumbnailAssetId != null) {
       json[r'albumThumbnailAssetId'] = this.albumThumbnailAssetId;
     } else {
       // json[r'albumThumbnailAssetId'] = null;
     }
-    json[r'shared'] = this.shared;
-    json[r'sharedUsers'] = this.sharedUsers;
-    json[r'assets'] = this.assets;
-    json[r'owner'] = this.owner;
+      json[r'shared'] = this.shared;
+      json[r'sharedUsers'] = this.sharedUsers;
+      json[r'assets'] = this.assets;
+      json[r'owner'] = this.owner;
     return json;
   }
 
@@ -113,13 +110,13 @@ class AlbumResponseDto {
       // Ensure that the map contains the required keys.
       // Note 1: the values aren't checked for validity beyond being non-null.
       // Note 2: this code is stripped in release mode!
-      // assert(() {
-      //   requiredKeys.forEach((key) {
-      //     assert(json.containsKey(key), 'Required key "AlbumResponseDto[$key]" is missing from JSON.');
-      //     assert(json[key] != null, 'Required key "AlbumResponseDto[$key]" has a null value in JSON.');
-      //   });
-      //   return true;
-      // }());
+      assert(() {
+        requiredKeys.forEach((key) {
+          assert(json.containsKey(key), 'Required key "AlbumResponseDto[$key]" is missing from JSON.');
+          assert(json[key] != null, 'Required key "AlbumResponseDto[$key]" has a null value in JSON.');
+        });
+        return true;
+      }());
 
       return AlbumResponseDto(
         assetCount: mapValueOfType<int>(json, r'assetCount')!,
@@ -128,8 +125,7 @@ class AlbumResponseDto {
         albumName: mapValueOfType<String>(json, r'albumName')!,
         createdAt: mapValueOfType<String>(json, r'createdAt')!,
         updatedAt: mapValueOfType<String>(json, r'updatedAt')!,
-        albumThumbnailAssetId:
-            mapValueOfType<String>(json, r'albumThumbnailAssetId'),
+        albumThumbnailAssetId: mapValueOfType<String>(json, r'albumThumbnailAssetId'),
         shared: mapValueOfType<bool>(json, r'shared')!,
         sharedUsers: UserResponseDto.listFromJson(json[r'sharedUsers'])!,
         assets: AssetResponseDto.listFromJson(json[r'assets'])!,
@@ -139,10 +135,7 @@ class AlbumResponseDto {
     return null;
   }
 
-  static List<AlbumResponseDto>? listFromJson(
-    dynamic json, {
-    bool growable = false,
-  }) {
+  static List<AlbumResponseDto>? listFromJson(dynamic json, {bool growable = false,}) {
     final result = <AlbumResponseDto>[];
     if (json is List && json.isNotEmpty) {
       for (final row in json) {
@@ -170,18 +163,12 @@ class AlbumResponseDto {
   }
 
   // maps a json object with a list of AlbumResponseDto-objects as value to a dart map
-  static Map<String, List<AlbumResponseDto>> mapListFromJson(
-    dynamic json, {
-    bool growable = false,
-  }) {
+  static Map<String, List<AlbumResponseDto>> mapListFromJson(dynamic json, {bool growable = false,}) {
     final map = <String, List<AlbumResponseDto>>{};
     if (json is Map && json.isNotEmpty) {
       json = json.cast<String, dynamic>(); // ignore: parameter_assignments
       for (final entry in json.entries) {
-        final value = AlbumResponseDto.listFromJson(
-          entry.value,
-          growable: growable,
-        );
+        final value = AlbumResponseDto.listFromJson(entry.value, growable: growable,);
         if (value != null) {
           map[entry.key] = value;
         }
@@ -205,3 +192,4 @@ class AlbumResponseDto {
     'owner',
   };
 }
+
