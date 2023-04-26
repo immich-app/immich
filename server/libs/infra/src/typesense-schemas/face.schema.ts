67d5bc359c139a0304c9aede6fd1ee60da9b5f00
Alex Tran <alex.tran1502@gmail.com>
2023-04-26 06:51:58 -0500

    add fields

--- server/libs/infra/src/typesense-schemas/face.schema.ts
+++ server/libs/infra/src/typesense-schemas/face.schema.ts
@@ -5,6 +5,10 @@ export const faceSchema: CollectionCreateSchema = {
   name: `faces-v${faceSchemaVersion}`,
   fields: [
     // faces
+    { name: 'ownerId', type: 'string', facet: false },
+    { name: 'fileCreatedAt', type: 'string', facet: false, sort: true },
+
     { name: 'faces', type: 'float[]', facet: false, optional: true, num_dim: 512 },
   ],
+  default_sorting_field: 'fileCreatedAt',
 };
