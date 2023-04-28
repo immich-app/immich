27e7fe9c1a46104478940dc5d9c488590e571c4f
Fynn Petersen-Frey <zody22@gmail.com>
2023-04-28 19:23:22 +0200

    tweak auto asset grouping

--- mobile/lib/modules/settings/ui/asset_list_settings/asset_list_storage_indicator.dart
+++ mobile/lib/modules/settings/ui/asset_list_settings/asset_list_storage_indicator.dart
@@ -4,7 +4,6 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/modules/settings/providers/app_settings.provider.dart';
 import 'package:immich_mobile/modules/settings/services/app_settings.service.dart';
-import 'package:immich_mobile/shared/providers/asset.provider.dart';
 
 class StorageIndicator extends HookConsumerWidget {
   const StorageIndicator({
