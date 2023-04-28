5d4ada60653a030e925323446abeb487eb1aaa68
Fynn Petersen-Frey <zody22@gmail.com>
2023-04-28 13:09:31 +0200

    simplify favorite provider

--- mobile/lib/modules/favorite/providers/favorite_provider.dart
+++ mobile/lib/modules/favorite/providers/favorite_provider.dart
@@ -9,48 +9,31 @@ import 'package:immich_mobile/shared/providers/db.provider.dart';
 import 'package:isar/isar.dart';
 
 class FavoriteSelectionNotifier extends StateNotifier<Set<int>> {
-  FavoriteSelectionNotifier(this.db, this.assetNotifier) : super({}) {
+  FavoriteSelectionNotifier(Isar db, this.assetNotifier) : super({}) {
     final query = db.assets
         .filter()
         .ownerIdEqualTo(Store.get(StoreKey.currentUser).isarId)
         .isFavoriteEqualTo(true)
         .idProperty();
-    state = query.findAllSync().toSet();
+    query.findAll().then((value) => state = value.toSet());
     query.watch().listen((event) {
       state = event.toSet();
     });
   }
 
-  final Isar db;
   final AssetNotifier assetNotifier;
-
-  void _setFavoriteForAssetId(int id, bool favorite) {
-    if (!favorite) {
-      state = state.difference({id});
-    } else {
-      state = state.union({id});
-    }
-  }
-
-  bool _isFavorite(int id) {
-    return state.contains(id);
-  }
 
   Future<void> toggleFavorite(Asset asset) async {
     // TODO support local favorite assets
     if (asset.storage == AssetState.local) return;
-    _setFavoriteForAssetId(asset.id, !_isFavorite(asset.id));
-
     await assetNotifier.toggleFavorite([asset], state.contains(asset.id));
   }
 
   Future<void> addToFavorites(List<Asset> assets) {
-    state = state.union(assets.map((a) => a.id).toSet());
     return assetNotifier.toggleFavorite(assets, true);
   }
 
   Future<void> removeFavorites(List<Asset> assets) {
-    state = state.difference(assets.map((a) => a.id).toSet());
     return assetNotifier.toggleFavorite(assets, false);
   }
 }
