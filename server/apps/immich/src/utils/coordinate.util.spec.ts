6127d3f8f6febf731dd433e8cd2f6fab7a4b2197
Matthias Rupp <matthias.rupp@posteo.de>
2023-05-07 05:24:04 -1100

    Extend coordinate rounding unit test

--- server/apps/immich/src/utils/coordinate.util.spec.ts
+++ server/apps/immich/src/utils/coordinate.util.spec.ts
@@ -1,7 +1,7 @@
 import { roundToDecimals } from './coordinate.util';
 
 describe('Coordinate utils roundToDecimals test', () => {
-  it('should round latitude and longitude to 5', () => {
+  it('should round latitude and longitude to 5 digits', () => {
     const lat = 49.533547;
     const lon = 10.703075;
 
@@ -10,5 +10,8 @@ describe('Coordinate utils roundToDecimals test', () => {
 
     expect(latRounded).toBeCloseTo(lat, 4);
     expect(lonRounded).toBeCloseTo(lon, 4);
+
+    expect(`${latRounded}`.split('.')[1]).toHaveLength(5);
+    expect(`${lonRounded}`.split('.')[1]).toHaveLength(5);
   });
 });
