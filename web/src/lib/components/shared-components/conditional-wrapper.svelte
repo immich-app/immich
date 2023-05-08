9a78ef41927ecc689b26d8b1d657eff88c29fda5
faupau03 <paul.paffe@gmx.de>
2023-05-08 11:02:49 +0200

    update conditional wrapper

--- web/src/lib/components/shared-components/conditional-wrapper.svelte
+++ web/src/lib/components/shared-components/conditional-wrapper.svelte
@@ -1,25 +1,11 @@
 <script lang="ts">
-	export let condition: boolean | undefined;
-	export let component: any | undefined = undefined;
-	export let a = false;
-	export let span = false;
-	export let element: HTMLElement | undefined = undefined;
-	$: passthrough = (({ condition, component, a, span, element, ...passthrough }) => passthrough)(
-		$$props
-	);
+	export let condition: boolean;
 </script>
 
-{#if condition || (typeof condition === 'undefined' && component)}
-	{#if component}
-		<svelte:component this={component} bind:element {...passthrough}><slot /></svelte:component>
-	{:else if a}
-		<!-- svelte-ignore a11y-missing-attribute -->
-		<a bind:this={element} {...passthrough}><slot /></a>
-	{:else if span}
-		<span bind:this={element} {...passthrough}><slot /></span>
-	{:else}
-		<div bind:this={element} {...passthrough}><slot /></div>
-	{/if}
+{#if condition}
+	<div class={$$restProps.class || ''}>
+		<slot />
+	</div>
 {:else}
 	<slot />
 {/if}
