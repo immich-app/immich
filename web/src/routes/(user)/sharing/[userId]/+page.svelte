afacf933551c753edc604202505c71cab0edbff7
brighteyed <sergey.kondrikov@gmail.com>
2023-05-11 14:08:19 +0300

    Remove redundant getUserAssets call

--- web/src/routes/(user)/sharing/[userId]/+page.svelte
+++ web/src/routes/(user)/sharing/[userId]/+page.svelte
@@ -1,22 +1,9 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import { api, AssetResponseDto } from '@api';
 	import AssetGrid from '$lib/components/photos-page/asset-grid.svelte';
 	import UserPageLayout from '$lib/components/layouts/user-page-layout.svelte';
-	import { handleError } from '$lib/utils/handle-error';
-	import { onMount } from 'svelte';
 
 	export let data: PageData;
-	let partnerAssets: AssetResponseDto[] = [];
-
-	onMount(async () => {
-		try {
-			const { data: assets } = await api.assetApi.getUserAssets(data.partner.id);
-			partnerAssets = assets;
-		} catch {
-			handleError(Error, 'Unable to load assets');
-		}
-	});
 </script>
 
 <UserPageLayout user={data.user} title="{data.partner.firstName} {data.partner.lastName}">
