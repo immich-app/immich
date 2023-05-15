007db307e4426f0f4ea73dc7e11c024aecaae64f
brighteyed <sergey.kondrikov@gmail.com>
2023-05-15 16:35:40 +0300

    Change layout of the partner's assets page

--- web/src/routes/(user)/partners/[userId]/+page.svelte
+++ web/src/routes/(user)/partners/[userId]/+page.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { AppRoute } from '$lib/constants';
+	import { goto } from '$app/navigation';
+	import ArrowLeft from 'svelte-material-icons/ArrowLeft.svelte';
+	import ControlAppBar from '$lib/components/shared-components/control-app-bar.svelte';
 	import AssetGrid from '$lib/components/photos-page/asset-grid.svelte';
-	import UserPageLayout from '$lib/components/layouts/user-page-layout.svelte';
 
 	export let data: PageData;
 </script>
 
-<UserPageLayout user={data.user} title="{data.partner.firstName} {data.partner.lastName}'s Photos">
+<main class="grid h-screen pt-[4.25rem] bg-immich-bg dark:bg-immich-dark-bg">
+	<ControlAppBar
+		showBackButton
+		backIcon={ArrowLeft}
+		on:close-button-click={() => goto(AppRoute.SHARING)}
+	>
+		<p slot="leading" class="text-immich-fg dark:text-immich-dark-fg">
+			{data.partner.firstName}
+			{data.partner.lastName}'s photos
+		</p>
+	</ControlAppBar>
+
 	<AssetGrid user={data.partner} />
-</UserPageLayout>
+</main>
