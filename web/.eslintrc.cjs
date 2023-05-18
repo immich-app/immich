c7c0ef6abc0ccc8a3547ad8f4784033acc4c1751
Michel Heusschen <59014050+michelheusschen@users.noreply.github.com>
2023-05-18 17:43:09 +0200

    chore(web): switch to eslint-plugin-svelte package (#2467)

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -1,23 +1,43 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
 	root: true,
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended'
+	],
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
+	plugins: ['@typescript-eslint'],
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte']
 	},
 	env: {
 		browser: true,
 		es2017: true,
 		node: true
 	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	],
 	globals: {
 		NodeJS: true
+	},
+	rules: {
+		'@typescript-eslint/no-unused-vars': [
+			'warn',
+			{
+				// Allow underscore (_) variables
+				argsIgnorePattern: '^_$',
+				varsIgnorePattern: '^_$'
+			}
+		]
 	}
 };
