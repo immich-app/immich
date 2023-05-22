576321ca2db71aa2fe618c1dfadd686568543440
Jonathan Jogenfors <jonathan@jogenfors.se>
2023-05-23 00:46:33 +0200

    Merge migrations

--- server/libs/infra/src/migrations/1684328185099-RemoveNotNullFromChecksumIndex.ts
+++ server/libs/infra/src/migrations/1684328185099-RemoveNotNullFromChecksumIndex.ts
@@ -1,20 +1,23 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class removeNotNullFromChecksumIndex1684328185099 implements MigrationInterface {
-    name = 'removeNotNullFromChecksumIndex1684328185099'
+export class RemoveNotNullFromChecksumIndex1684328185099 implements MigrationInterface {
+  name = 'removeNotNullFromChecksumIndex1684328185099';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX "public"."IDX_64c507300988dd1764f9a6530c"`);
-        await queryRunner.query(`ALTER TABLE "assets" ALTER COLUMN "deviceAssetId" SET NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "assets" ALTER COLUMN "deviceId" SET NOT NULL`);
-        await queryRunner.query(`CREATE INDEX "IDX_8d3efe36c0755849395e6ea866" ON "assets" ("checksum") `);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX "public"."IDX_64c507300988dd1764f9a6530c"`);
+    await queryRunner.query(`ALTER TABLE "assets" ALTER COLUMN "deviceAssetId" SET NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "assets" ALTER COLUMN "deviceId" SET NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "assets" ALTER COLUMN "checksum" SET NOT NULL`);
+    await queryRunner.query(`CREATE INDEX "IDX_8d3efe36c0755849395e6ea866" ON "assets" ("checksum") `);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX "public"."IDX_8d3efe36c0755849395e6ea866"`);
-        await queryRunner.query(`ALTER TABLE "assets" ALTER COLUMN "deviceId" DROP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "assets" ALTER COLUMN "deviceAssetId" DROP NOT NULL`);
-        await queryRunner.query(`CREATE INDEX "IDX_64c507300988dd1764f9a6530c" ON "assets" ("checksum") WHERE ('checksum' IS NOT NULL)`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX "public"."IDX_8d3efe36c0755849395e6ea866"`);
+    await queryRunner.query(`ALTER TABLE "assets" ALTER COLUMN "deviceId" DROP NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "assets" ALTER COLUMN "deviceAssetId" DROP NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "assets" ALTER COLUMN "checksum" DROP NOT NULL`);
+    await queryRunner.query(
+      `CREATE INDEX "IDX_64c507300988dd1764f9a6530c" ON "assets" ("checksum") WHERE ('checksum' IS NOT NULL)`,
+    );
+  }
 }
