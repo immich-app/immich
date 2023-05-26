79c0a6aac640b13e24f96b754ffee33f7934ef32
Jason Rasmussen <jrasm91@gmail.com>
2023-05-26 14:22:49 -0400

    chore: remove comment

--- server/libs/domain/src/asset/asset.service.ts
+++ server/libs/domain/src/asset/asset.service.ts
@@ -5,9 +5,7 @@ import { MapMarkerDto } from './dto/map-marker.dto';
 import { MapMarkerResponseDto } from './response-dto';
 
 export class AssetService {
-  constructor(
-    @Inject(IAssetRepository) private assetRepository: IAssetRepository, // @Inject(IJobRepository) jobRepository: IJobRepository,
-  ) {}
+  constructor(@Inject(IAssetRepository) private assetRepository: IAssetRepository) {}
 
   getMapMarkers(authUser: AuthUserDto, options: MapMarkerDto): Promise<MapMarkerResponseDto[]> {
     return this.assetRepository.getMapMarkers(authUser.id, options);
