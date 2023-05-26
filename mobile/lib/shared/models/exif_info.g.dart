71ef7685c50c23db842f22e149df258c1b2540fe
Fynn Petersen-Frey <10599762+fyfrey@users.noreply.github.com>
2023-05-26 15:09:44 +0200

    chore(mobile): update isar (#2571)

--- mobile/lib/shared/models/exif_info.g.dart
+++ mobile/lib/shared/models/exif_info.g.dart
@@ -7,7 +7,7 @@ part of 'exif_info.dart';
 // **************************************************************************
 
 // coverage:ignore-file
-// ignore_for_file: duplicate_ignore, non_constant_identifier_names, constant_identifier_names, invalid_use_of_protected_member, unnecessary_cast, prefer_const_constructors, lines_longer_than_80_chars, require_trailing_commas, inference_failure_on_function_invocation, unnecessary_parenthesis, unnecessary_raw_strings, unnecessary_null_checks, join_return_with_assignment, prefer_final_locals, avoid_js_rounded_ints, avoid_positional_boolean_parameters
+// ignore_for_file: duplicate_ignore, non_constant_identifier_names, constant_identifier_names, invalid_use_of_protected_member, unnecessary_cast, prefer_const_constructors, lines_longer_than_80_chars, require_trailing_commas, inference_failure_on_function_invocation, unnecessary_parenthesis, unnecessary_raw_strings, unnecessary_null_checks, join_return_with_assignment, prefer_final_locals, avoid_js_rounded_ints, avoid_positional_boolean_parameters, always_specify_types
 
 extension GetExifInfoCollection on Isar {
   IsarCollection<ExifInfo> get exifInfos => this.collection();
@@ -99,7 +99,7 @@ const ExifInfoSchema = CollectionSchema(
   getId: _exifInfoGetId,
   getLinks: _exifInfoGetLinks,
   attach: _exifInfoAttach,
-  version: '3.0.5',
+  version: '3.1.0+1',
 );
 
 int _exifInfoEstimateSize(
