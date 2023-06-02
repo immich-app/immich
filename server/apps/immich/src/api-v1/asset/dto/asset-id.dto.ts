e3388db191e12435ff0f4c94dfbd61071b95d2ac
Jason Rasmussen <jrasm91@gmail.com>
2023-06-01 21:53:48 -0400

    chore: remove unused dto

--- server/apps/immich/src/api-v1/asset/dto/asset-id.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsUUID } from 'class-validator';
-
-export class AssetIdDto {
-  @IsNotEmpty()
-  @IsUUID('4')
-  @ApiProperty({ format: 'uuid' })
-  assetId!: string;
-}
