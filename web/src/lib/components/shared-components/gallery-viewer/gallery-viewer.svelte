40c727b74a9300caed892a61b8703be2ef3a6a26
Yonggan <yonggan@obco.pro>
2023-06-10 15:03:06 +0200

    try to fix identation

--- web/src/lib/components/shared-components/gallery-viewer/gallery-viewer.svelte
+++ web/src/lib/components/shared-components/gallery-viewer/gallery-viewer.svelte
@@ -8,13 +8,13 @@
 </script>
 
 <script lang="ts">
-	import { page } from '$app/stores';
+	import {page} from '$app/stores';
 	import Thumbnail from '$lib/components/assets/thumbnail/thumbnail.svelte';
-	import { handleError } from '$lib/utils/handle-error';
-	import { AssetResponseDto, SharedLinkResponseDto, ThumbnailFormat } from '@api';
+	import {handleError} from '$lib/utils/handle-error';
+	import {AssetResponseDto, SharedLinkResponseDto, ThumbnailFormat} from '@api';
 	import AssetViewer from '../../asset-viewer/asset-viewer.svelte';
-	import { flip } from 'svelte/animate';
-	import { archivedAsset } from '$lib/stores/archived-asset.store';
+	import {flip} from 'svelte/animate';
+	import {archivedAsset} from '$lib/stores/archived-asset.store';
 
 	export let assets: AssetResponseDto[];
 	export let sharedLink: SharedLinkResponseDto | undefined = undefined;
@@ -46,7 +46,7 @@
 	}
 
 	const viewAssetHandler = (event: CustomEvent) => {
-		const { asset }: { asset: AssetResponseDto } = event.detail;
+		const {asset}: { asset: AssetResponseDto } = event.detail;
 
 		currentViewAssetIndex = assets.findIndex((a) => a.id == asset.id);
 		selectedAsset = assets[currentViewAssetIndex];
@@ -55,7 +55,7 @@
 	};
 
 	const selectAssetHandler = (event: CustomEvent) => {
-		const { asset }: { asset: AssetResponseDto } = event.detail;
+		const {asset}: { asset: AssetResponseDto } = event.detail;
 		let temp = new Set(selectedAssets);
 
 		if (selectedAssets.has(asset)) {
@@ -114,34 +114,34 @@
 </script>
 
 {#if assets.length > 0}
-	<div class="flex flex-wrap gap-1 w-full pb-20" bind:clientWidth={viewWidth}>
-		{#each assets as asset (asset.id)}
-			<div animate:flip={{ duration: 500 }}>
-				<Thumbnail
-					{asset}
-					{thumbnailSize}
-					readonly={disableAssetSelect}
-					publicSharedKey={sharedLink?.key}
-					format={assets.length < 7 ? ThumbnailFormat.Jpeg : ThumbnailFormat.Webp}
-					on:click={(e) => (isMultiSelectionMode ? selectAssetHandler(e) : viewAssetHandler(e))}
-					on:select={selectAssetHandler}
-					selected={selectedAssets.has(asset)}
-					{showArchiveIcon}
-				/>
-			</div>
-		{/each}
-	</div>
+    <div class="flex flex-wrap gap-1 w-full pb-20" bind:clientWidth={viewWidth}>
+        {#each assets as asset (asset.id)}
+            <div animate:flip={{ duration: 500 }}>
+                <Thumbnail
+                        {asset}
+                        {thumbnailSize}
+                        readonly={disableAssetSelect}
+                        publicSharedKey={sharedLink?.key}
+                        format={assets.length < 7 ? ThumbnailFormat.Jpeg : ThumbnailFormat.Webp}
+                        on:click={(e) => (isMultiSelectionMode ? selectAssetHandler(e) : viewAssetHandler(e))}
+                        on:select={selectAssetHandler}
+                        selected={selectedAssets.has(asset)}
+                        {showArchiveIcon}
+                />
+            </div>
+        {/each}
+    </div>
 {/if}
 
 <!-- Overlay Asset Viewer -->
 {#if isShowAssetViewer}
-	<AssetViewer
-		asset={selectedAsset}
-		publicSharedKey={sharedLink?.key}
-		{sharedLink}
-		on:navigate-previous={navigateAssetBackward}
-		on:navigate-next={navigateAssetForward}
-		on:close={closeViewer}
-		on:unarchived={handleUnarchivedSuccess}
-	/>
+    <AssetViewer
+            asset={selectedAsset}
+            publicSharedKey={sharedLink?.key}
+            {sharedLink}
+            on:navigate-previous={navigateAssetBackward}
+            on:navigate-next={navigateAssetForward}
+            on:close={closeViewer}
+            on:unarchived={handleUnarchivedSuccess}
+    />
 {/if}
--- web/src/routes/(user)/search/+page.svelte
+++ web/src/routes/(user)/search/+page.svelte
@@ -1,105 +1,105 @@
 <script lang="ts">
-  import {afterNavigate, goto} from '$app/navigation';
-  import {page} from '$app/stores';
-  import AddToAlbum from '$lib/components/photos-page/actions/add-to-album.svelte';
-  import ArchiveAction from '$lib/components/photos-page/actions/archive-action.svelte';
-  import CreateSharedLink from '$lib/components/photos-page/actions/create-shared-link.svelte';
-  import DeleteAssets from '$lib/components/photos-page/actions/delete-assets.svelte';
-  import DownloadAction from '$lib/components/photos-page/actions/download-action.svelte';
-  import FavoriteAction from '$lib/components/photos-page/actions/favorite-action.svelte';
-  import AssetSelectContextMenu from '$lib/components/photos-page/asset-select-context-menu.svelte';
-  import AssetSelectControlBar from '$lib/components/photos-page/asset-select-control-bar.svelte';
-  import ControlAppBar from '$lib/components/shared-components/control-app-bar.svelte';
-  import GalleryViewer from '$lib/components/shared-components/gallery-viewer/gallery-viewer.svelte';
-  import SearchBar from '$lib/components/shared-components/search-bar/search-bar.svelte';
-  import type {AssetResponseDto} from '@api';
-  import ArrowLeft from 'svelte-material-icons/ArrowLeft.svelte';
-  import DotsVertical from 'svelte-material-icons/DotsVertical.svelte';
-  import ImageOffOutline from 'svelte-material-icons/ImageOffOutline.svelte';
-  import Plus from 'svelte-material-icons/Plus.svelte';
-  import type {PageData} from './$types';
-  import SelectAll from 'svelte-material-icons/SelectAll.svelte';
-  import CircleIconButton from "$lib/components/elements/buttons/circle-icon-button.svelte";
+	import {afterNavigate, goto} from '$app/navigation';
+	import {page} from '$app/stores';
+	import AddToAlbum from '$lib/components/photos-page/actions/add-to-album.svelte';
+	import ArchiveAction from '$lib/components/photos-page/actions/archive-action.svelte';
+	import CreateSharedLink from '$lib/components/photos-page/actions/create-shared-link.svelte';
+	import DeleteAssets from '$lib/components/photos-page/actions/delete-assets.svelte';
+	import DownloadAction from '$lib/components/photos-page/actions/download-action.svelte';
+	import FavoriteAction from '$lib/components/photos-page/actions/favorite-action.svelte';
+	import AssetSelectContextMenu from '$lib/components/photos-page/asset-select-context-menu.svelte';
+	import AssetSelectControlBar from '$lib/components/photos-page/asset-select-control-bar.svelte';
+	import ControlAppBar from '$lib/components/shared-components/control-app-bar.svelte';
+	import GalleryViewer from '$lib/components/shared-components/gallery-viewer/gallery-viewer.svelte';
+	import SearchBar from '$lib/components/shared-components/search-bar/search-bar.svelte';
+	import type {AssetResponseDto} from '@api';
+	import ArrowLeft from 'svelte-material-icons/ArrowLeft.svelte';
+	import DotsVertical from 'svelte-material-icons/DotsVertical.svelte';
+	import ImageOffOutline from 'svelte-material-icons/ImageOffOutline.svelte';
+	import Plus from 'svelte-material-icons/Plus.svelte';
+	import type {PageData} from './$types';
+	import SelectAll from 'svelte-material-icons/SelectAll.svelte';
+	import CircleIconButton from "$lib/components/elements/buttons/circle-icon-button.svelte";
 
-  export let data: PageData;
+	export let data: PageData;
 
-  // The GalleryViewer pushes it's own history state, which causes weird
-  // behavior for history.back(). To prevent that we store the previous page
-  // manually and navigate back to that.
-  let previousRoute = '/explore';
+	// The GalleryViewer pushes it's own history state, which causes weird
+	// behavior for history.back(). To prevent that we store the previous page
+	// manually and navigate back to that.
+	let previousRoute = '/explore';
 
-  afterNavigate(({from}) => {
-    // Prevent setting previousRoute to the current page.
-    if (from && from.route.id !== $page.route.id) {
-      previousRoute = from.url.href;
-    }
-  });
+	afterNavigate(({from}) => {
+		// Prevent setting previousRoute to the current page.
+		if (from && from.route.id !== $page.route.id) {
+			previousRoute = from.url.href;
+		}
+	});
 
-  $: term = $page.url.searchParams.get('q') || data.term || '';
+	$: term = $page.url.searchParams.get('q') || data.term || '';
 
-  let selectedAssets: Set<AssetResponseDto> = new Set();
-  $: isMultiSelectionMode = selectedAssets.size > 0;
-  $: isAllArchived = Array.from(selectedAssets).every((asset) => asset.isArchived);
-  $: isAllFavorite = Array.from(selectedAssets).every((asset) => asset.isFavorite);
-  $: searchResultAssets = data.results.assets.items;
+	let selectedAssets: Set<AssetResponseDto> = new Set();
+	$: isMultiSelectionMode = selectedAssets.size > 0;
+	$: isAllArchived = Array.from(selectedAssets).every((asset) => asset.isArchived);
+	$: isAllFavorite = Array.from(selectedAssets).every((asset) => asset.isFavorite);
+	$: searchResultAssets = data.results.assets.items;
 
-  const onAssetDelete = (assetId: string) => {
-    searchResultAssets = searchResultAssets.filter((a: AssetResponseDto) => a.id !== assetId);
-  };
-  const handleSelectAll = () => {
-    selectedAssets = new Set(searchResultAssets)
-  }
+	const onAssetDelete = (assetId: string) => {
+		searchResultAssets = searchResultAssets.filter((a: AssetResponseDto) => a.id !== assetId);
+	};
+	const handleSelectAll = () => {
+		selectedAssets = new Set(searchResultAssets)
+	}
 </script>
 
 <section>
-  {#if isMultiSelectionMode}
-    <AssetSelectControlBar assets={selectedAssets} clearSelect={() => (selectedAssets = new Set())}>
-      <CreateSharedLink/>
-      <CircleIconButton title="Select all" logo={SelectAll} on:click={handleSelectAll}/>
-      <AssetSelectContextMenu icon={Plus} title="Add">
-        <AddToAlbum/>
-        <AddToAlbum shared/>
-      </AssetSelectContextMenu>
-      <DeleteAssets {onAssetDelete}/>
+    {#if isMultiSelectionMode}
+        <AssetSelectControlBar assets={selectedAssets} clearSelect={() => (selectedAssets = new Set())}>
+            <CreateSharedLink/>
+            <CircleIconButton title="Select all" logo={SelectAll} on:click={handleSelectAll}/>
+            <AssetSelectContextMenu icon={Plus} title="Add">
+                <AddToAlbum/>
+                <AddToAlbum shared/>
+            </AssetSelectContextMenu>
+            <DeleteAssets {onAssetDelete}/>
 
-      <AssetSelectContextMenu icon={DotsVertical} title="Add">
-        <DownloadAction menuItem/>
-        <FavoriteAction menuItem removeFavorite={isAllFavorite}/>
-        <ArchiveAction menuItem unarchive={isAllArchived}/>
-      </AssetSelectContextMenu>
-    </AssetSelectControlBar>
-  {:else}
-    <ControlAppBar on:close-button-click={() => goto(previousRoute)} backIcon={ArrowLeft}>
-      <div class="w-full max-w-2xl flex-1 pl-4">
-        <SearchBar grayTheme={false} value={term}/>
-      </div>
-    </ControlAppBar>
-  {/if}
+            <AssetSelectContextMenu icon={DotsVertical} title="Add">
+                <DownloadAction menuItem/>
+                <FavoriteAction menuItem removeFavorite={isAllFavorite}/>
+                <ArchiveAction menuItem unarchive={isAllArchived}/>
+            </AssetSelectContextMenu>
+        </AssetSelectControlBar>
+    {:else}
+        <ControlAppBar on:close-button-click={() => goto(previousRoute)} backIcon={ArrowLeft}>
+            <div class="w-full max-w-2xl flex-1 pl-4">
+                <SearchBar grayTheme={false} value={term}/>
+            </div>
+        </ControlAppBar>
+    {/if}
 </section>
 
 <section class="relative pt-32 mb-12 bg-immich-bg dark:bg-immich-dark-bg">
-  <section class="overflow-y-auto relative immich-scrollbar">
-    <section id="search-content" class="relative bg-immich-bg dark:bg-immich-dark-bg">
-      {#if data.results?.assets?.items.length > 0}
-        <div class="pl-4">
-          <GalleryViewer
-            assets={searchResultAssets}
-            bind:selectedAssets
-            viewFrom="search-page"
-            showArchiveIcon={true}
-          />
-        </div>
-      {:else}
-        <div
-          class="flex items-center place-content-center w-full min-h-[calc(66vh_-_11rem)] dark:text-white"
-        >
-          <div class="flex flex-col content-center items-center text-center">
-            <ImageOffOutline size="3.5em"/>
-            <p class="font-medium text-3xl mt-5">No results</p>
-            <p class="text-base font-normal">Try a synonym or more general keyword</p>
-          </div>
-        </div>
-      {/if}
+    <section class="overflow-y-auto relative immich-scrollbar">
+        <section id="search-content" class="relative bg-immich-bg dark:bg-immich-dark-bg">
+            {#if data.results?.assets?.items.length > 0}
+                <div class="pl-4">
+                    <GalleryViewer
+                            assets={searchResultAssets}
+                            bind:selectedAssets
+                            viewFrom="search-page"
+                            showArchiveIcon={true}
+                    />
+                </div>
+            {:else}
+                <div
+                        class="flex items-center place-content-center w-full min-h-[calc(66vh_-_11rem)] dark:text-white"
+                >
+                    <div class="flex flex-col content-center items-center text-center">
+                        <ImageOffOutline size="3.5em"/>
+                        <p class="font-medium text-3xl mt-5">No results</p>
+                        <p class="text-base font-normal">Try a synonym or more general keyword</p>
+                    </div>
+                </div>
+            {/if}
+        </section>
     </section>
-  </section>
 </section>
