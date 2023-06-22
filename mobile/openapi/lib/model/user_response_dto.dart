d1011f96adc4415443b399ca00b4962b11b5de44
Luke McCarthy <mail@lukehmcc.com>
2023-06-22 10:55:25 -0400

    update generated files

--- mobile/openapi/lib/model/user_response_dto.dart
+++ mobile/openapi/lib/model/user_response_dto.dart
@@ -18,6 +18,7 @@ class UserResponseDto {
     required this.firstName,
     required this.lastName,
     required this.storageLabel,
+    required this.externalPath,
     required this.profileImagePath,
     required this.shouldChangePassword,
     required this.isAdmin,
@@ -37,6 +38,8 @@ class UserResponseDto {
 
   String? storageLabel;
 
+  String? externalPath;
+
   String profileImagePath;
 
   bool shouldChangePassword;
@@ -52,64 +55,68 @@ class UserResponseDto {
   String oauthId;
 
   @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is UserResponseDto &&
-          other.id == id &&
-          other.email == email &&
-          other.firstName == firstName &&
-          other.lastName == lastName &&
-          other.storageLabel == storageLabel &&
-          other.profileImagePath == profileImagePath &&
-          other.shouldChangePassword == shouldChangePassword &&
-          other.isAdmin == isAdmin &&
-          other.createdAt == createdAt &&
-          other.deletedAt == deletedAt &&
-          other.updatedAt == updatedAt &&
-          other.oauthId == oauthId;
+  bool operator ==(Object other) => identical(this, other) || other is UserResponseDto &&
+     other.id == id &&
+     other.email == email &&
+     other.firstName == firstName &&
+     other.lastName == lastName &&
+     other.storageLabel == storageLabel &&
+     other.externalPath == externalPath &&
+     other.profileImagePath == profileImagePath &&
+     other.shouldChangePassword == shouldChangePassword &&
+     other.isAdmin == isAdmin &&
+     other.createdAt == createdAt &&
+     other.deletedAt == deletedAt &&
+     other.updatedAt == updatedAt &&
+     other.oauthId == oauthId;
 
   @override
   int get hashCode =>
-      // ignore: unnecessary_parenthesis
-      (id.hashCode) +
-      (email.hashCode) +
-      (firstName.hashCode) +
-      (lastName.hashCode) +
-      (storageLabel == null ? 0 : storageLabel!.hashCode) +
-      (profileImagePath.hashCode) +
-      (shouldChangePassword.hashCode) +
-      (isAdmin.hashCode) +
-      (createdAt.hashCode) +
-      (deletedAt == null ? 0 : deletedAt!.hashCode) +
-      (updatedAt.hashCode) +
-      (oauthId.hashCode);
+    // ignore: unnecessary_parenthesis
+    (id.hashCode) +
+    (email.hashCode) +
+    (firstName.hashCode) +
+    (lastName.hashCode) +
+    (storageLabel == null ? 0 : storageLabel!.hashCode) +
+    (externalPath == null ? 0 : externalPath!.hashCode) +
+    (profileImagePath.hashCode) +
+    (shouldChangePassword.hashCode) +
+    (isAdmin.hashCode) +
+    (createdAt.hashCode) +
+    (deletedAt == null ? 0 : deletedAt!.hashCode) +
+    (updatedAt.hashCode) +
+    (oauthId.hashCode);
 
   @override
-  String toString() =>
-      'UserResponseDto[id=$id, email=$email, firstName=$firstName, lastName=$lastName, storageLabel=$storageLabel, profileImagePath=$profileImagePath, shouldChangePassword=$shouldChangePassword, isAdmin=$isAdmin, createdAt=$createdAt, deletedAt=$deletedAt, updatedAt=$updatedAt, oauthId=$oauthId]';
+  String toString() => 'UserResponseDto[id=$id, email=$email, firstName=$firstName, lastName=$lastName, storageLabel=$storageLabel, externalPath=$externalPath, profileImagePath=$profileImagePath, shouldChangePassword=$shouldChangePassword, isAdmin=$isAdmin, createdAt=$createdAt, deletedAt=$deletedAt, updatedAt=$updatedAt, oauthId=$oauthId]';
 
   Map<String, dynamic> toJson() {
     final json = <String, dynamic>{};
-    json[r'id'] = this.id;
-    json[r'email'] = this.email;
-    json[r'firstName'] = this.firstName;
-    json[r'lastName'] = this.lastName;
+      json[r'id'] = this.id;
+      json[r'email'] = this.email;
+      json[r'firstName'] = this.firstName;
+      json[r'lastName'] = this.lastName;
     if (this.storageLabel != null) {
       json[r'storageLabel'] = this.storageLabel;
     } else {
       // json[r'storageLabel'] = null;
     }
-    json[r'profileImagePath'] = this.profileImagePath;
-    json[r'shouldChangePassword'] = this.shouldChangePassword;
-    json[r'isAdmin'] = this.isAdmin;
-    json[r'createdAt'] = this.createdAt.toUtc().toIso8601String();
+    if (this.externalPath != null) {
+      json[r'externalPath'] = this.externalPath;
+    } else {
+      // json[r'externalPath'] = null;
+    }
+      json[r'profileImagePath'] = this.profileImagePath;
+      json[r'shouldChangePassword'] = this.shouldChangePassword;
+      json[r'isAdmin'] = this.isAdmin;
+      json[r'createdAt'] = this.createdAt.toUtc().toIso8601String();
     if (this.deletedAt != null) {
       json[r'deletedAt'] = this.deletedAt!.toUtc().toIso8601String();
     } else {
       // json[r'deletedAt'] = null;
     }
-    json[r'updatedAt'] = this.updatedAt.toUtc().toIso8601String();
-    json[r'oauthId'] = this.oauthId;
+      json[r'updatedAt'] = this.updatedAt.toUtc().toIso8601String();
+      json[r'oauthId'] = this.oauthId;
     return json;
   }
 
@@ -125,10 +132,8 @@ class UserResponseDto {
       // Note 2: this code is stripped in release mode!
       assert(() {
         requiredKeys.forEach((key) {
-          assert(json.containsKey(key),
-              'Required key "UserResponseDto[$key]" is missing from JSON.');
-          assert(json[key] != null,
-              'Required key "UserResponseDto[$key]" has a null value in JSON.');
+          assert(json.containsKey(key), 'Required key "UserResponseDto[$key]" is missing from JSON.');
+          assert(json[key] != null, 'Required key "UserResponseDto[$key]" has a null value in JSON.');
         });
         return true;
       }());
@@ -139,9 +144,9 @@ class UserResponseDto {
         firstName: mapValueOfType<String>(json, r'firstName')!,
         lastName: mapValueOfType<String>(json, r'lastName')!,
         storageLabel: mapValueOfType<String>(json, r'storageLabel'),
+        externalPath: mapValueOfType<String>(json, r'externalPath'),
         profileImagePath: mapValueOfType<String>(json, r'profileImagePath')!,
-        shouldChangePassword:
-            mapValueOfType<bool>(json, r'shouldChangePassword')!,
+        shouldChangePassword: mapValueOfType<bool>(json, r'shouldChangePassword')!,
         isAdmin: mapValueOfType<bool>(json, r'isAdmin')!,
         createdAt: mapDateTime(json, r'createdAt', '')!,
         deletedAt: mapDateTime(json, r'deletedAt', ''),
@@ -152,10 +157,7 @@ class UserResponseDto {
     return null;
   }
 
-  static List<UserResponseDto> listFromJson(
-    dynamic json, {
-    bool growable = false,
-  }) {
+  static List<UserResponseDto> listFromJson(dynamic json, {bool growable = false,}) {
     final result = <UserResponseDto>[];
     if (json is List && json.isNotEmpty) {
       for (final row in json) {
@@ -183,19 +185,13 @@ class UserResponseDto {
   }
 
   // maps a json object with a list of UserResponseDto-objects as value to a dart map
-  static Map<String, List<UserResponseDto>> mapListFromJson(
-    dynamic json, {
-    bool growable = false,
-  }) {
+  static Map<String, List<UserResponseDto>> mapListFromJson(dynamic json, {bool growable = false,}) {
     final map = <String, List<UserResponseDto>>{};
     if (json is Map && json.isNotEmpty) {
       // ignore: parameter_assignments
       json = json.cast<String, dynamic>();
       for (final entry in json.entries) {
-        map[entry.key] = UserResponseDto.listFromJson(
-          entry.value,
-          growable: growable,
-        );
+        map[entry.key] = UserResponseDto.listFromJson(entry.value, growable: growable,);
       }
     }
     return map;
@@ -208,6 +204,7 @@ class UserResponseDto {
     'firstName',
     'lastName',
     'storageLabel',
+    'externalPath',
     'profileImagePath',
     'shouldChangePassword',
     'isAdmin',
@@ -217,3 +214,4 @@ class UserResponseDto {
     'oauthId',
   };
 }
+
