ec280f1ea5911150984815f11f218bde5ce6b670
Alex <alex.tran1502@gmail.com>
2023-06-24 23:38:08 -0500

    Nice page view

--- mobile/lib/modules/home/views/vertical_page_view.dart
+++ mobile/lib/modules/home/views/vertical_page_view.dart
@@ -6,49 +6,47 @@ class VerticalPageView extends HookConsumerWidget {
   const VerticalPageView({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final pageController = usePageController();
-
+    final verticalPageController = usePageController();
+    final horizontalPageController1 = usePageController();
+    final horizontalPageController2 = usePageController();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Vertical Page View'),
       ),
       body: PageView(
-        controller: pageController,
+        scrollDirection: Axis.vertical,
+        controller: verticalPageController,
+        onPageChanged: (value) => print("Change to memory lane $value"),
         children: [
-          Center(
-            child: Container(
-              height: 100,
-              width: 100,
-              color: Colors.red,
-            ),
+          PageView(
+            controller: horizontalPageController1,
+            children: const [
+              SizedBox(
+                height: 100,
+                width: 100,
+                child: Text("Page 1 - 1"),
+              ),
+              SizedBox(
+                height: 100,
+                width: 100,
+                child: Text("Page 1 - 2"),
+              ),
+            ],
           ),
-          Center(
-            child: Container(
-              height: 100,
-              width: 100,
-              color: Colors.blue,
-            ),
-          ),
-          Center(
-            child: Container(
-              height: 100,
-              width: 100,
-              color: Colors.green,
-            ),
-          ),
-          Center(
-            child: Container(
-              height: 100,
-              width: 100,
-              color: Colors.yellow,
-            ),
-          ),
-          Center(
-            child: Container(
-              height: 100,
-              width: 100,
-              color: Colors.purple,
-            ),
+          PageView(
+            controller: horizontalPageController2,
+            children: const [
+              SizedBox(
+                height: 100,
+                width: 100,
+                child: Text("Page 2 - 1"),
+              ),
+              SizedBox(
+                height: 100,
+                width: 100,
+                child: Text("Page 2 - 2"),
+              ),
+            ],
           ),
         ],
       ),
