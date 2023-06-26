adb1bb6fa59d0f60bcdd7bfa2dafe5f8d10ae0fe
Alex Tran <alex.tran1502@gmail.com>
2023-06-26 14:40:50 -0500

    Added card

--- mobile/lib/modules/memories/ui/memory_lane.dart
+++ mobile/lib/modules/memories/ui/memory_lane.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/modules/memories/providers/memory.provider.dart';
 import 'package:immich_mobile/routing/router.dart';
+import 'package:immich_mobile/shared/ui/immich_image.dart';
+import 'package:openapi/api.dart';
 
 class MemoryLane extends HookConsumerWidget {
   const MemoryLane({super.key});
@@ -20,21 +22,56 @@ class MemoryLane extends HookConsumerWidget {
                     shrinkWrap: true,
                     itemCount: memories.length,
                     itemBuilder: (context, index) {
+                      final memory = memories[index];
+
                       return Padding(
-                        padding: const EdgeInsets.all(8.0),
+                        padding: const EdgeInsets.only(right: 8.0, bottom: 8),
                         child: GestureDetector(
                           onTap: () {
                             AutoRouter.of(context)
                                 .push(const VerticalRouteView());
                           },
-                          child: SizedBox(
-                            height: 200,
-                            width: 150,
-                            child: Card(
-                              child: Center(
-                                child: Text(memories[index].title),
+                          child: Stack(
+                            children: [
+                              Card(
+                                elevation: 3,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(13.0),
+                                ),
+                                clipBehavior: Clip.hardEdge,
+                                child: ColorFiltered(
+                                  colorFilter: ColorFilter.mode(
+                                    Colors.black.withOpacity(0.1),
+                                    BlendMode.darken,
+                                  ),
+                                  child: ImmichImage(
+                                    memory.assets[0],
+                                    fit: BoxFit.cover,
+                                    width: 130,
+                                    height: 200,
+                                    type: ThumbnailFormat.JPEG,
+                                    useGrayBoxPlaceholder: true,
+                                  ),
+                                ),
                               ),
-                            ),
+                              Positioned(
+                                bottom: 16,
+                                left: 16,
+                                child: ConstrainedBox(
+                                  constraints: const BoxConstraints(
+                                    maxWidth: 114,
+                                  ),
+                                  child: Text(
+                                    memory.title,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       );
