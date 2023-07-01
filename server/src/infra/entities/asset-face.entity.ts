4b48edcb901d59f5d50bb7e78737c0c11ac20b12
Alex Tran <alex.tran1502@gmail.com>
2023-07-01 15:06:17 -0500

    verify running migration populate new value

--- server/src/infra/entities/asset-face.entity.ts
+++ server/src/infra/entities/asset-face.entity.ts
@@ -36,9 +36,6 @@ export class AssetFaceEntity {
   @Column({ default: 0, type: 'int' })
   boundingBoxY2!: number;
 
-  @Column({ default: 0, type: 'int' })
-  score!: number;
-
   @ManyToOne(() => AssetEntity, (asset) => asset.faces, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
   asset!: AssetEntity;
 
