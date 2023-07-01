35c997b95141673f0b2ff43f822b976c754c0d97
Jonathan Jogenfors <jonathan@jogenfors.se>
2023-07-01 15:08:35 +0200

    Fix typo in docs

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -8,7 +8,7 @@ program.name('immich').description('Immich command line interface');
 program
   .command('upload')
   .description('Upload assets')
-  .usage('upload [options] [paths...]')
+  .usage('[options] [paths...]')
   .addOption(new Option('-r, --recursive', 'Recursive').env('IMMICH_RECURSIVE').default(false))
   .addOption(new Option('-h, --skip-hash', "Don't hash files before upload").env('IMMICH_SKIP_HASH').default(false))
   .addOption(new Option('-da, --delete', 'Delete local assets after upload').env('IMMICH_DELETE_ASSETS'))
@@ -20,7 +20,7 @@ program
 program
   .command('import')
   .description('Import existing assets')
-  .usage('import [options] [paths...]')
+  .usage('[options] [paths...]')
   .addOption(new Option('-r, --recursive', 'Recursive').env('IMMICH_RECURSIVE').default(false))
   .argument('[paths...]', 'One or more paths to assets to be uploaded')
   .action((paths, options) => {
