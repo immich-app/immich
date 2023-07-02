16713b8d90b066cd35c084098bfd8685d056a5e0
Jonathan Jogenfors <jonathan@jogenfors.se>
2023-07-02 21:30:10 +0200

    Remove shorthand for delete

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -10,14 +10,14 @@ program
   .description('Upload assets')
   .usage('[options] [paths...]')
   .addOption(new Option('-r, --recursive', 'Recursive').env('IMMICH_RECURSIVE').default(false))
-  .addOption(new Option('-i, --ignore [paths...]', 'Paths to ignore').env('IMMICH_IGNORE_PATHS').default(false))
+  .addOption(new Option('-i, --ignore [paths...]', 'Paths to ignore').env('IMMICH_IGNORE_PATHS'))
   .addOption(new Option('-h, --skip-hash', "Don't hash files before upload").env('IMMICH_SKIP_HASH').default(false))
   .addOption(
     new Option('-n, --dry-run', "Don't perform any actions, just show what will be done")
       .env('IMMICH_DRY_RUN')
       .default(false),
   )
-  .addOption(new Option('-da, --delete', 'Delete local assets after upload').env('IMMICH_DELETE_ASSETS'))
+  .addOption(new Option('--delete', 'Delete local assets after upload').env('IMMICH_DELETE_ASSETS'))
   .argument('[paths...]', 'One or more paths to assets to be uploaded')
   .action((paths, options) => {
     options.excludePatterns = options.ignore;
