7ca94700c2773ff54ab4c5ec849d695f6bb4bd94
dmitry-brazhenko <brazhenko.dmitry@gmail.com>
2023-07-03 11:19:08 +0200

    panorama viewer.svelte

---
+++ web/src/lib/components/asset-viewer/panorama-viewer.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { fade } from 'svelte/transition';
+  import LoadingSpinner from '../shared-components/loading-spinner.svelte';
+  import { api, AssetResponseDto } from '@api';
+  import View360, { EquirectProjection } from '@egjs/svelte-view360';
+  import './panorama-viewer.css';
+
+  export let asset: AssetResponseDto;
+  export let publicSharedKey = '';
+
+  let assetData = '';
+  let errorMessage = '';
+
+  const loadAssetData = async () => {
+    try {
+      const { data } = await api.assetApi.serveFile(
+        { id: asset.id, isThumb: false, isWeb: false, key: publicSharedKey },
+        { responseType: 'blob' },
+      );
+
+      if (data instanceof Blob) {
+        assetData = URL.createObjectURL(data);
+        return assetData;
+      } else {
+        throw new Error('Invalid data format');
+      }
+    } catch (error) {
+      errorMessage = 'Failed to load asset';
+      return '';
+    }
+  };
+</script>
+
+<div transition:fade={{ duration: 150 }} class="flex place-items-center place-content-center h-full select-none">
+  {#await loadAssetData()}
+    <LoadingSpinner />
+  {:then assetData}
+    {#if assetData}
+      <View360 autoResize={true} projection={new EquirectProjection({ src: assetData })} />
+    {:else}
+      <p>{errorMessage}</p>
+    {/if}
+  {/await}
+</div>
