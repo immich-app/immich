61fd125a48f934ed1daf19f080bdd093064b35cd
Kasper Oikarinen <kasper@mrfriday.com>
2023-07-07 18:27:05 +0200

    Fix: enable transcoding of audioless videos & formatting

--- server/src/domain/media/media.service.ts
+++ server/src/domain/media/media.service.ts
@@ -187,16 +187,16 @@ export class MediaService {
 
     const isTargetVideoCodec = videoStream.codecName === ffmpegConfig.targetVideoCodec;
     const isTargetContainer = ['mov,mp4,m4a,3gp,3g2,mj2', 'mp4', 'mov'].includes(containerExtension);
-    const isTargetAudioCodec = (audioStream != null) && audioStream.codecName === ffmpegConfig.targetAudioCodec;
+    const isTargetAudioCodec = audioStream != null && audioStream.codecName === ffmpegConfig.targetAudioCodec;
 
     if (audioStream != null) {
-        this.logger.log(
-            `${asset.id}: AudioCodecName ${audioStream.codecName}, AudioStreamCodecType ${audioStream.codecType}, containerExtension ${containerExtension}`,
-        );
+      this.logger.verbose(
+        `${asset.id}: AudioCodecName ${audioStream.codecName}, AudioStreamCodecType ${audioStream.codecType}, containerExtension ${containerExtension}`,
+      );
     } else {
-        this.logger.log(
-            `${asset.id}: AudioCodecName None, AudioStreamCodecType None, containerExtension ${containerExtension}`,
-        );
+      this.logger.verbose(
+        `${asset.id}: AudioCodecName None, AudioStreamCodecType None, containerExtension ${containerExtension}`,
+      );
     }
 
     const allTargetsMatching = isTargetVideoCodec && isTargetAudioCodec && isTargetContainer;
