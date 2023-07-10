d74feb2c0a36e9f80fb9ada8f02b81681a849270
Jonathan Jogenfors <jonathan@jogenfors.se>
2023-07-11 00:07:34 +0200

    Cleaner setup ordering

--- server/e2e/load-testcontainers.ts
+++ server/e2e/load-testcontainers.ts
@@ -1,6 +1,9 @@
 import { GenericContainer, PostgreSqlContainer } from 'testcontainers';
 
 export default async () => {
+  process.env.NODE_ENV = 'development';
+  process.env.TYPESENSE_API_KEY = 'abc123';
+
   const pg = await new PostgreSqlContainer('postgres')
     .withExposedPorts(5432)
     .withDatabase('immich')
@@ -8,8 +11,6 @@ export default async () => {
     .withPassword('postgres')
     .start();
 
-  process.env.NODE_ENV = 'development';
-  process.env.TYPESENSE_API_KEY = 'abc123';
   process.env.DB_PORT = String(pg.getMappedPort(5432));
   process.env.DB_HOSTNAME = pg.getHost();
   process.env.DB_USERNAME = pg.getUsername();
