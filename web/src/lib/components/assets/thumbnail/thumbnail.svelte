47b267579a1fc1463eea107d110a94f962121279
brighteyed <sergey.kondrikov@gmail.com>
2023-07-11 12:01:02 +0300

    Remove focus outline from select asset button

--- web/src/lib/components/assets/thumbnail/thumbnail.svelte
+++ web/src/lib/components/assets/thumbnail/thumbnail.svelte
@@ -84,7 +84,7 @@
         {#if !readonly && (mouseOver || selected || selectionCandidate)}
           <button
             on:click={onIconClickedHandler}
-            class="absolute p-2"
+            class="absolute p-2 focus:outline-none"
             class:cursor-not-allowed={disabled}
             role="checkbox"
             aria-checked={selected}
