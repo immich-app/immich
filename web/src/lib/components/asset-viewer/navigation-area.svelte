289f44bb336882ca8e24001dc23423a52fe1eaf5
brighteyed <sergey.kondrikov@gmail.com>
2023-07-14 16:15:45 +0300

    Add NavigationButton and NavigationArea components

---
+++ web/src/lib/components/asset-viewer/navigation-area.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import NavigationButton from './navigation-button.svelte';
+
+  let hover = false;
+</script>
+
+<div
+  class="h-full flex place-items-center"
+  on:click
+  on:keydown
+  on:mouseenter={() => (hover = true)}
+  on:mouseleave={() => (hover = false)}
+>
+  <NavigationButton {hover}><slot /></NavigationButton>
+</div>
