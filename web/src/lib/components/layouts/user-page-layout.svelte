405a95be76386bbcbbe780e406de69f9129d0e61
martabal <74269598+martabal@users.noreply.github.com>
2023-07-17 00:48:17 +0200

    feat: add exit button

--- web/src/lib/components/layouts/user-page-layout.svelte
+++ web/src/lib/components/layouts/user-page-layout.svelte
@@ -3,11 +3,13 @@
   import NavigationBar from '../shared-components/navigation-bar/navigation-bar.svelte';
   import SideBar from '../shared-components/side-bar/side-bar.svelte';
   import EyeOutline from 'svelte-material-icons/EyeOutline.svelte';
+  import Close from 'svelte-material-icons/Close.svelte';
   import IconButton from '../elements/buttons/icon-button.svelte';
   import { fly } from 'svelte/transition';
   import type { UserResponseDto } from '@api';
   import { createEventDispatcher } from 'svelte';
   import { quintOut } from 'svelte/easing';
+  import CircleIconButton from '../elements/buttons/circle-icon-button.svelte';
 
   export let user: UserResponseDto;
   export let hideNavbar = false;
@@ -68,16 +70,27 @@
     class="absolute top-0 left-0 w-full h-full bg-immich-bg dark:bg-immich-dark-bg z-[9999]"
   >
     <div
-      class="absolute border-b dark:border-immich-dark-gray flex justify-between place-items-center dark:text-immich-dark-fg w-full p-4 h-16"
+      class="absolute border-b dark:border-immich-dark-gray flex justify-between place-items-center dark:text-immich-dark-fg w-full h-16"
     >
-      <p class="font-medium">Show & hide faces</p>
-      <IconButton
-        on:click={() => {
-          handleDoneClick();
-        }}
-      >
-        Done
-      </IconButton>
+      <div class="flex ml-8 items-center">
+        <CircleIconButton
+          logo={Close}
+          on:click={() => {
+            selectHidden = !selectHidden;
+          }}
+        />
+        <p class="ml-4">Show & hide faces</p>
+      </div>
+
+      <div class="flex mr-8">
+        <IconButton
+          on:click={() => {
+            handleDoneClick();
+          }}
+        >
+          Done
+        </IconButton>
+      </div>
       <div class="absolute top-16 h-[calc(100%-theme(spacing.16))] w-full immich-scrollbar p-4 pb-8">
         <slot />
       </div>
