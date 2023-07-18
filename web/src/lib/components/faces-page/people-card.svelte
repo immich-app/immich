dbf0f77331cfbd3b5454527c7df240b52f7579e8
Alex Tran <alex.tran1502@gmail.com>
2023-07-17 23:33:09 -0500

    context menu

--- web/src/lib/components/faces-page/people-card.svelte
+++ web/src/lib/components/faces-page/people-card.svelte
@@ -3,10 +3,26 @@
   import ImageThumbnail from '../assets/thumbnail/image-thumbnail.svelte';
   import IconButton from '../elements/buttons/icon-button.svelte';
   import DotsVertical from 'svelte-material-icons/DotsVertical.svelte';
+  import ContextMenu from '../shared-components/context-menu/context-menu.svelte';
+  import MenuOption from '../shared-components/context-menu/menu-option.svelte';
+  import Portal from '../shared-components/portal/portal.svelte';
+  import { createEventDispatcher } from 'svelte';
 
   export let person: PersonResponseDto;
+
+  let showContextMenu = false;
+  let dispatch = createEventDispatcher();
+
+  const onChangeNameClicked = () => {
+    dispatch('change-name', person);
+  };
+
+  const onMergeFacesClicked = () => {
+    dispatch('merge-faces', person);
+  };
 </script>
 
+<!-- svelte-ignore a11y-click-events-have-key-events -->
 <div id="people-card" class="relative">
   <a href="/people/{person.id}" draggable="false">
     <div class="filter brightness-95 rounded-xl w-48">
@@ -22,12 +38,28 @@
   </a>
 
   <button
-    class="absolute top-2 right-2 z-10"
-    on:click|stopPropagation|preventDefault={() => {}}
+    class="absolute top-2 right-2 z-20"
+    on:click|stopPropagation|preventDefault={() => {
+      showContextMenu = !showContextMenu;
+    }}
     data-testid="context-button-parent"
+    id={`icon-${person.id}`}
   >
     <IconButton color="transparent-gray">
       <DotsVertical size="20" />
     </IconButton>
+
+    {#if showContextMenu}
+      <ContextMenu on:outclick={() => (showContextMenu = false)}>
+        <MenuOption on:click={() => onChangeNameClicked()} text="Change name" />
+        <MenuOption on:click={() => onMergeFacesClicked()} text="Merge faces" />
+      </ContextMenu>
+    {/if}
   </button>
 </div>
+
+{#if showContextMenu}
+  <Portal target="body">
+    <div class="absolute top-0 left-0 heyo w-screen h-screen bg-transparent z-10" />
+  </Portal>
+{/if}
--- web/src/routes/(user)/people/+page.svelte
+++ web/src/routes/(user)/people/+page.svelte
@@ -7,20 +7,22 @@
 </script>
 
 <UserPageLayout user={data.user} showUploadButton title="People">
-  {#if data.people.length > 0}
-    <div class="pl-4">
-      <div class="flex flex-row flex-wrap gap-1">
-        {#each data.people as person (person.id)}
-          <PeopleCard {person} />
-        {/each}
+  <section class="people-page">
+    {#if data.people.length > 0}
+      <div class="pl-4">
+        <div class="flex flex-row flex-wrap gap-1">
+          {#each data.people as person (person.id)}
+            <PeopleCard {person} />
+          {/each}
+        </div>
       </div>
-    </div>
-  {:else}
-    <div class="flex items-center place-content-center w-full min-h-[calc(66vh_-_11rem)] dark:text-white">
-      <div class="flex flex-col content-center items-center text-center">
-        <AccountOff size="3.5em" />
-        <p class="font-medium text-3xl mt-5">No people</p>
+    {:else}
+      <div class="flex items-center place-content-center w-full min-h-[calc(66vh_-_11rem)] dark:text-white">
+        <div class="flex flex-col content-center items-center text-center">
+          <AccountOff size="3.5em" />
+          <p class="font-medium text-3xl mt-5">No people</p>
+        </div>
       </div>
-    </div>
-  {/if}
+    {/if}
+  </section>
 </UserPageLayout>
