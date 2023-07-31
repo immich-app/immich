4702de702f35e15f941f1daae239850439029086
mertalev <101130780+mertalev@users.noreply.github.com>
2023-07-31 01:05:13 -0400

    added sha256sum for other platforms

--- server/bin/install-ffmpeg.sh
+++ server/bin/install-ffmpeg.sh
@@ -3,10 +3,10 @@
 set -e
 
 LOCK=$(jq -c '.packages[] | select(.name == "ffmpeg")' build-lock.json)
+export TARGETARCH=${TARGETARCH:=$(dpkg --print-architecture)}
 FFMPEG_VERSION=${FFMPEG_VERSION:=$(echo $LOCK | jq -r '.version')}
-FFMPEG_SHA256=${FFMPEG_SHA256:=$(echo $LOCK | jq -r '.sha256')}
+FFMPEG_SHA256=${FFMPEG_SHA256:=$(echo $LOCK | jq -r '.sha256[$ENV.TARGETARCH]')}
 
-TARGETARCH=${TARGETARCH:=$(dpkg --print-architecture)}
 echo "$FFMPEG_SHA256  jellyfin-ffmpeg6_${FFMPEG_VERSION}-bookworm_${TARGETARCH}.deb" > ffmpeg.sha256
 
 wget -nv https://github.com/jellyfin/jellyfin-ffmpeg/releases/download/v${FFMPEG_VERSION}/jellyfin-ffmpeg6_${FFMPEG_VERSION}-bookworm_${TARGETARCH}.deb
