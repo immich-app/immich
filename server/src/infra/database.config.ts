fc51b5cb0045c58f0f88525c4a576c5b038f6b5f
Jonathan Jogenfors <jonathan@jogenfors.se>
2023-08-11 16:15:03 +0200

    Remove synchronize

--- server/src/infra/database.config.ts
+++ server/src/infra/database.config.ts
@@ -15,7 +15,7 @@ const urlOrParts = url
 export const databaseConfig: PostgresConnectionOptions = {
   type: 'postgres',
   entities: [__dirname + '/entities/*.entity.{js,ts}'],
-  synchronize: true,
+  synchronize: false,
   migrations: [__dirname + '/migrations/*.{js,ts}'],
   migrationsRun: true,
   connectTimeoutMS: 10000, // 10 seconds
