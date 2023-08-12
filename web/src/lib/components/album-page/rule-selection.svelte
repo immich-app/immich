968fc77183264645776155785af19423b0a1e151
Alex Tran <alex.tran1502@gmail.com>
2023-08-12 16:20:25 -0500

    styling

--- web/src/lib/components/album-page/rule-selection.svelte
+++ web/src/lib/components/album-page/rule-selection.svelte
@@ -2,6 +2,7 @@
   import BaseModal from '$lib/components/shared-components/base-modal.svelte';
   import { createEventDispatcher } from 'svelte';
   import { AlbumResponseDto } from '@api';
+  import Plus from 'svelte-material-icons/Plus.svelte';
 
   export let album: AlbumResponseDto;
   const dispatch = createEventDispatcher<{ close: void }>();
@@ -18,10 +19,41 @@
     </div>
   </svelte:fragment>
 
-  <div>
-    <div>
-      <p>Select people</p>
-      <div>People selection box</div>
+  <section class="px-5">
+    <!--  People Selection -->
+    <div id="people-selection">
+      <p class="text-sm">PEOPLE</p>
+      <div class="mt-4">
+        <button
+          class="flex h-20 w-20 place-content-center place-items-center rounded-lg border border-gray-300 text-immich-primary"
+        >
+          <Plus size="24" />
+        </button>
+      </div>
     </div>
-  </div>
+
+    <!-- Location Selection -->
+    <div id="location-selection" class="mt-5">
+      <p class="text-sm">LOCATION</p>
+      <div class="mt-4">
+        <button
+          class="flex h-20 w-20 place-content-center place-items-center rounded-lg border border-gray-300 text-immich-primary"
+        >
+          <Plus size="24" />
+        </button>
+      </div>
+    </div>
+
+    <!-- Date Selection -->
+    <div id="date-selection" class="mt-5">
+      <p class="text-sm">START DATE</p>
+      <div class="mt-4">
+        <button
+          class="flex h-20 w-20 place-content-center place-items-center rounded-lg border border-gray-300 text-immich-primary"
+        >
+          <Plus size="24" />
+        </button>
+      </div>
+    </div>
+  </section>
 </BaseModal>
