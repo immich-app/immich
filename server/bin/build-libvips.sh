60d39a7d1f786c23a46b4d0ae4d7b48743aa0cc4
Mert <101130780+mertalev@users.noreply.github.com>
2023-08-13 09:26:13 -0400

    fix(sever): meson flag (#3656)

--- server/bin/build-libvips.sh
+++ server/bin/build-libvips.sh
@@ -14,7 +14,7 @@ tar -xvf vips-${LIBVIPS_VERSION}.tar.xz -C libvips --strip-components=1
 rm vips-${LIBVIPS_VERSION}.tar.xz
 rm libvips.sha256
 cd libvips
-meson setup build --buildtype=release --libdir=lib -Dintrospection=disabled -Dtiff=disabled
+meson setup build --buildtype=release --libdir=lib -Dintrospection=false -Dtiff=disabled
 cd build
 # ninja test  # tests set concurrency too high for arm/v7
 ninja install
