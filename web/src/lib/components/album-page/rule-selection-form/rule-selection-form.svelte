89021ce99559423c08b70132e1326cb8428cd77a
Alex Tran <alex.tran1502@gmail.com>
2023-08-14 11:58:42 -0500

    styling

--- web/src/lib/components/album-page/rule-selection-form/rule-selection-form.svelte
+++ web/src/lib/components/album-page/rule-selection-form/rule-selection-form.svelte
@@ -72,7 +72,7 @@
   <section class="mb-5 px-5">
     <!--  People Selection -->
     <div id="people-selection">
-      <p class="text-sm font-medium">PEOPLE</p>
+      <p class="text-sm">PEOPLE</p>
 
       <div class="mt-4 flex flex-wrap gap-2">
         {#each selectedPeople as person (person.id)}
@@ -92,7 +92,7 @@
 
     <!-- Location Selection -->
     <div id="location-selection" class="mt-5">
-      <p class="text-sm font-medium">LOCATION</p>
+      <p class="text-sm">LOCATION</p>
       <div class="mt-4">
         <button
           class="immich-text-primary border-1 flex w-full place-content-center place-items-center rounded-3xl border border-gray-300 py-2 hover:bg-gray-500/20 dark:border-gray-500"
@@ -105,7 +105,7 @@
 
     <!-- Date Selection -->
     <div id="date-selection" class="mt-5">
-      <p class="text-sm font-medium">START DATE</p>
+      <p class="text-sm">START DATE</p>
       <div class="mt-2">
         <div class="text-xs">
           <p>Only include photos after the set date.</p>
