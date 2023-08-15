049628d483a1fcd74b304a62a088a5d80317ef92
Daniele Ricci <daniele@casaricci.it>
2023-08-15 11:52:25 +0200

    feat/libraries: downstream fixes (#3690)
    * Fix element reference between label and its input field
    
    * Fix edit import path dialog not appearing
    
    Because the first import path index could be zero, the check has to be against null
    
    * Fix import paths edit area collapsing when not requested
    
    Unfortunately the default button type is submit so it needs to be set explicitly.

--- web/src/lib/components/forms/library-import-path-form.svelte
+++ web/src/lib/components/forms/library-import-path-form.svelte
@@ -37,7 +37,7 @@
 
       <div class="m-4 flex flex-col gap-2">
         <label class="immich-form-label" for="path">Path</label>
-        <input class="immich-form-input" id="name" name="name" type="text" bind:value={importPath} />
+        <input class="immich-form-input" id="path" name="path" type="text" bind:value={importPath} />
       </div>
 
       <div class="mt-8 flex w-full gap-4 px-4">
