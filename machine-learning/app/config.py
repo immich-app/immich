60729a091ab0da18ec67d4a8d0ca8448715a91b6
Mert <101130780+mertalev@users.noreply.github.com>
2023-08-20 19:24:14 -0400

    make lazy loading default (#3797)

--- machine-learning/app/config.py
+++ machine-learning/app/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     clip_text_model: str = "clip-ViT-B-32"
     facial_recognition_model: str = "buffalo_l"
     min_tag_score: float = 0.9
-    eager_startup: bool = True
+    eager_startup: bool = False
     model_ttl: int = 0
     host: str = "0.0.0.0"
     port: int = 3003
