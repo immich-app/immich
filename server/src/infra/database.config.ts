12106aba380b49b32e4792bf6eacf3f1bd5a81dd
Jonathan Jogenfors <jonathan@jogenfors.se>
2023-08-27 22:46:46 +0200

    fix: Don't use sync in db settings

--- server/src/infra/database.config.ts
+++ server/src/infra/database.config.ts
@@ -15,7 +15,7 @@ const urlOrParts = url
 export const databaseConfig: PostgresConnectionOptions = {
   type: 'postgres',
   entities: [__dirname + '/entities/*.entity.{js,ts}'],
-  synchronize: true,
+  synchronize: false,
   migrations: [__dirname + '/migrations/*.{js,ts}'],
   subscribers: [__dirname + '/subscribers/*.{js,ts}'],
   migrationsRun: true,
