84ab16e93eef98a45748e64a59608223e5858637
Daniele Ricci <daniele@casaricci.it>
2023-09-04 19:15:12 +0200

    Fix migration

--- server/src/infra/migrations/1693833336881-AddPersonFaceAssetId.ts
+++ server/src/infra/migrations/1693833336881-AddPersonFaceAssetId.ts
@@ -3,10 +3,12 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class AddPersonFaceAssetId1693833336881 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "person" ADD "faceAssetId" uuid CONSTRAINT "FK_ab768006fba24450b92ead70700" REFERENCES assets(id) ON DELETE SET NULL`);
+    await queryRunner.query(`ALTER TABLE "person" ADD "faceAssetId" uuid`);
+    await queryRunner.query(`ALTER TABLE "person" ADD CONSTRAINT "FK_ab768006fba24450b92ead70700" FOREIGN KEY ("faceAssetId") REFERENCES "assets"("id") ON DELETE SET NULL ON UPDATE NO ACTION`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "person" DROP CONSTRAINT "FK_ab768006fba24450b92ead70700"`);
     await queryRunner.query(`ALTER TABLE "person" DROP COLUMN "faceAssetId"`);
   }
 
