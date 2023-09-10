89ab936450770627013d925c50834d3ecc16cd74
shalong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2023-09-10 07:53:55 +0530

    feat(mobile): use map settings from server-config to enable / disable map

--- mobile/lib/shared/models/server_info_state.model.dart
+++ mobile/lib/shared/models/server_info_state.model.dart
@@ -2,22 +2,30 @@ import 'package:openapi/api.dart';
 
 class ServerInfoState {
   final ServerVersionResponseDto serverVersion;
+  final ServerFeaturesDto serverFeatures;
+  final ServerConfigDto serverConfig;
   final bool isVersionMismatch;
   final String versionMismatchErrorMessage;
 
   ServerInfoState({
     required this.serverVersion,
+    required this.serverFeatures,
+    required this.serverConfig,
     required this.isVersionMismatch,
     required this.versionMismatchErrorMessage,
   });
 
   ServerInfoState copyWith({
     ServerVersionResponseDto? serverVersion,
+    ServerFeaturesDto? serverFeatures,
+    ServerConfigDto? serverConfig,
     bool? isVersionMismatch,
     String? versionMismatchErrorMessage,
   }) {
     return ServerInfoState(
       serverVersion: serverVersion ?? this.serverVersion,
+      serverFeatures: serverFeatures ?? this.serverFeatures,
+      serverConfig: serverConfig ?? this.serverConfig,
       isVersionMismatch: isVersionMismatch ?? this.isVersionMismatch,
       versionMismatchErrorMessage:
           versionMismatchErrorMessage ?? this.versionMismatchErrorMessage,
@@ -26,7 +34,7 @@ class ServerInfoState {
 
   @override
   String toString() {
-    return 'ServerInfoState( serverVersion: $serverVersion, isVersionMismatch: $isVersionMismatch, versionMismatchErrorMessage: $versionMismatchErrorMessage)';
+    return 'ServerInfoState( serverVersion: $serverVersion, serverFeatures: $serverFeatures, serverConfig: $serverConfig, isVersionMismatch: $isVersionMismatch, versionMismatchErrorMessage: $versionMismatchErrorMessage)';
   }
 
   @override
@@ -35,6 +43,8 @@ class ServerInfoState {
 
     return other is ServerInfoState &&
         other.serverVersion == serverVersion &&
+        other.serverFeatures == serverFeatures &&
+        other.serverConfig == serverConfig &&
         other.isVersionMismatch == isVersionMismatch &&
         other.versionMismatchErrorMessage == versionMismatchErrorMessage;
   }
@@ -42,6 +52,8 @@ class ServerInfoState {
   @override
   int get hashCode {
     return serverVersion.hashCode ^
+        serverFeatures.hashCode ^
+        serverConfig.hashCode ^
         isVersionMismatch.hashCode ^
         versionMismatchErrorMessage.hashCode;
   }
