737300d1341ede80feecd7b91b7961954c615d97
shalong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2023-09-14 00:37:21 +0530

    feat(web): empty trash

--- web/src/routes/(user)/trash/+page.svelte
+++ web/src/routes/(user)/trash/+page.svelte
@@ -8,8 +8,15 @@
   import EmptyPlaceholder from '$lib/components/shared-components/empty-placeholder.svelte';
   import { AppRoute, AssetAction } from '$lib/constants';
   import { createAssetInteractionStore } from '$lib/stores/asset-interaction.store';
+  import { handleError } from '$lib/utils/handle-error';
+  import {
+    NotificationType,
+    notificationController,
+  } from '$lib/components/shared-components/notification/notification';
+  import LinkButton from '$lib/components/elements/buttons/link-button.svelte';
   import { AssetStore } from '$lib/stores/assets.store';
-  import { TimeBucketSize } from '@api';
+  import { api, TimeBucketSize } from '@api';
+  import DeleteOutline from 'svelte-material-icons/DeleteOutline.svelte';
   import type { PageData } from './$types';
   import { featureFlags } from '$lib/stores/server-config.store';
   import { goto } from '$app/navigation';
@@ -22,6 +29,19 @@
   const assetStore = new AssetStore({ size: TimeBucketSize.Month, isTrashed: true });
   const assetInteractionStore = createAssetInteractionStore();
   const { isMultiSelectState, selectedAssets } = assetInteractionStore;
+
+  const handleEmptyTrash = async () => {
+    try {
+      await api.assetApi.deleteAssets({ assetBulkDeleteDto: { ids: [], emptyTrash: true } });
+
+      notificationController.show({
+        message: `Empty trash initiated. Refresh the page to see the changes`,
+        type: NotificationType.Info,
+      });
+    } catch (e) {
+      handleError(e, 'Error emptying trash');
+    }
+  };
 </script>
 
 {#if $isMultiSelectState}
@@ -34,8 +54,22 @@
 
 {#if $featureFlags.loaded && $featureFlags.trash}
   <UserPageLayout user={data.user} hideNavbar={$isMultiSelectState} title={data.meta.title}>
+    <div class="flex place-items-center gap-2" slot="buttons">
+      <LinkButton on:click={handleEmptyTrash}>
+        <div class="flex place-items-center gap-2 text-sm">
+          <DeleteOutline size="18" />
+          Empty Trash
+        </div>
+      </LinkButton>
+    </div>
+
     <AssetGrid forceDelete {assetStore} {assetInteractionStore} removeAction={AssetAction.UNARCHIVE}>
-      <EmptyPlaceholder text="Trashed photos and videos will show up here." alt="Empty trash can" slot="empty" src={empty3Url}  />
+      <EmptyPlaceholder
+        text="Trashed photos and videos will show up here."
+        alt="Empty trash can"
+        slot="empty"
+        src={empty3Url}
+      />
     </AssetGrid>
   </UserPageLayout>
 {/if}
