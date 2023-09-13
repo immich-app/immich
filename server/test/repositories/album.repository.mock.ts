c2df01c705b4eb9ec11acba843225064706ab9ad
Daniel Dietzler <mail@ddietzler.dev>
2023-09-13 23:27:15 +0200

    fix tests

--- server/test/repositories/album.repository.mock.ts
+++ server/test/repositories/album.repository.mock.ts
@@ -10,6 +10,7 @@ export const newAlbumRepositoryMock = (): jest.Mocked<IAlbumRepository> => {
     getOwned: jest.fn(),
     getShared: jest.fn(),
     getNotShared: jest.fn(),
+    softDeleteAll: jest.fn(),
     deleteAll: jest.fn(),
     getAll: jest.fn(),
     removeAsset: jest.fn(),
