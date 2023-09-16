f2a1deedac7e30283dafa79b890d51d2563c6e78
mertalev <101130780+mertalev@users.noreply.github.com>
2023-09-15 20:48:13 -0400

    simplified migration

--- server/src/infra/migrations/1694758412194-UpdateOpusCodecToLibopus.ts
+++ server/src/infra/migrations/1694758412194-UpdateOpusCodecToLibopus.ts
@@ -1,27 +1,21 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class UpdateOpusCodecToLibopus1694758412194 implements MigrationInterface {
-    name = 'UpdateOpusCodecToLibopus1694758412194'
+  name = 'UpdateOpusCodecToLibopus1694758412194'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
                 UPDATE system_config
-                SET
-                  key = 'ffmpeg.targetAudioCodec',
-                  value = '"libopus"'
-                WHERE
-                  key = 'ffmpeg.targetAudioCodec' AND value = '"opus"'
+                SET value = '"libopus"'
+                WHERE key = 'ffmpeg.targetAudioCodec' AND value = '"opus"'
             `);
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
                 UPDATE system_config
-                SET
-                  key = 'ffmpeg.targetAudioCodec',
-                  value = '"opus"'
-                WHERE
-                  key = 'ffmpeg.targetAudioCodec' AND value = '"libopus"'
+                SET value = '"opus"'
+                WHERE key = 'ffmpeg.targetAudioCodec' AND value = '"libopus"'
             `);
-    }
+  }
 }
