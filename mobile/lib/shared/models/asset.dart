ffb4aa0de71b0b3cd6deda0fb6d53e625775483e
Fynn Petersen-Frey <zody22@gmail.com>
2023-09-16 11:40:16 +0200

    fix(mobile): speed up RenderList creation for timeline

--- mobile/lib/shared/models/asset.dart
+++ mobile/lib/shared/models/asset.dart
@@ -100,12 +100,6 @@ class Asset {
 
   /// stores the raw SHA1 bytes as a base64 String
   /// because Isar cannot sort lists of byte arrays
-  @Index(
-    unique: true,
-    replace: false,
-    type: IndexType.hash,
-    composite: [CompositeIndex("ownerId")],
-  )
   String checksum;
 
   @Index(unique: false, replace: false, type: IndexType.hash)
@@ -114,6 +108,11 @@ class Asset {
   @Index(unique: false, replace: false, type: IndexType.hash)
   String? localId;
 
+  @Index(
+    unique: true,
+    replace: false,
+    composite: [CompositeIndex("checksum", type: IndexType.hash)],
+  )
   int ownerId;
 
   DateTime fileCreatedAt;
