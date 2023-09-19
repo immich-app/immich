7bce06890057287093de3869dbe4b0975c44997c
Jonathan Jogenfors <jonathan@jogenfors.se>
2023-09-20 00:11:13 +0200

    chore: localDateTime is non-null

--- web/src/lib/components/memory-page/memory-viewer.svelte
+++ web/src/lib/components/memory-page/memory-viewer.svelte
@@ -212,9 +212,7 @@
 
             <div class="absolute left-8 top-4 text-sm font-medium text-white">
               <p>
-                {DateTime.fromISO(
-                  currentMemory.assets[0].localDateTime ?? currentMemory.assets[0].fileCreatedAt,
-                ).toLocaleString(DateTime.DATE_FULL)}
+                {DateTime.fromISO(currentMemory.assets[0].localDateTime).toLocaleString(DateTime.DATE_FULL)}
               </p>
               <p>
                 {currentAsset.exifInfo?.city || ''}
