5763ceccff93abdbafd8280ee12c8dea74626656
Alex <alex.tran1502@gmail.com>
2023-09-22 23:12:34 +0700

    chore(web): remove flowbite

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
   darkMode: 'class',
   theme: {
     extend: {
@@ -22,20 +22,6 @@ module.exports = {
         'immich-dark-error': '#d32f2f',
         'immich-dark-success': '#388e3c',
         'immich-dark-warning': '#f57c00',
-
-        // flowbite-svelte
-        primary: {
-          50: '#FFF5F2',
-          100: '#FFF1EE',
-          200: '#FFE4DE',
-          300: '#FFD5CC',
-          400: '#FFBCAD',
-          500: '#FE795D',
-          600: '#EF562F',
-          700: '#EB4F27',
-          800: '#CC4522',
-          900: '#A5371B',
-        },
       },
       fontFamily: {
         'immich-title': ['Snowburst One', 'cursive'],
@@ -45,5 +31,4 @@ module.exports = {
       },
     },
   },
-  plugins: [require('flowbite/plugin')],
 };
