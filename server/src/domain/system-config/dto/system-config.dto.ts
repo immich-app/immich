99e9c2ada64e9d6ba3322892334af0be1ba88e76
Jason Rasmussen <jrasm91@gmail.com>
2023-10-10 14:16:57 -0400

    fix(server): schema generation (#4424)

--- server/src/domain/system-config/dto/system-config.dto.ts
+++ server/src/domain/system-config/dto/system-config.dto.ts
@@ -1,4 +1,3 @@
-import { SystemConfigThumbnailDto, SystemConfigTrashDto } from '@app/domain/system-config';
 import { SystemConfig } from '@app/infra/entities';
 import { Type } from 'class-transformer';
 import { IsObject, ValidateNested } from 'class-validator';
@@ -10,6 +9,8 @@ import { SystemConfigOAuthDto } from './system-config-oauth.dto';
 import { SystemConfigPasswordLoginDto } from './system-config-password-login.dto';
 import { SystemConfigReverseGeocodingDto } from './system-config-reverse-geocoding.dto';
 import { SystemConfigStorageTemplateDto } from './system-config-storage-template.dto';
+import { SystemConfigThumbnailDto } from './system-config-thumbnail.dto';
+import { SystemConfigTrashDto } from './system-config-trash.dto';
 
 export class SystemConfigDto implements SystemConfig {
   @Type(() => SystemConfigFFmpegDto)
