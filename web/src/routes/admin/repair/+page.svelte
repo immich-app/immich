f0c5e5475372e53cd5dd5ecbf2cf4c3c87d865a5
Jason Rasmussen <jrasm91@gmail.com>
2023-10-12 23:32:31 -0400

    feat: empty placeholder

--- web/src/routes/admin/repair/+page.svelte
+++ web/src/routes/admin/repair/+page.svelte
@@ -4,6 +4,10 @@
   import CircleIconButton from '$lib/components/elements/buttons/circle-icon-button.svelte';
   import LinkButton from '$lib/components/elements/buttons/link-button.svelte';
   import UserPageLayout from '$lib/components/layouts/user-page-layout.svelte';
+  import empty4Url from '$lib/assets/empty-4.svg';
+  import Button from '$lib/components/elements/buttons/button.svelte';
+  import CircleIconButton from '$lib/components/elements/buttons/circle-icon-button.svelte';
+  import EmptyPlaceholder from '$lib/components/shared-components/empty-placeholder.svelte';
   import {
     NotificationType,
     notificationController,
@@ -156,102 +160,113 @@
   </div> -->
   <section id="setting-content" class="flex place-content-center sm:mx-4">
     <section class="w-full pb-28 sm:w-5/6 md:w-[850px]">
-      <div class="flex flex-col gap-2">
-        <table class="mt-5 w-full text-left">
-          <thead
-            class="mb-4 flex h-12 w-full rounded-md border bg-gray-50 text-immich-primary dark:border-immich-dark-gray dark:bg-immich-dark-gray dark:text-immich-dark-primary"
-          >
-            <tr class="flex w-full place-items-center p-2 md:p-5">
-              <th class="w-full text-sm place-items-center font-medium flex justify-between" colspan="2">
-                <span>Matches (via checksum)</span>
-                <Button disabled={matches.length === 0} size="sm" on:click={() => handleRepair()}>Repair All</Button>
-              </th>
-            </tr>
-          </thead>
-          <tbody
-            class="block w-full overflow-y-auto rounded-md border dark:border-immich-dark-gray dark:text-immich-dark-fg"
-          >
-            {#each matches as match (match.extra.filename)}
-              <tr
-                class="w-full h-[75px] place-items-center border-[3px] border-transparent p-2 odd:bg-immich-gray even:bg-immich-bg hover:cursor-pointer hover:border-immich-primary/75 odd:dark:bg-immich-dark-gray/75 even:dark:bg-immich-dark-gray/50 dark:hover:border-immich-dark-primary/75 md:p-5 flex justify-between"
-                tabindex="0"
-                on:click={() => handleSplit(match)}
-              >
-                <td class="text-md text-ellipsis flex flex-col gap-1">
-                  <span>{match.orphan.pathValue} =></span>
-                  <span>{match.extra.filename}</span>
-                </td>
-                <td class="text-md text-ellipsis d-flex">
-                  <span>({match.orphan.entityType}/{match.orphan.pathType})</span>
-                </td>
+      {#if matches.length + extras.length + orphans.length === 0}
+        <div class="w-full">
+          <EmptyPlaceholder
+            fullWidth
+            text="Untracked and missing files will show up here"
+            alt="Empty report"
+            src={empty4Url}
+          />
+        </div>
+      {:else}
+        <div class="flex flex-col gap-2">
+          <table class="mt-5 w-full text-left">
+            <thead
+              class="mb-4 flex h-12 w-full rounded-md border bg-gray-50 text-immich-primary dark:border-immich-dark-gray dark:bg-immich-dark-gray dark:text-immich-dark-primary"
+            >
+              <tr class="flex w-full place-items-center p-2 md:p-5">
+                <th class="w-full text-sm place-items-center font-medium flex justify-between" colspan="2">
+                  <span>Matches (via checksum)</span>
+                  <Button disabled={matches.length === 0} size="sm" on:click={() => handleRepair()}>Repair All</Button>
+                </th>
               </tr>
-            {/each}
-          </tbody>
-        </table>
-
-        <table class="mt-5 w-full text-left">
-          <thead
-            class="mb-4 flex h-12 w-full rounded-md border bg-gray-50 text-immich-primary dark:border-immich-dark-gray dark:bg-immich-dark-gray dark:text-immich-dark-primary"
-          >
-            <tr class="flex w-full place-items-center p-2 md:p-5">
-              <th class="w-full text-sm font-medium justify-between place-items-center flex" colspan="2">
-                <span>Offline Paths</span>
-                <CircleIconButton logo={Refresh} on:click={() => handleRefresh()} />
-              </th>
-            </tr>
-          </thead>
-          <tbody
-            class="block w-full overflow-y-auto rounded-md border dark:border-immich-dark-gray dark:text-immich-dark-fg"
-          >
-            {#each orphans as orphan, index (index)}
-              <tr
-                class="w-full h-[50px] place-items-center border-[3px] border-transparent p-2 odd:bg-immich-gray even:bg-immich-bg hover:cursor-pointer hover:border-immich-primary/75 odd:dark:bg-immich-dark-gray/75 even:dark:bg-immich-dark-gray/50 dark:hover:border-immich-dark-primary/75 md:p-5 flex justify-between"
-                tabindex="0"
-              >
-                <td class="text-md text-ellipsis">
-                  <span>{orphan.pathValue}</span>
-                </td>
-                <td class="text-md text-ellipsis">
-                  <span>({orphan.entityType})</span>
-                </td>
-              </tr>
-            {/each}
-          </tbody>
-        </table>
-
-        <table class="mt-5 w-full text-left">
-          <thead
-            class="mb-4 flex h-12 w-full rounded-md border bg-gray-50 text-immich-primary dark:border-immich-dark-gray dark:bg-immich-dark-gray dark:text-immich-dark-primary"
-          >
-            <tr class="flex w-full place-items-center p-2 md:p-5">
-              <th class="w-full text-sm font-medium place-items-center flex justify-between" colspan="2">
-                <span>Untracked Files</span>
-                <Button disabled={extras.length === 0 || checking} size="sm" on:click={() => handleCheckAll()}
-                  >Check All</Button
+            </thead>
+            <tbody
+              class="block w-full overflow-y-auto rounded-md border dark:border-immich-dark-gray dark:text-immich-dark-fg"
+            >
+              {#each matches as match (match.extra.filename)}
+                <tr
+                  class="w-full h-[75px] place-items-center border-[3px] border-transparent p-2 odd:bg-immich-gray even:bg-immich-bg hover:cursor-pointer hover:border-immich-primary/75 odd:dark:bg-immich-dark-gray/75 even:dark:bg-immich-dark-gray/50 dark:hover:border-immich-dark-primary/75 md:p-5 flex justify-between"
+                  tabindex="0"
+                  on:click={() => handleSplit(match)}
                 >
-              </th>
-            </tr>
-          </thead>
-          <tbody
-            class="block w-full overflow-y-auto rounded-md border dark:border-immich-dark-gray dark:text-immich-dark-fg"
-          >
-            {#each extras as extra (extra.filename)}
-              <tr
-                class="flex h-[50px] w-full place-items-center border-[3px] border-transparent p-2 odd:bg-immich-gray even:bg-immich-bg hover:cursor-pointer hover:border-immich-primary/75 odd:dark:bg-immich-dark-gray/75 even:dark:bg-immich-dark-gray/50 dark:hover:border-immich-dark-primary/75 md:p-5 justify-between"
-                tabindex="0"
-                on:click={() => handleCheckOne(extra.filename)}
-              >
-                <td class="text-md text-ellipsis">{extra.filename}</td>
-                <td class="text-md text-ellipsis">
-                  {#if extra.checksum}
-                    [sha1:{extra.checksum}]
-                  {/if}
-                </td>
+                  <td class="text-md text-ellipsis flex flex-col gap-1">
+                    <span>{match.orphan.pathValue} =></span>
+                    <span>{match.extra.filename}</span>
+                  </td>
+                  <td class="text-md text-ellipsis d-flex">
+                    <span>({match.orphan.entityType}/{match.orphan.pathType})</span>
+                  </td>
+                </tr>
+              {/each}
+            </tbody>
+          </table>
+
+          <table class="mt-5 w-full text-left">
+            <thead
+              class="mb-4 flex h-12 w-full rounded-md border bg-gray-50 text-immich-primary dark:border-immich-dark-gray dark:bg-immich-dark-gray dark:text-immich-dark-primary"
+            >
+              <tr class="flex w-full place-items-center p-2 md:p-5">
+                <th class="w-full text-sm font-medium justify-between place-items-center flex" colspan="2">
+                  <span>Offline Paths</span>
+                  <CircleIconButton logo={Refresh} on:click={() => handleRefresh()} />
+                </th>
               </tr>
-            {/each}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody
+              class="block w-full overflow-y-auto rounded-md border dark:border-immich-dark-gray dark:text-immich-dark-fg"
+            >
+              {#each orphans as orphan, index (index)}
+                <tr
+                  class="w-full h-[50px] place-items-center border-[3px] border-transparent p-2 odd:bg-immich-gray even:bg-immich-bg hover:cursor-pointer hover:border-immich-primary/75 odd:dark:bg-immich-dark-gray/75 even:dark:bg-immich-dark-gray/50 dark:hover:border-immich-dark-primary/75 md:p-5 flex justify-between"
+                  tabindex="0"
+                >
+                  <td class="text-md text-ellipsis">
+                    <span>{orphan.pathValue}</span>
+                  </td>
+                  <td class="text-md text-ellipsis">
+                    <span>({orphan.entityType})</span>
+                  </td>
+                </tr>
+              {/each}
+            </tbody>
+          </table>
+
+          <table class="mt-5 w-full text-left">
+            <thead
+              class="mb-4 flex h-12 w-full rounded-md border bg-gray-50 text-immich-primary dark:border-immich-dark-gray dark:bg-immich-dark-gray dark:text-immich-dark-primary"
+            >
+              <tr class="flex w-full place-items-center p-2 md:p-5">
+                <th class="w-full text-sm font-medium place-items-center flex justify-between" colspan="2">
+                  <span>Untracked Files</span>
+                  <Button disabled={extras.length === 0 || checking} size="sm" on:click={() => handleCheckAll()}
+                    >Check All</Button
+                  >
+                </th>
+              </tr>
+            </thead>
+            <tbody
+              class="block w-full overflow-y-auto rounded-md border dark:border-immich-dark-gray dark:text-immich-dark-fg"
+            >
+              {#each extras as extra (extra.filename)}
+                <tr
+                  class="flex h-[50px] w-full place-items-center border-[3px] border-transparent p-2 odd:bg-immich-gray even:bg-immich-bg hover:cursor-pointer hover:border-immich-primary/75 odd:dark:bg-immich-dark-gray/75 even:dark:bg-immich-dark-gray/50 dark:hover:border-immich-dark-primary/75 md:p-5 justify-between"
+                  tabindex="0"
+                  on:click={() => handleCheckOne(extra.filename)}
+                >
+                  <td class="text-md text-ellipsis">{extra.filename}</td>
+                  <td class="text-md text-ellipsis">
+                    {#if extra.checksum}
+                      [sha1:{extra.checksum}]
+                    {/if}
+                  </td>
+                </tr>
+              {/each}
+            </tbody>
+          </table>
+        </div>
+      {/if}
     </section>
   </section>
 </UserPageLayout>
