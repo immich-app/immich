adbb2b2fcb7abc56ce8feb1c220454ce9610e29a
martabal <74269598+martabal@users.noreply.github.com>
2023-10-14 11:50:43 +0200

    edit migration script

--- server/src/infra/migrations/1697272818851-UnassignFace.ts
+++ server/src/infra/migrations/1697272818851-UnassignFace.ts
@@ -8,7 +8,8 @@ export class UnassignFace1697272818851 implements MigrationInterface {
     await queryRunner.query(`ALTER TABLE asset_faces ALTER COLUMN "id" SET DEFAULT uuid_generate_v4(), ALTER COLUMN "id" SET NOT NULL;`);
     await queryRunner.query(`ALTER TABLE asset_faces DROP CONSTRAINT "FK_95ad7106dd7b484275443f580f9";`);
     await queryRunner.query(`ALTER TABLE asset_faces ADD COLUMN "personId" uuid;`);
-    await queryRunner.query(`ALTER TABLE asset_faces ADD CONSTRAINT FK_xcpv88uznujgzbsvyvs4od67ui FOREIGN KEY ("personId") REFERENCES person("id") ON DELETE NO ACTION ON UPDATE NO ACTION;`);
+    await queryRunner.query(`ALTER TABLE asset_faces ADD CONSTRAINT FK_xcpv88uznujgzbsvyvs4od67ui FOREIGN KEY ("personId") REFERENCES person("id") ON DELETE CASCADE ON UPDATE CASCADE;`);
+    await queryRunner.query(`UPDATE asset_faces SET "personId" = id;`);
     await queryRunner.query(`ALTER TABLE asset_faces DROP CONSTRAINT "FK_02a43fd0b3c50fb6d7f0cb7282c";`);
     await queryRunner.query(`ALTER TABLE asset_faces ADD CONSTRAINT FK_67cmoiacdzi2d4rrszubchg6db FOREIGN KEY ("assetId") REFERENCES assets("id") ON DELETE CASCADE ON UPDATE CASCADE;`);
   }
@@ -18,7 +19,7 @@ export class UnassignFace1697272818851 implements MigrationInterface {
     await queryRunner.query(`ALTER TABLE asset_faces ADD CONSTRAINT "FK_02a43fd0b3c50fb6d7f0cb7282c" FOREIGN KEY ("assetId") REFERENCES assets("id") ON DELETE CASCADE ON UPDATE CASCADE;`);
     await queryRunner.query(`ALTER TABLE asset_faces DROP CONSTRAINT FK_xcpv88uznujgzbsvyvs4od67ui;`);
     await queryRunner.query(`ALTER TABLE asset_faces DROP COLUMN "personId";`);
-    await queryRunner.query(`ALTER TABLE asset_faces ADD CONSTRAINT "FK_95ad7106dd7b484275443f580f9" FOREIGN KEY ("personId") REFERENCES person("id") ON DELETE NO ACTION ON UPDATE NO ACTION;`);
+    await queryRunner.query(`ALTER TABLE asset_faces ADD CONSTRAINT "FK_95ad7106dd7b484275443f580f9" FOREIGN KEY ("id") REFERENCES person("id") ON DELETE CASCADE ON UPDATECASCADE;`);
     await queryRunner.query(`ALTER TABLE asset_faces ALTER COLUMN id DROP DEFAULT;`)
     await queryRunner.query(`ALTER TABLE asset_faces RENAME COLUMN id TO "personId";`);
   }
