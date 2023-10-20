7399a4a0faf8e4b10e690916bbb314201bd4ac70
shalong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2023-10-20 23:20:53 +0530

    mobile: show error on link creation

--- mobile/lib/modules/shared_link/views/shared_link_edit_page.dart
+++ mobile/lib/modules/shared_link/views/shared_link_edit_page.dart
@@ -3,10 +3,12 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/modules/shared_link/models/shared_link.dart';
 import 'package:immich_mobile/modules/shared_link/providers/shared_link.provider.dart';
 import 'package:immich_mobile/modules/shared_link/services/shared_link.service.dart';
+import 'package:immich_mobile/shared/ui/immich_toast.dart';
 import 'package:immich_mobile/utils/url_helper.dart';
 
 class SharedLinkEditPage extends HookConsumerWidget {
@@ -293,6 +295,13 @@ class SharedLinkEditPage extends HookConsumerWidget {
       final serverUrl = getServerUrl();
       if (newLink != null && serverUrl != null) {
         newShareLink.value = "$serverUrl/share/${newLink.key}";
+      } else if (newLink == null) {
+        ImmichToast.show(
+          context: context,
+          gravity: ToastGravity.BOTTOM,
+          toastType: ToastType.error,
+          msg: 'Error while creating shared link',
+        );
       }
     }
 
