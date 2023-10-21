d097dcfa11074d4897dc29d441160dc258cf11ee
Bogdan Cerovac <BogdanCerovac@users.noreply.github.com>
2023-10-21 15:48:22 +0200

    Reintroduced styling for navigation-bar.svelte
    Based on button styling

--- web/src/lib/components/shared-components/navigation-bar/navigation-bar.svelte
+++ web/src/lib/components/shared-components/navigation-bar/navigation-bar.svelte
@@ -77,28 +77,30 @@
 
         {#if user.isAdmin}
           <a data-sveltekit-preload-data="hover" href={AppRoute.ADMIN_USER_MANAGEMENT}>
-            <div class="hidden sm:block">
-                <span
+            <div class="inline-flex items-center justify-center transition-colors dark:text-immich-dark-fg p-2 font-medium rounded-lg">
+              <div class="hidden sm:block">
+                  <span
+                    class={$page.url.pathname.includes('/admin')
+                      ? 'item text-immich-primary underline dark:text-immich-dark-primary'
+                      : ''}
+                  >
+                    Administration
+                  </span>
+              </div>
+              <div class="block sm:hidden">
+                  <Cog
+                    size="1.5em"
+                    class="dark:text-immich-dark-fg {$page.url.pathname.includes('/admin')
+                      ? 'text-immich-primary dark:text-immich-dark-primary'
+                      : ''}"
+                  />
+                <hr
                   class={$page.url.pathname.includes('/admin')
-                    ? 'item text-immich-primary underline dark:text-immich-dark-primary'
-                    : ''}
-                >
-                  Administration
-                </span>
-            </div>
-            <div class="block sm:hidden">
-                <Cog
-                  size="1.5em"
-                  class="dark:text-immich-dark-fg {$page.url.pathname.includes('/admin')
-                    ? 'text-immich-primary dark:text-immich-dark-primary'
-                    : ''}"
+                    ? 'border-1 mx-auto block w-2/3 border-immich-primary dark:border-immich-dark-primary'
+                    : 'hidden'}
                 />
-              <hr
-                class={$page.url.pathname.includes('/admin')
-                  ? 'border-1 mx-auto block w-2/3 border-immich-primary dark:border-immich-dark-primary'
-                  : 'hidden'}
-              />
-            </div>
+              </div>
+           </div>
           </a>
         {/if}
 
