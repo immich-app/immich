f852a88db1512a995621cd53dbc1b77ffd2a711d
Wingy <git@wingysam.xyz>
2023-10-22 22:59:30 -0400

    add typecast

--- web/src/routes/custom.css/+server.ts
+++ web/src/routes/custom.css/+server.ts
@@ -1,5 +1,6 @@
 import { text } from '@sveltejs/kit';
-export const GET = async ({ locals: { api } }) => {
+import type { ImmichApi } from '../../api/api';
+export const GET = async ({ locals: { api } }: { locals: { api: ImmichApi } }) => {
   const { css } = await api.systemConfigApi.getConfig().then((res) => res.data.stylesheets);
   return text(css);
 };
