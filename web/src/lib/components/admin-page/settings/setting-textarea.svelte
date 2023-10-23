f189f91df39793bd6613c62da872b075f517e129
Wingy <git@wingysam.xyz>
2023-10-22 21:51:16 -0400

    add textarea

---
+++ web/src/lib/components/admin-page/settings/setting-textarea.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import { quintOut } from 'svelte/easing';
+  import { fly } from 'svelte/transition';
+
+  export let value: string;
+  export let label = '';
+  export let desc = '';
+  export let required = false;
+  export let disabled = false;
+  export let isEdited = false;
+
+  const handleInput = (e: Event) => {
+    value = (e.target as HTMLInputElement).value;
+  };
+</script>
+
+<div class="mb-4 w-full">
+  <div class={`flex h-[26px] place-items-center gap-1`}>
+    <label class={`immich-form-label text-sm`} for={label}>{label}</label>
+    {#if required}
+      <div class="text-red-400">*</div>
+    {/if}
+
+    {#if isEdited}
+      <div
+        transition:fly={{ x: 10, duration: 200, easing: quintOut }}
+        class="rounded-full bg-orange-100 px-2 text-[10px] text-orange-900"
+      >
+        Unsaved change
+      </div>
+    {/if}
+  </div>
+
+  {#if desc}
+    <p class="immich-form-label pb-2 text-sm" id="{label}-desc">
+      {desc}
+    </p>
+  {:else}
+    <slot name="desc" />
+  {/if}
+
+  <textarea
+    class="immich-form-input w-full pb-2"
+    aria-describedby={desc ? `${label}-desc` : undefined}
+    aria-labelledby="{label}-label"
+    id={label}
+    name={label}
+    {required}
+    {value}
+    on:input={handleInput}
+    {disabled}
+  />
+</div>
--- web/src/lib/components/admin-page/settings/stylesheets/stylesheets-settings.svelte
+++ web/src/lib/components/admin-page/settings/stylesheets/stylesheets-settings.svelte
@@ -8,7 +8,7 @@
   import { isEqual } from 'lodash-es';
   import { fade } from 'svelte/transition';
   import SettingButtonsRow from '../setting-buttons-row.svelte';
-  import SettingInputField, { SettingInputFieldType } from '../setting-input-field.svelte';
+  import SettingTextarea from '../setting-textarea.svelte';
 
   export let stylesheetsConfig: SystemConfigStylesheetsDto; // this is the config that is being edited
   export let disabled = false;
@@ -74,8 +74,7 @@
       <form autocomplete="off" on:submit|preventDefault>
         <div class="ml-4 mt-4 flex flex-col gap-4">
           <div class="ml-4">
-            <SettingInputField
-              inputType={SettingInputFieldType.TEXT}
+            <SettingTextarea
               {disabled}
               label="Custom CSS"
               desc="Cascading Style Sheets allow the design of Immich to be customized."
