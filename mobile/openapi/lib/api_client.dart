061f99d2418d2aa5d24c141986a65fa536ab89eb
Daniel Dietzler <mail@ddietzler.dev>
2023-10-23 11:28:43 +0200

    open api

--- mobile/openapi/lib/api_client.dart
+++ mobile/openapi/lib/api_client.dart
@@ -323,6 +323,8 @@ class ApiClient {
           return LogoutResponseDto.fromJson(value);
         case 'MapMarkerResponseDto':
           return MapMarkerResponseDto.fromJson(value);
+        case 'MapTheme':
+          return MapThemeTypeTransformer().decode(value);
         case 'MemoryLaneResponseDto':
           return MemoryLaneResponseDto.fromJson(value);
         case 'MergePersonDto':
