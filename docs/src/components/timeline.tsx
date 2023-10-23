a8f0f3745f7bdccf0c4ada341259c141ebcd0ace
Alex Tran <alex.tran1502@gmail.com>
2023-10-22 21:29:56 -0500

    responseive design and styling

--- docs/src/components/timeline.tsx
+++ docs/src/components/timeline.tsx
@@ -37,25 +37,26 @@ export default function Timeline({ items }: Props): JSX.Element {
 
         return (
           <li key={index} className="flex min-h-24 w-[700px] max-w-[90vw]">
-            <div className="flex justify-start w-36 mr-8 items-center dark:text-immich-dark-primary text-immich-primary">
+            <div className="md:flex justify-start w-36 mr-8 items-center dark:text-immich-dark-primary text-immich-primary hidden">
               {isBrowser ? item.date.toLocaleDateString(navigator.language) : ''}
             </div>
             <div className={`${isFirst && 'relative top-[50%]'} ${isLast && 'relative bottom-[50%]'}`}>
               <div
-                className={`h-full border-solid border-4 border-immich-primary ${isFirst && 'rounded rounded-t-full'} ${
-                  isLast && 'rounded rounded-b-full'
-                }`}
+                className={`h-full border-solid border-4 border-immich-primary dark:border-immich-dark-primary ${
+                  isFirst && 'rounded rounded-t-full'
+                } ${isLast && 'rounded rounded-b-full'}`}
               ></div>
             </div>
-            <div className="z-10 flex items-center bg-immich-primary border-2 border-solid rounded-full dark:text-black text-white relative top-[50%] left-[-3px] translate-y-[-50%] translate-x-[-50%] w-8 h-8 shadow-lg ">
+            <div className="z-10 flex items-center bg-immich-primary dark:bg-immich-dark-primary border-2 border-solid rounded-full dark:text-black text-white relative top-[50%] left-[-3px] translate-y-[-50%] translate-x-[-50%] w-8 h-8 shadow-lg ">
               <Icon path={mdiCheckboxMarkedCircleOutline} size={1.25} />
             </div>
-            <section className=" dark:bg-immich-dark-gray bg-immich-gray dark:border-0 border-gray-400 border-solid border shadow-lg rounded-2xl flex flex-col w-full gap-2 p-4 ml-4 my-2">
+            <section className=" dark:bg-immich-dark-gray bg-immich-gray dark:border-0 border-gray-200 border border-solid rounded-2xl flex flex-col w-full gap-2 p-4 ml-4 my-2 hover:bg-immich-primary/10 dark:hover:bg-immich-dark-primary/10 transition-all">
               <div className="m-0 text-lg flex w-full items-center justify-between gap-2">
                 <p className="m-0 items-start flex gap-2">
                   <Icon path={item.icon} size={1} />
                   <span>{item.title}</span>
                 </p>
+
                 <span className="dark:text-immich-dark-primary text-immich-primary">
                   {item.tag ? (
                     <a
@@ -63,14 +64,19 @@ export default function Timeline({ items }: Props): JSX.Element {
                       target="_blank"
                       rel="noopener"
                     >
-                      [{item.release}]
+                      [{item.release}]{' '}
                     </a>
                   ) : (
-                    <span>[{item.release}]</span>
+                    <span>
+                      [{item.release} {isBrowser ? item.date.toLocaleDateString(navigator.language) : ''}]
+                    </span>
                   )}
                 </span>
               </div>
-              <p className="m-0 text-sm">{item.description}</p>
+              <div className="md:hidden text-xs">
+                Release Date - {isBrowser ? item.date.toLocaleDateString(navigator.language) : ''}
+              </div>
+              <p className="m-0 text-sm text-gray-600 dark:text-gray-300">{item.description}</p>
             </section>
           </li>
         );
