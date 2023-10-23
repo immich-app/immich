43392a758aa3f9b571d76c72cd05ebf12db2f0ea
Wingy <git@wingysam.xyz>
2023-10-22 23:22:43 -0400

    set content-type properly

--- web/src/routes/custom.css/+server.ts
+++ web/src/routes/custom.css/+server.ts
@@ -1,5 +1,9 @@
 import { RequestHandler, text } from '@sveltejs/kit';
 export const GET = (async ({ locals: { api } }) => {
   const { css } = await api.systemConfigApi.getConfig().then((res) => res.data.stylesheets);
-  return text(css);
+  return text(css, {
+    headers: {
+      'Content-Type': 'text/css',
+    },
+  });
 }) satisfies RequestHandler;
