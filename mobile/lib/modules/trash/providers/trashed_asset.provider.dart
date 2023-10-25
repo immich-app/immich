52e09b48577be3a2f9653f790eb8c9552a9d5619
shenlong <139912620+shenlong-tanwen@users.noreply.github.com>
2023-10-25 16:02:59 +0000

    fix(mobile): asset deletion state management (#4568)

--- mobile/lib/modules/trash/providers/trashed_asset.provider.dart
+++ mobile/lib/modules/trash/providers/trashed_asset.provider.dart
@@ -2,9 +2,9 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/modules/home/ui/asset_grid/asset_grid_data_structure.dart';
 import 'package:immich_mobile/modules/trash/services/trash.service.dart';
 import 'package:immich_mobile/shared/models/asset.dart';
-import 'package:immich_mobile/shared/models/exif_info.dart';
 import 'package:immich_mobile/shared/providers/db.provider.dart';
 import 'package:immich_mobile/shared/providers/user.provider.dart';
+import 'package:immich_mobile/shared/services/sync.service.dart';
 import 'package:isar/isar.dart';
 import 'package:logging/logging.dart';
 
@@ -28,19 +28,18 @@ class TrashNotifier extends StateNotifier<bool> {
       }
       await _trashService.emptyTrash();
 
-      final dbIds = await _db.assets
+      final idsToRemove = await _db.assets
           .where()
           .remoteIdIsNotNull()
           .filter()
           .ownerIdEqualTo(user.isarId)
           .isTrashedEqualTo(true)
-          .idProperty()
+          .remoteIdProperty()
           .findAll();
 
-      await _db.writeTxn(() async {
-        await _db.exifInfos.deleteAll(dbIds);
-        await _db.assets.deleteAll(dbIds);
-      });
+      _ref
+          .read(syncServiceProvider)
+          .handleRemoteAssetRemoval(idsToRemove.cast<String>().toList());
     } catch (error, stack) {
       _log.severe("Cannot empty trash ${error.toString()}", error, stack);
     }
