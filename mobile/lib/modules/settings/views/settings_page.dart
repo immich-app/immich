aefd052888926c875928faa630c2b47de26ec682
Alex <alex.tran1502@gmail.com>
2023-10-25 09:53:16 -0500

    feat(mobile): clear local storage option (#4635)
    * feat(mobile): clear local storage option
    
    * en json

--- mobile/lib/modules/settings/views/settings_page.dart
+++ mobile/lib/modules/settings/views/settings_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/modules/settings/ui/advanced_settings/advanced_settings.dart';
 import 'package:immich_mobile/modules/settings/ui/asset_list_settings/asset_list_settings.dart';
+import 'package:immich_mobile/modules/settings/ui/local_storage_settings/local_storage_settings.dart';
 import 'package:immich_mobile/modules/settings/ui/image_viewer_quality_setting/image_viewer_quality_setting.dart';
 import 'package:immich_mobile/modules/settings/ui/notification_setting/notification_setting.dart';
 import 'package:immich_mobile/modules/settings/ui/theme_setting/theme_setting.dart';
@@ -42,6 +43,7 @@ class SettingsPage extends HookConsumerWidget {
               const AssetListSettings(),
               const NotificationSetting(),
               // const ExperimentalSettings(),
+              const LocalStorageSettings(),
               const AdvancedSettings(),
             ],
           ).toList(),
