2e75234359480e513fa62a8acac380197073a422
brighteyed <sergey.kondrikov@gmail.com>
2023-10-29 17:07:28 +0300

    Use traditional functions instead of arrow functions

--- web/src/lib/utils/slideshow-history.ts
+++ web/src/lib/utils/slideshow-history.ts
@@ -4,21 +4,21 @@ export class SlideshowHistory {
 
   constructor(private onNavigate: (assetId: string) => void) {}
 
-  reset = (assetId: string) => {
+  reset(assetId: string) {
     this.slideshowHistory = [assetId];
     this.slideshowHistoryIndex = 0;
-  };
+  }
 
-  append = (assetId: string) => {
+  append(assetId: string) {
     this.slideshowHistory.push(assetId);
 
     // If we were at the end of the slideshow history, move the index to the new end
     if (this.slideshowHistoryIndex === this.slideshowHistory.length - 2) {
       this.slideshowHistoryIndex++;
     }
-  };
+  }
 
-  navigateForward = (): boolean => {
+  navigateForward(): boolean {
     if (this.slideshowHistoryIndex === this.slideshowHistory.length - 1) {
       return false;
     }
@@ -26,9 +26,9 @@ export class SlideshowHistory {
     this.slideshowHistoryIndex++;
     this.onNavigate(this.slideshowHistory[this.slideshowHistoryIndex]);
     return true;
-  };
+  }
 
-  navigateBackward = (): boolean => {
+  navigateBackward(): boolean {
     if (this.slideshowHistoryIndex === 0) {
       return false;
     }
@@ -36,5 +36,5 @@ export class SlideshowHistory {
     this.slideshowHistoryIndex--;
     this.onNavigate(this.slideshowHistory[this.slideshowHistoryIndex]);
     return true;
-  };
+  }
 }
