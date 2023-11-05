3bd0579f58e58b685290d4666be628f2c1578841
shalong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2023-11-05 23:14:37 +0530

    mobile: change content color for local only

--- mobile/lib/modules/home/ui/delete_dialog.dart
+++ mobile/lib/modules/home/ui/delete_dialog.dart
@@ -4,13 +4,19 @@ import 'package:immich_mobile/shared/ui/confirm_dialog.dart';
 class DeleteDialog extends ConfirmDialog {
   final Function onDelete;
 
-  const DeleteDialog({Key? key, required this.onDelete, content})
-      : super(
+  const DeleteDialog({
+    Key? key,
+    required this.onDelete,
+    String? content,
+    String? ok,
+    Color? contentColor,
+  }) : super(
           key: key,
           title: "delete_dialog_title",
           content: content ?? "delete_dialog_alert",
           cancel: "delete_dialog_cancel",
-          ok: "delete_dialog_ok",
+          ok: ok ?? "delete_dialog_ok",
           onOk: onDelete,
+          contentColor: contentColor,
         );
 }
--- mobile/lib/shared/ui/confirm_dialog.dart
+++ mobile/lib/shared/ui/confirm_dialog.dart
@@ -7,6 +7,7 @@ class ConfirmDialog extends ConsumerWidget {
   final Function onOk;
   final String title;
   final String content;
+  final Color? contentColor;
   final String cancel;
   final String ok;
 
@@ -17,6 +18,7 @@ class ConfirmDialog extends ConsumerWidget {
     required this.content,
     this.cancel = "delete_dialog_cancel",
     this.ok = "backup_controller_page_background_battery_info_ok",
+    this.contentColor,
   }) : super(key: key);
 
   @override
@@ -24,7 +26,10 @@ class ConfirmDialog extends ConsumerWidget {
     return AlertDialog(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       title: Text(title).tr(),
-      content: Text(content).tr(),
+      content: Text(
+        content,
+        style: TextStyle(color: contentColor),
+      ).tr(),
       actions: [
         TextButton(
           onPressed: () => context.pop(false),
