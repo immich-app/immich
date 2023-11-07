66f69a6fb9ef2bf9c4249de75da3972f1cb95db4
Alex Tran <alex.tran1502@gmail.com>
2023-11-06 22:01:37 -0600

    refactor component

---
+++ web/src/lib/components/album-page/album-options.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+  import { mdiClose, mdiPlus } from '@mdi/js';
+  import SettingSwitch from '../admin-page/settings/setting-switch.svelte';
+  import CircleIconButton from '../elements/buttons/circle-icon-button.svelte';
+  import FullScreenModal from '../shared-components/full-screen-modal.svelte';
+  import UserAvatar from '../shared-components/user-avatar.svelte';
+  import { createEventDispatcher } from 'svelte';
+  import type { AlbumResponseDto, UserResponseDto } from '../../../api/open-api';
+  import Icon from '$lib/components/elements/icon.svelte';
+
+  export let album: AlbumResponseDto;
+  export let user: UserResponseDto;
+
+  const dispatch = createEventDispatcher<{
+    close: void;
+    toggleEnableActivity: void;
+    showSelectSharedUser: void;
+  }>();
+</script>
+
+<FullScreenModal on:clickOutside={() => dispatch('close')}>
+  <div class="flex h-full w-full place-content-center place-items-center overflow-hidden p-2 md:p-0">
+    <div
+      class="w-[550px] rounded-3xl border bg-immich-bg shadow-sm dark:border-immich-dark-gray dark:bg-immich-dark-gray dark:text-immich-dark-fg"
+    >
+      <div class="px-2 pt-2">
+        <div class="flex items-center">
+          <h1 class="px-4 w-full self-center font-medium text-immich-primary dark:text-immich-dark-primary">Options</h1>
+          <div>
+            <CircleIconButton icon={mdiClose} on:click={() => dispatch('close')} />
+          </div>
+        </div>
+
+        <div class=" items-center justify-center p-4">
+          <div class="py-2">
+            <h2 class="text-gray text-sm mb-3">SHARING</h2>
+            <div class="p-2">
+              <SettingSwitch
+                title="Comments & likes"
+                subtitle="Let others respond"
+                checked={album.isActivityEnabled}
+                on:toggle={() => dispatch('toggleEnableActivity')}
+              />
+            </div>
+          </div>
+          <div class="py-2">
+            <div class="text-gray text-sm mb-3">PEOPLE</div>
+            <div class="p-2">
+              <button class="flex items-center gap-2" on:click={() => dispatch('showSelectSharedUser')}>
+                <div class="rounded-full w-10 h-10 border border-gray-500 flex items-center justify-center">
+                  <div><Icon path={mdiPlus} size="25" /></div>
+                </div>
+                <div>Invite People</div>
+              </button>
+              <div class="flex items-center gap-2 py-2 mt-2">
+                <div>
+                  <UserAvatar {user} size="md" />
+                </div>
+                <div class="w-full">{`${user.firstName} ${user.lastName}`}</div>
+                <div>Owner</div>
+              </div>
+              {#each album.sharedUsers as user (user.id)}
+                <div class="flex items-center gap-2 py-2">
+                  <div>
+                    <UserAvatar {user} size="md" />
+                  </div>
+                  <div class="w-full">{`${user.firstName} ${user.lastName}`}</div>
+                </div>
+              {/each}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</FullScreenModal>
--- web/src/routes/(user)/albums/[albumId]/+page.svelte
+++ web/src/routes/(user)/albums/[albumId]/+page.svelte
@@ -58,6 +58,7 @@
   import { numberOfComments, setNumberOfComments, updateNumberOfComments } from '$lib/stores/activity.store';
   import FullScreenModal from '$lib/components/shared-components/full-screen-modal.svelte';
   import SettingSwitch from '$lib/components/admin-page/settings/setting-switch.svelte';
+  import AlbumOptions from '$lib/components/album-page/album-options.svelte';
 
   export let data: PageData;
 
@@ -735,64 +736,13 @@
 {/if}
 
 {#if viewMode === ViewMode.OPTIONS}
-  <FullScreenModal on:clickOutside={() => (viewMode = ViewMode.VIEW)}>
-    <div class="flex h-full w-full place-content-center place-items-center overflow-hidden p-2 md:p-0">
-      <div
-        class="w-[550px] rounded-3xl border bg-immich-bg shadow-sm dark:border-immich-dark-gray dark:bg-immich-dark-gray dark:text-immich-dark-fg"
-      >
-        <div class="px-2 pt-2">
-          <div class="flex items-center">
-            <h1 class="px-4 w-full self-center font-medium text-immich-primary dark:text-immich-dark-primary">
-              Options
-            </h1>
-            <div>
-              <CircleIconButton icon={mdiClose} on:click={() => (viewMode = ViewMode.VIEW)} />
-            </div>
-          </div>
-
-          <div class=" items-center justify-center p-4">
-            <div class="py-2">
-              <h2 class="text-gray text-sm mb-3">SHARING</h2>
-              <div class="p-2">
-                <SettingSwitch
-                  title="Comments & likes"
-                  subtitle="Let others respond"
-                  checked={album.isActivityEnabled}
-                  on:toggle={handleToggleEnableActivity}
-                />
-              </div>
-            </div>
-            <div class="py-2">
-              <div class="text-gray text-sm mb-3">PEOPLE</div>
-              <div class="p-2">
-                <button class="flex items-center gap-2" on:click={() => (viewMode = ViewMode.SELECT_USERS)}>
-                  <div class="rounded-full w-10 h-10 border border-gray-500 flex items-center justify-center">
-                    <div><Icon path={mdiPlus} size="25" /></div>
-                  </div>
-                  <div>Invite People</div>
-                </button>
-                <div class="flex items-center gap-2 py-2 mt-2">
-                  <div>
-                    <UserAvatar {user} size="md" />
-                  </div>
-                  <div class="w-full">{`${user.firstName} ${user.lastName}`}</div>
-                  <div>Owner</div>
-                </div>
-                {#each album.sharedUsers as user (user.id)}
-                  <div class="flex items-center gap-2 py-2">
-                    <div>
-                      <UserAvatar {user} size="md" />
-                    </div>
-                    <div class="w-full">{`${user.firstName} ${user.lastName}`}</div>
-                  </div>
-                {/each}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </FullScreenModal>
+  <AlbumOptions
+    {album}
+    {user}
+    on:close={() => (viewMode = ViewMode.VIEW)}
+    on:toggleEnableActivity={handleToggleEnableActivity}
+    on:showSelectSharedUser={() => (viewMode = ViewMode.SELECT_USERS)}
+  />
 {/if}
 
 {#if isEditingDescription}
