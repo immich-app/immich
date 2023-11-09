dfcf620180c4a55f41e6b778b5299ce410828071
shalong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2023-11-09 10:56:43 +0530

    refactor(mobile): add BuildContext extension

---
+++ mobile/lib/extensions/build_context_extensions.dart
@@ -0,0 +1,31 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+extension ContextHelper on BuildContext {
+  // Returns true if the app is running on a mobile device (!tablets)
+  bool get isMobile => MediaQuery.sizeOf(this).width < 550;
+
+  // Returns the current ThemeData
+  ThemeData get themeData => Theme.of(this);
+
+  // Returns true if the app is using a dark theme
+  bool get isDarkTheme => themeData.brightness == Brightness.dark;
+
+  // Returns the current Primary color of the Theme
+  Color get primaryColor => themeData.primaryColor;
+
+  // Returns the current TextTheme
+  TextTheme get textTheme => themeData.textTheme;
+
+  // Current ColorScheme used
+  ColorScheme get colorScheme => themeData.colorScheme;
+
+  // Pop-out from the current context with optional result
+  void pop<T>([T? result]) => Navigator.of(this).pop(result);
+
+  // Auto-Push new route from the current context
+  void autoPush(PageRouteInfo route) => AutoRouter.of(this).push(route);
+
+  // Auto-Pop from the current context
+  void autoPop<T>([T? result]) => AutoRouter.of(this).pop(result);
+}
