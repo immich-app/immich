24670178dc85f8822f888b7cb88a611f4e6c0e58
Jason Rasmussen <jrasm91@gmail.com>
2023-11-13 17:16:26 -0500

    feat(web): use css vraiables (#5022)

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -6,22 +6,22 @@ module.exports = {
     extend: {
       colors: {
         // Light Theme
-        'immich-primary': '#4250af',
-        'immich-bg': 'white',
-        'immich-fg': 'black',
-        'immich-gray': '#F6F6F4',
-        'immich-error': '#e57373',
-        'immich-success': '#81c784',
-        'immich-warning': '#ffb74d',
+        'immich-primary': 'rgb(var(--immich-primary) / <alpha-value>)',
+        'immich-bg': 'rgb(var(--immich-bg) / <alpha-value>)',
+        'immich-fg': 'rgb(var(--immich-fg) / <alpha-value>)',
+        'immich-gray': 'rgb(var(--immich-gray) / <alpha-value>)',
+        'immich-error': 'rgb(var(--immich-error) / <alpha-value>)',
+        'immich-success': 'rgb(var(--immich-success) / <alpha-value>)',
+        'immich-warning': 'rgb(var(--immich-warning) / <alpha-value>)',
 
         // Dark Theme
-        'immich-dark-primary': '#adcbfa',
-        'immich-dark-bg': 'black',
-        'immich-dark-fg': '#e5e7eb',
-        'immich-dark-gray': '#212121',
-        'immich-dark-error': '#d32f2f',
-        'immich-dark-success': '#388e3c',
-        'immich-dark-warning': '#f57c00',
+        'immich-dark-primary': 'rgb(var(--immich-dark-primary) / <alpha-value>)',
+        'immich-dark-bg': 'rgb(var(--immich-dark-bg) / <alpha-value>)',
+        'immich-dark-fg': 'rgb(var(--immich-dark-fg) / <alpha-value>)',
+        'immich-dark-gray': 'rgb(var(--immich-dark-gray) / <alpha-value>)',
+        'immich-dark-error': 'rgb(var(--immich-dark-error) / <alpha-value>)',
+        'immich-dark-success': 'rgb(var(--immich-dark-success) / <alpha-value>)',
+        'immich-dark-warning': 'rgb(var(--immich-dark-warning) / <alpha-value>)',
       },
       fontFamily: {
         'immich-title': ['Snowburst One', 'cursive'],
