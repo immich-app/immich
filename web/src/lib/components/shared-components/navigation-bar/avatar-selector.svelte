9c7e77cd14a9394186f0b4b03dc510d3fb55ecf1
Alex Tran <alex.tran1502@gmail.com>
2023-11-13 20:36:03 -0600

    minor styling

--- web/src/lib/components/shared-components/navigation-bar/avatar-selector.svelte
+++ web/src/lib/components/shared-components/navigation-bar/avatar-selector.svelte
@@ -15,7 +15,7 @@
 <FullScreenModal on:clickOutside={() => dispatch('close')} on:escape={() => dispatch('close')}>
   <div class="flex h-full w-full place-content-center place-items-center overflow-hidden">
     <div
-      class=" rounded-3xl border bg-immich-bg shadow-sm dark:border-immich-dark-gray dark:bg-immich-dark-gray dark:text-immich-dark-fg py-4 px-6"
+      class=" rounded-3xl border bg-immich-bg shadow-sm dark:border-immich-dark-gray dark:bg-immich-dark-gray dark:text-immich-dark-fg p-4"
     >
       <div class="flex items-center">
         <h1 class="px-4 w-full self-center font-medium text-immich-primary dark:text-immich-dark-primary text-sm">
@@ -25,14 +25,12 @@
           <CircleIconButton icon={mdiClose} on:click={() => dispatch('close')} />
         </div>
       </div>
-      <div class="flex items-center justify-center p-4">
+      <div class="flex items-center justify-center p-4 mt-4">
         <div class="grid grid-cols-2 md:grid-cols-5 gap-4">
           {#each colors as color}
-            <div>
-              <button on:click={() => dispatch('choose', color)}>
-                <UserAvatar {user} {color} size="xl" showProfileImage={false} />
-              </button>
-            </div>
+            <button on:click={() => dispatch('choose', color)}>
+              <UserAvatar {user} {color} size="xl" showProfileImage={false} />
+            </button>
           {/each}
         </div>
       </div>
