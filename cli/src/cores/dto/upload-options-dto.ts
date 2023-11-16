5e56b65b38607f1c913ded0afe2a0dab6e60167a
Jonathan Jogenfors <jonathan@jogenfors.se>
2023-11-17 00:58:36 +0100

    git ignore for geocode in cli

--- cli/src/cores/dto/upload-options-dto.ts
+++ cli/src/cores/dto/upload-options-dto.ts
@@ -1,9 +1,8 @@
 export class UploadOptionsDto {
-  recursive = false;
-  exclusionPatterns!: string[];
-  dryRun = false;
-  skipHash = false;
-  delete = false;
-  readOnly = true;
-  album = false;
+  recursive? = false;
+  exclusionPatterns?: string[] = [];
+  dryRun? = false;
+  skipHash? = false;
+  delete? = false;
+  album? = false;
 }
--- cli/test/e2e/cli.e2e-spec.ts
+++ cli/test/e2e/cli.e2e-spec.ts
@@ -7,6 +7,8 @@ import {
   testApp,
 } from 'immich/test/test-utils';
 import { LoginResponseDto } from 'src/api/open-api';
+import ServerInfo from 'src/commands/server-info';
+import Upload from 'src/commands/upload';
 
 describe(`CLI (e2e)`, () => {
   let server: any;
@@ -26,5 +28,23 @@ describe(`CLI (e2e)`, () => {
     await restoreTempFolder();
     await api.authApi.adminSignUp(server);
     admin = await api.authApi.adminLogin(server);
+  });
+
+  describe('server-info', () => {
+    it('should show server version', async () => {
+      await new ServerInfo().run();
+    });
+  });
+
+  describe('upload', () => {
+    it('should upload an asset', async () => {
+      await fs.promises.cp(`${IMMICH_TEST_ASSET_PATH}/albums/nature`, `${IMMICH_TEST_ASSET_TEMP_PATH}/albums/nature`, {
+        recursive: true,
+      });
+
+      await new Upload().run([`${IMMICH_TEST_ASSET_TEMP_PATH}/albums/nature`], {});
+      const assets = await api.assetApi.getAllAssets(server, admin.accessToken);
+      console.log(assets);
+    });
   });
 });
