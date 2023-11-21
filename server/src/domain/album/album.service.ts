a7fc06dfdf48f7c37b241aca31891394a706d99d
martabal <74269598+martabal@users.noreply.github.com>
2023-11-21 18:53:02 +0100

    fix: current behavior

--- server/src/domain/album/album.service.ts
+++ server/src/domain/album/album.service.ts
@@ -101,7 +101,7 @@ export class AlbumService {
   async get(authUser: AuthUserDto, id: string, dto: AlbumInfoDto): Promise<AlbumResponseDto> {
     await this.access.requirePermission(authUser, Permission.ALBUM_READ, id);
     await this.albumRepository.updateThumbnails();
-    const withAssets = dto.withoutAssets === undefined ? false : !dto.withoutAssets;
+    const withAssets = dto.withoutAssets === undefined ? true : !dto.withoutAssets;
     return mapAlbum(await this.findOrFail(id, { withAssets }), withAssets);
   }
 
