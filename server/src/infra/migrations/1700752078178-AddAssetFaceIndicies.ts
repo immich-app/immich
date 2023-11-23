d10aecce7753db5f69d473104cebc63e0f10e23a
Emanuel Bennici <eb@fabmation.de>
2023-11-23 15:11:44 +0100

    feat(server): load face entities faster
    The query executed when loading the "People" page joins, among others, over "personId".
    The added indices improve the overall performance of those JOIN queries.
    
    Additionally, one ORDER BY clause is dropped since the resulting values
    will always be TRUE, and thus, sorting them does not change the result.

---
+++ server/src/infra/migrations/1700752078178-AddAssetFaceIndicies.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddAssetFaceIndicies1700752078178 implements MigrationInterface {
+    name = 'AddAssetFaceIndicies1700752078178'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE INDEX "IDX_bf339a24070dac7e71304ec530" ON "asset_faces" ("personId", "assetId") `);
+        await queryRunner.query(`CREATE INDEX "IDX_b463c8edb01364bf2beba08ef1" ON "assets" ("stackParentId") `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX "public"."IDX_b463c8edb01364bf2beba08ef1"`);
+        await queryRunner.query(`DROP INDEX "public"."IDX_bf339a24070dac7e71304ec530"`);
+    }
+
+}
