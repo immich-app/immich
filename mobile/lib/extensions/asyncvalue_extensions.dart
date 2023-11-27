f72cbfe26e3d86dd252b7dd20dbb1a97a9d49964
shalong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2023-11-27 07:30:13 +0530

    refactor: add skiploadingonrefresh

--- mobile/lib/extensions/asyncvalue_extensions.dart
+++ mobile/lib/extensions/asyncvalue_extensions.dart
@@ -8,15 +8,23 @@ extension LogOnError<T> on AsyncValue<T> {
   static final Logger _asyncErrorLogger = Logger("AsyncValue");
 
   Widget widgetWhen({
+    bool skipLoadingOnRefresh = true,
     Widget Function()? onLoading,
     Widget Function(Object? error, StackTrace? stack)? onError,
     required Widget Function(T data) onData,
   }) {
     if (isLoading) {
-      return onLoading?.call() ??
-          const Center(
-            child: ImmichLoadingIndicator(),
-          );
+      bool skip = false;
+      if (isRefreshing) {
+        skip = skipLoadingOnRefresh;
+      }
+
+      if (!skip) {
+        return onLoading?.call() ??
+            const Center(
+              child: ImmichLoadingIndicator(),
+            );
+      }
     }
 
     if (hasError && !hasValue) {
