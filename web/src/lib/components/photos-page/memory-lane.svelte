3992119e32c936b1584463e8c71316fc6fabe2e2
Jason Rasmussen <jrasm91@gmail.com>
2023-11-27 11:42:04 -0500

    fix(deps): prettier 3 (#5336)

--- web/src/lib/components/photos-page/memory-lane.svelte
+++ web/src/lib/components/photos-page/memory-lane.svelte
@@ -91,6 +91,8 @@
 
 <style>
   .memory-card {
-    box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+    box-shadow:
+      rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+      rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
   }
 </style>
