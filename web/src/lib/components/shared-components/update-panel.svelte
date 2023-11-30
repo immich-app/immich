e7bd870aa3679e960e327f3171e2f7291e5c2556
Alex Tran <alex.tran1502@gmail.com>
2023-11-29 21:47:39 -0600

    fix test

--- web/src/lib/components/shared-components/update-panel.svelte
+++ web/src/lib/components/shared-components/update-panel.svelte
@@ -3,14 +3,9 @@
   import type { AssetStore } from '$lib/stores/assets.store';
 
   export let assetStore: AssetStore | null;
-  let assetUpdateCount = 0;
-  let lastAssetName: string;
 
   websocketStore.onAssetUpdate.subscribe((asset) => {
     if (asset && asset.originalFileName && assetStore) {
-      lastAssetName = asset.originalFileName;
-      assetUpdateCount++;
-
       assetStore.updateAsset(asset, true);
 
       assetStore.removeAsset(asset.id); // Update timeline
