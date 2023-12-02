bb4c9bc64de57e571d3018e207859a33461b0392
martabal <74269598+martabal@users.noreply.github.com>
2023-12-02 22:38:15 +0100

    fix: stick action bar on search

--- web/src/routes/(user)/search/+page.svelte
+++ web/src/routes/(user)/search/+page.svelte
@@ -106,23 +106,25 @@
 
 <section>
   {#if isMultiSelectionMode}
-    <AssetSelectControlBar assets={selectedAssets} clearSelect={() => (selectedAssets = new Set())}>
-      <CreateSharedLink />
-      <CircleIconButton title="Select all" icon={mdiSelectAll} on:click={handleSelectAll} />
-      <AssetSelectContextMenu icon={mdiPlus} title="Add">
-        <AddToAlbum />
-        <AddToAlbum shared />
-      </AssetSelectContextMenu>
-      <DeleteAssets {onAssetDelete} />
+    <div class="fixed z-[100] top-0 left-0 w-full">
+      <AssetSelectControlBar assets={selectedAssets} clearSelect={() => (selectedAssets = new Set())}>
+        <CreateSharedLink />
+        <CircleIconButton title="Select all" icon={mdiSelectAll} on:click={handleSelectAll} />
+        <AssetSelectContextMenu icon={mdiPlus} title="Add">
+          <AddToAlbum />
+          <AddToAlbum shared />
+        </AssetSelectContextMenu>
+        <DeleteAssets {onAssetDelete} />
 
-      <AssetSelectContextMenu icon={mdiDotsVertical} title="Add">
-        <DownloadAction menuItem />
-        <FavoriteAction menuItem removeFavorite={isAllFavorite} />
-        <ArchiveAction menuItem unarchive={isAllArchived} />
-        <ChangeDate menuItem />
-        <ChangeLocation menuItem />
-      </AssetSelectContextMenu>
-    </AssetSelectControlBar>
+        <AssetSelectContextMenu icon={mdiDotsVertical} title="Add">
+          <DownloadAction menuItem />
+          <FavoriteAction menuItem removeFavorite={isAllFavorite} />
+          <ArchiveAction menuItem unarchive={isAllArchived} />
+          <ChangeDate menuItem />
+          <ChangeLocation menuItem />
+        </AssetSelectContextMenu>
+      </AssetSelectControlBar>
+    </div>
   {:else}
     <ControlAppBar on:close-button-click={() => goto(previousRoute)} backIcon={mdiArrowLeft}>
       <div class="w-full flex-1 pl-4">
