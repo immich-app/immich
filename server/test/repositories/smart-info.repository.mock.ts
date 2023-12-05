3dd965049e0c68a8af47416d5cbdb0b5bfde247b
mertalev <101130780+mertalev@users.noreply.github.com>
2023-12-04 19:14:37 -0500

    fixed e2e

--- server/test/repositories/smart-info.repository.mock.ts
+++ server/test/repositories/smart-info.repository.mock.ts
@@ -2,6 +2,7 @@ import { ISmartInfoRepository } from '@app/domain';
 
 export const newSmartInfoRepositoryMock = (): jest.Mocked<ISmartInfoRepository> => {
   return {
+    init: jest.fn(),
     searchCLIP: jest.fn(),
     searchFaces: jest.fn(),
     upsert: jest.fn(),
