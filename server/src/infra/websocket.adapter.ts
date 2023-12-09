9b817aa945e7a379a1b285ad485be7528ed23d4f
Jason Rasmussen <jrasm91@gmail.com>
2023-12-08 20:32:03 -0500

    refactor: create attachment table via migration

--- server/src/infra/websocket.adapter.ts
+++ server/src/infra/websocket.adapter.ts
@@ -5,23 +5,14 @@ import { ServerOptions } from 'socket.io';
 import { DataSource } from 'typeorm';
 import { PostgresDriver } from 'typeorm/driver/postgres/PostgresDriver.js';
 
-const CREATE_ATTACHMENT_TABLE = `
-  CREATE TABLE IF NOT EXISTS socket_io_attachments (
-      id          bigserial UNIQUE,
-      created_at  timestamptz DEFAULT NOW(),
-      payload     bytea
-  );
-`;
-
 export class WebSocketAdapter extends IoAdapter {
   constructor(private app: INestApplicationContext) {
     super(app);
   }
 
   createIOServer(port: number, options?: ServerOptions): any {
-    const pool = (this.app.get(DataSource).driver as PostgresDriver).master;
-    pool.query(CREATE_ATTACHMENT_TABLE);
     const server = super.createIOServer(port, options);
+    const pool = (this.app.get(DataSource).driver as PostgresDriver).master;
     server.adapter(createAdapter(pool));
     return server;
   }
