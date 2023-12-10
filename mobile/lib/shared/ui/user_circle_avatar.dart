960b68b02ff537bf4f937824e86df0d4f77dac58
shenlong <139912620+shenlong-tanwen@users.noreply.github.com>
2023-12-10 15:56:39 +0000

    fix(mobile): live / motion photo download (#5607)
    * reverts: 5566
    
    * fix: stitch livePhoto only in iOS
    
    * fix: PMProgressHandler only on iOS
    
    * ios: fallback to saving image if livephoto fails
    
    ---------
    
    Co-authored-by: shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>

--- mobile/lib/shared/ui/user_circle_avatar.dart
+++ mobile/lib/shared/ui/user_circle_avatar.dart
@@ -3,7 +3,6 @@ import 'dart:math';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:immich_mobile/extensions/build_context_extensions.dart';
 import 'package:immich_mobile/shared/models/store.dart';
 import 'package:immich_mobile/shared/models/user.dart';
 import 'package:immich_mobile/shared/ui/transparent_image.dart';
@@ -35,11 +34,10 @@ class UserCircleAvatar extends ConsumerWidget {
         color: isDarkTheme && user.avatarColor == AvatarColorEnum.primary
             ? Colors.black
             : Colors.white,
-        backgroundColor: user.avatarColor.toColor(),
       ),
     );
     return CircleAvatar(
-      backgroundColor: context.primaryColor,
+      backgroundColor: user.avatarColor.toColor(),
       radius: radius,
       child: user.profileImagePath.isEmpty
           ? textIcon
