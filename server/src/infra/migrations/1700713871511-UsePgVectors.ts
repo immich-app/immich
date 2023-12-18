96fc14a587dcd1620ac0f0d0e98e2be8642509c5
mertalev <101130780+mertalev@users.noreply.github.com>
2023-12-18 18:09:15 -0500

    remove assert from migration

--- server/src/infra/migrations/1700713871511-UsePgVectors.ts
+++ server/src/infra/migrations/1700713871511-UsePgVectors.ts
@@ -1,13 +1,10 @@
 import { getCLIPModelInfo } from '@app/domain/smart-info/smart-info.constant';
 import { MigrationInterface, QueryRunner } from 'typeorm';
-import { assertVectors } from '../database.config';
 
 export class UsePgVectors1700713871511 implements MigrationInterface {
   name = 'UsePgVectors1700713871511';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await assertVectors(queryRunner);
-
     const faceDimQuery = await queryRunner.query(`
         SELECT CARDINALITY(embedding::real[]) as dimsize
         FROM asset_faces
