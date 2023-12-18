dff8cfb18acc6982ce4deb58feaf4eeedf5b7ee5
Alex Tran <alex.tran1502@gmail.com>
2023-12-17 20:36:13 -0600

    fix(server): metadata search does not return all EXIF info

--- web/src/routes/(user)/search/+page.ts
+++ web/src/routes/(user)/search/+page.ts
@@ -7,7 +7,7 @@ export const load = (async (data) => {
   const url = new URL(data.url.href);
   const term = url.searchParams.get('q') || url.searchParams.get('query') || undefined;
   const { data: results } = await api.searchApi.search({}, { params: url.searchParams });
-
+  console.log(results.assets.items[0]);
   return {
     term,
     results,
