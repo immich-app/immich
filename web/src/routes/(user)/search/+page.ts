46f93d9493226680e68a59aafe3cd4a1d7858bdf
Alex Tran <alex.tran1502@gmail.com>
2023-12-18 14:31:32 -0600

    fix(web): access /search throw error

--- web/src/routes/(user)/search/+page.ts
+++ web/src/routes/(user)/search/+page.ts
@@ -1,12 +1,16 @@
 import { authenticate } from '$lib/utils/auth';
-import { api } from '@api';
+import { SearchResponseDto, api } from '@api';
 import type { PageLoad } from './$types';
 
 export const load = (async (data) => {
   await authenticate();
   const url = new URL(data.url.href);
   const term = url.searchParams.get('q') || url.searchParams.get('query') || undefined;
-  const { data: results } = await api.searchApi.search({}, { params: url.searchParams });
+  let results: SearchResponseDto | null = null;
+  if (term) {
+    const { data } = await api.searchApi.search({}, { params: url.searchParams });
+    results = data;
+  }
 
   return {
     term,
