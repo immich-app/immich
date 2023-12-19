d219ceb62a6245b47c9110b7bdee9ddf05268698
mertalev <101130780+mertalev@users.noreply.github.com>
2023-12-19 00:06:09 -0500

    fixed logging

--- server/src/domain/database/database.service.ts
+++ server/src/domain/database/database.service.ts
@@ -1,6 +1,7 @@
 import { ImmichLogger } from '@app/infra/logger';
 import { Inject, Injectable } from '@nestjs/common';
 import { DatabaseExtension, IDatabaseRepository } from '../repositories';
+import { QueryFailedError } from 'typeorm';
 
 @Injectable()
 export class DatabaseService {
@@ -10,9 +11,13 @@ export class DatabaseService {
   constructor(@Inject(IDatabaseRepository) private databaseRepository: IDatabaseRepository) {}
 
   async init() {
+    this.logger.log('Initializing database service');
     await this.assertVectors();
+    this.logger.log('Enabling pgvecto.rs prefilter');
     await this.databaseRepository.enablePrefilter();
+    this.logger.log('Running database migrations');
     await this.databaseRepository.runMigrations();
+    this.logger.log('Initialized database service');
   }
 
   async assertVectors() {
@@ -30,12 +35,10 @@ export class DatabaseService {
 
   async createVectors() {
     const image = await this.getVectorsImage();
-    await this.databaseRepository.createExtension(DatabaseExtension.VECTORS).catch((err) => {
-      this.logger.error(
-        'Failed to create pgvecto.rs extension. ' +
-          `If you have not updated your Postgres instance to an image that supports pgvecto.rs (such as '${image}'), please do so. ` +
-          'See the v1.91.0 release notes for more info: https://github.com/immich-app/immich/releases/tag/v1.91.0',
-      );
+    await this.databaseRepository.createExtension(DatabaseExtension.VECTORS).catch((err: QueryFailedError) => {
+      this.logger.fatal('Failed to create pgvecto.rs extension.');
+      this.logger.fatal(`If you have not updated your Postgres instance to an image that supports pgvecto.rs (such as '${image}'), please do so.`);
+      this.logger.fatal('See the v1.91.0 release notes for more info: https://github.com/immich-app/immich/releases/tag/v1.91.0');
       throw err;
     });
   }
