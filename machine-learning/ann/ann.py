8abb4a390942c38d4d39ae4cde00a86e0a238655
mertalev <101130780+mertalev@users.noreply.github.com>
2023-12-21 02:56:16 -0500

    strict typing

--- machine-learning/ann/ann.py
+++ machine-learning/ann/ann.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 from ctypes import CDLL, Array, c_bool, c_char_p, c_int, c_ulong, c_void_p
 from os.path import exists
+from typing import Any, Generic, Protocol, Type, TypeVar
 
 import numpy as np
 from numpy.typing import NDArray
@@ -25,13 +28,20 @@ except OSError as e:
     log.debug("Could not load ANN shared libraries, using ONNX: %s", e)
     is_available = False
 
+T = TypeVar("T", covariant=True)
 
-class _Singleton(type):
-    _instances = {}
 
-    def __call__(cls, *args, **kwargs):
+class Newable(Protocol[T]):
+    def new(self) -> None:
+        ...
+
+
+class _Singleton(type, Newable[T]):
+    _instances: dict[_Singleton[T], Newable[T]] = {}
+
+    def __call__(cls, *args: Any, **kwargs: Any) -> Newable[T]:
         if cls not in cls._instances:
-            obj = super(_Singleton, cls).__call__(*args, **kwargs)
+            obj: Newable[T] = super(_Singleton, cls).__call__(*args, **kwargs)
             cls._instances[cls] = obj
         else:
             obj = cls._instances[cls]
@@ -40,7 +50,7 @@ class _Singleton(type):
 
 
 class Ann(metaclass=_Singleton):
-    def __init__(self, log_level=3, tuning_level=1, tuning_file: str | None = None) -> None:
+    def __init__(self, log_level: int = 3, tuning_level: int = 1, tuning_file: str | None = None) -> None:
         if not is_available:
             raise RuntimeError("libann is not available!")
         if tuning_file and not exists(tuning_file):
@@ -54,17 +64,17 @@ class Ann(metaclass=_Singleton):
         self.log_level = log_level
         self.tuning_level = tuning_level
         self.tuning_file = tuning_file
-        self.output_shapes: dict[int, tuple[tuple[int, ...]]] = {}
-        self.input_shapes: dict[int, tuple[tuple[int, ...]]] = {}
+        self.output_shapes: dict[int, tuple[tuple[int], ...]] = {}
+        self.input_shapes: dict[int, tuple[tuple[int], ...]] = {}
         self.ann: int | None = None
         self.new()
 
-    def new(self):
+    def new(self) -> None:
         if self.ann is None:
             self.ann = libann.init(
                 self.log_level,
                 self.tuning_level,
-                self.tuning_file.encode() if self.tuning_file else None,
+                self.tuning_file,
             )
             self.ref_count = 0
 
@@ -84,24 +94,26 @@ class Ann(metaclass=_Singleton):
     def load(
         self,
         model_path: str,
-        fast_math=True,
-        fp16=False,
-        save_cached_network=False,
+        fast_math: bool = True,
+        fp16: bool = False,
+        save_cached_network: bool = False,
         cached_network_path: str | None = None,
     ) -> int:
-        if not (exists(model_path) and model_path.endswith((".armnn", ".tflite", ".onnx"))):
+        if not model_path.endswith((".armnn", ".tflite", ".onnx")):
             raise ValueError("model_path must be a file with extension .armnn, .tflite or .onnx")
-        if cached_network_path and not exists(cached_network_path):
+        if not exists(model_path):
+            raise ValueError("model_path must point to an existing file!")
+        if cached_network_path is not None and not exists(cached_network_path):
             raise ValueError("cached_network_path must point to an existing (possibly empty) file!")
         if save_cached_network and cached_network_path is None:
             raise ValueError("save_cached_network is True, cached_network_path must be specified!")
-        net_id = libann.load(
+        net_id: int = libann.load(
             self.ann,
-            model_path.encode(),
+            model_path,
             fast_math,
             fp16,
             save_cached_network,
-            cached_network_path.encode() if cached_network_path else None,
+            cached_network_path,
         )
 
         self.input_shapes[net_id] = tuple(
@@ -116,7 +128,7 @@ class Ann(metaclass=_Singleton):
         libann.unload(self.ann, network_id)
         del self.output_shapes[network_id]
 
-    def execute(self, network_id: int, input_tensors: list[NDArray]) -> list[NDArray]:
+    def execute(self, network_id: int, input_tensors: list[NDArray[np.float32]]) -> list[NDArray[np.float32]]:
         if not isinstance(input_tensors, list):
             raise ValueError("input_tensors needs to be a list!")
         net_input_shapes = self.input_shapes[network_id]
@@ -125,7 +137,9 @@ class Ann(metaclass=_Singleton):
         for net_input_shape, input_tensor in zip(net_input_shapes, input_tensors):
             if net_input_shape != input_tensor.shape:
                 raise ValueError(f"input_tensor shape {input_tensor.shape} != network input shape {net_input_shape}")
-        output_tensors = [np.ndarray(s, dtype=np.float32) for s in self.output_shapes[network_id]]
+        output_tensors: list[NDArray[np.float32]] = [
+            np.ndarray(s, dtype=np.float32) for s in self.output_shapes[network_id]
+        ]
         input_type = c_void_p * len(input_tensors)
         inputs = input_type(*[t.ctypes.data_as(c_void_p) for t in input_tensors])
         output_type = c_void_p * len(output_tensors)
@@ -133,7 +147,7 @@ class Ann(metaclass=_Singleton):
         libann.execute(self.ann, network_id, inputs, outputs)
         return output_tensors
 
-    def shape(self, network_id: int, input=False, index=0) -> tuple[int]:
+    def shape(self, network_id: int, input: bool = False, index: int = 0) -> tuple[int]:
         s = libann.shape(self.ann, network_id, input, index)
         a = []
         while s != 0:
@@ -141,5 +155,6 @@ class Ann(metaclass=_Singleton):
             s >>= 16
         return tuple(a)
 
-    def tensors(self, network_id: int, input=False) -> int:
-        return libann.tensors(self.ann, network_id, input)
+    def tensors(self, network_id: int, input: bool = False) -> int:
+        tensors: int = libann.tensors(self.ann, network_id, input)
+        return tensors
--- machine-learning/ann/export/run.py
+++ machine-learning/ann/export/run.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 import logging
 import os
 import platform
@@ -12,6 +13,8 @@ from tinynn.converter import TFLiteConverter
 
 
 class ExportBase(torch.nn.Module):
+    input_shape: tuple[int, ...]
+
     def __init__(self, device: torch.device, name: str):
         super().__init__()
         self.device = device
@@ -19,15 +22,20 @@ class ExportBase(torch.nn.Module):
         self.optimize = 5
         self.nchw_transpose = False
 
-    def dummy_input(self):
+    @abstractmethod
+    def forward(self, input_tensor: torch.Tensor) -> torch.Tensor | tuple[torch.Tensor]:
         pass
+
+    def dummy_input(self) -> torch.FloatTensor:
+        return torch.rand((1, 3, 224, 224), device=self.device)
 
 
 class ArcFace(ExportBase):
+    input_shape = (1, 3, 112, 112)
+
     def __init__(self, onnx_model_path: str, device: torch.device):
         name, _ = os.path.splitext(os.path.basename(onnx_model_path))
         super().__init__(device, name)
-        self.input_shape = (1, 3, 112, 112)
         onnx_model = onnx.load_model(onnx_model_path)
         make_input_shape_fixed(onnx_model.graph, onnx_model.graph.input[0].name, self.input_shape)
         fix_output_shapes(onnx_model)
@@ -35,15 +43,20 @@ class ArcFace(ExportBase):
         if self.device.type == "cuda":
             self.model = self.model.half()
 
-    def forward(self, input_tensor):
-        embedding = self.model(input_tensor.half() if self.device.type == "cuda" else input_tensor)
-        return embedding.float()
+    def forward(self, input_tensor: torch.Tensor) -> torch.FloatTensor:
+        embedding: torch.FloatTensor = self.model(
+            input_tensor.half() if self.device.type == "cuda" else input_tensor
+        ).float()
+        assert isinstance(embedding, torch.FloatTensor)
+        return embedding
 
-    def dummy_input(self):
+    def dummy_input(self) -> torch.FloatTensor:
         return torch.rand(self.input_shape, device=self.device)
 
 
 class RetinaFace(ExportBase):
+    input_shape = (1, 3, 640, 640)
+
     def __init__(self, onnx_model_path: str, device: torch.device):
         name, _ = os.path.splitext(os.path.basename(onnx_model_path))
         super().__init__(device, name)
@@ -52,12 +65,17 @@ class RetinaFace(ExportBase):
         if self.device.type == "cuda":
             self.model = self.model.half()
 
-    def forward(self, input_tensor):
-        out = self.model(input_tensor.half() if self.device.type == "cuda" else input_tensor)
+    def forward(self, input_tensor: torch.Tensor) -> tuple[torch.FloatTensor]:
+        out: torch.Tensor = self.model(input_tensor.half() if self.device.type == "cuda" else input_tensor)
         return tuple(o.float() for o in out)
+
+    def dummy_input(self) -> torch.FloatTensor:
+        return torch.rand(self.input_shape, device=self.device)
 
 
 class ClipVision(ExportBase):
+    input_shape = (1, 3, 224, 224)
+
     def __init__(self, model_name: str, weights: str, device: torch.device):
         super().__init__(device, model_name + "__" + weights)
         self.model = open_clip.create_model(
@@ -69,21 +87,20 @@ class ClipVision(ExportBase):
             device=device,
         )
 
-    def forward(self, input_tensor: torch.FloatTensor):
-        embedding = self.model.encode_image(input_tensor.half() if self.device.type == "cuda" else input_tensor)
-        return embedding.float()
+    def forward(self, input_tensor: torch.Tensor) -> torch.FloatTensor:
+        embedding: torch.Tensor = self.model.encode_image(
+            input_tensor.half() if self.device.type == "cuda" else input_tensor
+        ).float()
+        return embedding
 
-    def dummy_input(self):
-        return torch.rand((1, 3, 224, 224), device=self.device)
 
-
-def export(model: ExportBase):
+def export(model: ExportBase) -> None:
     model.eval()
     for param in model.parameters():
         param.requires_grad = False
     dummy_input = model.dummy_input()
     model(dummy_input)
-    jit = torch.jit.trace(model, dummy_input)
+    jit = torch.jit.trace(model, dummy_input)  # type: ignore[no-untyped-call,attr-defined]
     tflite_model_path = f"output/{model.name}.tflite"
     os.makedirs("output", exist_ok=True)
 
@@ -98,7 +115,7 @@ def export(model: ExportBase):
     converter.convert()
 
     armnn_model_path = f"output/{model.name}.armnn"
-    os.environ.LD_LIBRARY_PATH = "armnn"
+    os.environ["LD_LIBRARY_PATH"] = "armnn"
     subprocess.run(
         [
             "./armnnconverter",
@@ -116,7 +133,7 @@ def export(model: ExportBase):
     )
 
 
-def main():
+def main() -> None:
     if platform.machine() not in ("x86_64", "AMD64"):
         raise RuntimeError(f"Can only run on x86_64 / AMD64, not {platform.machine()}")
 
