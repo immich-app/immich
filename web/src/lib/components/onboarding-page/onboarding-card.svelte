1981961cb58b37b07b6b21d5b96da010134bcfa3
Alex Tran <alex.tran1502@gmail.com>
2023-12-30 10:35:21 -0600

    Animation

--- web/src/lib/components/onboarding-page/onboarding-card.svelte
+++ web/src/lib/components/onboarding-page/onboarding-card.svelte
@@ -1,6 +1,11 @@
+<script lang="ts">
+  import { fly, fade } from 'svelte/transition';
+</script>
+
 <div
   id="onboarding-card"
   class="flex w-full max-w-xl flex-col gap-4 rounded-3xl border bg-white p-8 shadow-sm dark:border-immich-dark-gray dark:bg-immich-dark-gray"
+  in:fade={{ duration: 250 }}
 >
   <slot />
 </div>
