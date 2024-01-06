807627e03e957b1c49f9d89820ae1c488feb40f4
Alex Tran <alex.tran1502@gmail.com>
2024-01-06 14:12:56 -0600

    chore(server): set onboarding for existing instances

---
+++ server/src/infra/migrations/1704571051932-DefaultOnboardingForExistingInstallations.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class DefaultOnboardingForExistingInstallations1704571051932 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const adminCount = await queryRunner.query(`SELECT COUNT(*) FROM users WHERE "isAdmin" = true`);
+    if (adminCount[0].count > 0) {
+      await queryRunner.query(`INSERT INTO system_metadata (key, value) VALUES ($1, $2)`, [
+        'admin-onboarding',
+        '"{\\"isOnboarded\\":true}"',
+      ]);
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
