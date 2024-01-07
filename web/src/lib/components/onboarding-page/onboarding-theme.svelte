42f1d3d0be892f3978fbb05af133ee82a021bc7d
Jason Rasmussen <jrasm91@gmail.com>
2024-01-06 19:06:49 -0500

    chore: set insetad of toggle on click

--- web/src/lib/components/onboarding-page/onboarding-theme.svelte
+++ web/src/lib/components/onboarding-page/onboarding-theme.svelte
@@ -4,8 +4,9 @@
   import Icon from '../elements/icon.svelte';
   import OnboardingCard from './onboarding-card.svelte';
   import { createEventDispatcher } from 'svelte';
-  import { handleToggleTheme } from '$lib/stores/preferences.store';
+  import { colorTheme } from '$lib/stores/preferences.store';
   import { moonPath, moonViewBox, sunPath, sunViewBox } from '$lib/assets/svg-paths';
+  import { Theme } from '$lib/constants';
 
   const dispatch = createEventDispatcher<{
     done: void;
@@ -23,7 +24,7 @@
   <div class="flex gap-4 mb-6">
     <button
       class="w-1/2 aspect-square bg-immich-bg rounded-3xl transition-all shadow-sm hover:shadow-xl border-[3px] border-immich-dark-primary/80 border-immich-primary dark:border dark:border-transparent"
-      on:click={handleToggleTheme}
+      on:click={() => ($colorTheme.value = Theme.LIGHT)}
     >
       <div
         class="flex flex-col place-items-center place-content-center justify-around h-full w-full text-immich-primary"
@@ -34,7 +35,7 @@
     </button>
     <button
       class="w-1/2 aspect-square bg-immich-dark-bg rounded-3xl dark:border-[3px] dark:border-immich-dark-primary/80 dark:border-immich-dark-primary border border-transparent"
-      on:click={handleToggleTheme}
+      on:click={() => ($colorTheme.value = Theme.DARK)}
     >
       <div
         class="flex flex-col place-items-center place-content-center justify-around h-full w-full text-immich-dark-primary"
