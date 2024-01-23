2068ccc64e04dc09192e1096ad3e225a62425256
Alex Tran <alex.tran1502@gmail.com>
2024-01-23 16:04:08 -0600

    filter box component

---
+++ web/src/lib/components/shared-components/search-bar/search-filter-box.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import Icon from '$lib/components/elements/icon.svelte';
+  import { savedSearchTerms } from '$lib/stores/search.store';
+  import { mdiMagnify, mdiClose } from '@mdi/js';
+  import { createEventDispatcher } from 'svelte';
+  import { fly } from 'svelte/transition';
+
+  const dispatch = createEventDispatcher<{
+    selectSearchTerm: string;
+    clearSearchTerm: string;
+    clearAllSearchTerms: void;
+  }>();
+</script>
+
+<div
+  transition:fly={{ y: 25, duration: 250 }}
+  class="absolute w-full rounded-b-3xl border border-gray-200 bg-white pb-5 shadow-2xl transition-all dark:border-gray-800 dark:bg-immich-dark-gray dark:text-gray-300 p-6"
+>
+  <p>FILTERS</p>
+  <hr />
+
+  <form>
+    <div class="flex py-3">
+      <label for="subject">Subject</label>
+      <input type="text" id="subject" name="subjet" />
+    </div>
+  </form>
+</div>
