d30ec7f8c1b913e0a938f45c026e13832cc01567
Alex Tran <alex.tran1502@gmail.com>
2024-01-25 13:50:33 -0600

    chore(web): sveltekit deployment

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
