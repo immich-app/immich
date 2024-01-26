c55779f99ef01132f5e22709c43c6f4709e7eb78
Jason Rasmussen <jrasm91@gmail.com>
2024-01-25 23:11:07 -0500

    chore: fix wrong rename

--- mobile/lib/modules/trash/providers/trashed_asset.provider.dart
+++ mobile/lib/modules/trash/providers/trashed_asset.provider.dart
@@ -77,7 +77,7 @@ class TrashNotifier extends StateNotifier<bool> {
 
   Future<bool> restoreAssets(Iterable<Asset> assetList) async {
     try {
-      final result = await _trashService.restoreAssetsDeprecated(assetList);
+      final result = await _trashService.restoreAssets(assetList);
 
       if (result) {
         final remoteAssets = assetList.where((a) => a.isRemote).toList();
