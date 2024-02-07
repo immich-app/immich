b57c9b617d5ba133a4cc20e812d804e9c9ea91ac
mertalev <101130780+mertalev@users.noreply.github.com>
2024-02-06 20:37:24 -0500

    handle different db name

---
+++ server/src/infra/migrations/1707000751533-AddVectorsToSearchPath.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddVectorsToSearchPath1707000751533 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const databaseName = await queryRunner.query(`SELECT current_database()`);
+    await queryRunner.query(`ALTER DATABASE ${databaseName} SET search_path TO "$user", public, vectors`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const databaseName = await queryRunner.query(`SELECT current_database()`);
+    await queryRunner.query(`ALTER DATABASE ${databaseName} SET search_path TO "$user", public`);
+  }
+}
