7b0ca46bf055c3fc6b933c52814013d8a4a4c4d4
mertalev <101130780+mertalev@users.noreply.github.com>
2024-02-06 21:02:43 -0500

    linting

--- server/src/infra/migrations/1707000751533-AddVectorsToSearchPath.ts
+++ server/src/infra/migrations/1707000751533-AddVectorsToSearchPath.ts
@@ -2,7 +2,8 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddVectorsToSearchPath1707000751533 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const databaseName = (await queryRunner.query(`SELECT current_database() as db`))[0]['db'];
+    const res = await queryRunner.query(`SELECT current_database() as db`);
+    const databaseName = res[0]['db'];
     await queryRunner.query(`ALTER DATABASE ${databaseName} SET search_path TO "$user", public, vectors`);
   }
 
