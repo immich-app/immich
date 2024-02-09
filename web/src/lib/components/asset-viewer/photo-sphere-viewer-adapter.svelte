0bf3b837c4c2c8ef77bab70f1aa6457d65935617
dmitry-brazhenko <brazhenko.dmitry@gmail.com>
2024-02-09 12:26:34 +0100

    panorama viewer fixes

---
+++ web/src/lib/components/asset-viewer/photo-sphere-viewer-adapter.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { Viewer } from '@photo-sphere-viewer/core';
+  import '@photo-sphere-viewer/core/index.css';
+  import { onDestroy, onMount } from 'svelte';
+
+  export let panorama: string;
+  let container: HTMLDivElement;
+  let viewer: Viewer;
+
+  onMount(() => {
+    viewer = new Viewer({
+      container,
+      panorama,
+      touchmoveTwoFingers: true,
+      mousewheelCtrlKey: false,
+      navbar: false 
+    });
+  });
+
+  onDestroy(() => {
+    if (viewer) {
+      viewer.destroy();
+    }
+  });
+</script>
+
+<div bind:this={container} />
+
+<style>
+  div {
+    width: 100%;
+    height: 100%;
+    margin-bottom: 0;
+  }
+</style>
