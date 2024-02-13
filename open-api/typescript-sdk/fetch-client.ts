a507c9f0e0df7ff236543244e3cbbd128db10966
Alex Tran <alex.tran1502@gmail.com>
2024-02-13 12:54:11 -0600

    openapi

--- open-api/typescript-sdk/fetch-client.ts
+++ open-api/typescript-sdk/fetch-client.ts
@@ -4,8 +4,8 @@
  * DO NOT MODIFY - This file has been generated using oazapfts.
  * See https://www.npmjs.com/package/oazapfts
  */
-import * as Oazapfts from "oazapfts/lib/runtime";
-import * as QS from "oazapfts/lib/runtime/query";
+import * as Oazapfts from "@oazapfts/runtime";
+import * as QS from "@oazapfts/runtime/query";
 export const defaults: Oazapfts.Defaults<Oazapfts.CustomHeaders> = {
     headers: {},
     baseUrl: "/api",
