15b50ac6d1f66149e3755e767f78d14ccc556702
Robert Vollmer <dev@robv.de>
2024-02-16 22:40:31 +0100

    Fix analyze errors

--- mobile/lib/extensions/response_extensions.dart
+++ mobile/lib/extensions/response_extensions.dart
@@ -2,5 +2,5 @@ import 'package:http/http.dart';
 
 extension LoggerExtension on Response {
   String toLoggerString() =>
-      "Status: ${statusCode} ${reasonPhrase}\n\n${body}";
+      "Status: $statusCode $reasonPhrase\n\n$body";
 }
