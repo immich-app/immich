9decabd942c06dc5ed2df2fa404d6673e7219a89
Marty Fuhry <martyfuhry@gmail.com>
2024-02-16 07:09:14 -0500

    removed unused import

--- mobile/lib/modules/home/ui/asset_grid/thumbnail_image.dart
+++ mobile/lib/modules/home/ui/asset_grid/thumbnail_image.dart
@@ -2,7 +2,6 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:immich_mobile/extensions/build_context_extensions.dart';
-import 'package:immich_mobile/modules/home/ui/asset_grid/thumbnail_placeholder.dart';
 import 'package:immich_mobile/routing/router.dart';
 import 'package:immich_mobile/shared/models/asset.dart';
 import 'package:immich_mobile/shared/ui/immich_image.dart';
