fdd52d6f7ff3932cef4debc4396a1e31d12cf737
mertalev <101130780+mertalev@users.noreply.github.com>
2024-02-17 23:31:45 -0500

    remove stack relation

--- server/src/immich/api-v1/asset/asset.service.ts
+++ server/src/immich/api-v1/asset/asset.service.ts
@@ -116,9 +116,9 @@ export class AssetService {
     await this.access.requirePermission(auth, Permission.TIMELINE_READ, userId);
     const assets = await this.assetRepository.getAllByFileCreationDate(
       { take: dto.take ?? 1000, skip: dto.skip },
-      { ...dto, withDeleted: true, orderDirection: 'DESC', withExif: true, withStacked: true },
+      { ...dto, withDeleted: true, orderDirection: 'DESC', withExif: true },
     );
-    return assets.items.map((asset) => mapAsset(asset, { withStack: true }));
+    return assets.items.map((asset) => mapAsset(asset));
   }
 
   async serveThumbnail(auth: AuthDto, assetId: string, dto: GetAssetThumbnailDto): Promise<ImmichFileResponse> {
