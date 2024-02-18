763aa10b177823ffb6fff5584fc1cbbb86017e4c
martabal <74269598+martabal@users.noreply.github.com>
2024-02-19 00:50:16 +0100

    fix: bash scripts

--- server/start-server.sh
+++ server/start-server.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
+
 ./start.sh immich
--- server/start.sh
+++ server/start.sh
@@ -1,42 +1,17 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-lib_path="/usr/lib/$(arch)-linux-gnu/libmimalloc.so.2"
-export LD_PRELOAD="$lib_path"
-
-if [ "$DB_URL_FILE" ]; then
-	DB_URL_CONTENT=$(cat "$DB_URL_FILE")
-	export DB_URL="$DB_URL_CONTENT"
-	unset DB_URL_FILE
-fi
-
-if [ "$DB_HOSTNAME_FILE" ]; then
-	DB_HOSTNAME_CONTENT=$(cat "$DB_HOSTNAME_FILE")
-	export DB_HOSTNAME="$DB_HOSTNAME_CONTENT"
-	unset DB_HOSTNAME_FILE
-fi
-
-if [ "$DB_DATABASE_NAME_FILE" ]; then
-	DB_DATABASE_CONTENT=$(cat "$DB_DATABASE_NAME_FILE")
-	export DB_DATABASE_NAME="$DB_DATABASE_CONTENT"
-	unset DB_DATABASE_NAME_FILE
-fi
-
-if [ "$DB_USERNAME_FILE" ]; then
-	DB_USERNAME_CONTENT=$(cat "$DB_USERNAME_FILE")
-	export DB_USERNAME="$DB_USERNAME_CONTENT"
-	unset DB_USERNAME_FILE
-fi
-
-if [ "$DB_PASSWORD_FILE" ]; then
-	DB_PASSWORD_CONTENT=$(cat "$DB_PASSWORD_FILE")
-	export DB_PASSWORD="$DB_PASSWORD_CONTENT"
-	unset DB_PASSWORD_FILE
-fi
-
-if [ "$REDIS_PASSWORD_FILE" ]; then
-	REDIS_PASSWORD_CONTENT=$(cat "$REDIS_PASSWORD_FILE")
-	export DB_PASSWORD="$REDIS_PASSWORD_CONTENT"
-	unset REDIS_PASSWORD_FILE
-fi
+read_file_and_export() {
+	if [ -n "${!1}" ]; then
+		content="$(cat "${!1}")"
+		export "$2"="${content}"
+		unset "$1"
+	fi
+}
+read_file_and_export "DB_URL_FILE" "DB_URL"
+read_file_and_export "DB_HOSTNAME_FILE" "DB_HOSTNAME"
+read_file_and_export "DB_DATABASE_NAME_FILE" "DB_DATABASE_NAME"
+read_file_and_export "DB_USERNAME_FILE" "DB_USERNAME"
+read_file_and_export "DB_PASSWORD_FILE" "DB_PASSWORD"
+read_file_and_export "REDIS_PASSWORD_FILE" "REDIS_PASSWORD"
 
 exec node /usr/src/app/dist/main "$@"
