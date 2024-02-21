2ebb57cbd40510be988e0aa1185d3a77f75690cc
Jason Rasmussen <jrasm91@gmail.com>
2024-02-21 17:34:11 -0500

    refactor: e2e client (#7324)

--- server/e2e/client/index.ts
+++ server/e2e/client/index.ts
@@ -1,25 +1,15 @@
-import { activityApi } from './activity-api';
-import { albumApi } from './album-api';
-import { apiKeyApi } from './api-key-api';
 import { assetApi } from './asset-api';
 import { authApi } from './auth-api';
 import { libraryApi } from './library-api';
-import { partnerApi } from './partner-api';
-import { serverInfoApi } from './server-info-api';
 import { sharedLinkApi } from './shared-link-api';
 import { trashApi } from './trash-api';
 import { userApi } from './user-api';
 
 export const api = {
-  activityApi,
   authApi,
-  apiKeyApi,
   assetApi,
   libraryApi,
-  serverInfoApi,
   sharedLinkApi,
   trashApi,
-  albumApi,
   userApi,
-  partnerApi,
 };
