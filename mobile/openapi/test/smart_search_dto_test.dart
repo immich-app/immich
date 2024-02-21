986c8b06f07e265e3b00bbf24ccb0d8d981a3d45
mertalev <101130780+mertalev@users.noreply.github.com>
2024-02-20 22:55:35 -0500

    update api

--- mobile/openapi/test/smart_search_dto_test.dart
+++ mobile/openapi/test/smart_search_dto_test.dart
@@ -156,7 +156,7 @@ void main() {
       // TODO
     });
 
-    // bool withArchived
+    // bool withArchived (default value: false)
     test('to test the property `withArchived`', () async {
       // TODO
     });
