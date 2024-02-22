c3c692aad8460d81894d2615349d7e738d302b02
Alex Tran <alex.tran1502@gmail.com>
2024-02-22 09:24:41 -0600

    Revert "Revert "fix(server): on_asset_update event sends varying data types (#7179)""
    This reverts commit 26c3635291e9479e0bb6e931228c618ff28644d9.

--- server/src/domain/repositories/communication.repository.ts
+++ server/src/domain/repositories/communication.repository.ts
@@ -1,3 +1,5 @@
+import { AssetResponseDto, ReleaseNotification, ServerVersionResponseDto } from '@app/domain';
+
 export const ICommunicationRepository = 'ICommunicationRepository';
 
 export enum ClientEvent {
@@ -7,6 +9,7 @@ export enum ClientEvent {
   ASSET_UPDATE = 'on_asset_update',
   ASSET_HIDDEN = 'on_asset_hidden',
   ASSET_RESTORE = 'on_asset_restore',
+  ASSET_STACK_UPDATE = 'on_asset_stack_update',
   PERSON_THUMBNAIL = 'on_person_thumbnail',
   SERVER_VERSION = 'on_server_version',
   CONFIG_UPDATE = 'on_config_update',
@@ -17,12 +20,26 @@ export enum ServerEvent {
   CONFIG_UPDATE = 'config:update',
 }
 
+export interface ClientEventMap {
+  [ClientEvent.UPLOAD_SUCCESS]: AssetResponseDto;
+  [ClientEvent.ASSET_DELETE]: string;
+  [ClientEvent.ASSET_TRASH]: string[];
+  [ClientEvent.ASSET_UPDATE]: AssetResponseDto;
+  [ClientEvent.ASSET_HIDDEN]: string;
+  [ClientEvent.ASSET_RESTORE]: string[];
+  [ClientEvent.ASSET_STACK_UPDATE]: string[];
+  [ClientEvent.PERSON_THUMBNAIL]: string;
+  [ClientEvent.SERVER_VERSION]: ServerVersionResponseDto;
+  [ClientEvent.CONFIG_UPDATE]: Record<string, never>;
+  [ClientEvent.NEW_RELEASE]: ReleaseNotification;
+}
+
 export type OnConnectCallback = (userId: string) => Promise<void>;
 export type OnServerEventCallback = () => Promise<void>;
 
 export interface ICommunicationRepository {
-  send(event: ClientEvent, userId: string, data: any): void;
-  broadcast(event: ClientEvent, data: any): void;
+  send<E extends keyof ClientEventMap>(event: E, userId: string, data: ClientEventMap[E]): void;
+  broadcast<E extends keyof ClientEventMap>(event: E, data: ClientEventMap[E]): void;
   on(event: 'connect', callback: OnConnectCallback): void;
   on(event: ServerEvent, callback: OnServerEventCallback): void;
   sendServerEvent(event: ServerEvent): void;
