4db49e9e1db586fe8f89068863f7f93f92ed9295
Michel Heusschen <59014050+michelheusschen@users.noreply.github.com>
2024-02-24 19:10:45 +0100

    fix(web): fetch error reporting

--- web/src/hooks.client.ts
+++ web/src/hooks.client.ts
@@ -1,34 +1,22 @@
+import { isHttpError } from '@immich/sdk';
 import type { HandleClientError } from '@sveltejs/kit';
-import type { AxiosError, AxiosResponse } from 'axios';
 
 const LOG_PREFIX = '[hooks.client.ts]';
 const DEFAULT_MESSAGE = 'Hmm, not sure about that. Check the logs or open a ticket?';
 
 const parseError = (error: unknown) => {
-  const httpError = error as AxiosError;
-  const request = httpError?.request as Request & { path: string };
-  const response = httpError?.response as AxiosResponse<{
-    message: string;
-    statusCode: number;
-    error: string;
-  }>;
+  const httpError = isHttpError(error) ? error : undefined;
+  const statusCode = httpError?.status || httpError?.data?.statusCode || 500;
+  const message = httpError?.data?.message || (httpError?.data && String(httpError.data)) || httpError?.message;
 
-  let code = response?.data?.statusCode || response?.status || httpError.code || '500';
-  if (response) {
-    code += ` - ${response.data?.error || response.statusText}`;
-  }
-
-  if (request && response) {
-    console.log({
-      status: response.status,
-      url: `${request.method} ${request.path}`,
-      response: response.data || 'No data',
-    });
-  }
+  console.log({
+    status: statusCode,
+    response: httpError?.data || 'No data',
+  });
 
   return {
-    message: response?.data?.message || httpError?.message || DEFAULT_MESSAGE,
-    code,
+    message: message || DEFAULT_MESSAGE,
+    code: statusCode,
     stack: httpError?.stack,
   };
 };
