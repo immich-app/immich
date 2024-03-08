0e13c9d696709831d575d731c1a87911ae2af32f
Andrew Roberts <andrew@azroberts.com>
2024-03-08 10:15:11 -0500

    Fixed dark mode password reset success

--- web/src/routes/admin/user-management/+page.svelte
+++ web/src/routes/admin/user-management/+page.svelte
@@ -140,13 +140,14 @@
 
       {#if shouldShowInfoPanel}
         <FullScreenModal onClose={() => (shouldShowInfoPanel = false)}>
-          <div class="w-[500px] max-w-[95vw] rounded-3xl border bg-white p-8 text-sm shadow-sm">
-            <h1 class="mb-4 text-lg font-medium text-immich-primary">Password reset success</h1>
+          <div class="w-[500px] max-w-[95vw] rounded-3xl bg-immich-bg p-8 text-immich-fg shadow-sm dark:bg-immich-dark-gray dark:text-immich-dark-fg">
+            <h1 class="mb-4 text-2xl font-medium text-immich-primary dark:text-immich-dark-primary">Password reset success</h1>
 
             <p>
               The user's password has been reset to the default <code
-                class="rounded-md bg-gray-200 px-2 py-1 font-bold text-immich-primary">password</code
+                class="rounded-md bg-gray-200 px-2 py-1 font-bold text-immich-primary dark:text-immich-dark-primary dark:bg-gray-700">password</code
               >
+              <br />
               <br />
               Please inform the user, and they will need to change the password at the next log-on.
             </p>
