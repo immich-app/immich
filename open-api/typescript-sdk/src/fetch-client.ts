c3931d9500431896a216031b248416c4ef80a010
Alex Tran <alex.tran1502@gmail.com>
2024-03-09 13:57:37 -0600

    openapi

--- open-api/typescript-sdk/src/fetch-client.ts
+++ open-api/typescript-sdk/src/fetch-client.ts
@@ -2210,7 +2210,7 @@ export function searchMetadata({ metadataSearchDto }: {
     metadataSearchDto: MetadataSearchDto;
 }, opts?: Oazapfts.RequestOpts) {
     return oazapfts.ok(oazapfts.fetchJson<{
-        status: 201;
+        status: 200;
         data: SearchResponseDto;
     }>("/search/metadata", oazapfts.json({
         ...opts,
@@ -2248,7 +2248,7 @@ export function searchSmart({ smartSearchDto }: {
     smartSearchDto: SmartSearchDto;
 }, opts?: Oazapfts.RequestOpts) {
     return oazapfts.ok(oazapfts.fetchJson<{
-        status: 201;
+        status: 200;
         data: SearchResponseDto;
     }>("/search/smart", oazapfts.json({
         ...opts,
