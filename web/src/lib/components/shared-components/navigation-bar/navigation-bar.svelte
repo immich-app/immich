db1282d625ee86b3c6cd860896325ab8037dd8bb
Alex Tran <alex.tran1502@gmail.com>
2024-03-09 15:20:11 -0600

    feat(web): update logo

--- web/src/lib/components/shared-components/navigation-bar/navigation-bar.svelte
+++ web/src/lib/components/shared-components/navigation-bar/navigation-bar.svelte
@@ -44,11 +44,8 @@
   <div
     class="grid h-full grid-cols-[theme(spacing.18)_auto] items-center border-b bg-immich-bg py-2 dark:border-b-immich-dark-gray dark:bg-immich-dark-bg md:grid-cols-[theme(spacing.64)_auto]"
   >
-    <a data-sveltekit-preload-data="hover" class="mx-4 flex place-items-center gap-2 md:mx-6" href={AppRoute.PHOTOS}>
-      <ImmichLogo height="35" width="35" />
-      <h1 class="hidden font-immich-title text-2xl text-immich-primary dark:text-immich-dark-primary md:block">
-        IMMICH
-      </h1>
+    <a data-sveltekit-preload-data="hover" class="ml-4" href={AppRoute.PHOTOS}>
+      <ImmichLogo width="55%" />
     </a>
     <div class="flex justify-between gap-16 pr-6">
       <div class="hidden w-full max-w-5xl flex-1 pl-4 sm:block">
