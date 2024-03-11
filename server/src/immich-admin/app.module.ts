846a236e5bf6d0977978a5f45184745e0c791acb
Dhrumil Shah <ddshd@users.noreply.github.com>
2024-03-11 01:48:45 -0400

    format fix

--- server/src/immich-admin/app.module.ts
+++ server/src/immich-admin/app.module.ts
@@ -2,9 +2,9 @@ import { DomainModule } from '@app/domain';
 import { InfraModule } from '@app/infra';
 import { Module } from '@nestjs/common';
 import { ListUsersCommand } from './commands/list-users.command';
+import { DisableOauthAutoRedirect, EnableOauthAutoRedirect } from './commands/oauth-redirect';
 import { DisablePasswordLoginCommand, EnablePasswordLoginCommand } from './commands/password-login';
 import { PromptPasswordQuestions, ResetAdminPasswordCommand } from './commands/reset-admin-password.command';
-import { DisableOauthAutoRedirect, EnableOauthAutoRedirect } from './commands/oauth-redirect';
 
 @Module({
   imports: [InfraModule, DomainModule],
