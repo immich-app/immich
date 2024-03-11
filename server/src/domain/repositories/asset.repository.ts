5bd597f14bf920d83e6193fae47ab841d49c2680
Mert <101130780+mertalev@users.noreply.github.com>
2024-03-10 22:30:57 -0400

    fix(server): external library sync not working for large libraries (#7759)

--- server/src/domain/repositories/asset.repository.ts
+++ server/src/domain/repositories/asset.repository.ts
@@ -109,6 +109,8 @@ export interface MetadataSearchOptions {
   numResults: number;
 }
 
+export type AssetPathEntity = Pick<AssetEntity, 'id' | 'originalPath' | 'isOffline'>;
+
 export const IAssetRepository = 'IAssetRepository';
 
 export interface IAssetRepository {
@@ -129,10 +131,8 @@ export interface IAssetRepository {
   getRandom(userId: string, count: number): Promise<AssetEntity[]>;
   getFirstAssetForAlbumId(albumId: string): Promise<AssetEntity | null>;
   getLastUpdatedAssetForAlbumId(albumId: string): Promise<AssetEntity | null>;
-  getByLibraryId(libraryIds: string[]): Promise<AssetEntity[]>;
+  getLibraryAssetPaths(pagination: PaginationOptions, libraryId: string): Paginated<AssetPathEntity>;
   getByLibraryIdAndOriginalPath(libraryId: string, originalPath: string): Promise<AssetEntity | null>;
-  getPathsNotInLibrary(libraryId: string, originalPaths: string[]): Promise<string[]>;
-  updateOfflineLibraryAssets(libraryId: string, originalPaths: string[]): Promise<void>;
   deleteAll(ownerId: string): Promise<void>;
   getAll(pagination: PaginationOptions, options?: AssetSearchOptions): Paginated<AssetEntity>;
   getAllByDeviceId(userId: string, deviceId: string): Promise<string[]>;
