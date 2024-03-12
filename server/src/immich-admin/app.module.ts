11b8f5453756fdd6d1385e28b3a91cc4b3624070
Dhrumil Shah <ddshd@users.noreply.github.com>
2024-03-11 23:24:01 -0400

    Fix filename

--- server/src/immich-admin/app.module.ts
+++ server/src/immich-admin/app.module.ts
@@ -2,7 +2,7 @@ import { DomainModule } from '@app/domain';
 import { InfraModule } from '@app/infra';
 import { Module } from '@nestjs/common';
 import { ListUsersCommand } from './commands/list-users.command';
-import { DisableOAuthLogin, EnableOAuthLogin } from './commands/oauth-redirect';
+import { DisableOAuthLogin, EnableOAuthLogin } from './commands/oauth-login';
 import { DisablePasswordLoginCommand, EnablePasswordLoginCommand } from './commands/password-login';
 import { PromptPasswordQuestions, ResetAdminPasswordCommand } from './commands/reset-admin-password.command';
 
