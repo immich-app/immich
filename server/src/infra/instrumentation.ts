f1595fe450c7203fc8d3196291381086602cadbc
mertalev <101130780+mertalev@users.noreply.github.com>
2024-03-12 01:08:21 -0400

    use more specific env

--- server/src/infra/instrumentation.ts
+++ server/src/infra/instrumentation.ts
@@ -17,22 +17,23 @@ import { performance } from 'node:perf_hooks';
 import { excludePaths } from './infra.config';
 import { DecorateAll } from './infra.utils';
 
+let metricsEnabled = process.env.IMMICH_METRICS === 'true';
+const hostMetrics =
+  process.env.IMMICH_HOST_METRICS != null ? process.env.IMMICH_HOST_METRICS === 'true' : metricsEnabled;
+const apiMetrics = process.env.IMMICH_API_METRICS != null ? process.env.IMMICH_API_METRICS === 'true' : metricsEnabled;
+const repoMetrics = process.env.IMMICH_IO_METRICS != null ? process.env.IMMICH_IO_METRICS === 'true' : metricsEnabled;
+
+metricsEnabled ||= hostMetrics || apiMetrics || repoMetrics;
+if (!metricsEnabled && process.env.OTEL_SDK_DISABLED === undefined) {
+  process.env.OTEL_SDK_DISABLED = 'true';
+}
+
 const aggregation = new ExplicitBucketHistogramAggregation(
   [0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10, 25, 50, 75, 100, 250, 500, 750, 1000, 2500, 5000, 7500, 10_000],
   true,
 );
 
 const metricsPort = Number.parseInt(process.env.IMMICH_METRICS_PORT ?? '8081');
-
-let metricsEnabled = process.env.IMMICH_METRICS === 'true';
-const hostMetrics = metricsEnabled || process.env.IMMICH_HOST_METRICS === 'true';
-const apiMetrics = metricsEnabled || process.env.IMMICH_API_METRICS === 'true';
-const repoMetrics = metricsEnabled || process.env.IMMICH_IO_METRICS === 'true';
-
-metricsEnabled ||= hostMetrics || apiMetrics || repoMetrics;
-if (!metricsEnabled && process.env.OTEL_SDK_DISABLED === undefined) {
-  process.env.OTEL_SDK_DISABLED = 'true';
-}
 
 export const otelSDK = new NodeSDK({
   resource: new Resource({
