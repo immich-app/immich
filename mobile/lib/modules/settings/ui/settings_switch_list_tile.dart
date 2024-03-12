7489db9481ddd934e77974c5210474d34c78b5b3
shenlong <139912620+shenlong-tanwen@users.noreply.github.com>
2024-03-12 14:56:08 +0000

    refactor(mobile): app settings (#7749)
    * refactor(mobile): app settings
    
    * Font size
    
    * refactor(mobile): backup settings ui (#7771)
    
    * refactor: SettingsButtonListTile
    
    * refactor: Backup settings to App settings
    
    ---------
    
    Co-authored-by: shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
    
    * fix: invalidate appsettingsprovider on timeline setting change
    
    * styling
    
    ---------
    
    Co-authored-by: shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
    Co-authored-by: Alex Tran <alex.tran1502@gmail.com>

--- mobile/lib/modules/settings/ui/settings_switch_list_tile.dart
+++ mobile/lib/modules/settings/ui/settings_switch_list_tile.dart
@@ -1,51 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:immich_mobile/extensions/build_context_extensions.dart';
-import 'package:immich_mobile/modules/settings/services/app_settings.service.dart';
 
 class SettingsSwitchListTile extends StatelessWidget {
-  final AppSettingsService appSettingService;
   final ValueNotifier<bool> valueNotifier;
-  final AppSettingsEnum settingsEnum;
   final String title;
   final bool enabled;
   final String? subtitle;
+  final IconData? icon;
   final Function(bool)? onChanged;
 
-  SettingsSwitchListTile({
-    required this.appSettingService,
+  const SettingsSwitchListTile({
     required this.valueNotifier,
-    required this.settingsEnum,
     required this.title,
     this.subtitle,
+    this.icon,
     this.enabled = true,
     this.onChanged,
-  }) : super(key: Key(settingsEnum.name));
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
+    void onSwitchChanged(bool value) {
+      if (!enabled) return;
+
+      valueNotifier.value = value;
+      onChanged?.call(value);
+    }
+
     return SwitchListTile.adaptive(
+      contentPadding: const EdgeInsets.symmetric(horizontal: 20),
       selectedTileColor: enabled ? null : context.themeData.disabledColor,
       value: valueNotifier.value,
-      onChanged: (bool value) {
-        if (enabled) {
-          valueNotifier.value = value;
-          appSettingService.setSetting(settingsEnum, value);
-        }
-        if (onChanged != null) {
-          onChanged!(value);
-        }
-      },
+      onChanged: onSwitchChanged,
       activeColor:
           enabled ? context.primaryColor : context.themeData.disabledColor,
       dense: true,
+      secondary: icon != null
+          ? Icon(
+              icon!,
+              color: valueNotifier.value ? context.primaryColor : null,
+            )
+          : null,
       title: Text(
         title,
-        style: context.textTheme.titleSmall,
+        style: context.textTheme.bodyLarge?.copyWith(
+          fontWeight: FontWeight.w500,
+          color: enabled ? null : context.themeData.disabledColor,
+          height: 1.5,
+        ),
       ),
       subtitle: subtitle != null
           ? Text(
               subtitle!,
-              style: context.textTheme.bodyMedium,
+              style: context.textTheme.bodyMedium?.copyWith(
+                color: enabled ? null : context.themeData.disabledColor,
+              ),
             )
           : null,
     );
