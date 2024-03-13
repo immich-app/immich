37e5b91dc29da67575e4584a6645bbebfa91d08e
Andrew Rowson <github@growse.com>
2024-03-13 17:13:56 +0000

    fix(server): ml gunicorn listen on ipv4 and ipv6 by default (#7930)

--- machine-learning/start.sh
+++ machine-learning/start.sh
@@ -4,7 +4,7 @@ lib_path="/usr/lib/$(arch)-linux-gnu/libmimalloc.so.2"
 export LD_PRELOAD="$lib_path"
 export LD_BIND_NOW=1
 
-: "${MACHINE_LEARNING_HOST:=0.0.0.0}"
+: "${MACHINE_LEARNING_HOST:=[::]}"
 : "${MACHINE_LEARNING_PORT:=3003}"
 : "${MACHINE_LEARNING_WORKERS:=1}"
 : "${MACHINE_LEARNING_WORKER_TIMEOUT:=120}"
