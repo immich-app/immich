33418def0b5d50616ac464ef7f708e21851144ff
mertalev <101130780+mertalev@users.noreply.github.com>
2024-03-12 22:07:19 -0400

    add indices to entities
    remove index from person entity
    
    add to face entity
    
    fix

--- server/src/infra/migrations/1710293990203-AddAssetRelationIndices.ts
+++ server/src/infra/migrations/1710293990203-AddAssetRelationIndices.ts
@@ -2,14 +2,14 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class AddAssetRelationIndices1710293990203 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        queryRunner.query(`CREATE INDEX IDX_asset_id_stackId on assets ("id", "stackId")`);
-        queryRunner.query(`CREATE INDEX IDX_tag_asset_assetsId_tagsId on tag_asset ("assetsId", "tagsId")`);
-        queryRunner.query(`CREATE INDEX IDX_asset_faces_assetId_personId on asset_faces ("assetId", "personId")`);
+        queryRunner.query(`CREATE INDEX "IDX_asset_id_stackId" on assets ("id", "stackId")`);
+        queryRunner.query(`CREATE INDEX "IDX_tag_asset_assetsId_tagsId" on tag_asset ("assetsId", "tagsId")`);
+        queryRunner.query(`CREATE INDEX "IDX_asset_faces_assetId_personId" on asset_faces ("assetId", "personId")`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        queryRunner.query(`DROP INDEX IDX_asset_id_stackId on assets ("id", "stackId")`);
-        queryRunner.query(`DROP INDEX IDX_tag_asset_assetsId_tagsId on tag_asset ("assetsId", "tagsId")`);
-        queryRunner.query(`DROP INDEX IDX_asset_faces_assetId_personId on asset_faces ("assetId", "personId")`);
+        queryRunner.query(`DROP INDEX "IDX_asset_id_stackId" on assets ("id", "stackId")`);
+        queryRunner.query(`DROP INDEX "IDX_tag_asset_assetsId_tagsId" on tag_asset ("assetsId", "tagsId")`);
+        queryRunner.query(`DROP INDEX "IDX_asset_faces_assetId_personId" on asset_faces ("assetId", "personId")`);
     }
 }
