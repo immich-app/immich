b278e761a9594dea5f2113691352d443e87ca989
Alex Tran <alex.tran1502@gmail.com>
2024-03-20 09:08:53 -0500

    remove unused code

--- web/src/lib/components/photos-page/asset-grid.svelte
+++ web/src/lib/components/photos-page/asset-grid.svelte
@@ -8,7 +8,7 @@
   import { isSearchEnabled } from '$lib/stores/search.store';
   import { featureFlags } from '$lib/stores/server-config.store';
   import { deleteAssets } from '$lib/utils/actions';
-  import { shortcuts, type ShortcutOptions, matchesShortcut } from '$lib/utils/shortcut';
+  import { shortcuts, type ShortcutOptions } from '$lib/utils/shortcut';
   import { formatGroupTitle, splitBucketIntoDateGroups } from '$lib/utils/timeline-util';
   import type { AlbumResponseDto, AssetResponseDto } from '@immich/sdk';
   import { DateTime } from 'luxon';
