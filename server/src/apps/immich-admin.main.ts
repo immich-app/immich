e7c15c846aca518517ec6d74585b1aab684f63b2
Jason Rasmussen <jrasm91@gmail.com>
2024-03-21 09:25:59 -0400

    refactor: app modules, main.ts

--- server/src/apps/immich-admin.main.ts
+++
@@ -1,8 +0,0 @@
-import { CommandFactory } from 'nest-commander';
-import { ImmichAdminModule } from 'src/apps/immich-admin.module';
-import { LogLevel } from 'src/entities/system-config.entity';
-
-export async function bootstrapImmichAdmin() {
-  process.env.LOG_LEVEL = LogLevel.WARN;
-  await CommandFactory.run(ImmichAdminModule);
-}
