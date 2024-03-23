604b8ff17cb72693e47922da530280533853a6aa
Mert <101130780+mertalev@users.noreply.github.com>
2024-03-23 00:20:16 -0400

    chore(server): remove getByDate from asset repo (#8211)
    * remove getByDate
    
    * remove unused import

--- server/src/repositories/asset.repository.ts
+++ server/src/repositories/asset.repository.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { DateTime } from 'luxon';
 import path from 'node:path';
 import { Chunked, ChunkedArray, DummyValue, GenerateSql } from 'src/decorators';
 import { AssetOrder } from 'src/entities/album.entity';
@@ -74,41 +73,6 @@ export class AssetRepository implements IAssetRepository {
 
   create(asset: AssetCreate): Promise<AssetEntity> {
     return this.repository.save(asset);
-  }
-
-  @GenerateSql({ params: [DummyValue.UUID, DummyValue.DATE] })
-  getByDate(ownerId: string, date: Date): Promise<AssetEntity[]> {
-    // For reference of a correct approach although slower
-
-    // let builder = this.repository
-    //   .createQueryBuilder('asset')
-    //   .leftJoin('asset.exifInfo', 'exifInfo')
-    //   .where('asset.ownerId = :ownerId', { ownerId })
-    //   .andWhere(
-    //     `coalesce(date_trunc('day', asset."fileCreatedAt", "exifInfo"."timeZone") at TIME ZONE "exifInfo"."timeZone", date_trunc('day', asset."fileCreatedAt")) IN (:date)`,
-    //     { date },
-    //   )
-    //   .andWhere('asset.isVisible = true')
-    //   .andWhere('asset.isArchived = false')
-    //   .orderBy('asset.fileCreatedAt', 'DESC');
-
-    // return builder.getMany();
-
-    return this.repository.find({
-      where: {
-        ownerId,
-        isVisible: true,
-        isArchived: false,
-        resizePath: Not(IsNull()),
-        fileCreatedAt: OptionalBetween(date, DateTime.fromJSDate(date).plus({ day: 1 }).toJSDate()),
-      },
-      relations: {
-        exifInfo: true,
-      },
-      order: {
-        fileCreatedAt: 'DESC',
-      },
-    });
   }
 
   @GenerateSql({ params: [DummyValue.UUID, { day: 1, month: 1 }] })
