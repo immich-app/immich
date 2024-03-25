ce55d0ffda76ed656fae1be9b3c10db22e4596f6
mertalev <101130780+mertalev@users.noreply.github.com>
2024-03-25 18:49:43 -0400

    refactor

--- server/src/repositories/metric.repository.ts
+++ server/src/repositories/metric.repository.ts
@@ -1,31 +1,75 @@
-import { Inject } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { MetricOptions } from '@opentelemetry/api';
 import { MetricService } from 'nestjs-otel';
-import { CustomMetricOptions, IMetricRepository } from 'src/interfaces/metric.interface';
+import { IMetricGroupRepository, IMetricRepository, MetricGroupOptions } from 'src/interfaces/metric.interface';
+import { apiMetrics, hostMetrics, jobMetrics, repoMetrics } from 'src/utils/instrumentation';
 
+class MetricGroupRepository implements IMetricGroupRepository {
+  private enabled = false;
+  constructor(private readonly metricService: MetricService) {}
+
+  addToCounter(name: string, value: number, options?: MetricOptions): void {
+    if (this.enabled) {
+      this.metricService.getCounter(name, options).add(value);
+    }
+  }
+
+  addToGauge(name: string, value: number, options?: MetricOptions): void {
+    if (this.enabled) {
+      this.metricService.getUpDownCounter(name, options).add(value);
+    }
+  }
+
+  addToHistogram(name: string, value: number, options?: MetricOptions): void {
+    if (this.enabled) {
+      this.metricService.getHistogram(name, options).record(value);
+    }
+  }
+
+  configure(options: MetricGroupOptions): void {
+    this.enabled = options.enabled;
+  }
+}
+
+class ApiMetricRepository extends MetricGroupRepository implements IMetricGroupRepository {
+  constructor(metricService: MetricService) {
+    super(metricService);
+    this.configure({ enabled: apiMetrics });
+  }
+}
+
+class HostMetricRepository extends MetricGroupRepository implements IMetricGroupRepository {
+  constructor(metricService: MetricService) {
+    super(metricService);
+    this.configure({ enabled: hostMetrics });
+  }
+}
+
+class JobMetricRepository extends MetricGroupRepository implements IMetricGroupRepository {
+  constructor(metricService: MetricService) {
+    super(metricService);
+    this.configure({ enabled: jobMetrics });
+  }
+}
+
+class RepoMetricRepository extends MetricGroupRepository implements IMetricGroupRepository {
+  constructor(metricService: MetricService) {
+    super(metricService);
+    this.configure({ enabled: repoMetrics });
+  }
+}
+
+@Injectable()
 export class MetricRepository implements IMetricRepository {
-  constructor(@Inject(MetricService) private readonly metricService: MetricService) {}
+  api: ApiMetricRepository;
+  host: HostMetricRepository;
+  jobs: JobMetricRepository;
+  repo: RepoMetricRepository;
 
-  addToCounter(name: string, value: number, options?: CustomMetricOptions): void {
-    if (options?.enabled === false) {
-      return;
-    }
-
-    this.metricService.getCounter(name, options).add(value);
-  }
-
-  updateGauge(name: string, value: number, options?: CustomMetricOptions): void {
-    if (options?.enabled === false) {
-      return;
-    }
-
-    this.metricService.getUpDownCounter(name, options).add(value);
-  }
-
-  updateHistogram(name: string, value: number, options?: CustomMetricOptions): void {
-    if (options?.enabled === false) {
-      return;
-    }
-
-    this.metricService.getHistogram(name, options).record(value);
+  constructor(metricService: MetricService) {
+    this.api = new ApiMetricRepository(metricService);
+    this.host = new HostMetricRepository(metricService);
+    this.jobs = new JobMetricRepository(metricService);
+    this.repo = new RepoMetricRepository(metricService);
   }
 }
