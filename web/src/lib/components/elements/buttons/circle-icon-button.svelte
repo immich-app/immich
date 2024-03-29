508d0e2526ec063e8f8b7233eef0ac375b12f97f
Ben Basten <45583362+ben-basten@users.noreply.github.com>
2024-03-29 07:56:04 -0400

    fix: move required prop to the top of the list

--- web/src/lib/components/elements/buttons/circle-icon-button.svelte
+++ web/src/lib/components/elements/buttons/circle-icon-button.svelte
@@ -2,11 +2,11 @@
   import Icon from '$lib/components/elements/icon.svelte';
 
   export let icon: string;
+  export let title: string;
   export let backgroundColor = '';
   export let hoverColor = '#e2e7e9';
   export let padding = '3';
   export let size = '24';
-  export let title: string;
   export let isOpacity = false;
   export let forceDark = false;
   export let hideMobile = false;
