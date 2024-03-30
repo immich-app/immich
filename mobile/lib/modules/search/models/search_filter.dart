75ce4942011bf77256d8ddb3e656bae3b89b71a7
Alex Tran <alex.tran1502@gmail.com>
2024-03-30 14:11:25 -0500

    static analysis

--- mobile/lib/modules/search/models/search_filter.dart
+++ mobile/lib/modules/search/models/search_filter.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
-import 'package:collection/collection.dart';
 import 'package:immich_mobile/shared/models/asset.dart';
 import 'package:openapi/api.dart';
 
