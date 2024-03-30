935dfe7641dc1f7d08311018d8d73770aefc5928
Markus Klein <klein.markus90@gmx.de>
2024-03-30 17:23:34 +0100

    make asset viewer setting a stateless widget

--- mobile/lib/modules/settings/ui/asset_viewer_settings/asset_viewer_settings.dart
+++ mobile/lib/modules/settings/ui/asset_viewer_settings/asset_viewer_settings.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/modules/settings/ui/asset_viewer_settings/image_viewer_quality_setting.dart';
 import 'package:immich_mobile/modules/settings/ui/settings_sub_page_scaffold.dart';
 import 'video_viewer_settings.dart';
 
-class AssetViewerSettings extends HookConsumerWidget {
+class AssetViewerSettings extends StatelessWidget {
   const AssetViewerSettings({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final assetViewerSetting = [
       const ImageViewerQualitySetting(),
       const VideoViewerSettings(),
