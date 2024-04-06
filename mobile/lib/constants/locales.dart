c915d1d3135b16e1994e71eb9081310915ed7ce5
Alex Tran <alex.tran1502@gmail.com>
2024-04-06 14:11:19 -0500

    add additional locale

--- mobile/lib/constants/locales.dart
+++ mobile/lib/constants/locales.dart
@@ -4,80 +4,90 @@ const List<Locale> locales = [
   // Default locale
   Locale('en', 'US'),
   // Additional locales
-  Locale('de', 'DE'),
-  Locale('da', 'DK'),
-  Locale('it', 'IT'),
-  Locale('es', 'ES'),
-  Locale('vi', 'VN'),
-  Locale('fr', 'CA'),
-  Locale('fr', 'FR'),
-  Locale('ja', 'JP'),
-  Locale('pl', 'PL'),
-  Locale('fi', 'FI'),
-  Locale('pt', 'PT'),
-  Locale('cs', 'CZ'),
-  Locale('uk', 'UA'),
-  Locale('ru', 'RU'),
-  Locale('zh', 'CN'),
-  Locale('sk', 'SK'),
-  Locale('nl', 'NL'),
-  Locale('nb', 'NO'),
-  Locale('sv', 'SE'),
-  Locale('mn', 'MN'),
-  Locale('ko', 'KR'),
-  Locale('sr', 'Latn'),
-  Locale('sr', 'Cyrl'),
-  Locale('hi', 'IN'),
-  Locale('es', 'PE'),
-  Locale('es', 'MX'),
-  Locale('es', 'US'),
-  Locale('sv', 'FI'),
-  Locale('ca', 'CA'),
-  Locale('hu', 'HU'),
-  Locale('lv', 'LV'),
-  Locale('zh', 'Hans'),
-  Locale('th', 'TH'),
-  Locale('sl', 'SI'),
+  Locale('ar', 'JO'), // Arabic (Jordan)
+  Locale('ca', 'CA'), // Catalan
+  Locale('cs', 'CZ'), // Czech
+  Locale('da', 'DK'), // Danish
+  Locale('de', 'DE'), // German
+  Locale('el', 'GR'), // Greek (GR)
+  Locale('es', 'ES'), // Spanish (ES)
+  Locale('es', 'MX'), // Spanish (MX)
+  Locale('es', 'PE'), // Spanish (PE)
+  Locale('es', 'US'), // Spanish (US)
+  Locale('fi', 'FI'), // Finnish
+  Locale('fr', 'CA'), // French (CA)
+  Locale('fr', 'FR'), // French
+  Locale('he', 'IL'), // Hebrew (IL)
+  Locale('hi', 'IN'), // Hindi
+  Locale('hu', 'HU'), // Hungarian
+  Locale('it', 'IT'), // Italian
+  Locale('ja', 'JP'), // Japanese
+  Locale('ko', 'KR'), // Korean
+  Locale('lt', 'LT'), // Lithuanian
+  Locale('lv', 'LV'), // Latvian
+  Locale('mn', 'MN'), // Mongolian
+  Locale('nb', 'NO'), // Norwegian Bokmål
+  Locale('nl', 'NL'), // Dutch
+  Locale('pl', 'PL'), // Polish
+  Locale('pt', 'PT'), // Portuguese
+  Locale('ro', 'RO'), // Romanian (RO)
+  Locale('ru', 'RU'), // Russian
+  Locale('sk', 'SK'), // Slovak
+  Locale('sl', 'SI'), // Slovenian
+  Locale('sr', 'Cyrl'), // Serbian Cyrillic
+  Locale('sr', 'Latn'), // Serbian Latin
+  Locale('sv', 'FI'), // Swedish (FI)
+  Locale('sv', 'SE'), // Swedish (SE)
+  Locale('th', 'TH'), // Thai (TH)
+  Locale('uk', 'UA'), // Ukrainian (UA)
+  Locale('vi', 'VN'), // Vietnamese (VN)
+  Locale('zh', 'CN'), // Chinese (CN)
+  Locale('zh', 'Hans'), // Chinese Simplified
 ];
 
 const List<Map<String, Locale>> localeWithLanguageName = [
   // Default locale
   {'English (US)': Locale('en', 'US')},
   // Additional locales
-  {'German (DE)': Locale('de', 'DE')},
-  {'': Locale('da', 'DK')},
-  {'Italian (IT)': Locale('it', 'IT')},
-  {'': Locale('es', 'ES')},
-  {'': Locale('vi', 'VN')},
-  {'': Locale('fr', 'CA')},
-  {'': Locale('fr', 'FR')},
-  {'': Locale('ja', 'JP')},
-  {'': Locale('pl', 'PL')},
-  {'': Locale('fi', 'FI')},
-  {'': Locale('pt', 'PT')},
-  {'': Locale('cs', 'CZ')},
-  {'': Locale('uk', 'UA')},
-  {'': Locale('ru', 'RU')},
-  {'': Locale('zh', 'CN')},
-  {'': Locale('sk', 'SK')},
+  {'Arabic (Jordan)': Locale('ar', 'JO')},
+  {'Catalan': Locale('ca', 'CA')},
+  {'Chinese (CN)': Locale('zh', 'CN')},
+  {'Chinese Simplified': Locale('zh', 'Hans')},
+  {'Czech (CZ)': Locale('cs', 'CZ')},
+  {'Danish (DK)': Locale('da', 'DK')},
   {'Dutch (NL)': Locale('nl', 'NL')},
-  {'': Locale('nb', 'NO')},
-  {'': Locale('sv', 'SE')},
-  {'': Locale('mn', 'MN')},
-  {'': Locale('ko', 'KR')},
-  {'': Locale('sr', 'Latn')},
-  {'': Locale('sr', 'Cyrl')},
-  {'': Locale('hi', 'IN')},
-  {'': Locale('es', 'PE')},
-  {'': Locale('es', 'MX')},
-  {'': Locale('es', 'US')},
-  {'': Locale('sv', 'FI')},
-  {'': Locale('ca', 'CA')},
-  {'': Locale('hu', 'HU')},
-  {'': Locale('lv', 'LV')},
-  {'': Locale('zh', 'Hans')},
-  {'': Locale('th', 'TH')},
-  {'': Locale('sl', 'SI')},
+  {'Finnish (FI)': Locale('fi', 'FI')},
+  {'French (CA)': Locale('fr', 'CA')},
+  {'French (FR)': Locale('fr', 'FR')},
+  {'German (DE)': Locale('de', 'DE')},
+  {'Greek (GR)': Locale('el', 'GR')},
+  {'Hebrew (IL)': Locale('he', 'IL')},
+  {'Hindi (IN)': Locale('hi', 'IN')},
+  {'Hungarian (HU)': Locale('hu', 'HU')},
+  {'Italian (IT)': Locale('it', 'IT')},
+  {'Japanese (JP)': Locale('ja', 'JP')},
+  {'Korean (KR)': Locale('ko', 'KR')},
+  {'Latvian (LV)': Locale('lv', 'LV')},
+  {'Lithuanian (LT)': Locale('lt', 'LT')},
+  {'Mongolian': Locale('mn', 'MN')},
+  {'Norwegian Bokmål (NO)': Locale('nb', 'NO')},
+  {'Polish (PL)': Locale('pl', 'PL')},
+  {'Portuguese (PT)': Locale('pt', 'PT')},
+  {'Romanian (RO)': Locale('ro', 'RO')},
+  {'Russian (RU)': Locale('ru', 'RU')},
+  {'Serbian Cyrillic': Locale('sr', 'Cyrl')},
+  {'Serbian Latin': Locale('sr', 'Latn')},
+  {'Slovak (SK)': Locale('sk', 'SK')},
+  {'Slovenian (SI)': Locale('sl', 'SI')},
+  {'Spanish (ES)': Locale('es', 'ES')},
+  {'Spanish (MX)': Locale('es', 'MX')},
+  {'Spanish (PE)': Locale('es', 'PE')},
+  {'Spanish (US)': Locale('es', 'US')},
+  {'Swedish (FI)': Locale('sv', 'FI')},
+  {'Swedish (SE)': Locale('sv', 'SE')},
+  {'Thai (TH)': Locale('th', 'TH')},
+  {'Ukrainian (UA)': Locale('uk', 'UA')},
+  {'Vietnamese (VN)': Locale('vi', 'VN')},
 ];
 
 const String translationsPath = 'assets/i18n';
