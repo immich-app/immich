baf0a0aa14561d2c7a0350984af49b25b6dcb053
Alex <alex.tran1502@gmail.com>
2024-04-11 12:44:07 +0200

    linting

--- mobile/lib/modules/backup/ui/album_info_list_tile.dart
+++ mobile/lib/modules/backup/ui/album_info_list_tile.dart
@@ -1,6 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
--- mobile/lib/modules/home/ui/asset_grid/group_divider_title.dart
+++ mobile/lib/modules/home/ui/asset_grid/group_divider_title.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/extensions/build_context_extensions.dart';
