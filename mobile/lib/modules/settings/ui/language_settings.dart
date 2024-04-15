0a94152e010437a344fb6958f0f85ec2f76560c5
Alex <alex.tran1502@gmail.com>
2024-04-15 13:51:07 +0200

    linting

--- mobile/lib/modules/settings/ui/language_settings.dart
+++ mobile/lib/modules/settings/ui/language_settings.dart
@@ -1,6 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/constants/locales.dart';
