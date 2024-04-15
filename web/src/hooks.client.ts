1dbb58bc5bfbc57936a57b75c76f5be56d855ee6
martabal <74269598+martabal@users.noreply.github.com>
2024-04-15 21:06:51 +0200

    fix: 404 error

--- web/src/hooks.client.ts
+++ web/src/hooks.client.ts
@@ -3,10 +3,13 @@ import type { HandleClientError } from '@sveltejs/kit';
 
 const DEFAULT_MESSAGE = 'Hmm, not sure about that. Check the logs or open a ticket?';
 
-const parseError = (error: unknown) => {
+const parseError = (error: unknown, statusCode: number) => {
   const httpError = isHttpError(error) ? error : undefined;
-  const statusCode = httpError?.status || httpError?.data?.statusCode || 500;
-  const message = httpError?.data?.message || (httpError?.data && String(httpError.data)) || httpError?.message;
+
+  const message =
+    statusCode === 404
+      ? 'This page does not exist'
+      : undefined || httpError?.data?.message || (httpError?.data && String(httpError.data)) || httpError?.message;
 
   console.log({
     status: statusCode,
@@ -20,8 +23,8 @@ const parseError = (error: unknown) => {
   };
 };
 
-export const handleError: HandleClientError = ({ error }) => {
-  const result = parseError(error);
+export const handleError: HandleClientError = ({ error, status }) => {
+  const result = parseError(error, status);
   console.error(`[hooks.client.ts]:handleError ${result.message}`);
   return result;
 };
