01bff4866b5de7d8b43e336499fc4dfb02c6812a
Diego Brusa <diegobr89@gmail.com>
2024-04-19 17:59:12 -0300

    [web] Bugfix on album options

--- web/src/lib/components/album-page/album-options.svelte
+++ web/src/lib/components/album-page/album-options.svelte
@@ -53,6 +53,26 @@
 
 <FullScreenModal id="album-options-modal" title="Options" onClose={() => dispatch('close')}>
   <div class="items-center justify-center">
+    {#if album.people}
+      <div class="py-2">
+        <h2 class="text-gray text-sm mb-2">FACES</h2>
+        <div class="flex p-2 {album.peopleTogether ? 'gap-0' : 'gap-2'} items-center text-gray-900 dark:text-gray-400">
+          {#each album.people as person, index}
+            <div class={album.peopleTogether && index != 0 ? '-ml-4' : ''}>
+              <FaceThumbnail
+                {person}
+                circle={true}
+                thumbnailSize={54}
+                showName={false}
+                on:click={() => {}}
+                tooltip={true}
+              />
+            </div>
+          {/each}
+        </div>
+      </div>
+    {/if}
+
     <div class="py-2">
       <h2 class="text-gray text-sm mb-2">SETTINGS</h2>
       <div class="grid p-2 gap-y-2">
@@ -73,6 +93,7 @@
         />
       </div>
     </div>
+
     <div class="py-2">
       <div class="text-gray text-sm mb-3">PEOPLE</div>
       <div class="p-2">
@@ -89,80 +110,14 @@
           <div class="w-full">{user.name}</div>
           <div>Owner</div>
         </div>
-
-        <div class=" items-center justify-center p-4">
-          {#if album.people}
-            <div class="py-2">
-              <h2 class="text-gray text-sm mb-2">PEOPLE</h2>
-              <div
-                class="flex p-2 {album.peopleTogether
-                  ? 'gap-0'
-                  : 'gap-2'} items-center text-gray-900 dark:text-gray-400"
-              >
-                {#each album.people as person, index}
-                  <div class={album.peopleTogether && index != 0 ? '-ml-4' : ''}>
-                    <FaceThumbnail
-                      {person}
-                      circle={true}
-                      thumbnailSize={54}
-                      showName={false}
-                      on:click={() => {}}
-                      tooltip={true}
-                    />
-                  </div>
-                {/each}
-              </div>
-            </div>
-          {/if}
-
-          <div class="py-2">
-            <h2 class="text-gray text-sm mb-2">SETTINGS</h2>
-            <div class="grid p-2 gap-y-2">
-              {#if order}
-                <SettingDropdown
-                  title="Display order"
-                  options={Object.values(options)}
-                  selectedOption={options[order]}
-                  onToggle={handleToggle}
-                />
-              {/if}
-              <SettingSwitch
-                id="comments-likes"
-                title="Comments & likes"
-                subtitle="Let others respond"
-                checked={album.isActivityEnabled}
-                on:toggle={() => dispatch('toggleEnableActivity')}
-              />
+        {#each album.sharedUsers as user (user.id)}
+          <div class="flex items-center gap-2 py-2">
+            <div>
+              <UserAvatar {user} size="md" />
             </div>
             <div class="w-full">{user.name}</div>
           </div>
-          <div class="py-2">
-            <div class="text-gray text-sm mb-3">SHARE WITH</div>
-            <div class="p-2">
-              <button class="flex items-center gap-2" on:click={() => dispatch('showSelectSharedUser')}>
-                <div class="rounded-full w-10 h-10 border border-gray-500 flex items-center justify-center">
-                  <div><Icon path={mdiPlus} size="25" /></div>
-                </div>
-                <div>Invite People</div>
-              </button>
-              <div class="flex items-center gap-2 py-2 mt-2">
-                <div>
-                  <UserAvatar {user} size="md" />
-                </div>
-                <div class="w-full">{user.name}</div>
-                <div>Owner</div>
-              </div>
-              {#each album.sharedUsers as user (user.id)}
-                <div class="flex items-center gap-2 py-2">
-                  <div>
-                    <UserAvatar {user} size="md" />
-                  </div>
-                  <div class="w-full">{user.name}</div>
-                </div>
-              {/each}
-            </div>
-          </div>
-        </div>
+        {/each}
       </div>
     </div>
   </div>
