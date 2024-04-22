d5575cc9331b4a06e1faf707a32cbe9a7a7e43ed
Marty Fuhry <martyfuhry@gmail.com>
2024-04-22 09:37:05 -0400

    Fixes equality operator for immich local image provider

--- mobile/lib/shared/cache/custom_image_cache.dart
+++ mobile/lib/shared/cache/custom_image_cache.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/painting.dart';
 import 'package:immich_mobile/modules/asset_viewer/image_providers/immich_local_image_provider.dart';
+import 'package:immich_mobile/modules/asset_viewer/image_providers/immich_local_thumbnail_provider.dart';
 import 'package:immich_mobile/modules/asset_viewer/image_providers/immich_remote_image_provider.dart';
+import 'package:immich_mobile/modules/asset_viewer/image_providers/immich_remote_thumbnail_provider.dart';
 
 /// [ImageCache] that uses two caches for small and large images
 /// so that a single large image does not evict all small iamges
@@ -33,6 +35,8 @@ final class CustomImageCache implements ImageCache {
   }
 
   /// Gets the cache for the given key
+  /// [_large] is used for [ImmichLocalImageProvider] and [ImmichRemoteImageProvider]
+  /// [_small] is used for [ImmichLocalThumbnailProvider] and [ImmichRemoteThumbnailProvider]
   ImageCache _cacheForKey(Object key) =>
       (key is ImmichLocalImageProvider || key is ImmichRemoteImageProvider)
           ? _large
