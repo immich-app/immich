9bbe6044b10f18575c9a91eec9bc66f85c0ff7bb
Clément Delestre <clementdelestre@gmail.com>
2024-04-23 02:33:44 +0300

    fix overflow in person result page

--- mobile/lib/modules/search/views/person_result_page.dart
+++ mobile/lib/modules/search/views/person_result_page.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart' hide Store;
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/extensions/build_context_extensions.dart';
@@ -83,6 +85,8 @@ class PersonResultPage extends HookConsumerWidget {
                   Text(
                     'search_page_person_add_name_subtitle',
                     style: context.textTheme.labelLarge,
+                    maxLines: 2,
+                    softWrap: true,
                   ).tr(),
                 ],
               )
@@ -127,9 +131,11 @@ class PersonResultPage extends HookConsumerWidget {
                   },
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 16.0),
-                child: buildTitleBlock(),
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16.0),
+                  child: buildTitleBlock(),
+                ),
               ),
             ],
           ),
