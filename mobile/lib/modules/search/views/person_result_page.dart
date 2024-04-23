91be9a26e8d03bdf03b9e0896d7fb345187086b8
Clément Delestre <clementdelestre@gmail.com>
2024-04-23 03:06:56 +0300

    fix import

--- mobile/lib/modules/search/views/person_result_page.dart
+++ mobile/lib/modules/search/views/person_result_page.dart
@@ -1,8 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart' hide Store;
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/extensions/build_context_extensions.dart';
