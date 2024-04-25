942914fdc9c9eeb64c36d773b3d778d06d63ed55
Alex Tran <alex.tran1502@gmail.com>
2024-04-25 14:15:24 -0500

    remove console.log

--- web/src/routes/(user)/share/[key]/[[photos=photos]]/[[assetId=id]]/+page.ts
+++ web/src/routes/(user)/share/[key]/[[photos=photos]]/[[assetId=id]]/+page.ts
@@ -6,9 +6,7 @@ import type { PageLoad } from './$types';
 
 export const load = (async ({ params }) => {
   const { key } = params;
-  console.log('authen ticate in key/photo/id page', key);
   await authenticate({ public: true });
-  console.log('authenticated');
   const asset = await getAssetInfoFromParam(params, key);
 
   try {
