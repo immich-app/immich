912d0c4d742726b5a2dcb46e352955a726edc6c3
Alex <alex.tran1502@gmail.com>
2024-04-25 14:32:21 -0500

    feat(web): permanent url in shared link (#9083)

--- web/src/routes/(user)/share/[key]/[[photos=photos]]/[[assetId=id]]/+page.ts
+++ web/src/routes/(user)/share/[key]/[[photos=photos]]/[[assetId=id]]/+page.ts
@@ -1,11 +1,13 @@
 import { getAssetThumbnailUrl, setSharedLink } from '$lib/utils';
 import { authenticate } from '$lib/utils/auth';
+import { getAssetInfoFromParam } from '$lib/utils/navigation';
 import { ThumbnailFormat, getMySharedLink, isHttpError } from '@immich/sdk';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ params }) => {
   const { key } = params;
   await authenticate({ public: true });
+  const asset = await getAssetInfoFromParam(params);
 
   try {
     const sharedLink = await getMySharedLink({ key });
@@ -15,6 +17,8 @@ export const load = (async ({ params }) => {
 
     return {
       sharedLink,
+      asset,
+      key,
       meta: {
         title: sharedLink.album ? sharedLink.album.albumName : 'Public Share',
         description: sharedLink.description || `${assetCount} shared photos & videos.`,
