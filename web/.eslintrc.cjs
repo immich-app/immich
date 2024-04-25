d21ac8e6024309047f04817143911ff56ef50a05
nghduc97 <lazyc97@protonmail.com>
2024-04-25 13:03:45 +0700

    update lint rule

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -34,6 +34,10 @@ module.exports = {
     NodeJS: true,
   },
   rules: {
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      { checksVoidReturn: false }, // https://github.com/typescript-eslint/typescript-eslint/issues/4619
+    ],
     '@typescript-eslint/no-unused-vars': [
       'warn',
       {
