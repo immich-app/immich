b6bff5b5a571d5cd99c3919c47829ad4ee2f10c9
Ethan Margaillan <ethan.margaillan@gmail.com>
2024-04-26 00:30:37 +0200

    fix autogrow still showing an scroll bar

--- web/src/lib/utils/autogrow.ts
+++ web/src/lib/utils/autogrow.ts
@@ -1,4 +1,18 @@
 export const autoGrowHeight = (textarea: HTMLTextAreaElement, height = 'auto') => {
-  textarea.style.height = height;
-  textarea.style.height = `${textarea.scrollHeight}px`;
+  const resize = () => {
+    textarea.style.height = height;
+    textarea.style.height = `${textarea.scrollHeight + 5}px`;
+  };
+
+  resize();
+
+  textarea.addEventListener('input', resize);
+  window.addEventListener('resize', resize);
+
+  return {
+    destroy: () => {
+      textarea.removeEventListener('input', resize);
+      window.removeEventListener('resize', resize);
+    },
+  };
 };
