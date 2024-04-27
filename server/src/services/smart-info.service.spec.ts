684a013c269c82faea0b83d1a80a5e2f940a0e6e
mertalev <101130780+mertalev@users.noreply.github.com>
2024-04-27 19:12:20 -0400

    fix tests

--- server/src/services/smart-info.service.spec.ts
+++ server/src/services/smart-info.service.spec.ts
@@ -109,7 +109,7 @@ describe(SmartInfoService.name, () => {
       expect(machineMock.encodeImage).toHaveBeenCalledWith(
         'http://immich-machine-learning:3003',
         { imagePath: assetStub.image.previewPath },
-        { enabled: true, modelName: 'ViT-B-32__openai' },
+        { duplicateThreshold: 0.02, enabled: true, modelName: 'ViT-B-32__openai' },
       );
       expect(searchMock.upsert).toHaveBeenCalledWith(assetStub.image.id, [0.01, 0.02, 0.03]);
     });
