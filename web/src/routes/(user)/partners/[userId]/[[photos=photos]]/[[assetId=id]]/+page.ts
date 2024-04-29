02726579f28abc8fe2bf57bfee7f5fe8915768c5
Alex Tran <alex.tran1502@gmail.com>
2024-04-29 11:27:40 -0500

    fix(web): permanent url for partner page

--- web/src/routes/(user)/partners/[userId]/[[photos=photos]]/[[assetId=id]]/+page.ts
+++ web/src/routes/(user)/partners/[userId]/[[photos=photos]]/[[assetId=id]]/+page.ts
@@ -1,13 +1,15 @@
 import { authenticate } from '$lib/utils/auth';
 import { getUserById } from '@immich/sdk';
 import type { PageLoad } from './$types';
+import { getAssetInfoFromParam } from '$lib/utils/navigation';
 
 export const load = (async ({ params }) => {
   await authenticate();
 
   const partner = await getUserById({ id: params.userId });
-
+  const asset = await getAssetInfoFromParam(params);
   return {
+    asset,
     partner,
     meta: {
       title: 'Partner',
