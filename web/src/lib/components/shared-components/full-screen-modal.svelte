461f2595b50ce7e3e716e8c88d10631a74226301
Yoonjae Choi <123538138+uniqueeest@users.noreply.github.com>
2024-04-30 06:59:32 +0900

    fix(web): Prevent DELETE key from clearing DateInput in modal (#8846)
    * fix(web): Prevent DELETE key from clearing DateInput in modal (#8804)
    
    * chore: test prettier
    
    * fix: stop propagation
    
    * fix: stop propagation in modal
    
    * fix: remove unnecessary function and revise lint issue
    
    * chore: cleanup
    
    ---------
    
    Co-authored-by: Jason Rasmussen <jrasm91@gmail.com>

--- web/src/lib/components/shared-components/full-screen-modal.svelte
+++ web/src/lib/components/shared-components/full-screen-modal.svelte
@@ -45,6 +45,7 @@
 
 <FocusTrap>
   <section
+    role="presentation"
     in:fade={{ duration: 100 }}
     out:fade={{ duration: 100 }}
     class="fixed left-0 top-0 z-[9990] flex h-screen w-screen place-content-center place-items-center bg-black/40"
