c5e8a46084f71009ba49fd12181dd94e63105094
mertalev <101130780+mertalev@users.noreply.github.com>
2024-04-30 21:25:08 -0400

    remove unused import

--- server/src/services/person.service.ts
+++ server/src/services/person.service.ts
@@ -39,7 +39,7 @@ import {
   QueueName,
 } from 'src/interfaces/job.interface';
 import { ILoggerRepository } from 'src/interfaces/logger.interface';
-import { BoundingBox, IMachineLearningRepository } from 'src/interfaces/machine-learning.interface';
+import { IMachineLearningRepository } from 'src/interfaces/machine-learning.interface';
 import { CropOptions, ImageDimensions, IMediaRepository } from 'src/interfaces/media.interface';
 import { IMoveRepository } from 'src/interfaces/move.interface';
 import { IPersonRepository, UpdateFacesData } from 'src/interfaces/person.interface';
