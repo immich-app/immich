01172660d79e53279ef8b54e8a9be39132e79eb7
Min Idzelis <min123@gmail.com>
2024-05-07 03:09:30 +0000

    Create common update dto

--- server/src/dtos/asset-v1.dto.ts
+++ server/src/dtos/asset-v1.dto.ts
@@ -63,52 +63,6 @@ export class CheckExistingAssetsDto {
   deviceId!: string;
 }
 
-export class CreateAssetDto {
-  @ValidateUUID({ optional: true })
-  libraryId?: string;
-
-  @IsNotEmpty()
-  @IsString()
-  deviceAssetId!: string;
-
-  @IsNotEmpty()
-  @IsString()
-  deviceId!: string;
-
-  @ValidateDate()
-  fileCreatedAt!: Date;
-
-  @ValidateDate()
-  fileModifiedAt!: Date;
-
-  @Optional()
-  @IsString()
-  duration?: string;
-
-  @ValidateBoolean({ optional: true })
-  isFavorite?: boolean;
-
-  @ValidateBoolean({ optional: true })
-  isArchived?: boolean;
-
-  @ValidateBoolean({ optional: true })
-  isVisible?: boolean;
-
-  @ValidateBoolean({ optional: true })
-  isOffline?: boolean;
-
-  // The properties below are added to correctly generate the API docs
-  // and client SDKs. Validation should be handled in the controller.
-  @ApiProperty({ type: 'string', format: 'binary' })
-  [UploadFieldName.ASSET_DATA]!: any;
-
-  @ApiProperty({ type: 'string', format: 'binary', required: false })
-  [UploadFieldName.LIVE_PHOTO_DATA]?: any;
-
-  @ApiProperty({ type: 'string', format: 'binary', required: false })
-  [UploadFieldName.SIDECAR_DATA]?: any;
-}
-
 export class UpdateAssetDataDto {
   @IsNotEmpty()
   @IsString()
@@ -138,6 +92,23 @@ export class UpdateAssetDataDto {
 
   @ApiProperty({ type: 'string', format: 'binary', required: false })
   [UploadFieldName.SIDECAR_DATA]?: any;
+}
+
+export class CreateAssetDto extends UpdateAssetDataDto {
+  @ValidateUUID({ optional: true })
+  libraryId?: string;
+
+  @ValidateBoolean({ optional: true })
+  isFavorite?: boolean;
+
+  @ValidateBoolean({ optional: true })
+  isArchived?: boolean;
+
+  @ValidateBoolean({ optional: true })
+  isVisible?: boolean;
+
+  @ValidateBoolean({ optional: true })
+  isOffline?: boolean;
 }
 
 export enum GetAssetThumbnailFormatEnum {
