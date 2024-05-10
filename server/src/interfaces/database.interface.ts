f68d5bc6a3d39d04bf437202117ec77196bb6e57
Mert <101130780+mertalev@users.noreply.github.com>
2024-05-10 14:10:09 -0400

    critical fix
    Co-authored-by: Daniel Dietzler <36593685+danieldietzler@users.noreply.github.com>

--- server/src/interfaces/database.interface.ts
+++ server/src/interfaces/database.interface.ts
@@ -20,7 +20,7 @@ export enum DatabaseLock {
   StorageTemplateMigration = 420,
   CLIPDimSize = 512,
   LibraryWatch = 1337,
-  GetSystemConfig = 304,
+  GetSystemConfig = 69,
 }
 
 export const extName: Record<DatabaseExtension, string> = {
