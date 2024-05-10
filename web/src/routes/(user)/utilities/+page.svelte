176802d3ae4b1f26709940b5991ce4f1249b102c
Nicholas Flamy <30300649+NicholasFlamy@users.noreply.github.com>
2024-05-10 08:09:41 -0400

    Update +page.svelte

--- web/src/routes/(user)/utilities/+page.svelte
+++ web/src/routes/(user)/utilities/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
   import UserPageLayout from '$lib/components/layouts/user-page-layout.svelte';
   import type { PageData } from './$types';
+  import {
+    mdiContentDuplicate
+  } from '@mdi/js';
   import EmptyPlaceholder from '$lib/components/shared-components/empty-placeholder.svelte';
+  import LinkButton from '$lib/components/elements/buttons/link-button.svelte';
 
   export let data: PageData;
 </script>
@@ -10,7 +14,7 @@
   <section>
     <LinkButton href="./duplicates">
       <div class="flex place-items-center gap-2 text-sm">
-        <Icon path={mdiContentCopy} size="18" />
+        <Icon path={mdiContentDuplicate} size="18" />
       </div>
     </LinkButton>
   </section>
