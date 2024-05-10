c12ffc5cdda3948230b3688b63578451ae3eba6a
Nicholas Flamy <30300649+NicholasFlamy@users.noreply.github.com>
2024-05-10 13:45:57 -0400

    Update +page.svelte

--- web/src/routes/(user)/utilities/+page.svelte
+++ web/src/routes/(user)/utilities/+page.svelte
@@ -32,18 +32,20 @@
     </Button>
   </section>
 
-<div class="flex flex-col space-y-2">
-  <button type="button" class="flex items-center px-4 py-2 space-x-2 rounded bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-    <span class="material-icons text-indigo-500">home</span>
-    <span>Home</span>
-  </button>
-  <button type="button" class="flex items-center px-4 py-2 space-x-2 rounded bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-    <span class="material-icons text-indigo-500">settings</span>
-    <span>Settings</span>
-  </button>
-  <button type="button" class="flex items-center px-4 py-2 space-x-2 rounded bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-    <span class="material-icons text-indigo-500">account</span>
-    <span>Profile</span>
-  </button>
+<div class="bg-gray-100 rounded overflow-hidden shadow-sm">
+  <div class="flex flex-col space-y-2 px-4 py-4">
+    <button type="button" class="flex items-center px-4 py-2 space-x-2 rounded bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+      <span class="material-icons text-indigo-500">home</span>
+      <span>Home</span>
+    </button>
+    <button type="button" class="flex items-center px-4 py-2 space-x-2 rounded bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+      <span class="material-icons text-indigo-500">settings</span>
+      <span>Settings</span>
+    </button>
+    <button type="button" class="flex items-center px-4 py-2 space-x-2 rounded bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+      <span class="material-icons text-indigo-500">account</span>
+      <span>Profile</span>
+    </button>
+  </div>
 </div>
 </UserPageLayout>
