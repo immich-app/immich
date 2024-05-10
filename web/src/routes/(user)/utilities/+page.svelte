2eaf8560f345b392403f6d2f559985e3aa45a7ad
Nicholas Flamy <30300649+NicholasFlamy@users.noreply.github.com>
2024-05-10 14:00:26 -0400

    Update +page.svelte

--- web/src/routes/(user)/utilities/+page.svelte
+++ web/src/routes/(user)/utilities/+page.svelte
@@ -13,26 +13,28 @@
 </script>
 
 <UserPageLayout title={data.meta.title}>
-  <section class="flex place-items-center gap-2">
-    <Button title="Duplicates" size="lg" color="overlay-primary" shadow={false} rounded="lg" disabled={false} on:click fullwidth={false}>
-      <a href="{AppRoute.DUPLICATES}" class="text-sm">
-        <Icon path={mdiContentDuplicate} size="18" />
-        <p>
-          Duplicates
-        </p>
-      </a>
-    </Button>
-    <Button title="Duplicates" size="lg" color="dark-gray" shadow={false} rounded="lg" disabled={false} on:click fullwidth={false}>
-      <a href="{AppRoute.DUPLICATES}" class="flex inset-y-0 gap-2 text-sm">
-        <Icon path={mdiContentDuplicate} size="18" />
-        <p>
-          Duplicates
-        </p>
-      </a>
-    </Button>
+  <section class="bg-gray-200 rounded my-10 mx-10">
+    <div class="flex flex-col space-y-2 px-4 py-4">
+      <Button title="Duplicates" size="lg" color="overlay-primary" shadow={false} rounded="lg" disabled={false} on:click fullwidth={false}>
+        <a href="{AppRoute.DUPLICATES}" class="text-sm">
+          <Icon path={mdiContentDuplicate} size="18" />
+          <p>
+            Duplicates
+          </p>
+        </a>
+      </Button>
+      <Button title="Duplicates" size="lg" color="dark-gray" shadow={false} rounded="lg" disabled={false} on:click fullwidth={false}>
+        <a href="{AppRoute.DUPLICATES}" class="flex inset-y-0 gap-2 text-sm">
+          <Icon path={mdiContentDuplicate} size="18" />
+          <p>
+            Duplicates
+          </p>
+        </a>
+      </Button>
+    </div>
   </section>
 
-<div class="bg-gray-100 rounded overflow-hidden shadow-sm my-4 mx-40">
+<div class="bg-gray-200 rounded overflow-hidden shadow-sm my-10 mx-10">
   <div class="flex flex-col space-y-2 px-4 py-4">
     <button type="button" class="flex items-center px-4 py-2 space-x-2 rounded bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
       <span class="material-icons text-indigo-500">home</span>
