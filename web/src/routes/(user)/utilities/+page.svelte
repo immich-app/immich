3eaa971845dd1fb24ada7519be61085700a0760a
Nicholas Flamy <30300649+NicholasFlamy@users.noreply.github.com>
2024-05-10 00:27:15 -0400

    Create utilities +page.svelte

---
+++ web/src/routes/(user)/utilities/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import UserPageLayout from '$lib/components/layouts/user-page-layout.svelte';
+  import type { PageData } from './$types';
+  import EmptyPlaceholder from '$lib/components/shared-components/empty-placeholder.svelte';
+
+  export let data: PageData;
+</script>
+
+<UserPageLayout title={data.meta.title}>
+  <section>
+    <EmptyPlaceholder slot="empty" text="Create an album to organize your photos and videos" />
+  </section>
+</UserPageLayout>
