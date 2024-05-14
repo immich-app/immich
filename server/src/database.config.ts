6e0c41bf3f7df6e183d3f4c14bbb66dd293685d9
bzp2010 <bzp2010@apache.org>
2024-05-14 18:48:04 +0800

    feat: add database tls options

--- server/src/database.config.ts
+++ server/src/database.config.ts
@@ -1,16 +1,25 @@
+import { TLSSocketOptions } from 'node:tls';
 import { DatabaseExtension } from 'src/interfaces/database.interface';
 import { DataSource } from 'typeorm';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions.js';
 
+const ssl: TLSSocketOptions = {
+  ca: process.env.DB_TLS_CA || undefined,
+  cert: process.env.DB_TLS_CLIENT_CERT || undefined,
+  key: process.env.DB_TLS_CLIENT_KEY || undefined,
+  rejectUnauthorized: process.env.DB_TLS_SKIP_VERIFY === 'true' || false,
+};
+
 const url = process.env.DB_URL;
 const urlOrParts = url
-  ? { url }
+  ? { url, ssl }
   : {
       host: process.env.DB_HOSTNAME || 'database',
       port: Number.parseInt(process.env.DB_PORT || '5432'),
       username: process.env.DB_USERNAME || 'postgres',
       password: process.env.DB_PASSWORD || 'postgres',
       database: process.env.DB_DATABASE_NAME || 'immich',
+      ssl,
     };
 
 /* eslint unicorn/prefer-module: "off" -- We can fix this when migrating to ESM*/
