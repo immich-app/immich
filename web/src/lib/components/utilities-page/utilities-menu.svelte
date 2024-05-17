67131157cfdd0881f195bb7106bf56d36919a044
Alex Tran <alex.tran1502@gmail.com>
2024-05-17 09:40:47 -0500

    utilities menu

---
+++ web/src/lib/components/utilities-page/utilities-menu.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { mdiContentDuplicate } from '@mdi/js';
+  import Icon from '$lib/components/elements/icon.svelte';
+</script>
+
+<div class="border border-gray-300 rounded-3xl pt-4 pb-6 dark:text-white">
+  <p class="text-xs font-medium p-4">ORGANIZE YOUR LIBRARY</p>
+
+  <button class="w-full hover:bg-gray-100 dark:hover:bg-immich-dark-gray flex gap-4 p-4">
+    <span
+      ><Icon path={mdiContentDuplicate} class="text-immich-primary dark:text-immich-dark-primary" size="24" />
+    </span>
+    <p>Manage photos duplication</p>
+  </button>
+</div>
--- web/src/routes/(user)/utilities/+page.svelte
+++ web/src/routes/(user)/utilities/+page.svelte
@@ -1,73 +1,13 @@
 <script lang="ts">
   import UserPageLayout from '$lib/components/layouts/user-page-layout.svelte';
   import type { PageData } from './$types';
-  import { mdiContentDuplicate } from '@mdi/js';
-  import Button from '$lib/components/elements/buttons/button.svelte';
-  import Icon from '$lib/components/elements/icon.svelte';
-  import { AppRoute } from '$lib/constants';
-  import UtilitiesPageLink from '$lib/components/utilities-page/utilities-page-link.svelte';
+  import UtilitiesMenu from '$lib/components/utilities-page/utilities-menu.svelte';
 
   export let data: PageData;
 </script>
 
 <UserPageLayout title={data.meta.title}>
-  <div class="flex flex-col overflow-hidden rounded-3xl bg-gray-100 dark:bg-immich-dark-gray sm:flex-row">
-    <div class="flex flex-col space-y-2 px-4 py-4">
-      <UtilitiesPageLink title="Duplicates" routeId={AppRoute.DUPLICATES} icon={mdiContentDuplicate}>
-        <p>Duplicates</p>
-      </UtilitiesPageLink>
-      <Button
-        title="Duplicates"
-        size="lg"
-        color="dark-gray"
-        shadow={false}
-        rounded="lg"
-        disabled={false}
-        on:click
-        fullwidth={false}
-      >
-        <a href={AppRoute.DUPLICATES} class="flex inset-y-0 gap-2 text-sm">
-          <Icon path={mdiContentDuplicate} size="18" />
-          <p>Duplicates</p>
-        </a>
-      </Button>
-    </div>
-  </div>
-
-  <br /><br /><br />
-
-  <div class="flex flex-col overflow-hidden rounded-3xl bg-gray-100 dark:bg-immich-dark-gray sm:flex-row">
-    <div class="flex flex-col space-y-2 px-4 py-4">
-      <Button
-        title="Duplicates"
-        size="lg"
-        color="overlay-primary"
-        shadow={false}
-        rounded="lg"
-        disabled={false}
-        on:click
-        fullwidth={false}
-      >
-        <a href={AppRoute.DUPLICATES} class="text-sm">
-          <Icon path={mdiContentDuplicate} size="18" />
-          <p>Duplicates</p>
-        </a>
-      </Button>
-      <Button
-        title="Duplicates"
-        size="lg"
-        color="dark-gray"
-        shadow={false}
-        rounded="lg"
-        disabled={false}
-        on:click
-        fullwidth={false}
-      >
-        <a href={AppRoute.DUPLICATES} class="flex inset-y-0 gap-2 text-sm">
-          <Icon path={mdiContentDuplicate} size="18" />
-          <p>Duplicates</p>
-        </a>
-      </Button>
-    </div>
+  <div class="w-full max-w-xl m-auto">
+    <UtilitiesMenu />
   </div>
 </UserPageLayout>
