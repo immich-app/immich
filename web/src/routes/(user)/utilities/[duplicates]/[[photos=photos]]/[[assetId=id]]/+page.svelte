534142270fc92ed0954f3807f2761ab765d06edd
Alex Tran <alex.tran1502@gmail.com>
2024-05-17 16:16:47 -0500

    get assets

--- web/src/routes/(user)/utilities/[duplicates]/[[photos=photos]]/[[assetId=id]]/+page.svelte
+++ web/src/routes/(user)/utilities/[duplicates]/[[photos=photos]]/[[assetId=id]]/+page.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
   import UserPageLayout from '$lib/components/layouts/user-page-layout.svelte';
-  import { type AssetResponseDto } from '@immich/sdk';
-  import type { Viewport } from '$lib/stores/assets.store';
+  import { getAssetDuplicates, type AssetResponseDto } from '@immich/sdk';
   import { onMount } from 'svelte';
 
   import type { PageData } from './$types';
   export let data: PageData;
   let assets: AssetResponseDto[] = [];
 
-  const viewport: Viewport = { width: 0, height: 0 };
-
-  onMount(async () => {});
+  onMount(async () => {
+    assets = await getAssetDuplicates();
+    console.log(assets);
+  });
 </script>
 
 <UserPageLayout title={data.meta.title} scrollbar={false}>OK</UserPageLayout>
