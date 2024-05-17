2689178a355f9f513ab36fe6c0f389eaf95555b3
Alex <alex.tran1502@gmail.com>
2024-05-17 12:05:23 -0500

    chore(server): openapi generation for Duplicate controller (#9560)

--- mobile/openapi/lib/api.dart
+++ mobile/openapi/lib/api.dart
@@ -36,6 +36,7 @@ part 'api/asset_api.dart';
 part 'api/audit_api.dart';
 part 'api/authentication_api.dart';
 part 'api/download_api.dart';
+part 'api/duplicate_api.dart';
 part 'api/face_api.dart';
 part 'api/file_report_api.dart';
 part 'api/job_api.dart';
