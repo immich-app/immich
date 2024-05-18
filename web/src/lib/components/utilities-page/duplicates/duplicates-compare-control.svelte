20f8c660316e37a388ced909839dd8ddfb77b8af
Alex Tran <alex.tran1502@gmail.com>
2024-05-18 14:17:09 -0500

    update

--- web/src/lib/components/utilities-page/duplicates/duplicates-compare-control.svelte
+++ web/src/lib/components/utilities-page/duplicates/duplicates-compare-control.svelte
@@ -1,1 +1,9 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import type { DuplicateResponseDto } from '@immich/sdk';
+
+  export let duplicate: DuplicateResponseDto;
+</script>
+
+<div class="border border-gray-200">
+  {duplicate.duplicateId}
+</div>
--- web/src/routes/(user)/utilities/[duplicates]/[[photos=photos]]/[[assetId=id]]/+page.svelte
+++ web/src/routes/(user)/utilities/[duplicates]/[[photos=photos]]/[[assetId=id]]/+page.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
   import UserPageLayout from '$lib/components/layouts/user-page-layout.svelte';
   import { getAssetDuplicates, type AssetResponseDto } from '@immich/sdk';
-  import { onMount } from 'svelte';
+  import DuplicatesCompareControl from '$lib/components/utilities-page/duplicates/duplicates-compare-control.svelte';
 
   import type { PageData } from './$types';
   export let data: PageData;
   let assets: AssetResponseDto[] = [];
-
-  onMount(async () => {
-    assets = await getAssetDuplicates();
-    console.log(assets);
-  });
 </script>
 
-<UserPageLayout title={data.meta.title} scrollbar={false}></UserPageLayout>
+<UserPageLayout title={data.meta.title} scrollbar={false}>
+  <div class="flex gap-4 flex-wrap justify-around">
+    {#each data.duplicates as duplicate (duplicate.duplicateId)}
+      <DuplicatesCompareControl {duplicate} />
+    {/each}
+  </div>
+</UserPageLayout>
