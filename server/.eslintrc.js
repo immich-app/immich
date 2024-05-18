d26ba627f832e36baedc4c117ba755442bdf7e9b
Coda Bool <codabool@pm.me>
2024-05-18 12:50:41 -0400

    add healthcheck

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   env: {
     node: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'healthcheck.ts'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
