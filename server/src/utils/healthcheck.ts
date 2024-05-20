715214a5211af28f01565b2c4e9f2f5eaf52c1af
Jason Rasmussen <jrasm91@gmail.com>
2024-05-20 18:25:32 -0400

    chore: clean up nodejs healthcheck

--- server/src/utils/healthcheck.ts
+++ server/src/utils/healthcheck.ts
@@ -1,35 +1,31 @@
-/* eslint-disable no-process-exit */
-import http from 'http';
-
+#!/usr/bin/env node
 const port = Number(process.env.IMMICH_PORT) || 3001;
-const options = {
-  host: 'localhost',
-  timeout: 2000,
-  path: '/api/server-info/ping',
-  port,
+const controller = new AbortController();
+
+const main = async () => {
+  const timeout = setTimeout(() => controller.abort(), 2000);
+  try {
+    const response = await fetch(`http://localhost:${port}/api/server-info/ping`, {
+      signal: controller.signal,
+    });
+
+    if (response.ok) {
+      const body = await response.json();
+      if (body.res === 'pong') {
+        console.log('Server is running');
+        process.exit();
+      }
+    }
+  } catch (error) {
+    if (error instanceof DOMException === false) {
+      console.error(error);
+    }
+  } finally {
+    clearTimeout(timeout);
+  }
+
+  console.log('Server is NOT reachable');
+  process.exit(1);
 };
 
-const request = http.request(options, (res: any) => {
-  // TODO: assert on response body
-  // I need to look at what it looks like first
-
-  // 2024-05-19 20:47:35 URL: http://localhost:3001/api/server-info/ping 200 OK
-
-  if (res.statusMessage === 'OK') {
-    console.log('Response contains "OK"');
-  } else {
-    console.log('Response does not contain "OK"');
-  }
-
-  if (res.statusCode === 200) {
-    process.exit(0);
-  } else {
-    process.exit(1);
-  }
-});
-
-request.on('error', () => {
-  process.exit(1);
-});
-
-request.end();
+void main();
