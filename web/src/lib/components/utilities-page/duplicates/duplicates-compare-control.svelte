659478de7041d274deb4bc84b69ba01bbd0c0dbe
Alex Tran <alex.tran1502@gmail.com>
2024-05-20 13:26:14 -0500

    color

--- web/src/lib/components/utilities-page/duplicates/duplicates-compare-control.svelte
+++ web/src/lib/components/utilities-page/duplicates/duplicates-compare-control.svelte
@@ -1,58 +1,54 @@
 <script lang="ts">
   import { getAssetThumbnailUrl } from '$lib/utils';
-  import { ThumbnailFormat, type DuplicateResponseDto } from '@immich/sdk';
-  import Icon from '$lib/components/elements/icon.svelte';
-  import { mdiCheckCircleOutline, mdiCloseCircleOutline } from '@mdi/js';
+  import { ThumbnailFormat, type AssetResponseDto, type DuplicateResponseDto } from '@immich/sdk';
 
   export let duplicate: DuplicateResponseDto;
+
+  let selectedAsset = duplicate.assets.sort((a, b) => b.exifInfo!.fileSizeInByte! - a.exifInfo!.fileSizeInByte!)[0];
+
+  const onSelectAsset = (asset: AssetResponseDto) => {
+    selectedAsset = asset;
+  };
 </script>
 
-<div class="border border-gray-200 pt-4 rounded-3xl bg-neutral-100 max-w-[900px] m-auto mb-4">
+<div class="pt-4 rounded-3xl bg-neutral-100 max-w-[900px] m-auto mb-8">
   <div class="flex gap-1 place-items-center place-content-center px-4">
     {#each duplicate.assets as asset, index (index)}
-      <div class="">
-        <!-- THUMBNAIL-->
-        <img
-          src={getAssetThumbnailUrl(asset.id, ThumbnailFormat.Webp)}
-          alt={asset.id}
-          title={`assetId: ${asset.id}`}
-          class="w-[250px] h-[250px] object-cover rounded-t-xl"
-        />
-        <!-- ASSET INFO-->
+      {@const isCandidate = selectedAsset.id === asset.id}
+      <div>
+        <button on:click={() => onSelectAsset(asset)} class="block relative">
+          <!-- THUMBNAIL-->
+          <img
+            src={getAssetThumbnailUrl(asset.id, ThumbnailFormat.Webp)}
+            alt={asset.id}
+            title={`assetId: ${asset.id}`}
+            class={`w-[250px] h-[250px] object-cover rounded-t-xl border-t-[4px] border-l-[4px] border-r-[4px] border-gray-300 ${isCandidate ? 'border-immich-primary dark:border-immich-dark-primary' : ''}`}
+            draggable="false"
+          />
+        </button>
 
-        <table class="text-xs w-full bg-gray-200">
-          <tr class="h-[30px]">
-            <td class="border border-gray-300 text-center">{asset.exifInfo?.fileSizeInByte} bytes</td>
+        <!-- ASSET INFO-->
+        <table
+          class={`text-xs w-full rounded-b-xl font-semibold ${isCandidate ? 'bg-immich-primary text-white' : 'bg-gray-200'} mt-0`}
+        >
+          <tr class={`h-[32px] ${isCandidate ? 'border-immich-primary rounded-xl' : 'border-gray-300'} text-center `}>
+            <td>{asset.exifInfo?.fileSizeInByte} bytes</td>
           </tr>
-          <tr class="h-[30px]">
-            <td class="border border-gray-300 text-center">{asset.originalFileName}</td>
+          <tr class={`h-[32px] ${isCandidate ? 'border-immich-primary rounded-xl' : 'border-gray-300'} text-center `}>
+            <td>{asset.originalFileName}</td>
           </tr>
         </table>
-
-        <!-- ACTION BUTTONS-->
-        <div class="flex justify-between rounded-b-xl">
-          <button
-            class="border border-gray-300 h-10 bg-red-100 hover:bg-red-300 flex place-items-center place-content-center w-full rounded-bl-xl"
-            ><Icon path={mdiCloseCircleOutline} size="20" />
-          </button>
-          <button
-            class="border border-gray-300 h-10 bg-immich-primary/10 hover:bg-immich-primary/60 flex place-items-center place-content-center w-full rounded-br-xl"
-            ><Icon path={mdiCheckCircleOutline} size="20" /></button
-          >
-        </div>
       </div>
     {/each}
   </div>
 
   <!-- CONFIRM BUTTONS -->
-  <div class="flex mt-6 border-transparent w-full">
-    <button
-      class="h-14 bg-gray-200 hover:bg-gray-300 flex place-items-center place-content-center w-full rounded-bl-3xl"
-    >
-      Ignore
+  <div class="flex mt-6 border-transparent w-full rounded-3xl">
+    <button class="h-12 bg-red-100 hover:bg-red-300 flex place-items-center place-content-center w-full rounded-bl-3xl">
+      Trash all
     </button>
     <button
-      class="h-14 bg-immich-primary/90 hover:bg-immich-primary flex place-items-center place-content-center w-full rounded-br-3xl text-white"
+      class="h-12 bg-immich-primary hover:bg-immich-primary flex place-items-center place-content-center w-full rounded-br-3xl text-white"
       >Resolve</button
     >
   </div>
