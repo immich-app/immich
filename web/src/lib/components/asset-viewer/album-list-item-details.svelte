8090e14430eac1d6a676fa51696acb1bc323bfd9
snowknight26 <Snowknight26@users.noreply.github.com>
2024-05-21 21:29:54 -0500

    refactor(web): refactor album selection modal

---
+++ web/src/lib/components/asset-viewer/album-list-item-details.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type { AlbumResponseDto } from '@immich/sdk';
+
+  export let album: AlbumResponseDto;
+</script>
+
+<span>{album.assetCount} items</span>
+{#if album.shared}
+  <span>• Shared</span>
+{/if}
