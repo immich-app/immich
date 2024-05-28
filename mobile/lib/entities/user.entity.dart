8c5f1951b47fa28bd6e8d16f4bd772d8046c7e14
Alex <alex.tran1502@gmail.com>
2024-05-28 08:41:56 +0700

    mobile: user preference

--- mobile/lib/entities/user.entity.dart
+++ mobile/lib/entities/user.entity.dart
@@ -27,8 +27,10 @@ class User {
 
   Id get isarId => fastHash(id);
 
-  User.fromUserDto(UserAdminResponseDto dto)
-      : id = dto.id,
+  User.fromUserDto(
+    UserAdminResponseDto dto,
+    UserPreferencesResponseDto? preference,
+  )   : id = dto.id,
         updatedAt = dto.updatedAt,
         email = dto.email,
         name = dto.name,
@@ -36,7 +38,7 @@ class User {
         isPartnerSharedWith = false,
         profileImagePath = dto.profileImagePath,
         isAdmin = dto.isAdmin,
-        memoryEnabled = false,
+        memoryEnabled = preference?.memories.enabled ?? false,
         avatarColor = dto.avatarColor.toAvatarColor(),
         inTimeline = false,
         quotaUsageInBytes = dto.quotaUsageInBytes ?? 0,
