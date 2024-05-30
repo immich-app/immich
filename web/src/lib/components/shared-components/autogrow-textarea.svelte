0a21063179ba156ba43ee1fe7b838599291b7f49
snowknight26 <Snowknight26@users.noreply.github.com>
2024-05-29 20:22:53 -0500

    Deduplicate description textarea

---
+++ web/src/lib/components/shared-components/autogrow-textarea.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { autoGrowHeight } from '$lib/actions/autogrow';
+  import { clickOutside } from '$lib/actions/click-outside';
+  import { shortcut } from '$lib/actions/shortcut';
+  import { tick } from 'svelte';
+
+  export let content: string;
+  export let elementClass: string;
+  export let onContentUpdate: (newContent) => void;
+  export let placeholder: string;
+
+  let textarea: HTMLTextAreaElement;
+  $: newContent = content;
+
+  $: if (textarea) {
+    newContent;
+    void tick().then(() => autoGrowHeight(textarea));
+  }
+
+  const updateContent = () => {
+    if (content === newContent) {
+      return;
+    }
+    onContentUpdate(newContent);
+  };
+</script>
+
+<textarea
+  bind:this={textarea}
+  class={elementClass}
+  on:focusout={updateContent}
+  on:input={(e) => (newContent = e.currentTarget.value)}
+  use:clickOutside={{ onOutclick: updateContent }}
+  {placeholder}
+  use:shortcut={{
+    shortcut: { key: 'Enter', ctrl: true },
+    onShortcut: (e) => e.currentTarget.blur(),
+  }}>{content}</textarea
+>
