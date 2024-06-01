69b1750474b19411cd1cd8cc18ed3033b0a7c469
ben-basten <45583362+ben-basten@users.noreply.github.com>
2024-06-01 18:34:14 -0400

    chore: add test for id store

--- web/src/lib/stores/unique-id.store.ts
+++ web/src/lib/stores/unique-id.store.ts
@@ -1,10 +1,11 @@
 import { get, writable } from 'svelte/store';
 
 function createIdStore() {
-  const { subscribe, update } = writable(0);
+  const { subscribe, update } = writable(-1);
 
   return {
     subscribe,
+    update,
     generateId: () => {
       update((value) => value + 1);
       return `id-${get(uniqueIdStore)}`;
