47e9757eb6885e2793b63a193c449a52efa698ab
Daniel Dietzler <mail@ddietzler.dev>
2024-06-03 23:53:22 +0200

    fix formatting

--- web/src/lib/i18n/index.ts
+++ web/src/lib/i18n/index.ts
@@ -1,12 +1,12 @@
-import { browser } from '$app/environment'
-import { init, register } from 'svelte-i18n'
+import { browser } from '$app/environment';
+import { init, register } from 'svelte-i18n';
 
-const defaultLocale = 'en-EN'
-const localeModules = import.meta.glob("./locales/*.json");
+const defaultLocale = 'en-EN';
+const localeModules = import.meta.glob('./locales/*.json');
 
 for (const modulePath in localeModules) {
-	let code = modulePath.substring(modulePath.lastIndexOf('/') + 1, modulePath.lastIndexOf('.'))
-	register(code, () => import(/* @vite-ignore */ modulePath))
+  let code = modulePath.substring(modulePath.lastIndexOf('/') + 1, modulePath.lastIndexOf('.'));
+  register(code, () => import(/* @vite-ignore */ modulePath));
 }
 
 // register('en', () => import('./locales/en-EN.json'))
@@ -14,6 +14,6 @@ for (const modulePath in localeModules) {
 // register('fr', () => import('./locales/fr.json'))
 
 init({
-	fallbackLocale: defaultLocale,
-	initialLocale: browser ? window.navigator.language : defaultLocale,
-})
+  fallbackLocale: defaultLocale,
+  initialLocale: browser ? window.navigator.language : defaultLocale,
+});
