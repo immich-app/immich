7180ce590a9c0dcb55bf66676879fa8546a26c57
ben-basten <45583362+ben-basten@users.noreply.github.com>
2024-06-04 23:36:05 -0400

    feat: immich branding

---
+++ web/.storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api';
+import theme from './theme';
+
+addons.setConfig({
+  theme,
+});
---
+++ web/.storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+import brandImage from '../../design/immich-logo-inline-light.png';
+
+export default create({
+  base: 'light',
+  brandTitle: 'immich',
+  brandUrl: 'https://immich.app/',
+  brandImage,
+});
