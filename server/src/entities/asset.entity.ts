6051d72a8a6cc37a8dcb5c5a49824f2f0d4f6075
Fynn Petersen-Frey <zody22@gmail.com>
2024-06-09 00:26:35 +0200

    remove createdAt index

--- server/src/entities/asset.entity.ts
+++ server/src/entities/asset.entity.ts
@@ -83,7 +83,6 @@ export class AssetEntity {
   @Column({ type: 'varchar', nullable: true, default: '' })
   encodedVideoPath!: string | null;
 
-  @Index('idx_asset_created_at')
   @CreateDateColumn({ type: 'timestamptz' })
   createdAt!: Date;
 
