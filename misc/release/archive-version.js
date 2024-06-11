321c3ccfc6b1948b734909f0813cc60b13d467c5
Jason Rasmussen <jrasm91@gmail.com>
2024-06-11 05:33:45 -0400

    docs: version switcher (#10091)
    * docs: version switcher
    
    * chore: pump script
    
    * chore: fix linting on bash script
    
    * chore: remove 1.106.0 from archived versions
    
    * chore: change version archive script to take next server version not current version
    
    ---------
    
    Co-authored-by: Zack Pollard <zackpollard@ymail.com>

---
+++ misc/release/archive-version.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+const { readFileSync, writeFileSync } = require('node:fs');
+
+const lastVersion = process.argv[2];
+if (!lastVersion) {
+  console.log('Usage: archive-version.js <version>');
+  process.exit(1);
+}
+
+const filename = './docs/static/archived-versions.json';
+const oldVersions = JSON.parse(readFileSync(filename));
+const newVersions = [
+  { label: lastVersion, url: `https://${lastVersion}.archive.immich.app` },
+  ...oldVersions,
+];
+
+writeFileSync(filename, JSON.stringify(newVersions, null, 2) + '\n');
