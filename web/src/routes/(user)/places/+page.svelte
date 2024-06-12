476eea44df0643d0b4fb3da7fd2cfe4b9e56bb7c
Alex <alex.tran1502@gmail.com>
2024-06-12 06:12:58 -0500

    chore(web): remove thumbnail usage for places card (#10142)
    * chore(web): remove thumbnail usage for places
    
    * remove href attribute from Thumbnail
    
    * linting

--- web/src/routes/(user)/places/+page.svelte
+++ web/src/routes/(user)/places/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-  import Thumbnail from '$lib/components/assets/thumbnail/thumbnail.svelte';
   import Icon from '$lib/components/elements/icon.svelte';
   import UserPageLayout from '$lib/components/layouts/user-page-layout.svelte';
   import { AppRoute } from '$lib/constants';
   import { mdiMapMarkerOff } from '@mdi/js';
   import type { PageData } from './$types';
   import { getMetadataSearchQuery } from '$lib/utils/metadata-search';
-  import type { AssetResponseDto } from '@immich/sdk';
+  import { AssetMediaSize, type AssetResponseDto } from '@immich/sdk';
   import { t } from 'svelte-i18n';
+  import { getAssetThumbnailUrl } from '$lib/utils';
 
   export let data: PageData;
 
@@ -34,7 +34,11 @@
           <div
             class="flex w-[calc((100vw-(72px+5rem))/2)] max-w-[156px] justify-center overflow-hidden rounded-xl brightness-75 filter"
           >
-            <Thumbnail thumbnailSize={156} asset={item} readonly />
+            <img
+              src={getAssetThumbnailUrl({ id: item.id, size: AssetMediaSize.Thumbnail })}
+              alt={city}
+              class="object-cover w-[156px] h-[156px]"
+            />
           </div>
           <span
             class="w-100 absolute bottom-2 w-full text-ellipsis px-1 text-center text-sm font-medium capitalize text-white backdrop-blur-[1px] hover:cursor-pointer"
