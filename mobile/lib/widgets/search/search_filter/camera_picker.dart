e43840cf683611fcb561c4820c2073344861747e
RanKKI <youloveonlymeh@gmail.com>
2024-06-16 18:01:31 +1000

    refactor(search): fix dropdown menu's width

--- mobile/lib/widgets/search/search_filter/camera_picker.dart
+++ mobile/lib/widgets/search/search_filter/camera_picker.dart
@@ -2,9 +2,9 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:immich_mobile/extensions/build_context_extensions.dart';
 import 'package:immich_mobile/models/search/search_filter.model.dart';
 import 'package:immich_mobile/providers/search/search_filter.provider.dart';
+import 'package:immich_mobile/widgets/search/search_filter/common/dropdown.dart';
 import 'package:openapi/api.dart';
 
 class CameraPicker extends HookConsumerWidget {
@@ -32,29 +32,11 @@ class CameraPicker extends HookConsumerWidget {
       ),
     );
 
-    final inputDecorationTheme = InputDecorationTheme(
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      contentPadding: const EdgeInsets.only(left: 16),
-    );
-
-    final menuStyle = MenuStyle(
-      shape: WidgetStatePropertyAll<OutlinedBorder>(
-        RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
-        ),
-      ),
-    );
-
-    return Container(
-      padding: const EdgeInsets.only(
-          // bottom: MediaQuery.of(context).viewInsets.bottom,
-          ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          DropdownMenu(
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(
+          child: SearchDropdown(
             dropdownMenuEntries: switch (make) {
               AsyncError() => [],
               AsyncData(:final value) => value
@@ -67,15 +49,9 @@ class CameraPicker extends HookConsumerWidget {
                   .toList(),
               _ => [],
             },
-            width: context.width * 0.45,
-            menuHeight: 400,
             label: const Text('search_filter_camera_make').tr(),
-            inputDecorationTheme: inputDecorationTheme,
             controller: makeTextController,
-            menuStyle: menuStyle,
             leadingIcon: const Icon(Icons.photo_camera_rounded),
-            trailingIcon: const Icon(Icons.arrow_drop_down_rounded),
-            selectedTrailingIcon: const Icon(Icons.arrow_drop_up_rounded),
             onSelected: (value) {
               selectedMake.value = value.toString();
               onSelect({
@@ -84,7 +60,10 @@ class CameraPicker extends HookConsumerWidget {
               });
             },
           ),
-          DropdownMenu(
+        ),
+        const SizedBox(width: 16),
+        Expanded(
+          child: SearchDropdown(
             dropdownMenuEntries: switch (models) {
               AsyncError() => [],
               AsyncData(:final value) => value
@@ -97,15 +76,9 @@ class CameraPicker extends HookConsumerWidget {
                   .toList(),
               _ => [],
             },
-            width: context.width * 0.45,
-            menuHeight: 400,
             label: const Text('search_filter_camera_model').tr(),
-            inputDecorationTheme: inputDecorationTheme,
             controller: modelTextController,
-            menuStyle: menuStyle,
             leadingIcon: const Icon(Icons.camera),
-            trailingIcon: const Icon(Icons.arrow_drop_down_rounded),
-            selectedTrailingIcon: const Icon(Icons.arrow_drop_up_rounded),
             onSelected: (value) {
               selectedModel.value = value.toString();
               onSelect({
@@ -114,8 +87,8 @@ class CameraPicker extends HookConsumerWidget {
               });
             },
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
