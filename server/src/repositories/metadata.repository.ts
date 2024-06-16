d3b1b5f609cf23a48601452ede546e06c632fe77
Stephen Smith <stephen304@gmail.com>
2024-06-15 20:39:14 -0400

    chore(server): consolidate exiftool config into constructor along with writeArgs

--- server/src/repositories/metadata.repository.ts
+++ server/src/repositories/metadata.repository.ts
@@ -29,26 +29,24 @@ export class MetadataRepository implements IMetadataRepository {
 
   private initExiftool() {
     return new ExifTool({
+      defaultVideosToUTC: true,
+      backfillTimezones: true,
+      inferTimezoneFromDatestamps: true,
+      useMWG: true,
+      numericTags: [...DefaultReadTaskOptions.numericTags, 'FocalLength'],
+      /* eslint unicorn/no-array-callback-reference: off, unicorn/no-array-method-this-argument: off */
+      geoTz: (lat, lon) => geotz.find(lat, lon)[0],
       // Enable exiftool LFS to parse metadata for files larger than 2GB.
       readArgs: ['-api', 'largefilesupport=1'],
+      writeArgs: ['-api', 'largefilesupport=1', '-overwrite_original'],
     });
   }
 
   readTags(path: string): Promise<ImmichTags | null> {
-    return this.exiftool
-      .read(path, {
-        defaultVideosToUTC: true,
-        backfillTimezones: true,
-        inferTimezoneFromDatestamps: true,
-        useMWG: true,
-        numericTags: [...DefaultReadTaskOptions.numericTags, 'FocalLength'],
-        /* eslint unicorn/no-array-callback-reference: off, unicorn/no-array-method-this-argument: off */
-        geoTz: (lat, lon) => geotz.find(lat, lon)[0],
-      })
-      .catch((error) => {
-        this.logger.warn(`Error reading exif data (${path}): ${error}`, error?.stack);
-        return null;
-      }) as Promise<ImmichTags | null>;
+    return this.exiftool.read(path).catch((error) => {
+      this.logger.warn(`Error reading exif data (${path}): ${error}`, error?.stack);
+      return null;
+    }) as Promise<ImmichTags | null>;
   }
 
   extractBinaryTag(path: string, tagName: string): Promise<Buffer> {
@@ -57,9 +55,7 @@ export class MetadataRepository implements IMetadataRepository {
 
   async writeTags(path: string, tags: Partial<Tags>): Promise<void> {
     try {
-      await this.exiftool.write(path, tags, {
-        writeArgs: ['-overwrite_original'],
-      });
+      await this.exiftool.write(path, tags);
     } catch (error) {
       this.logger.warn(`Error writing exif data (${path}): ${error}`);
     }
