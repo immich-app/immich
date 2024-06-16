8f7e6199918988c2f4ddad4b455b37cf9566ea01
RanKKI <youloveonlymeh@gmail.com>
2024-06-16 19:16:45 +1000

    feat(search): show camera make/model vertically on mobile devices

--- mobile/lib/widgets/search/search_filter/camera_picker.dart
+++ mobile/lib/widgets/search/search_filter/camera_picker.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:immich_mobile/extensions/build_context_extensions.dart';
 import 'package:immich_mobile/models/search/search_filter.model.dart';
 import 'package:immich_mobile/providers/search/search_filter.provider.dart';
 import 'package:immich_mobile/widgets/search/search_filter/common/dropdown.dart';
@@ -12,6 +13,7 @@ class CameraPicker extends HookConsumerWidget {
 
   final Function(Map<String, String?>) onSelect;
   final SearchCameraFilter? filter;
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final makeTextController = useTextEditingController(text: filter?.make);
@@ -32,62 +34,72 @@ class CameraPicker extends HookConsumerWidget {
       ),
     );
 
+    final makeWidget = SearchDropdown(
+      dropdownMenuEntries: switch (make) {
+        AsyncError() => [],
+        AsyncData(:final value) => value
+            .map(
+              (e) => DropdownMenuEntry(
+                value: e,
+                label: e,
+              ),
+            )
+            .toList(),
+        _ => [],
+      },
+      label: const Text('search_filter_camera_make').tr(),
+      controller: makeTextController,
+      leadingIcon: const Icon(Icons.photo_camera_rounded),
+      onSelected: (value) {
+        selectedMake.value = value.toString();
+        onSelect({
+          'make': selectedMake.value,
+          'model': selectedModel.value,
+        });
+      },
+    );
+
+    final modelWidget = SearchDropdown(
+      dropdownMenuEntries: switch (models) {
+        AsyncError() => [],
+        AsyncData(:final value) => value
+            .map(
+              (e) => DropdownMenuEntry(
+                value: e,
+                label: e,
+              ),
+            )
+            .toList(),
+        _ => [],
+      },
+      label: const Text('search_filter_camera_model').tr(),
+      controller: modelTextController,
+      leadingIcon: const Icon(Icons.camera),
+      onSelected: (value) {
+        selectedModel.value = value.toString();
+        onSelect({
+          'make': selectedMake.value,
+          'model': selectedModel.value,
+        });
+      },
+    );
+
+    if (context.isMobile) {
+      return Column(
+        children: [
+          makeWidget,
+          const SizedBox(height: 8),
+          modelWidget,
+        ],
+      );
+    }
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Expanded(
-          child: SearchDropdown(
-            dropdownMenuEntries: switch (make) {
-              AsyncError() => [],
-              AsyncData(:final value) => value
-                  .map(
-                    (e) => DropdownMenuEntry(
-                      value: e,
-                      label: e,
-                    ),
-                  )
-                  .toList(),
-              _ => [],
-            },
-            label: const Text('search_filter_camera_make').tr(),
-            controller: makeTextController,
-            leadingIcon: const Icon(Icons.photo_camera_rounded),
-            onSelected: (value) {
-              selectedMake.value = value.toString();
-              onSelect({
-                'make': selectedMake.value,
-                'model': selectedModel.value,
-              });
-            },
-          ),
-        ),
+        Expanded(child: makeWidget),
         const SizedBox(width: 16),
-        Expanded(
-          child: SearchDropdown(
-            dropdownMenuEntries: switch (models) {
-              AsyncError() => [],
-              AsyncData(:final value) => value
-                  .map(
-                    (e) => DropdownMenuEntry(
-                      value: e,
-                      label: e,
-                    ),
-                  )
-                  .toList(),
-              _ => [],
-            },
-            label: const Text('search_filter_camera_model').tr(),
-            controller: modelTextController,
-            leadingIcon: const Icon(Icons.camera),
-            onSelected: (value) {
-              selectedModel.value = value.toString();
-              onSelect({
-                'make': selectedMake.value,
-                'model': selectedModel.value,
-              });
-            },
-          ),
-        ),
+        Expanded(child: modelWidget),
       ],
     );
   }
