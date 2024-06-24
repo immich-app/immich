7a215fdfa68e552740ca7ef6a23d3bafe9926d18
Jason Rasmussen <jrasm91@gmail.com>
2024-06-24 09:22:28 -0400

    docs: update

---
+++ docs/docs/developer/translations.md
@@ -0,0 +1,21 @@
+# Translations
+
+:::tip
+You can request a new language [here](https://hosted.weblate.org/new-lang/immich/immich/).
+:::
+
+## Weblate
+
+[Weblate](https://weblate.org/) is a "libre software web-based continuous localization system". Immich localization efforts are managed on their [hosted platform](https://hosted.weblate.org/projects/immich/immich/).
+
+## International message format
+
+Plurals, numbers, dates and other locale specific message formats can be handled by using the [ICU message format](https://unicode-org.github.io/icu/userguide/format_parse/messages/). Internally, this is handled by the [intl-messageformat](https://www.npmjs.com/package/intl-messageformat) library. Their [documentation](https://formatjs.io/docs/intl-messageformat/) includes common, editable examples via a "live editor" feature, which can be useful to test and debug message formats.
+
+## Progress
+
+Immich currently supports the following languages:
+
+<a href="https://hosted.weblate.org/engage/immich/">
+<img src="https://hosted.weblate.org/widget/immich/immich/multi-auto.svg" alt="Translation status" />
+</a>
--- docs/src/pages/roadmap.tsx
+++ docs/src/pages/roadmap.tsx
@@ -14,6 +14,7 @@ import {
   mdiCheckboxMarked,
   mdiCloudUploadOutline,
   mdiCollage,
+  mdiContentDuplicate,
   mdiDevices,
   mdiEmailOutline,
   mdiExpansionCard,
@@ -28,6 +29,7 @@ import {
   mdiForum,
   mdiHandshakeOutline,
   mdiHeart,
+  mdiHistory,
   mdiImage,
   mdiImageAlbum,
   mdiImageEdit,
@@ -63,14 +65,13 @@ import {
   mdiVectorCombine,
   mdiVideo,
   mdiWeb,
-  mdiContentDuplicate,
 } from '@mdi/js';
 import Layout from '@theme/Layout';
 import React from 'react';
 import { Item, Timeline } from '../components/timeline';
 
 const releases = {
-  'v1.106.0': new Date(2024, 5, 11),
+  'v1.106.1': new Date(2024, 5, 11),
   'v1.104.0': new Date(2024, 4, 13),
   'v1.103.0': new Date(2024, 3, 29),
   'v1.102.0': new Date(2024, 3, 15),
@@ -201,14 +202,6 @@ const roadmap: Item[] = [
   },
   {
     done: false,
-    icon: mdiWeb,
-    iconColor: 'royalblue',
-    title: 'Web translations',
-    description: 'Translate the web application to multiple languages',
-    getDateLabel: () => 'Planned for 2024',
-  },
-  {
-    done: false,
     icon: mdiCameraBurst,
     iconColor: 'rebeccapurple',
     title: 'Auto stacking',
@@ -219,17 +212,30 @@ const roadmap: Item[] = [
 
 const milestones: Item[] = [
   withRelease({
+    icon: mdiHistory,
+    title: 'Versioned documentation',
+    description: 'View documentation as it was at the time of past releases',
+    release: 'v1.106.1',
+  }),
+  withRelease({
+    icon: mdiWeb,
+    iconColor: 'royalblue',
+    title: 'Web translations',
+    description: 'Translate the web application to multiple languages',
+    release: 'v1.106.1',
+  }),
+  withRelease({
     icon: mdiContentDuplicate,
     title: 'Similar image detection',
     description: 'Detect duplicate assets that aren’t exactly identical',
-    release: 'v1.106.0',
+    release: 'v1.106.1',
   }),
   withRelease({
     icon: mdiVectorCombine,
     title: 'Container consolidation',
     description:
       'The microservices container can be run as a worker within the server image, allowing us to remove it from the default stack.',
-    release: 'v1.106.0',
+    release: 'v1.106.1',
   }),
   withRelease({
     icon: mdiPencil,
