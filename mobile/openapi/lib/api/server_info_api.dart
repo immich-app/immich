221b835e3bab28902ba9015bd03ecf75357f404c
Jason Rasmussen <jrasm91@gmail.com>
2024-06-25 10:59:42 -0400

    feat: build metadata

--- mobile/openapi/lib/api/server_info_api.dart
+++ mobile/openapi/lib/api/server_info_api.dart
@@ -16,6 +16,47 @@ class ServerInfoApi {
 
   final ApiClient apiClient;
 
+  /// Performs an HTTP 'GET /server-info/about' operation and returns the [Response].
+  Future<Response> getAboutInfoWithHttpInfo() async {
+    // ignore: prefer_const_declarations
+    final path = r'/server-info/about';
+
+    // ignore: prefer_final_locals
+    Object? postBody;
+
+    final queryParams = <QueryParam>[];
+    final headerParams = <String, String>{};
+    final formParams = <String, String>{};
+
+    const contentTypes = <String>[];
+
+
+    return apiClient.invokeAPI(
+      path,
+      'GET',
+      queryParams,
+      postBody,
+      headerParams,
+      formParams,
+      contentTypes.isEmpty ? null : contentTypes.first,
+    );
+  }
+
+  Future<ServerAboutResponseDto?> getAboutInfo() async {
+    final response = await getAboutInfoWithHttpInfo();
+    if (response.statusCode >= HttpStatus.badRequest) {
+      throw ApiException(response.statusCode, await _decodeBodyBytes(response));
+    }
+    // When a remote server returns no body with a status of 204, we shall not decode it.
+    // At the time of writing this, `dart:convert` will throw an "Unexpected end of input"
+    // FormatException when trying to decode an empty string.
+    if (response.body.isNotEmpty && response.statusCode != HttpStatus.noContent) {
+      return await apiClient.deserializeAsync(await _decodeBodyBytes(response), 'ServerAboutResponseDto',) as ServerAboutResponseDto;
+    
+    }
+    return null;
+  }
+
   /// Performs an HTTP 'GET /server-info/config' operation and returns the [Response].
   Future<Response> getServerConfigWithHttpInfo() async {
     // ignore: prefer_const_declarations
