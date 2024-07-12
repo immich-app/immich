0ddd31d6047018d310cba170f7c0cf40a0a37fc0
YuvRaj-at-Unity <yuvilds@gmail.com>
2024-07-12 17:04:00 -0400

    Small edits

--- mobile/lib/pages/editing/edit.page.dart
+++ mobile/lib/pages/editing/edit.page.dart
@@ -6,57 +6,52 @@ import 'package:immich_mobile/pages/editing/crop.page.dart';
 import 'package:immich_mobile/entities/asset.entity.dart';
 import 'package:immich_mobile/widgets/common/immich_image.dart';
 import 'package:immich_mobile/widgets/common/immich_toast.dart';
+
 @immutable
 class EditImagePage extends StatelessWidget {
   final Asset? asset;
   final Image? image;
+
   const EditImagePage({
     super.key,
     this.image,
     this.asset,
-  }) : assert((image != null && asset == null) || (image == null && asset != null), 'Must supply one of asset or image');
-  ImageProvider _getImageProvider() {
-    if (asset is Asset) {
-      return ImmichImage.imageProvider(asset: asset);
-    } else if (image is Image) {
-      return (image as Image).image;
-    } else {
-      throw Exception('Invalid image source type');
-    }
-  }
-  void _navigateToCropImagePage(BuildContext context) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => CropImagePage(image: _getImageWidget()),
-      ),
-    );
-  }
+  }) : assert(
+            (image != null && asset == null) ||
+                (image == null && asset != null),
+            'Must supply one of asset or image');
+
   Image _getImageWidget() {
     if (asset != null) {
-      return Image(image: ImmichImage.imageProvider(asset: asset));
+      return Image(image: ImmichImage.imageProvider(asset: asset!));
     } else if (image != null) {
       return image!;
     } else {
       throw Exception('Invalid image source type');
     }
   }
+
   @override
   Widget build(BuildContext context) {
-    final ImageProvider provider = _getImageProvider();
+    final ImageProvider provider = (asset != null) 
+        ? ImmichImage.imageProvider(asset: asset!) 
+        : (image != null) 
+            ? image!.image 
+            : throw Exception('Invalid image source type');
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
         leading: IconButton(
           icon: const Icon(Icons.close_rounded, color: Colors.white, size: 24),
-          onPressed: () => Navigator.of(context).popUntil((route) => route.isFirst),
+          onPressed: () =>
+              Navigator.of(context).popUntil((route) => route.isFirst),
         ),
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.done_rounded, color: Colors.white, size: 24),
             onPressed: () {
-              // Save the image into cloud
+              
               if (asset != null) {
                 ImmichToast.show(
                   durationInSecond: 1,
@@ -94,10 +89,17 @@ class EditImagePage extends StatelessWidget {
           children: <Widget>[
             IconButton(
               icon: Icon(
-                Platform.isAndroid ? Icons.crop_rotate_rounded : Icons.crop_rotate_rounded,
+                Platform.isAndroid
+                    ? Icons.crop_rotate_rounded
+                    : Icons.crop_rotate_rounded,
                 color: Colors.white,
               ),
-              onPressed: () => _navigateToCropImagePage(context),
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CropImagePage(image: _getImageWidget()),
+                ),
+              ),
             ),
           ],
         ),
