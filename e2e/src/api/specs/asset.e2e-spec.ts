09415a563a3a78019441f145d80ed8a74a1f8640
Zack Pollard <zackpollard@ymail.com>
2024-07-18 11:22:25 +0100

    test: update e2e naming

--- e2e/src/api/specs/asset.e2e-spec.ts
+++ e2e/src/api/specs/asset.e2e-spec.ts
@@ -507,7 +507,7 @@ describe('/asset', () => {
       expect(status).toEqual(200);
     });
 
-    it('should geocode gps data in the middle of nowhere', async () => {
+    it('should geocode country from gps data in the middle of nowhere', async () => {
       const { status } = await request(app)
         .put(`/assets/${user1Assets[0].id}`)
         .set('Authorization', `Bearer ${user1.accessToken}`)
