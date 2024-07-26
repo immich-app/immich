b630e4fb4acfa1bf6145bf02aad065c473a1f3c9
Calum Dingwall <29152895+caburum@users.noreply.github.com>
2024-07-26 17:59:36 -0600

    feat(web): persist albums page scroll position

--- web/src/lib/components/layouts/user-page-layout.svelte
+++ web/src/lib/components/layouts/user-page-layout.svelte
@@ -11,7 +11,7 @@
   export let scrollbar = true;
   export let admin = false;
 
-  export let scrollSlot: HTMLDivElement;
+  export let scrollSlot: HTMLDivElement | undefined = undefined;
 
   $: scrollbarClass = scrollbar ? 'immich-scrollbar p-2 pb-8' : 'scrollbar-hidden';
   $: hasTitleClass = title ? 'top-16 h-[calc(100%-theme(spacing.16))]' : 'top-0 h-full';
--- web/src/routes/(user)/albums/+page.svelte
+++ web/src/routes/(user)/albums/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { PageData } from './$types';
+  import { beforeNavigate } from '$app/navigation';
   import { AlbumFilter, albumViewSettings } from '$lib/stores/preferences.store';
   import { createAlbumAndRedirect } from '$lib/utils/album-utils';
   import UserPageLayout from '$lib/components/layouts/user-page-layout.svelte';
@@ -8,15 +9,36 @@
   import EmptyPlaceholder from '$lib/components/shared-components/empty-placeholder.svelte';
   import GroupTab from '$lib/components/elements/group-tab.svelte';
   import SearchBar from '$lib/components/elements/search-bar.svelte';
+  import { AppRoute, SessionStorageKey } from '$lib/constants';
+  import { onMount } from 'svelte';
   import { t } from 'svelte-i18n';
 
   export let data: PageData;
+
+  let scrollSlot: HTMLDivElement;
+  beforeNavigate(({ to }) => {
+    // Save current scroll information when going into a person page.
+    if (to && to.url.pathname.startsWith(AppRoute.ALBUMS)) {
+      sessionStorage.setItem(SessionStorageKey.SCROLL_POSITION, scrollSlot.scrollTop.toString());
+    } else {
+      sessionStorage.removeItem(SessionStorageKey.SCROLL_POSITION);
+    }
+  });
+  onMount(() => {
+    let newScroll = sessionStorage.getItem(SessionStorageKey.SCROLL_POSITION);
+    if (newScroll)
+      scrollSlot.scroll({
+        top: parseFloat(newScroll),
+        behavior: 'instant',
+      });
+    sessionStorage.removeItem(SessionStorageKey.SCROLL_POSITION);
+  });
 
   let searchQuery = '';
   let albumGroups: string[] = [];
 </script>
 
-<UserPageLayout title={data.meta.title}>
+<UserPageLayout title={data.meta.title} bind:scrollSlot>
   <div class="flex place-items-center gap-2" slot="buttons">
     <AlbumsControls {albumGroups} bind:searchQuery />
   </div>
