d0f1e7ff843be2705fbaf846758834a4eca3dcd4
mertalev <101130780+mertalev@users.noreply.github.com>
2024-07-28 12:57:14 -0400

    onnx save function

--- machine-learning/export/models/optimize.py
+++ machine-learning/export/models/optimize.py
@@ -4,6 +4,16 @@ import onnx
 import onnxruntime as ort
 import onnxsim
 
+
+def save_onnx(model: onnx.ModelProto, output_path: Path | str) -> None:
+    try:
+        onnx.save(model, output_path)
+    except ValueError as e:
+        if "The proto size is larger than the 2 GB limit." in str(e):
+            onnx.save(model, output_path, save_as_external_data=True, size_threshold=1_000_000)
+        else:
+            raise e
+
 def optimize_onnxsim(model_path: Path | str, output_path: Path | str) -> None:
     model_path = Path(model_path)
     output_path = Path(output_path)
@@ -13,13 +23,7 @@ def optimize_onnxsim(model_path: Path | str, output_path: Path | str) -> None:
     for file in model_path.parent.iterdir():
         if file.name.startswith("Constant") or "onnx" in file.name or file.suffix == ".weight":
             file.unlink()
-    try:
-        onnx.save(model, output_path.as_posix())
-    except ValueError as e:
-        if "The proto size is larger than the 2 GB limit." in str(e):
-            onnx.save(model, output_path.as_posix(), save_as_external_data=True, size_threshold=1_000_000)
-        else:
-            raise e
+    save_onnx(model, output_path)
 
 
 def optimize_ort(
