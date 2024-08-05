19289a27fab00d03c97de1d4ef56d9c3dbe44f90
Daniel Dietzler <mail@ddietzler.dev>
2024-08-05 21:43:38 +0200

    chore: migrate to eslint flat config files

--- web/src/lib/components/asset-viewer/detail-panel.svelte
+++ web/src/lib/components/asset-viewer/detail-panel.svelte
@@ -391,7 +391,7 @@
           <p>{asset.exifInfo.make || ''} {asset.exifInfo.model || ''}</p>
           <div class="flex gap-2 text-sm">
             {#if asset.exifInfo?.fNumber}
-              <p>{`ƒ/${asset.exifInfo.fNumber.toLocaleString($locale)}` || ''}</p>
+              <p>{$locale ? `ƒ/${asset.exifInfo.fNumber.toLocaleString($locale)}` : ''}</p>
             {/if}
 
             {#if asset.exifInfo.exposureTime}
