0eacdf93ebf32dfdeb672e7b67d2aa57e96baea4
Pruthvi Bugidi <pruthvi.bugidi@proton.me>
2024-08-06 19:50:27 +0530

    feat(mobile): add support for material themes (#11560)
    * feat(mobile): add support for material themes
    
    Added support for custom theming and updated all elements accordingly.
    
    * fix(mobile): Restored immich brand colors to default theme
    
    * fix(mobile): make ListTile titles bold in settings main page
    
    * feat(mobile): update bottom nav and appbar colors
    
    * small tweaks
    
    ---------
    
    Co-authored-by: Alex <alex.tran1502@gmail.com>

---
+++ mobile/lib/extensions/theme_extensions.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+extension ImmichColorSchemeExtensions on ColorScheme {
+  bool get _isDarkMode => brightness == Brightness.dark;
+  Color get onSurfaceSecondary => _isDarkMode
+      ? onSurface.darken(amount: .3)
+      : onSurface.lighten(amount: .3);
+}
+
+extension ColorExtensions on Color {
+  Color lighten({double amount = 0.1}) {
+    return Color.alphaBlend(
+      Colors.white.withOpacity(amount),
+      this,
+    );
+  }
+
+  Color darken({double amount = 0.1}) {
+    return Color.alphaBlend(
+      Colors.black.withOpacity(amount),
+      this,
+    );
+  }
+}
