b785af962e266ebd0b451da91bc1d5606428bb87
YuvRaj-at-Unity <yuvilds@gmail.com>
2024-08-13 00:59:16 -0400

    Fix null name

--- mobile/lib/pages/editing/edit.page.dart
+++ mobile/lib/pages/editing/edit.page.dart
@@ -24,18 +24,16 @@ import 'package:immich_mobile/providers/album/album.provider.dart';
 @immutable
 @RoutePage()
 class EditImagePage extends ConsumerWidget {
-  final Asset? asset;
-  final Image? image;
+  final Asset asset;
+  final Image image;
+  final bool isEdited;
 
   const EditImagePage({
     super.key,
-    this.image,
-    this.asset,
-  }) : assert(
-          (image != null && asset == null) || (image == null && asset != null),
-          'Must supply one of asset or image',
-        );
-
+    required this.asset,
+    required this.image,
+    required this.isEdited,
+  });
   Future<Uint8List> _imageToUint8List(Image image) async {
     final Completer<Uint8List> completer = Completer();
     image.image.resolve(const ImageConfiguration()).addListener(
@@ -60,17 +58,8 @@ class EditImagePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final ImageProvider provider = (asset != null)
-        ? ImmichImage.imageProvider(asset: asset!)
-        : (image != null)
-            ? image!.image
-            : throw Exception('Invalid image source type');
-
-    final Image imageWidget = (asset != null)
-        ? Image(image: ImmichImage.imageProvider(asset: asset!))
-        : (image != null)
-            ? image!
-            : throw Exception('Invalid image source type');
+    final Image imageWidget =
+        Image(image: ImmichImage.imageProvider(asset: asset));
 
     return Scaffold(
       appBar: AppBar(
@@ -85,44 +74,43 @@ class EditImagePage extends ConsumerWidget {
               Navigator.of(context).popUntil((route) => route.isFirst),
         ),
         actions: <Widget>[
-          if (image != null)
-            TextButton(
-              onPressed: () async {
-                try {
-                  final Uint8List imageData = await _imageToUint8List(image!);
-                  ImmichToast.show(
-                    durationInSecond: 3,
-                    context: context,
-                    msg: 'Image Saved!',
-                    gravity: ToastGravity.CENTER,
-                  );
+          TextButton(
+            onPressed: () async {
+              try {
+                final Uint8List imageData = await _imageToUint8List(image);
+                ImmichToast.show(
+                  durationInSecond: 3,
+                  context: context,
+                  msg: 'Image Saved!',
+                  gravity: ToastGravity.CENTER,
+                );
 
-                  await PhotoManager.editor.saveImage(
-                    imageData,
-                    title: '${asset!.fileName}_edited.jpg',
-                  );
-                  await ref.read(albumProvider.notifier).getDeviceAlbums();
-                  Navigator.of(context).popUntil((route) => route.isFirst);
-                } catch (e) {
-                  ImmichToast.show(
-                    durationInSecond: 6,
-                    context: context,
-                    msg: 'Error: ${e.toString()}',
-                    gravity: ToastGravity.BOTTOM,
-                  );
-                }
-              },
-              child: Text(
-                'Save to gallery',
-                style: Theme.of(context).textTheme.displayMedium,
-              ),
+                await PhotoManager.editor.saveImage(
+                  imageData,
+                  title: "${asset.fileName.split('.').first}_edited.jpg",
+                );
+                await ref.read(albumProvider.notifier).getDeviceAlbums();
+                Navigator.of(context).popUntil((route) => route.isFirst);
+              } catch (e) {
+                ImmichToast.show(
+                  durationInSecond: 6,
+                  context: context,
+                  msg: 'Error: ${e.toString()}',
+                  gravity: ToastGravity.BOTTOM,
+                );
+              }
+            },
+            child: Text(
+              'Save to gallery',
+              style: Theme.of(context).textTheme.displayMedium,
             ),
+          ),
         ],
       ),
       body: Column(
         children: <Widget>[
           Expanded(
-            child: Image(image: provider),
+            child: image,
           ),
           Container(
             height: 80,
@@ -148,7 +136,9 @@ class EditImagePage extends ConsumerWidget {
                 color: Theme.of(context).iconTheme.color,
               ),
               onPressed: () {
-                context.pushRoute(CropImageRoute(image: imageWidget));
+                context.pushRoute(
+                  CropImageRoute(asset: asset, image: imageWidget),
+                );
               },
             ),
             Text('Crop', style: Theme.of(context).textTheme.displayMedium),
--- mobile/lib/widgets/asset_viewer/bottom_gallery_bar.dart
+++ mobile/lib/widgets/asset_viewer/bottom_gallery_bar.dart
@@ -16,6 +16,7 @@ import 'package:immich_mobile/widgets/asset_grid/asset_grid_data_structure.dart'
 import 'package:immich_mobile/widgets/asset_viewer/video_controls.dart';
 import 'package:immich_mobile/widgets/asset_grid/delete_dialog.dart';
 import 'package:immich_mobile/routing/router.dart';
+import 'package:immich_mobile/widgets/common/immich_image.dart';
 import 'package:immich_mobile/entities/asset.entity.dart';
 import 'package:immich_mobile/providers/asset.provider.dart';
 import 'package:immich_mobile/providers/server_info.provider.dart';
@@ -236,6 +237,7 @@ class BottomGalleryBar extends ConsumerWidget {
     }
 
     void handleEdit() async {
+      final image = Image(image: ImmichImage.imageProvider(asset: asset));
       if (asset.isOffline) {
         ImmichToast.show(
           durationInSecond: 1,
@@ -247,8 +249,11 @@ class BottomGalleryBar extends ConsumerWidget {
       }
       Navigator.of(context).push(
         MaterialPageRoute(
-          builder: (context) =>
-              EditImagePage(asset: asset), // Send the Asset object
+          builder: (context) => EditImagePage(
+            asset: asset,
+            image: image,
+            isEdited: false,
+          ),
         ),
       );
     }
