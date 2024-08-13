9cb81836b1cdafe93abfd7eedc79d556168c6a6c
Alex <alex.tran1502@gmail.com>
2024-08-13 11:33:19 -0500

    creating patch

---
+++ mobile/lib/utils/openapi_patching.dart
@@ -0,0 +1,11 @@
+String upgradeDto(dynamic value, String targetType) {
+  switch (targetType) {
+    case 'UserPreferencesResponseDto':
+      // patch here
+      print("doing stuffssssssss");
+
+      break;
+  }
+
+  return targetType;
+}
