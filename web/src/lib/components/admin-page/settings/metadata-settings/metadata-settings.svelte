6289e0ebb4266b1a84a7f68c87e823610a351aae
BugFest <bugfest.dev@pm.me>
2024-08-16 12:34:38 +0200

    fix(web): simplify metadata admin settings and i18n keys

--- web/src/lib/components/admin-page/settings/metadata-settings/metadata-settings.svelte
+++ web/src/lib/components/admin-page/settings/metadata-settings/metadata-settings.svelte
@@ -3,7 +3,6 @@
   import { isEqual } from 'lodash-es';
   import { fade } from 'svelte/transition';
   import type { SettingsResetEvent, SettingsSaveEvent } from '../admin-settings';
-  import SettingAccordion from '$lib/components/shared-components/settings/setting-accordion.svelte';
   import SettingButtonsRow from '$lib/components/shared-components/settings/setting-buttons-row.svelte';
   import SettingSwitch from '$lib/components/shared-components/settings/setting-switch.svelte';
   import { t } from 'svelte-i18n';
@@ -19,21 +18,14 @@
 <div class="mt-2">
   <div in:fade={{ duration: 500 }}>
     <form autocomplete="off" on:submit|preventDefault class="mx-4 mt-4">
-      <SettingAccordion
-        key="import-faces-metadata"
-        title={$t('admin.import_faces_metadata')}
-        subtitle={$t('admin.import_faces_metadata_description')}
-        isOpen
-      >
-        <div class="ml-4 mt-4 flex flex-col gap-4">
-          <SettingSwitch
-            title={$t('admin.import_faces_metadata_setting')}
-            subtitle={$t('admin.import_faces_metadata_setting_description')}
-            bind:checked={config.metadata.faces.import}
-            {disabled}
-          />
-        </div>
-      </SettingAccordion>
+      <div class="ml-4 mt-4 flex flex-col gap-4">
+        <SettingSwitch
+          title={$t('admin.metadata_faces_import_setting')}
+          subtitle={$t('admin.metadata_faces_import_setting_description')}
+          bind:checked={config.metadata.faces.import}
+          {disabled}
+        />
+      </div>
 
       <SettingButtonsRow
         onReset={(options) => onReset({ ...options, configKeys: ['metadata'] })}
