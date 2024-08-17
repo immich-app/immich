feea44af1028b988545fc712798e7c6a6fcca661
BugFest <bugfest.dev@pm.me>
2024-08-17 10:31:43 +0200

    fix(server): asset_faces.sourceType column not nullable

--- server/src/migrations/1721249222549-AddSourceColumnToAssetFace.ts
+++ server/src/migrations/1721249222549-AddSourceColumnToAssetFace.ts
@@ -5,7 +5,7 @@ export class AddSourceColumnToAssetFace1721249222549 implements MigrationInterfa
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TYPE sourceType AS ENUM ('machine-learning', 'exif');`);
-        await queryRunner.query(`ALTER TABLE "asset_faces" ADD "sourceType" sourceType DEFAULT 'machine-learning'`);
+        await queryRunner.query(`ALTER TABLE "asset_faces" ADD "sourceType" sourceType NOT NULL DEFAULT 'machine-learning'`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
