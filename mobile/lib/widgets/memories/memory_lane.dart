335d002cff6b47860a17747cb1c6dad124bd5965
Alex <alex.tran1502@gmail.com>
2024-08-18 15:37:59 -0500

    feat(mobile): memories lane with the new CarouselView

--- mobile/lib/widgets/memories/memory_lane.dart
+++ mobile/lib/widgets/memories/memory_lane.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:immich_mobile/models/memories/memory.model.dart';
 import 'package:immich_mobile/widgets/asset_grid/thumbnail_placeholder.dart';
 import 'package:immich_mobile/providers/memory.provider.dart';
 import 'package:immich_mobile/routing/router.dart';
@@ -9,6 +10,7 @@ import 'package:immich_mobile/widgets/common/immich_image.dart';
 
 class MemoryLane extends HookConsumerWidget {
   const MemoryLane({super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final memoryLaneFutureProvider = ref.watch(memoryFutureProvider);
@@ -16,82 +18,36 @@ class MemoryLane extends HookConsumerWidget {
     final memoryLane = memoryLaneFutureProvider
         .whenData(
           (memories) => memories != null
-              ? SizedBox(
-                  height: 200,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    shrinkWrap: true,
-                    itemCount: memories.length,
-                    padding: const EdgeInsets.only(
-                      right: 8.0,
-                      bottom: 8,
-                      top: 10,
-                      left: 10,
+              ? Center(
+                  child: ConstrainedBox(
+                    constraints: const BoxConstraints(
+                      maxHeight: 180,
                     ),
-                    itemBuilder: (context, index) {
-                      final memory = memories[index];
-
-                      return GestureDetector(
-                        onTap: () {
-                          ref
-                              .read(hapticFeedbackProvider.notifier)
-                              .heavyImpact();
-                          context.pushRoute(
-                            MemoryRoute(
-                              memories: memories,
-                              memoryIndex: index,
-                            ),
-                          );
-                        },
-                        child: Stack(
-                          children: [
-                            Card(
-                              elevation: 3,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(13.0),
-                              ),
-                              clipBehavior: Clip.hardEdge,
-                              child: ColorFiltered(
-                                colorFilter: ColorFilter.mode(
-                                  Colors.black.withOpacity(0.2),
-                                  BlendMode.darken,
-                                ),
-                                child: Hero(
-                                  tag: 'memory-${memory.assets[0].id}',
-                                  child: ImmichImage(
-                                    memory.assets[0],
-                                    fit: BoxFit.cover,
-                                    width: 130,
-                                    height: 200,
-                                    placeholder: const ThumbnailPlaceholder(
-                                      width: 130,
-                                      height: 200,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Positioned(
-                              bottom: 16,
-                              left: 16,
-                              child: ConstrainedBox(
-                                constraints: const BoxConstraints(
-                                  maxWidth: 114,
-                                ),
-                                child: Text(
-                                  memory.title,
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.w600,
-                                    color: Colors.white,
-                                    fontSize: 15,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
+                    child: CarouselView(
+                      itemExtent: 130,
+                      shrinkExtent: 1.0,
+                      elevation: 1,
+                      backgroundColor: Colors.black,
+                      overlayColor: WidgetStateProperty.all(
+                        Colors.white.withOpacity(0.1),
+                      ),
+                      onTap: (memoryIndex) {
+                        ref.read(hapticFeedbackProvider.notifier).heavyImpact();
+                        context.pushRoute(
+                          MemoryRoute(
+                            memories: memories,
+                            memoryIndex: memoryIndex,
+                          ),
+                        );
+                      },
+                      children:
+                          List<Widget>.generate(memories.length, (int index) {
+                        return MemoryCard(
+                          memory: memories[index],
+                          index: index,
+                        );
+                      }),
+                    ),
                   ),
                 )
               : const SizedBox(),
@@ -99,5 +55,60 @@ class MemoryLane extends HookConsumerWidget {
         .value;
 
     return memoryLane ?? const SizedBox();
+  }
+}
+
+class MemoryCard extends ConsumerWidget {
+  const MemoryCard({
+    super.key,
+    required this.index,
+    required this.memory,
+  });
+
+  final int index;
+  final Memory memory;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Stack(
+      children: [
+        ColorFiltered(
+          colorFilter: ColorFilter.mode(
+            Colors.black.withOpacity(0.2),
+            BlendMode.darken,
+          ),
+          child: Hero(
+            tag: 'memory-${memory.assets[0].id}',
+            child: ImmichImage(
+              memory.assets[0],
+              fit: BoxFit.cover,
+              width: 145,
+              height: 200,
+              placeholder: const ThumbnailPlaceholder(
+                width: 145,
+                height: 200,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 16,
+          left: 16,
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxWidth: 114,
+            ),
+            child: Text(
+              memory.title,
+              style: const TextStyle(
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+                fontSize: 15,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
