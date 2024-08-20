8ab7bdd54395d282939e05548993433051f7adc1
Alex Tran <alex.tran1502@gmail.com>
2024-08-20 18:08:36 -0500

    clean as new

--- web/src/routes/(user)/folders/[[photos=photos]]/[[assetId=id]]/+page.svelte
+++ web/src/routes/(user)/folders/[[photos=photos]]/[[assetId=id]]/+page.svelte
@@ -4,7 +4,7 @@
   import UserPageLayout from '$lib/components/layouts/user-page-layout.svelte';
   import { type AssetResponseDto } from '@immich/sdk';
   import Icon from '$lib/components/elements/icon.svelte';
-  import { mdiArrowUpLeft, mdiChevronRight, mdiFolder } from '@mdi/js';
+  import { mdiArrowUpLeft, mdiChevronRight, mdiFolder, mdiFolderHome } from '@mdi/js';
   import CircleIconButton from '$lib/components/elements/buttons/circle-icon-button.svelte';
   import GalleryViewer from '$lib/components/shared-components/gallery-viewer/gallery-viewer.svelte';
   import type { Viewport } from '$lib/stores/assets.store';
@@ -47,10 +47,13 @@
     {#if data.path}
       <CircleIconButton icon={mdiArrowUpLeft} title="Back" on:click={handleBackNavigation} class="mr-2" padding="2" />
     {/if}
+
     <div
-      class="flex place-items-center gap-2 bg-slate-50 dark:bg-immich-dark-gray/75 w-full py-2 px-4 rounded-2xl mr-4"
+      class="flex place-items-center gap-2 bg-gray-50 dark:bg-immich-dark-gray/50 w-full py-2 px-4 rounded-2xl border border-gray-100 dark:border-gray-900"
     >
-      <Icon path={mdiFolder} class="text-immich-primary dark:text-immich-dark-primary" size={28} />
+      <a href={`${AppRoute.FOLDERS}`} title="To root">
+        <Icon path={mdiFolderHome} class="text-immich-primary dark:text-immich-dark-primary mr-2" size={28} />
+      </a>
       {#each pathSegments as segment, index}
         <button
           class="text-sm font-mono underline hover:font-semibold"
@@ -68,27 +71,31 @@
     </div>
   </section>
 
-  <section id="folder-detail-view" class="mt-4">
+  <section id="folder-detail-view" class="mt-2">
     <!-- Sub Folders -->
-    <div class="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 gap-2">
-      {#each data.currentFolders as subFolder}
-        <button
-          class="flex flex-col place-items-center gap-2 py-2 px-4 hover:bg-immich-primary/10 dark:hover:bg-immich-primary/40 rounded-xl"
-          on:click={() => handleNavigation(subFolder)}
-          title={subFolder}
-          type="button"
-        >
-          <Icon path={mdiFolder} class="text-immich-primary dark:text-immich-dark-primary" size={64} />
-          <p class="text-sm dark:text-gray-200 text-nowrap text-ellipsis overflow-clip w-full">{subFolder}</p>
-        </button>
-      {/each}
-    </div>
+    {#if data.currentFolders.length > 0}
+      <div
+        class="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 2xl:grid-cols-8 gap-2 bg-gray-50 dark:bg-immich-dark-gray/50 rounded-2xl border border-gray-100 dark:border-gray-900 max-h-[500px] overflow-auto"
+      >
+        {#each data.currentFolders as folder}
+          <button
+            class="flex flex-col place-items-center gap-2 py-2 px-4 hover:bg-immich-primary/10 dark:hover:bg-immich-primary/40 rounded-xl"
+            on:click={() => handleNavigation(folder)}
+            title={folder}
+            type="button"
+          >
+            <Icon path={mdiFolder} class="text-immich-primary dark:text-immich-dark-primary" size={64} />
+            <p class="text-sm dark:text-gray-200 text-nowrap text-ellipsis overflow-clip w-full">{folder}</p>
+          </button>
+        {/each}
+      </div>
+    {/if}
 
     <!-- Assets -->
     <div
       bind:clientHeight={viewport.height}
       bind:clientWidth={viewport.width}
-      class:mt-5={data.currentFolders.length > 0}
+      class:mt-4={data.currentFolders.length > 0}
     >
       {#if data.pathAssets && data.pathAssets.length > 0}
         <GalleryViewer assets={data.pathAssets} bind:selectedAssets on:intersected={loadNextPage} {viewport} />
