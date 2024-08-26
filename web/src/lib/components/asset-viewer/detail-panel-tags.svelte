10a242eaef1f8b89b18db4fa71e6a918c415b67a
Alex Tran <alex.tran1502@gmail.com>
2024-08-26 14:19:06 -0500

    feat: navigate to tag page from clicking on a tag

--- web/src/lib/components/asset-viewer/detail-panel-tags.svelte
+++ web/src/lib/components/asset-viewer/detail-panel-tags.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Badge from '$lib/components/elements/badge.svelte';
+  import { AppRoute } from '$lib/constants';
   import { isSharedLink } from '$lib/utils';
   import { type AssetResponseDto } from '@immich/sdk';
   import { t } from 'svelte-i18n';
@@ -15,9 +16,11 @@
     <div class="flex h-10 w-full items-center justify-between text-sm">
       <h2>{$t('tags').toUpperCase()}</h2>
     </div>
-    <section class="flex-wrap px-4 pt-2 gap-1">
+    <section class="flex flex-wrap pt-2 gap-1">
       {#each tags as tag (tag.id)}
-        <Badge rounded="full"><span class="text-xs px-1">{tag.value}</span></Badge>
+        <a href={encodeURI(`${AppRoute.TAGS}/?path=${tag.value}`)}>
+          <Badge rounded="full"><span class="text-xs px-1">{tag.value}</span></Badge>
+        </a>
       {/each}
     </section>
   </section>
