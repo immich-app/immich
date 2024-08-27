d0363737a2af339f623ffdb50e609fc6959c6654
Alex Tran <alex.tran1502@gmail.com>
2024-08-27 00:13:21 -0500

    Tag position and add tag button

--- web/src/lib/components/asset-viewer/detail-panel-tags.svelte
+++ web/src/lib/components/asset-viewer/detail-panel-tags.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import Badge from '$lib/components/elements/badge.svelte';
+  import Icon from '$lib/components/elements/icon.svelte';
   import { AppRoute } from '$lib/constants';
   import { isSharedLink } from '$lib/utils';
   import { type AssetResponseDto } from '@immich/sdk';
+  import { mdiPlus } from '@mdi/js';
   import { t } from 'svelte-i18n';
 
   export let asset: AssetResponseDto;
@@ -12,16 +14,22 @@
 </script>
 
 {#if isOwner && !isSharedLink() && tags.length > 0}
-  <section class="px-4 pt-2">
+  <section class="px-4">
     <div class="flex h-10 w-full items-center justify-between text-sm">
       <h2>{$t('tags').toUpperCase()}</h2>
     </div>
     <section class="flex flex-wrap pt-2 gap-1">
       {#each tags as tag (tag.id)}
         <a href={encodeURI(`${AppRoute.TAGS}/?path=${tag.value}`)}>
-          <Badge rounded="full"><span class="text-xs px-1">{tag.value}</span></Badge>
+          <Badge rounded="full"><span class="text-sm px-1" title={tag.value}>{tag.value}</span></Badge>
         </a>
       {/each}
+      <button
+        class="rounded-full bg-gray-100 dark:bg-gray-800 text-gray-600 dark:text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-700 hover:text-gray-700 dark:hover:text-gray-200 flex place-items-center place-content-center gap-1 px-2 py-1"
+        title="Add tag"
+      >
+        <span class="text-sm px-1 flex place-items-center place-content-center gap-1"><Icon path={mdiPlus} />Add</span>
+      </button>
     </section>
   </section>
 {/if}
