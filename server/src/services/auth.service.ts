1d2500e11bcbfe71387e8bbaa77a4241e8c94d92
Jason Rasmussen <jason@rasm.me>
2024-08-28 09:58:43 -0400

    chore: add oauth-callback path

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -356,7 +356,7 @@ export class AuthService {
   }
 
   private normalize(config: SystemConfig, redirectUri: string) {
-    const isMobile = redirectUri.startsWith(MOBILE_REDIRECT);
+    const isMobile = redirectUri.startsWith('app.immich:/');
     const { mobileRedirectUri, mobileOverrideEnabled } = config.oauth;
     if (isMobile && mobileOverrideEnabled && mobileRedirectUri) {
       return mobileRedirectUri;
