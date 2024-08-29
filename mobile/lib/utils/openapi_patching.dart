d8ace2694b324f5b43f19501bc49f2ab2e035d18
Alex <alex.tran1502@gmail.com>
2024-08-29 14:16:35 -0500

    Fix logic bug and add tests

--- mobile/lib/utils/openapi_patching.dart
+++ mobile/lib/utils/openapi_patching.dart
@@ -4,25 +4,30 @@ dynamic upgradeDto(dynamic value, String targetType) {
   switch (targetType) {
     case 'UserPreferencesResponseDto':
       if (value is Map) {
-        _addDefault(value, 'download.includeEmbeddedVideos', false);
-        _addDefault(value, 'folders', FoldersResponse().toJson());
-        _addDefault(value, 'memories', MemoriesResponse().toJson());
-        _addDefault(value, 'ratings', RatingsResponse().toJson());
-        _addDefault(value, 'people', PeopleResponse().toJson());
-        _addDefault(value, 'tags', TagsResponse().toJson());
+        addDefault(value, 'download.includeEmbeddedVideos', false);
+        addDefault(value, 'folders', FoldersResponse().toJson());
+        addDefault(value, 'memories', MemoriesResponse().toJson());
+        addDefault(value, 'ratings', RatingsResponse().toJson());
+        addDefault(value, 'people', PeopleResponse().toJson());
+        addDefault(value, 'tags', TagsResponse().toJson());
       }
       break;
   }
 }
 
-_addDefault(dynamic value, String keys, dynamic defaultValue) {
+addDefault(dynamic value, String keys, dynamic defaultValue) {
   // Loop through the keys and assign the default value if the key is not present
   List<String> keyList = keys.split('.');
   dynamic current = value;
-  for (int i = 0; i < keyList.length; i++) {
+
+  for (int i = 0; i < keyList.length - 1; i++) {
     if (current[keyList[i]] == null) {
-      current[keyList[i]] = defaultValue;
+      current[keyList[i]] = {};
     }
     current = current[keyList[i]];
+  }
+
+  if (current[keyList.last] == null) {
+    current[keyList.last] = defaultValue;
   }
 }
