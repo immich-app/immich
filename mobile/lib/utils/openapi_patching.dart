e9e7418aff15a6a2695a58a1c97d4c7206f17682
Alex <alex.tran1502@gmail.com>
2024-08-29 12:01:54 -0500

    implement helper method

--- mobile/lib/utils/openapi_patching.dart
+++ mobile/lib/utils/openapi_patching.dart
@@ -4,31 +4,25 @@ dynamic upgradeDto(dynamic value, String targetType) {
   switch (targetType) {
     case 'UserPreferencesResponseDto':
       if (value is Map) {
-        if (value['download']['includeEmbeddedVideos'] == null) {
-          value['download']['includeEmbeddedVideos'] = false;
-        }
-
-        // v1.113.0
-        if (value['folders'] == null) {
-          value['folders'] = FoldersResponse().toJson();
-        }
-
-        if (value['memories'] == null) {
-          value['memories'] = MemoriesResponse().toJson();
-        }
-
-        if (value['ratings'] == null) {
-          value['ratings'] = RatingsResponse().toJson();
-        }
-
-        if (value['people'] == null) {
-          value['people'] = PeopleResponse().toJson();
-        }
-
-        if (value['tags'] == null) {
-          value['tags'] = TagsResponse().toJson();
-        }
+        _addDefault(value, 'download.includeEmbeddedVideos', false);
+        _addDefault(value, 'folders', FoldersResponse().toJson());
+        _addDefault(value, 'memories', MemoriesResponse().toJson());
+        _addDefault(value, 'ratings', RatingsResponse().toJson());
+        _addDefault(value, 'people', PeopleResponse().toJson());
+        _addDefault(value, 'tags', TagsResponse().toJson());
       }
       break;
+  }
+}
+
+_addDefault(dynamic value, String keys, dynamic defaultValue) {
+  // Loop through the keys and assign the default value if the key is not present
+  List<String> keyList = keys.split('.');
+  dynamic current = value;
+  for (int i = 0; i < keyList.length; i++) {
+    if (current[keyList[i]] == null) {
+      current[keyList[i]] = defaultValue;
+    }
+    current = current[keyList[i]];
   }
 }
