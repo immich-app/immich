9f5a3f1e84b6cb6f0144eec1b0a3b248784a6da1
Michel Heusschen <59014050+michelheusschen@users.noreply.github.com>
2024-08-29 14:41:39 +0200

    chore(web): enforce valid translation keys using typescript (#12106)

--- web/src/lib/components/i18n/format-bold-message.svelte
+++ web/src/lib/components/i18n/format-bold-message.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import FormatMessage from '$lib/components/i18n/format-message.svelte';
   import type { InterpolationValues } from '$lib/components/i18n/format-message.svelte';
+  import type { Translations } from 'svelte-i18n';
 
-  export let key: string;
+  export let key: Translations;
   export let values: InterpolationValues = {};
 </script>
 
