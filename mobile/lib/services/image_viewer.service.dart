a883f0d1e5f4ba21cee661ac2958e54ff8792198
Alex <alex.tran1502@gmail.com>
2024-09-05 10:38:04 -0500

    remove unused import

--- mobile/lib/services/image_viewer.service.dart
+++ mobile/lib/services/image_viewer.service.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/extensions/response_extensions.dart';
 import 'package:immich_mobile/entities/asset.entity.dart';
