c5848112bb7c6d5019b783081f537076017d3e1d
Ben <45583362+ben-basten@users.noreply.github.com>
2024-09-05 09:24:24 -0400

    feat(web): add skip link to sidebar (#12330)
    Co-authored-by: Alex <alex.tran1502@gmail.com>

--- web/src/routes/(user)/folders/[[photos=photos]]/[[assetId=id]]/+page.svelte
+++ web/src/routes/(user)/folders/[[photos=photos]]/[[assetId=id]]/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { goto } from '$app/navigation';
   import { page } from '$app/stores';
-  import UserPageLayout from '$lib/components/layouts/user-page-layout.svelte';
+  import UserPageLayout, { headerId } from '$lib/components/layouts/user-page-layout.svelte';
   import GalleryViewer from '$lib/components/shared-components/gallery-viewer/gallery-viewer.svelte';
   import SideBarSection from '$lib/components/shared-components/side-bar/side-bar-section.svelte';
   import TreeItemThumbnails from '$lib/components/shared-components/tree/tree-item-thumbnails.svelte';
@@ -16,6 +16,7 @@
   import { t } from 'svelte-i18n';
   import type { PageData } from './$types';
   import Breadcrumbs from '$lib/components/shared-components/tree/breadcrumbs.svelte';
+  import SkipLink from '$lib/components/elements/buttons/skip-link.svelte';
 
   export let data: PageData;
 
@@ -48,6 +49,7 @@
 
 <UserPageLayout title={data.meta.title}>
   <SideBarSection slot="sidebar">
+    <SkipLink target={`#${headerId}`} text={$t('skip_to_folders')} />
     <section>
       <div class="text-xs pl-4 mb-2 dark:text-white">{$t('explorer').toUpperCase()}</div>
       <div class="h-full">
