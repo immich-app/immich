75a3d76a9c575b0d2953d8324fcc224b25d49d7d
martabal <74269598+martabal@users.noreply.github.com>
2024-09-06 07:52:38 +0200

    fix: dimensions

--- web/src/lib/utils/asset-utils.ts
+++ web/src/lib/utils/asset-utils.ts
@@ -536,8 +536,8 @@ const imgToBlob = async (imageElement: HTMLImageElement) => {
   const canvas = document.createElement('canvas');
   const context = canvas.getContext('2d');
 
-  canvas.width = imageElement.width;
-  canvas.height = imageElement.height;
+  canvas.width = imageElement.naturalWidth;
+  canvas.height = imageElement.naturalHeight;
 
   if (context) {
     context.drawImage(imageElement, 0, 0);
