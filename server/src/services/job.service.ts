0b979b6db401226b3499342a0037dba4208f1b3b
Jason Rasmussen <jason@rasm.me>
2024-09-12 09:33:38 -0400

    fix(server): missing break statement

--- server/src/services/job.service.ts
+++ server/src/services/job.service.ts
@@ -231,6 +231,7 @@ export class JobService {
           name: JobName.METADATA_EXTRACTION,
           data: { id: item.data.id, source: 'sidecar-write' },
         });
+        break;
       }
 
       case JobName.METADATA_EXTRACTION: {
@@ -289,6 +290,7 @@ export class JobService {
       }
 
       case JobName.GENERATE_THUMBNAIL: {
+        console.log({ data: item.data });
         if (!(item.data.notify || item.data.source === 'upload')) {
           break;
         }
