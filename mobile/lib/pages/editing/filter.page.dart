873b59e3b0f04dc7ec2e7dfd37173ef1164e5bf4
yuvilds <yuvilds@gmail.com>
2024-09-12 23:22:50 +0000

    Logic for saving the image

--- mobile/lib/pages/editing/filter.page.dart
+++ mobile/lib/pages/editing/filter.page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:immich_mobile/extensions/build_context_extensions.dart';
@@ -5,27 +7,66 @@ import 'package:immich_mobile/entities/asset.entity.dart';
 import 'package:immich_mobile/constants/filters.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:immich_mobile/routing/router.dart';
 
 @RoutePage()
 class FilterImagePage extends HookWidget {
   final Image image;
   final Asset asset;
-  const FilterImagePage({super.key, required this.image, required this.asset});
+
+  const FilterImagePage({
+    super.key,
+    required this.image,
+    required this.asset,
+  });
 
   @override
   Widget build(BuildContext context) {
     final colorFilter = useState<ColorFilter>(filters[0]);
     final selectedFilterIndex = useState<int>(0);
 
-    void applyFilter(ColorFilter filter, int index) {
-      colorFilter.value = filter;
-      selectedFilterIndex.value = index;
+    Future<ui.Image> createFilteredImage(
+        ui.Image inputImage, ColorFilter filter) {
+      final completer = Completer<ui.Image>();
+      final size =
+          Size(inputImage.width.toDouble(), inputImage.height.toDouble());
+      final recorder = ui.PictureRecorder();
+      final canvas = Canvas(recorder);
+
+      final paint = Paint()..colorFilter = filter;
+      canvas.drawImage(inputImage, Offset.zero, paint);
+
+      recorder
+          .endRecording()
+          .toImage(size.width.round(), size.height.round())
+          .then((image) {
+        completer.complete(image);
+      });
+
+      return completer.future;
+    }
+
+    Future<Image> applyFilterAndConvert(ColorFilter filter) async {
+      final completer = Completer<ui.Image>();
+      image.image
+          .resolve(ImageConfiguration.empty)
+          .addListener(ImageStreamListener((ImageInfo info, bool _) {
+        completer.complete(info.image);
+      }));
+      final uiImage = await completer.future;
+
+      final filteredUiImage = await createFilteredImage(uiImage, filter);
+      final byteData =
+          await filteredUiImage.toByteData(format: ui.ImageByteFormat.png);
+      final pngBytes = byteData!.buffer.asUint8List();
+
+      return Image.memory(pngBytes, fit: BoxFit.contain);
     }
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: context.scaffoldBackgroundColor,
-        title: Text("filters".tr()),
+        title: Text("filter".tr()),
         leading: CloseButton(color: context.primaryColor),
         actions: [
           IconButton(
@@ -35,52 +76,67 @@ class FilterImagePage extends HookWidget {
               size: 24,
             ),
             onPressed: () async {
-              //context.pushRoute(
-              //EditImageRoute(
-              //asset: asset,
-              //image: ,
-              //isEdited: true,
-              //),
-              //);
+              final filteredImage =
+                  await applyFilterAndConvert(colorFilter.value);
+              context.pushRoute(EditImageRoute(
+                asset: asset,
+                image: filteredImage,
+                isEdited: true,
+              ));
             },
           ),
         ],
       ),
       backgroundColor: context.scaffoldBackgroundColor,
-      body: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          return Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.only(top: 20),
-                width: double.infinity,
-                height: constraints.maxHeight * 0.8,
-                child: ColorFiltered(
-                  colorFilter: colorFilter.value,
-                  child: image,
-                ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: ColorFiltered(
+                colorFilter: colorFilter.value,
+                child: image,
               ),
-              Expanded(
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: filters.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: _FilterButton(
-                        image: image,
-                        label: filterNames[index],
-                        filter: filters[index],
-                        isSelected: selectedFilterIndex.value == index,
-                        onTap: () => applyFilter(filters[index], index),
+            ),
+          ),
+          SizedBox(
+            height: 100,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: filters.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Column(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          colorFilter.value = filters[index];
+                          selectedFilterIndex.value = index;
+                        },
+                        child: Container(
+                          width: 80,
+                          height: 80,
+                          decoration: BoxDecoration(
+                            border: selectedFilterIndex.value == index
+                                ? Border.all(
+                                    color: context.primaryColor, width: 2)
+                                : null,
+                          ),
+                          child: ColorFiltered(
+                            colorFilter: filters[index],
+                            child: Image(image: image.image, fit: BoxFit.cover),
+                          ),
+                        ),
                       ),
-                    );
-                  },
-                ),
-              )
-            ],
-          );
-        },
+                      Text(filterNames[index],
+                          style: Theme.of(context).textTheme.bodySmall),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
