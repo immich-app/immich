ba88eac25a8fd1121e54eff3ffceb061212b7b2a
Fynn Petersen-Frey <zody22@gmail.com>
2024-09-18 13:16:57 +0200

    try custom_lint on github actions

---
+++ mobile/immich_lint/lib/immich_mobile_immich_lint.dart
@@ -0,0 +1,46 @@
+import 'package:analyzer/error/listener.dart';
+import 'package:analyzer/error/error.dart' show ErrorSeverity;
+import 'package:custom_lint_builder/custom_lint_builder.dart';
+
+PluginBase createPlugin() => ImmichLinter();
+
+class ImmichLinter extends PluginBase {
+  @override
+  List<LintRule> getLintRules(CustomLintConfigs configs) => [
+        PhotoManagerRule(configs.rules[PhotoManagerRule._code.name]),
+      ];
+}
+
+class PhotoManagerRule extends DartLintRule {
+  PhotoManagerRule(LintOptions? options) : super(code: _code) {
+    final excludeOption = options?.json["exclude"];
+    if (excludeOption is String) {
+      _excludePaths.add(excludeOption);
+    } else if (excludeOption is List) {
+      _excludePaths.addAll(excludeOption.map((option) => option));
+    }
+  }
+
+  final Set<String> _excludePaths = {};
+
+  static const _code = LintCode(
+    name: 'photo_manager',
+    problemMessage:
+        'photo_manager library must only be used in MediaRepository',
+    errorSeverity: ErrorSeverity.WARNING,
+  );
+
+  @override
+  void run(
+    CustomLintResolver resolver,
+    ErrorReporter reporter,
+    CustomLintContext context,
+  ) {
+    if (_excludePaths.contains(resolver.source.shortName)) return;
+    context.registry.addImportDirective((node) {
+      if (node.uri.stringValue?.startsWith("package:photo_manager") == true) {
+        reporter.atNode(node, code);
+      }
+    });
+  }
+}
