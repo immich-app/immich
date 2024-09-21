d6495f014d559349850ae3c51e65d864857af44d
shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2024-09-21 12:11:33 +0530

    add license page

--- mobile-v2/lib/presentation/modules/settings/pages/about_settings.page.dart
+++ mobile-v2/lib/presentation/modules/settings/pages/about_settings.page.dart
@@ -1,6 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:immich_mobile/i18n/strings.g.dart';
+import 'package:immich_mobile/presentation/components/image/immich_logo.widget.dart';
 import 'package:immich_mobile/presentation/components/scaffold/adaptive_route_appbar.widget.dart';
+import 'package:immich_mobile/utils/constants/size_constants.dart';
 
 @RoutePage()
 class AboutSettingsPage extends StatelessWidget {
@@ -8,9 +11,16 @@ class AboutSettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: ImAdaptiveRouteSecondaryAppBar(),
-      body: Center(child: Text('About Settings')),
+    return Scaffold(
+      appBar: const ImAdaptiveRouteSecondaryAppBar(),
+      body: ListTile(
+        title: Text(context.t.settings.about.third_party_title),
+        subtitle: Text(context.t.settings.about.third_party_sub_title),
+        onTap: () => showLicensePage(
+            context: context,
+            applicationName: "Immich",
+            applicationIcon: const ImLogo(width: SizeConstants.xl)),
+      ),
     );
   }
 }
