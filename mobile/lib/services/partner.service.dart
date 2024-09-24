1dd9b17edfda52d4434da626d9ddbaa51d290062
Fynn Petersen-Frey <zody22@gmail.com>
2024-09-24 10:34:14 +0200

    finish refactor partner service

--- mobile/lib/services/partner.service.dart
+++ mobile/lib/services/partner.service.dart
@@ -2,35 +2,30 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/entities/user.entity.dart';
 import 'package:immich_mobile/interfaces/user.interface.dart';
 import 'package:immich_mobile/interfaces/user_api.interface.dart';
-import 'package:immich_mobile/providers/api.provider.dart';
 import 'package:immich_mobile/repositories/user.repository.dart';
 import 'package:immich_mobile/repositories/user_api.repository.dart';
-import 'package:immich_mobile/services/api.service.dart';
 import 'package:logging/logging.dart';
 
 final partnerServiceProvider = Provider(
   (ref) => PartnerService(
     ref.watch(userApiRepositoryProvider),
     ref.watch(userRepositoryProvider),
-    ref.watch(apiServiceProvider),
   ),
 );
 
 class PartnerService {
   final IUserApiRepository _userApiRepository;
   final IUserRepository _userRepository;
-  final ApiService _apiService;
   final Logger _log = Logger("PartnerService");
 
   PartnerService(
     this._userApiRepository,
     this._userRepository,
-    this._apiService,
   );
 
   Future<bool> removePartner(User partner) async {
     try {
-      await _apiService.partnersApi.removePartner(partner.id);
+      await _userApiRepository.removePartner(partner.id);
       partner.isPartnerSharedBy = false;
       await _userRepository.update(partner);
     } catch (e) {
