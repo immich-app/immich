0015266f192e3dc1ad79dd70fc7cad0e775ca53e
Alex <alex.tran1502@gmail.com>
2024-10-01 17:39:09 +0700

    announcement bar

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -78,8 +78,6 @@ const config = {
       announcementBar: {
         id: 'site_announcement_immich',
         content: `⚠️ The project is under <strong>very active</strong> development. Expect bugs and changes. Do not use it as <strong>the only way</strong> to store your photos and videos!`,
-        backgroundColor: '#593f00',
-        textColor: '#ffefc9',
         isCloseable: false,
       },
       docs: {
