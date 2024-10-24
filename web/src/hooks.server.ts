e5aafff9ec8d91ca9b015f6113f30799c7a7bb12
martabal <74269598+martabal@users.noreply.github.com>
2024-10-24 15:40:03 +0200

    pr feedback

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -1,9 +1,16 @@
 import overpass from '$lib/assets/fonts/overpass/Overpass.ttf?url';
 import overpassMono from '$lib/assets/fonts/overpass/OverpassMono.ttf?url';
-import { transpileFile } from '$lib/utils/app-utils';
 import fouc from '$lib/utils/app?raw';
 import theme from '$lib/utils/theme?raw';
 import type { Handle } from '@sveltejs/kit';
+import { ModuleKind, transpileModule } from 'typescript';
+
+export const transpileFile = (content: string) => {
+  const result = transpileModule(content, {
+    compilerOptions: { module: ModuleKind.ES2020, removeComments: true },
+  });
+  return result.outputText;
+};
 
 // only used during the build to replace the variables from app.html
 export const handle = (async ({ event, resolve }) => {
--- web/src/lib/utils/app-utils.ts
+++
@@ -1,8 +0,0 @@
-import { ModuleKind, transpileModule } from 'typescript';
-
-export const transpileFile = (content: string) => {
-  const result = transpileModule(content, {
-    compilerOptions: { module: ModuleKind.ES2020, removeComments: true },
-  });
-  return result.outputText;
-};
