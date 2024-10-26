e490b02581dec963c078af27ff1b6aec5c069c6d
Pranay <pranaypandeyofficial@gmail.com>
2024-10-26 22:46:45 +0530

    chore: handle bulk update for rectifying unique assets

--- web/src/routes/(user)/utilities/duplicates/[[photos=photos]]/[[assetId=id]]/+page.ts
+++ web/src/routes/(user)/utilities/duplicates/[[photos=photos]]/[[assetId=id]]/+page.ts
@@ -4,18 +4,23 @@ import { getAssetInfoFromParam } from '$lib/utils/navigation';
 import { getAssetDuplicates, updateAssets, type DuplicateResponseDto } from '@immich/sdk';
 import type { PageLoad } from './$types';
 
-const rectifyDuplicate = (duplicate: DuplicateResponseDto) => {
-  updateAssets({ assetBulkUpdateDto: { ids: duplicate.assets.map((asset) => asset.id), duplicateId: null } });
+const rectifyDuplicate = (uniqueAssetIds: string[]) => {
+  if (uniqueAssetIds.length > 0) {
+    updateAssets({ assetBulkUpdateDto: { ids: uniqueAssetIds, duplicateId: null } });
+  }
 };
 
 const processDuplicates = (duplicates: DuplicateResponseDto[]) => {
-  return duplicates.filter((duplicate) => {
+  let uniqueAssetIds: string[] = [];
+  const validDuplicates = duplicates.filter((duplicate) => {
     if (duplicate.assets.length <= 1) {
-      rectifyDuplicate(duplicate);
+      uniqueAssetIds.push(duplicate.assets[0].id);
       return false;
     }
     return true;
   });
+  rectifyDuplicate(uniqueAssetIds);
+  return validDuplicates;
 };
 
 export const load = (async ({ params }) => {
