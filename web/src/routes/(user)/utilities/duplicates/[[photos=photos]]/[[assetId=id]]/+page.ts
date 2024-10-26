f0c402d9a613ba4443f79b59c679f6f4a19e81f5
Pranay <pranaypandeyofficial@gmail.com>
2024-10-26 22:24:10 +0530

    fix: remove duplicateIds on unique assets

--- web/src/routes/(user)/utilities/duplicates/[[photos=photos]]/[[assetId=id]]/+page.ts
+++ web/src/routes/(user)/utilities/duplicates/[[photos=photos]]/[[assetId=id]]/+page.ts
@@ -1,13 +1,28 @@
 import { authenticate } from '$lib/utils/auth';
 import { getFormatter } from '$lib/utils/i18n';
 import { getAssetInfoFromParam } from '$lib/utils/navigation';
-import { getAssetDuplicates } from '@immich/sdk';
+import { getAssetDuplicates, updateAssets, type DuplicateResponseDto } from '@immich/sdk';
 import type { PageLoad } from './$types';
+
+const rectifyDuplicate = (duplicate: DuplicateResponseDto) => {
+  updateAssets({ assetBulkUpdateDto: { ids: duplicate.assets.map((asset) => asset.id), duplicateId: null } });
+};
+
+const processDuplicates = (duplicates: DuplicateResponseDto[]) => {
+  return duplicates.filter((duplicate) => {
+    if (duplicate.assets.length <= 1) {
+      rectifyDuplicate(duplicate);
+      return false;
+    }
+    return true;
+  });
+};
 
 export const load = (async ({ params }) => {
   await authenticate();
   const asset = await getAssetInfoFromParam(params);
-  const duplicates = await getAssetDuplicates();
+  const Allduplicates = await getAssetDuplicates();
+  const duplicates = processDuplicates(Allduplicates);
   const $t = await getFormatter();
 
   return {
