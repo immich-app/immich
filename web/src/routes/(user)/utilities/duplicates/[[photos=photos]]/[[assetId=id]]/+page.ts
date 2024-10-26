fec4eec0cb414790eab35a4d0ec9cfa0f64ee4cc
Pranay <pranaypandeyofficial@gmail.com>
2024-10-27 02:46:00 +0530

    revert: remove identify unique assets from duplicates in web

--- web/src/routes/(user)/utilities/duplicates/[[photos=photos]]/[[assetId=id]]/+page.ts
+++ web/src/routes/(user)/utilities/duplicates/[[photos=photos]]/[[assetId=id]]/+page.ts
@@ -4,30 +4,10 @@ import { getAssetInfoFromParam } from '$lib/utils/navigation';
 import { getAssetDuplicates, updateAssets, type DuplicateResponseDto } from '@immich/sdk';
 import type { PageLoad } from './$types';
 
-const rectifyDuplicate = (uniqueAssetIds: string[]) => {
-  if (uniqueAssetIds.length > 0) {
-    void updateAssets({ assetBulkUpdateDto: { ids: uniqueAssetIds, duplicateId: null } });
-  }
-};
-
-const processDuplicates = (duplicates: DuplicateResponseDto[]) => {
-  const uniqueAssetIds: string[] = [];
-  const validDuplicates = duplicates.filter((duplicate) => {
-    if (duplicate.assets.length <= 1) {
-      uniqueAssetIds.push(duplicate.assets[0].id);
-      return false;
-    }
-    return true;
-  });
-  rectifyDuplicate(uniqueAssetIds);
-  return validDuplicates;
-};
-
 export const load = (async ({ params }) => {
   await authenticate();
   const asset = await getAssetInfoFromParam(params);
-  const Allduplicates = await getAssetDuplicates();
-  const duplicates = processDuplicates(Allduplicates);
+  const duplicates = await getAssetDuplicates();
   const $t = await getFormatter();
 
   return {
