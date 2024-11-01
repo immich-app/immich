e8dba0d0d60ecf9860a511442f4ac53501c0af25
pontussjostedt <po7853sj-s@student.lu.se>
2024-11-01 14:52:21 +0100

    replace immich logo spinner with E-guild one, and allow for different light mode and darkmode no text logo

--- web/src/lib/components/shared-components/immich-logo.svelte
+++ web/src/lib/components/shared-components/immich-logo.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import logoDarkUrl from '$lib/assets/immich-logo-inline-dark.svg';
   import logoLightUrl from '$lib/assets/immich-logo-inline-light.svg';
-  import logoNoText from '$lib/assets/immich-logo.svg';
+  import logoNoTextLight from '$lib/assets/immich-logo-light.svg';
+  import logoNoTextDark from '$lib/assets/immich-logo-dark.svg';
   import { content as alternativeLogo } from '$lib/assets/immich-logo.json';
   import { Theme } from '$lib/constants';
   import { colorTheme } from '$lib/stores/preferences.store';
@@ -25,7 +26,13 @@
   <img src="data:image/png;base64, {alternativeLogo}" alt={$t('immich_logo')} class="h-20" {draggable} />
 {:else}
   <img
-    src={noText ? logoNoText : $colorTheme.value == Theme.LIGHT ? logoLightUrl : logoDarkUrl}
+    src={noText
+      ? $colorTheme.value == Theme.LIGHT
+        ? logoNoTextLight
+        : logoNoTextDark
+      : $colorTheme.value == Theme.LIGHT
+        ? logoLightUrl
+        : logoDarkUrl}
     alt={$t('immich_logo')}
     {draggable}
     {...$$restProps}
