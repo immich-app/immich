b36de7d7d4a8295df077d3afa94e7e61d56b45c8
Alex <alex.tran1502@gmail.com>
2024-11-05 10:34:24 -0600

    chore(mobile): upgrade gradle (#13901)
    * chore(mobile): upgrade gradle
    
    * update photo_manager
    
    * migrate from kapt to ksp
    
    pumps kotlin and other dependency version to fix build issues
    
    * increase java heap size
    
    ---------
    
    Co-authored-by: shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>

--- mobile/lib/entities/ios_device_asset.entity.g.dart
+++ mobile/lib/entities/ios_device_asset.entity.g.dart
@@ -66,7 +66,7 @@ const IOSDeviceAssetSchema = CollectionSchema(
   getId: _iOSDeviceAssetGetId,
   getLinks: _iOSDeviceAssetGetLinks,
   attach: _iOSDeviceAssetAttach,
-  version: '3.1.0+1',
+  version: '3.1.8',
 );
 
 int _iOSDeviceAssetEstimateSize(
