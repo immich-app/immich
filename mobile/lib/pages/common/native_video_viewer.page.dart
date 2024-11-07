3da28a46859be022c19ea02c0d190669b878bee9
mertalev <101130780+mertalev@users.noreply.github.com>
2024-11-07 17:48:25 -0500

    removed unnecessary import

--- mobile/lib/pages/common/native_video_viewer.page.dart
+++ mobile/lib/pages/common/native_video_viewer.page.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart' hide Store;
 import 'package:hooks_riverpod/hooks_riverpod.dart';
