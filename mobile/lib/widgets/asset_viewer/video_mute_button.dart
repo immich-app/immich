57505b00a3204bf9780cfbc1635be3d141011097
mertalev <101130780+mertalev@users.noreply.github.com>
2024-11-11 16:21:13 -0500

    make mute icon const

--- mobile/lib/widgets/asset_viewer/video_mute_button.dart
+++ mobile/lib/widgets/asset_viewer/video_mute_button.dart
@@ -2,20 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/providers/asset_viewer/video_player_controls_provider.dart';
 
-/// The video controls for the [videPlayerControlsProvider]
 class VideoMuteButton extends ConsumerWidget {
   const VideoMuteButton({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return IconButton(
-      icon: Icon(
-        ref.watch(
-          videoPlayerControlsProvider.select((value) => value.mute),
-        )
-            ? Icons.volume_off
-            : Icons.volume_up,
-      ),
+      icon: ref.watch(
+        videoPlayerControlsProvider.select((value) => value.mute),
+      )
+          ? const Icon(Icons.volume_off)
+          : const Icon(Icons.volume_up),
       onPressed: () =>
           ref.read(videoPlayerControlsProvider.notifier).toggleMute(),
       color: Colors.white,
