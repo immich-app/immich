d02c0e6095b4388cb8cfd42e3e9f3bfb02f7e4f5
johnstef99 <john@johnstef.com>
2024-11-12 19:53:20 +0200

    chore(mobile): refactor timeout durations

--- mobile/lib/providers/authentication.provider.dart
+++ mobile/lib/providers/authentication.provider.dart
@@ -171,10 +171,10 @@ class AuthenticationNotifier extends StateNotifier<AuthenticationState> {
     UserPreferencesResponseDto? userPreferences;
     try {
       final responses = await Future.wait([
-        _apiService.usersApi.getMyUser().timeout(const Duration(seconds: 7)),
+        _apiService.usersApi.getMyUser().timeout(_timeoutDuration),
         _apiService.usersApi
             .getMyPreferences()
-            .timeout(const Duration(seconds: 7)),
+            .timeout(_timeoutDuration),
       ]);
       userResponse = responses[0] as UserAdminResponseDto;
       userPreferences = responses[1] as UserPreferencesResponseDto;
