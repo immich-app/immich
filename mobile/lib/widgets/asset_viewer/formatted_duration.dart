2d6a79395cdd54aaf98503911efb2e8a6a03b791
Alex <alex.tran1502@gmail.com>
2024-11-13 09:39:21 -0600

    fix(mobile): duration ui overflow (#14120)
    * fix(mobile): duration ui overflow
    
    * pr feedback

--- mobile/lib/widgets/asset_viewer/formatted_duration.dart
+++ mobile/lib/widgets/asset_viewer/formatted_duration.dart
@@ -19,7 +19,7 @@ class FormattedDuration extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: data.inHours > 0 ? 64 : 43, // use a fixed width to prevent jitter
+      width: data.inHours > 0 ? 70 : 60, // use a fixed width to prevent jitter
       child: Text(
         _formatDuration(data),
         style: const TextStyle(
