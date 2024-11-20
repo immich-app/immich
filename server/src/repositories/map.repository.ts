a6674c243da52abad21036d63241bf2e11485ced
mertalev <101130780+mertalev@users.noreply.github.com>
2024-11-20 00:07:02 -0500

    unlogged tables

--- server/src/repositories/map.repository.ts
+++ server/src/repositories/map.repository.ts
@@ -174,7 +174,9 @@ export class MapRepository implements IMapRepository {
     }
 
     await this.dataSource.query('DROP TABLE IF EXISTS naturalearth_countries_tmp');
-    await this.dataSource.query('CREATE TABLE naturalearth_countries_tmp (LIKE naturalearth_countries INCLUDING ALL)');
+    await this.dataSource.query(
+      'CREATE UNLOGGED TABLE naturalearth_countries_tmp (LIKE naturalearth_countries INCLUDING ALL)',
+    );
     const entities: Omit<NaturalEarthCountriesTempEntity, 'id'>[] = [];
     for (const feature of geoJSONData.features) {
       for (const entry of feature.geometry.coordinates) {
@@ -209,7 +211,7 @@ export class MapRepository implements IMapRepository {
 
     await this.dataSource.query('DROP TABLE IF EXISTS geodata_places_tmp');
     await this.dataSource.query(
-      'CREATE TABLE geodata_places_tmp (LIKE geodata_places INCLUDING ALL EXCLUDING INDEXES)',
+      'CREATE UNLOGGED TABLE geodata_places_tmp (LIKE geodata_places INCLUDING ALL EXCLUDING INDEXES)',
     );
     await this.dataSource.query(`ALTER TABLE geodata_places_tmp DROP "earthCoord", ADD PRIMARY KEY ("id")`);
     await this.loadCities500(admin1, admin2);
