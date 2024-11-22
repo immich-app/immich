ebc75c75fe7afd61fecf4014479cedae7243fd87
thexeroxbe <tim@xeweb.be>
2024-11-22 19:27:52 +0100

    wip

--- web/src/lib/components/shared-components/side-bar/recent-albums.svelte
+++ web/src/lib/components/shared-components/side-bar/recent-albums.svelte
@@ -3,7 +3,7 @@
   import { getAssetThumbnailUrl } from '$lib/utils';
   import Icon from '$lib/components/elements/icon.svelte';
   import { getAllAlbums, type AlbumResponseDto } from '@immich/sdk';
-  import { mdiArrowRight } from '@mdi/js';
+  import { mdiArrowRight, mdiImageAlbum } from '@mdi/js';
   import { handleError } from '$lib/utils/handle-error';
   import { t } from 'svelte-i18n';
 
@@ -26,27 +26,17 @@
       title={album.albumName}
       class="flex w-full place-items-center justify-between gap-4 rounded-r-full py-3 transition-[padding] delay-100 duration-100 hover:cursor-pointer hover:bg-immich-gray hover:text-immich-primary dark:text-immich-dark-fg dark:hover:bg-immich-dark-gray dark:hover:text-immich-dark-primary pl-10 group-hover:sm:px-10 md:px-10"
     >
-      {#if album.albumThumbnailAssetId}
-        <div>
-          <div
-            class="h-6 w-6 bg-cover rounded"
-            style={"background-image:url('" + getAssetThumbnailUrl({ id: album.albumThumbnailAssetId }) + "')"}
-          ></div>
-        </div>
-      {/if}
+      <div>
+        <div
+          class="h-6 w-6 bg-cover rounded"
+          style={album.albumThumbnailAssetId
+            ? `background-image:url('${getAssetThumbnailUrl({ id: album.albumThumbnailAssetId })}')`
+            : ''}
+        ></div>
+      </div>
       <div class="grow text-sm font-medium truncate">
         {album.albumName}
       </div>
     </a>
   {/each}
-  <a
-    href="/albums"
-    aria-label="albums"
-    class="flex w-full place-items-center justify-between gap-4 rounded-r-full py-3 transition-[padding] delay-100 duration-100 hover:cursor-pointer hover:bg-immich-gray hover:text-immich-primary dark:text-immich-dark-fg dark:hover:bg-immich-dark-gray dark:hover:text-immich-dark-primary pl-10 group-hover:sm:px-10 md:px-10"
-  >
-    <div>
-      <div class="h-6 w-6 bg-cover rounded"><Icon path={mdiArrowRight}></Icon></div>
-    </div>
-    <div class="grow text-sm font-medium">{$t('all_albums')}</div>
-  </a>
 {/if}
