6c5fd82feb7f019ef4c7007e83676b17ab0945c4
thexeroxbe <tim@xeweb.be>
2024-11-22 21:01:49 +0100

    removed if

--- web/src/lib/components/shared-components/side-bar/recent-albums.svelte
+++ web/src/lib/components/shared-components/side-bar/recent-albums.svelte
@@ -17,24 +17,22 @@
   });
 </script>
 
-{#if albums.length > 0}
-  {#each albums as album}
-    <a
-      href={'/albums/' + album.id}
-      title={album.albumName}
-      class="flex w-full place-items-center justify-between gap-4 rounded-r-full py-3 transition-[padding] delay-100 duration-100 hover:cursor-pointer hover:bg-immich-gray hover:text-immich-primary dark:text-immich-dark-fg dark:hover:bg-immich-dark-gray dark:hover:text-immich-dark-primary pl-10 group-hover:sm:px-10 md:px-10"
-    >
-      <div>
-        <div
-          class="h-6 w-6 bg-cover rounded"
-          style={album.albumThumbnailAssetId
-            ? `background-image:url('${getAssetThumbnailUrl({ id: album.albumThumbnailAssetId })}')`
-            : ''}
-        ></div>
-      </div>
-      <div class="grow text-sm font-medium truncate">
-        {album.albumName}
-      </div>
-    </a>
-  {/each}
-{/if}
+{#each albums as album}
+  <a
+    href={'/albums/' + album.id}
+    title={album.albumName}
+    class="flex w-full place-items-center justify-between gap-4 rounded-r-full py-3 transition-[padding] delay-100 duration-100 hover:cursor-pointer hover:bg-immich-gray hover:text-immich-primary dark:text-immich-dark-fg dark:hover:bg-immich-dark-gray dark:hover:text-immich-dark-primary pl-10 group-hover:sm:px-10 md:px-10"
+  >
+    <div>
+      <div
+        class="h-6 w-6 bg-cover rounded"
+        style={album.albumThumbnailAssetId
+          ? `background-image:url('${getAssetThumbnailUrl({ id: album.albumThumbnailAssetId })}')`
+          : ''}
+      ></div>
+    </div>
+    <div class="grow text-sm font-medium truncate">
+      {album.albumName}
+    </div>
+  </a>
+{/each}
