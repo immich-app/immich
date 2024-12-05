93e086fed33c6cb6bd03bc5ad4a4c9f70c72233f
Alex <alex.tran1502@gmail.com>
2024-12-04 21:31:15 -0600

    lint

--- mobile/lib/widgets/settings/networking_settings/external_network_preference.dart
+++ mobile/lib/widgets/settings/networking_settings/external_network_preference.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:ui';
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
--- mobile/lib/widgets/settings/networking_settings/local_network_preference.dart
+++ mobile/lib/widgets/settings/networking_settings/local_network_preference.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
