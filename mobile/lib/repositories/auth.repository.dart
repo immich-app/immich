055f1fc72fd1b3cecd00a5110e1f830a71a41945
Alex <alex.tran1502@gmail.com>
2024-12-05 09:11:48 -0600

    feat(mobile): Auto switching server URLs (#14437)

--- mobile/lib/repositories/auth.repository.dart
+++ mobile/lib/repositories/auth.repository.dart
@@ -1,10 +1,14 @@
+import 'dart:convert';
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/entities/album.entity.dart';
 import 'package:immich_mobile/entities/asset.entity.dart';
 import 'package:immich_mobile/entities/etag.entity.dart';
 import 'package:immich_mobile/entities/exif_info.entity.dart';
+import 'package:immich_mobile/entities/store.entity.dart';
 import 'package:immich_mobile/entities/user.entity.dart';
 import 'package:immich_mobile/interfaces/auth.interface.dart';
+import 'package:immich_mobile/models/auth/auxilary_endpoint.model.dart';
 import 'package:immich_mobile/providers/db.provider.dart';
 import 'package:immich_mobile/repositories/database.repository.dart';
 
@@ -26,5 +30,40 @@ class AuthRepository extends DatabaseRepository implements IAuthRepository {
         db.users.clear(),
       ]);
     });
+  }
+
+  @override
+  String getAccessToken() {
+    return Store.get(StoreKey.accessToken);
+  }
+
+  @override
+  bool getEndpointSwitchingFeature() {
+    return Store.tryGet(StoreKey.autoEndpointSwitching) ?? false;
+  }
+
+  @override
+  String? getPreferredWifiName() {
+    return Store.tryGet(StoreKey.preferredWifiName);
+  }
+
+  @override
+  String? getLocalEndpoint() {
+    return Store.tryGet(StoreKey.localEndpoint);
+  }
+
+  @override
+  List<AuxilaryEndpoint> getExternalEndpointList() {
+    final jsonString = Store.tryGet(StoreKey.externalEndpointList);
+
+    if (jsonString == null) {
+      return [];
+    }
+
+    final List<dynamic> jsonList = jsonDecode(jsonString);
+    final endpointList =
+        jsonList.map((e) => AuxilaryEndpoint.fromJson(e)).toList();
+
+    return endpointList;
   }
 }
