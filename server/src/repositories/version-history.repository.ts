517b970a839f0d32347c30fa386f75a1a57dadf0
bo0tzz <git@bo0tzz.me>
2025-01-11 20:25:18 +0100

    chore: generate sql for version-history repository

--- server/src/repositories/version-history.repository.ts
+++ server/src/repositories/version-history.repository.ts
@@ -3,20 +3,24 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { VersionHistoryEntity } from 'src/entities/version-history.entity';
 import { IVersionHistoryRepository } from 'src/interfaces/version-history.interface';
 import { Repository } from 'typeorm';
+import { DummyValue, GenerateSql } from 'src/decorators';
 
 @Injectable()
 export class VersionHistoryRepository implements IVersionHistoryRepository {
   constructor(@InjectRepository(VersionHistoryEntity) private repository: Repository<VersionHistoryEntity>) {}
 
+  @GenerateSql()
   async getAll(): Promise<VersionHistoryEntity[]> {
     return this.repository.find({ order: { createdAt: 'DESC' } });
   }
 
+  @GenerateSql()
   async getLatest(): Promise<VersionHistoryEntity | null> {
     const results = await this.repository.find({ order: { createdAt: 'DESC' }, take: 1 });
     return results[0] || null;
   }
 
+  @GenerateSql({ params: [DummyValue.STRING] })
   create(version: Omit<VersionHistoryEntity, 'id' | 'createdAt'>): Promise<VersionHistoryEntity> {
     return this.repository.save(version);
   }
