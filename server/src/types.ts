9a1068c867d7cd128a1ee2f618c39ad44376cb57
Jason Rasmussen <jason@rasm.me>
2025-01-21 11:45:59 -0500

    refactor: api key repository (#15491)

--- server/src/types.ts
+++ server/src/types.ts
@@ -2,6 +2,7 @@ import { UserEntity } from 'src/entities/user.entity';
 import { Permission } from 'src/enum';
 import { AccessRepository } from 'src/repositories/access.repository';
 import { ActivityRepository } from 'src/repositories/activity.repository';
+import { ApiKeyRepository } from 'src/repositories/api-key.repository';
 
 export type AuthApiKey = {
   id: string;
@@ -14,7 +15,13 @@ export type RepositoryInterface<T extends object> = Pick<T, keyof T>;
 
 export type IActivityRepository = RepositoryInterface<ActivityRepository>;
 export type IAccessRepository = { [K in keyof AccessRepository]: RepositoryInterface<AccessRepository[K]> };
+export type IApiKeyRepository = RepositoryInterface<ApiKeyRepository>;
 
 export type ActivityItem =
   | Awaited<ReturnType<IActivityRepository['create']>>
   | Awaited<ReturnType<IActivityRepository['search']>>[0];
+
+export type ApiKeyItem =
+  | Awaited<ReturnType<IApiKeyRepository['create']>>
+  | NonNullable<Awaited<ReturnType<IApiKeyRepository['getById']>>>
+  | Awaited<ReturnType<IApiKeyRepository['getByUserId']>>[0];
