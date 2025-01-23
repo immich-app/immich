e8536552a25efb21c78d1384662cea38a94b0180
Alex Tran <alex.tran1502@gmail.com>
2025-01-23 16:10:06 -0600

    opacity and feedback

--- web/src/lib/components/layouts/AuthPageLayout.svelte
+++ web/src/lib/components/layouts/AuthPageLayout.svelte
@@ -10,10 +10,10 @@
   let { title, children }: Props = $props();
 </script>
 
-<section class="min-w-screen flex min-h-screen items-center justify-center relative">
+<section class="min-w-screen flex min-h-dvh items-center justify-center relative">
   <div class="absolute -z-10 w-[99%] h-[99%] flex place-items-center place-content-center">
     <img src="/immich-logo-no-bg.svg" class="w-[750px] mb-2 antialiased -z-10" alt="Immich logo" />
-    <div class="w-full h-[99%] absolute left-0 top-0 backdrop-blur-3xl bg-immich-bg/40 dark:bg-immich-dark-bg/20"></div>
+    <div class="w-full h-[99%] absolute left-0 top-0 backdrop-blur-3xl bg-immich-bg/80 dark:bg-immich-dark-bg/20"></div>
   </div>
 
   <Card color="secondary" class="w-full max-w-lg border m-2">
