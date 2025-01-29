51a06b55a7c02b3ea4fe0b8585e538dcf4c8dd2b
Justin Forseth <justin@jforseth.com>
2025-01-29 11:19:35 -0700

    Add filter to people_picker.dart

--- mobile/lib/widgets/search/search_filter/people_picker.dart
+++ mobile/lib/widgets/search/search_filter/people_picker.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/extensions/asyncvalue_extensions.dart';
@@ -17,62 +18,86 @@ class PeoplePicker extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     var imageSize = 45.0;
+    final searchQuery = useState('');
     final people = ref.watch(getAllPeopleProvider);
     final headers = ApiService.getRequestHeaders();
     final selectedPeople = useState<Set<Person>>(filter ?? {});
 
-    return people.widgetWhen(
-      onData: (people) {
-        return ListView.builder(
-          shrinkWrap: true,
-          itemCount: people.length,
-          padding: const EdgeInsets.all(8),
-          itemBuilder: (context, index) {
-            final person = people[index];
-            return Card(
-              elevation: 0,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(15)),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: TextField(
+            onChanged: (value) => searchQuery.value = value,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(25),
               ),
-              child: ListTile(
-                title: Text(
-                  person.name,
-                  style: context.textTheme.bodyLarge,
-                ),
-                leading: SizedBox(
-                  height: imageSize,
-                  child: Material(
-                    shape: const CircleBorder(side: BorderSide.none),
-                    elevation: 3,
-                    child: CircleAvatar(
-                      maxRadius: imageSize / 2,
-                      backgroundImage: NetworkImage(
-                        getFaceThumbnailUrl(person.id),
-                        headers: headers,
-                      ),
-                    ),
-                  ),
-                ),
-                onTap: () {
-                  if (selectedPeople.value.contains(person)) {
-                    selectedPeople.value.remove(person);
-                  } else {
-                    selectedPeople.value.add(person);
-                  }
-
-                  selectedPeople.value = {...selectedPeople.value};
-                  onSelect(selectedPeople.value);
-                },
-                selected: selectedPeople.value.contains(person),
-                selectedTileColor: context.primaryColor.withOpacity(0.2),
+              prefixIcon: const Icon(Icons.search_rounded),
+              hintText: 'search_filter_people_hint'.tr(),
+            ),
+          ),
+        ),
+        Expanded(child: people.widgetWhen(onData: (people) {
+          return ListView.builder(
+            shrinkWrap: true,
+            itemCount: people
+                .where((person) => person.name
+                    .toLowerCase()
+                    .contains(searchQuery.value.toLowerCase()))
+                .length,
+            padding: const EdgeInsets.all(8),
+            itemBuilder: (context, index) {
+              final person = people
+                  .where((person) => person.name
+                      .toLowerCase()
+                      .contains(searchQuery.value.toLowerCase()))
+                  .toList()[index];
+              return Card(
+                elevation: 0,
                 shape: const RoundedRectangleBorder(
                   borderRadius: BorderRadius.all(Radius.circular(15)),
                 ),
-              ),
-            );
-          },
-        );
-      },
+                child: ListTile(
+                  title: Text(
+                    person.name,
+                    style: context.textTheme.bodyLarge,
+                  ),
+                  leading: SizedBox(
+                    height: imageSize,
+                    child: Material(
+                      shape: const CircleBorder(side: BorderSide.none),
+                      elevation: 3,
+                      child: CircleAvatar(
+                        maxRadius: imageSize / 2,
+                        backgroundImage: NetworkImage(
+                          getFaceThumbnailUrl(person.id),
+                          headers: headers,
+                        ),
+                      ),
+                    ),
+                  ),
+                  onTap: () {
+                    if (selectedPeople.value.contains(person)) {
+                      selectedPeople.value.remove(person);
+                    } else {
+                      selectedPeople.value.add(person);
+                    }
+
+                    selectedPeople.value = {...selectedPeople.value};
+                    onSelect(selectedPeople.value);
+                  },
+                  selected: selectedPeople.value.contains(person),
+                  selectedTileColor: context.primaryColor.withOpacity(0.2),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                ),
+              );
+            },
+          );
+        }))
+      ],
     );
   }
 }
