82e9366f64aadc56db51d3f6d6a400bfd4ff3c65
Arno Wiest <arnolicious.git@gmail.com>
2025-02-02 17:42:15 +0100

    removed unused import

--- web/src/routes/(user)/utilities/duplicates/[[photos=photos]]/[duplicateId=id]/+page.ts
+++ web/src/routes/(user)/utilities/duplicates/[[photos=photos]]/[duplicateId=id]/+page.ts
@@ -1,19 +1,13 @@
 import { authenticate } from '$lib/utils/auth';
 import { getFormatter } from '$lib/utils/i18n';
-import { getAssetInfoFromParam } from '$lib/utils/navigation';
 import { getAssetDuplicates } from '@immich/sdk';
 import { fail } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ params }) => {
   await authenticate();
-  // const asset = await getAssetInfoFromParam(params);
   const duplicates = await getAssetDuplicates();
   const $t = await getFormatter();
-
-  // if (!asset) {
-  //   return fail(404);
-  // }
 
   const activeDuplicate = duplicates.find((duplicate) => duplicate.duplicateId === params.duplicateId);
 
@@ -22,7 +16,6 @@ export const load = (async ({ params }) => {
   }
 
   return {
-    // asset,
     duplicates,
     activeDuplicate,
     meta: {
