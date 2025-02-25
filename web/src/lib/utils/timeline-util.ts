bbcaee82f03c607b462a6ccb169b13ba6cdadccd
Alex <alex.tran1502@gmail.com>
2025-02-25 09:39:56 -0600

    chore(web): revert wasm new justify layout (#16277)
    * Revert "fix(web): justify layout import (#16267) "
    
    This reverts commit ec58e1065fafc34ab675d8403cc2635566f5d912.
    
    * Revert "fix(web): dynamically import wasm module (#16261)"
    
    This reverts commit 4376fd72b760defd2afe3a47eb10c42c1cde7333.
    
    * Revert "feat(web): use wasm for justified layout calculation (#15524)"
    
    This reverts commit 3925445de8206946de8c7f9b7a6742b2df5e110d.
    
    * Revert "fix(web): viewport reactivity, off-screen thumbhashes being rendered (#15435)"
    
    This reverts commit 52f21fb331f8f01b55f8e0b934ddb48999b78664.

--- web/src/lib/utils/timeline-util.ts
+++ web/src/lib/utils/timeline-util.ts
@@ -1,7 +1,7 @@
 import type { AssetBucket } from '$lib/stores/assets.store';
 import { locale } from '$lib/stores/preferences.store';
-import type { JustifiedLayout } from '@immich/justified-layout-wasm';
 import type { AssetResponseDto } from '@immich/sdk';
+import type createJustifiedLayout from 'justified-layout';
 import { groupBy, memoize, sortBy } from 'lodash-es';
 import { DateTime } from 'luxon';
 import { get } from 'svelte/store';
@@ -13,7 +13,7 @@ export type DateGroup = {
   height: number;
   heightActual: boolean;
   intersecting: boolean;
-  geometry: JustifiedLayout | null;
+  geometry: Geometry;
   bucket: AssetBucket;
 };
 export type ScrubberListener = (
@@ -80,6 +80,19 @@ export function formatGroupTitle(_date: DateTime): string {
   return date.toLocaleString(groupDateFormat);
 }
 
+type Geometry = ReturnType<typeof createJustifiedLayout> & {
+  containerWidth: number;
+};
+
+function emptyGeometry() {
+  return {
+    containerWidth: 0,
+    containerHeight: 0,
+    widowCount: 0,
+    boxes: [],
+  };
+}
+
 const formatDateGroupTitle = memoize(formatGroupTitle);
 
 export function splitBucketIntoDateGroups(bucket: AssetBucket, locale: string | undefined): DateGroup[] {
@@ -87,7 +100,6 @@ export function splitBucketIntoDateGroups(bucket: AssetBucket, locale: string | 
     fromLocalDateTime(asset.localDateTime).toLocaleString(groupDateFormat, { locale }),
   );
   const sorted = sortBy(grouped, (group) => bucket.assets.indexOf(group[0]));
-
   return sorted.map((group) => {
     const date = fromLocalDateTime(group[0].localDateTime).startOf('day');
     return {
@@ -97,10 +109,29 @@ export function splitBucketIntoDateGroups(bucket: AssetBucket, locale: string | 
       height: 0,
       heightActual: false,
       intersecting: false,
-      geometry: null,
+      geometry: emptyGeometry(),
       bucket,
     };
   });
+}
+
+export type LayoutBox = {
+  aspectRatio: number;
+  top: number;
+  width: number;
+  height: number;
+  left: number;
+  forcedAspectRatio?: boolean;
+};
+
+export function calculateWidth(boxes: LayoutBox[]): number {
+  let width = 0;
+  for (const box of boxes) {
+    if (box.top < 100) {
+      width = box.left + box.width;
+    }
+  }
+  return width;
 }
 
 export function findTotalOffset(element: HTMLElement, stop: HTMLElement) {
