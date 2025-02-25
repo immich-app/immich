26668c04554a5444edb147b9cc26a1cb4970a893
Alex <alex.tran1502@gmail.com>
2025-02-25 14:59:58 -0600

    feat(mobile): use memories api

--- mobile/lib/services/api.service.dart
+++ mobile/lib/services/api.service.dart
@@ -31,6 +31,7 @@ class ApiService implements Authentication {
   late DownloadApi downloadApi;
   late TrashApi trashApi;
   late StacksApi stacksApi;
+  late MemoriesApi memoriesApi;
 
   ApiService() {
     final endpoint = Store.tryGet(StoreKey.serverEndpoint);
@@ -63,6 +64,7 @@ class ApiService implements Authentication {
     downloadApi = DownloadApi(_apiClient);
     trashApi = TrashApi(_apiClient);
     stacksApi = StacksApi(_apiClient);
+    memoriesApi = MemoriesApi(_apiClient);
   }
 
   Future<String> resolveAndSetEndpoint(String serverUrl) async {
