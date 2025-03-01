aa24f0f1892b0ee0225f5eb0313a29add98c06eb
Luz Paz <luzpaz@pm.me>
2025-03-01 15:13:56 -0500

    fix(web): fix typos Found via codespell

--- web/src/lib/components/shared-components/purchasing/individual-purchase-option-card.svelte
+++ web/src/lib/components/shared-components/purchasing/individual-purchase-option-card.svelte
@@ -7,7 +7,7 @@
   import { t } from 'svelte-i18n';
 </script>
 
-<!-- Inidvidual Purchase Option -->
+<!-- Individual Purchase Option -->
 <div class="border border-gray-300 dark:border-gray-800 w-[375px] p-8 rounded-3xl bg-gray-100 dark:bg-gray-900">
   <div class="text-immich-primary dark:text-immich-dark-primary">
     <Icon path={mdiAccount} size="56" />
