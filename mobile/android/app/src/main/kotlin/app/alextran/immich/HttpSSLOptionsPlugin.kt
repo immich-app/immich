4d264c75f5df8ee56f4527651af1ad676411837f
Robert Vollmer <dev@robv.de>
2025-03-03 23:42:22 +0100

    Allow self-signed certificates

--- mobile/android/app/src/main/kotlin/app/alextran/immich/HttpSSLOptionsPlugin.kt
+++ mobile/android/app/src/main/kotlin/app/alextran/immich/HttpSSLOptionsPlugin.kt
@@ -1,16 +1,24 @@
 package app.alextran.immich
 
+import android.annotation.SuppressLint
 import android.content.Context
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import java.io.ByteArrayInputStream
+import java.net.InetSocketAddress
+import java.net.Socket
 import java.security.KeyStore
+import java.security.cert.X509Certificate
 import javax.net.ssl.HttpsURLConnection
 import javax.net.ssl.KeyManager
 import javax.net.ssl.KeyManagerFactory
 import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLEngine
+import javax.net.ssl.TrustManager
+import javax.net.ssl.TrustManagerFactory
+import javax.net.ssl.X509ExtendedTrustManager
 
 /**
  * Android plugin for Dart `HttpSSLOptions`
@@ -40,11 +48,17 @@ class HttpSSLOptionsPlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
   override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
     when (call.method) {
       "apply" -> {
-        var km: Array<KeyManager>? = null
         val args = call.arguments<ArrayList<*>>()!!
-        if (args[0] != null) {
-          val cert = ByteArrayInputStream(args[0] as ByteArray)
-          val password = (args[1] as String).toCharArray()
+
+        var tm: Array<TrustManager>? = null
+        if (args[0] as Boolean) {
+          tm = arrayOf(AllowSelfSignedTrustManager(args[1] as String))
+        }
+
+        var km: Array<KeyManager>? = null
+        if (args[2] != null) {
+          val cert = ByteArrayInputStream(args[2] as ByteArray)
+          val password = (args[3] as String).toCharArray()
           val keyStore = KeyStore.getInstance("PKCS12")
           keyStore.load(cert, password)
           val keyManagerFactory =
@@ -54,13 +68,58 @@ class HttpSSLOptionsPlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
         }
 
         val sslContext = SSLContext.getInstance("TLS")
-        sslContext.init(km, null, null)
+        sslContext.init(km, tm, null)
         HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.socketFactory)
 
         result.success(true)
       }
 
       else -> result.notImplemented()
+    }
+  }
+
+  @SuppressLint("CustomX509TrustManager")
+  class AllowSelfSignedTrustManager(private val host: String?) : X509ExtendedTrustManager() {
+    private val defaultTrustManager: X509ExtendedTrustManager = getDefaultTrustManager()
+
+    override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) =
+      defaultTrustManager.checkClientTrusted(chain, authType)
+
+    override fun checkClientTrusted(
+      chain: Array<out X509Certificate>?, authType: String?, socket: Socket?
+    ) = defaultTrustManager.checkClientTrusted(chain, authType, socket)
+
+    override fun checkClientTrusted(
+      chain: Array<out X509Certificate>?, authType: String?, engine: SSLEngine?
+    ) = defaultTrustManager.checkClientTrusted(chain, authType, engine)
+
+    override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+      if (host == null) return
+      defaultTrustManager.checkServerTrusted(chain, authType)
+    }
+
+    override fun checkServerTrusted(
+      chain: Array<out X509Certificate>?, authType: String?, socket: Socket?
+    ) {
+      if (host == null) return
+      val socketAddress = socket?.remoteSocketAddress
+      if (socketAddress is InetSocketAddress && socketAddress.hostName == host) return
+      defaultTrustManager.checkServerTrusted(chain, authType, socket)
+    }
+
+    override fun checkServerTrusted(
+      chain: Array<out X509Certificate>?, authType: String?, engine: SSLEngine?
+    ) {
+      if (host == null || engine?.peerHost == host) return
+      defaultTrustManager.checkServerTrusted(chain, authType, engine)
+    }
+
+    override fun getAcceptedIssuers(): Array<X509Certificate> = defaultTrustManager.acceptedIssuers
+
+    private fun getDefaultTrustManager(): X509ExtendedTrustManager {
+      val factory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+      factory.init(null as KeyStore?)
+      return factory.trustManagers.filterIsInstance<X509ExtendedTrustManager>().first()
     }
   }
 }
