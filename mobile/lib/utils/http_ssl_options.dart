1ec3dfb96ef1bd246e98abdb8613e600796ce403
Robert Vollmer <dev@robv.de>
2025-03-03 23:37:04 +0100

    Refactor self-signed check

--- mobile/lib/utils/http_ssl_options.dart
+++ mobile/lib/utils/http_ssl_options.dart
@@ -1,17 +1,33 @@
 import 'dart:io';
 
 import 'package:flutter/services.dart';
+import 'package:immich_mobile/domain/models/store.model.dart';
 import 'package:immich_mobile/entities/store.entity.dart';
+import 'package:immich_mobile/services/app_settings.service.dart';
 import 'package:immich_mobile/utils/http_ssl_cert_override.dart';
 
 class HttpSSLOptions {
   static const MethodChannel _channel = MethodChannel('immich/httpSSLOptions');
 
   static void apply() {
+    AppSettingsEnum setting = AppSettingsEnum.allowSelfSignedSSLCert;
+    bool allowSelfSignedSSLCert =
+        Store.get(setting.storeKey as StoreKey<bool>, setting.defaultValue);
+    String? serverHost;
+    if (allowSelfSignedSSLCert && Store.tryGet(StoreKey.currentUser) != null) {
+      serverHost = Uri.parse(Store.tryGet(StoreKey.serverEndpoint) ?? "").host;
+    }
+
     SSLClientCertStoreVal? clientCert = SSLClientCertStoreVal.load();
-    HttpOverrides.global = HttpSSLCertOverride(clientCert);
+
+    HttpOverrides.global =
+        HttpSSLCertOverride(allowSelfSignedSSLCert, serverHost, clientCert);
+
     if (Platform.isAndroid) {
-      _channel.invokeMethod("apply", [clientCert?.data, clientCert?.password]);
+      _channel.invokeMethod("apply", [
+        clientCert?.data,
+        clientCert?.password,
+      ]);
     }
   }
 }
