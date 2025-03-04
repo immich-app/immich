e82cbf8aeb63f7201e6d5a52f4f4fe3f0814aa5b
Alex <alex.tran1502@gmail.com>
2025-03-04 15:43:06 -0600

    refactor

--- mobile/lib/providers/infrastructure/sync_stream.provider.dart
+++ mobile/lib/providers/infrastructure/sync_stream.provider.dart
@@ -1,11 +1,20 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/domain/services/sync_stream.service.dart';
 import 'package:immich_mobile/infrastructure/repositories/sync_api.repository.dart';
+import 'package:immich_mobile/providers/api.provider.dart';
+import 'package:immich_mobile/providers/db.provider.dart';
 import 'package:immich_mobile/repositories/user.repository.dart';
 
 final syncStreamServiceProvider = Provider(
   (ref) => SyncStreamService(
     ref.watch(syncApiRepositoryProvider),
     ref.watch(userRepositoryProvider),
+  ),
+);
+
+final syncApiRepositoryProvider = Provider(
+  (ref) => SyncApiRepository(
+    ref.watch(dbProvider),
+    ref.watch(apiServiceProvider),
   ),
 );
