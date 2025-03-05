0527354d5a64810f19e9592d90800e17978d5a9a
Alex <alex.tran1502@gmail.com>
2025-03-04 20:59:56 -0600

    remove database operation

--- mobile/lib/domain/services/sync_stream.service.dart
+++ mobile/lib/domain/services/sync_stream.service.dart
@@ -1,38 +1,38 @@
 import 'package:flutter/foundation.dart';
 import 'package:immich_mobile/domain/interfaces/sync_api.interface.dart';
-import 'package:immich_mobile/interfaces/user.interface.dart';
 import 'package:openapi/api.dart';
 
 class SyncStreamService {
   final ISyncApiRepository _syncApiRepository;
-  final IUserRepository _userRepository;
 
-  SyncStreamService(this._syncApiRepository, this._userRepository);
+  SyncStreamService(this._syncApiRepository);
 
   void syncUsers() {
     _syncApiRepository.watchUserSyncEvent().listen((events) async {
       for (final event in events) {
         if (event.data is SyncUserV1) {
           final data = event.data as SyncUserV1;
-          final user = await _userRepository.get(data.id);
+          debugPrint("User Update: $data");
 
-          if (user == null) {
-            continue;
-          }
+          // final user = await _userRepository.get(data.id);
 
-          user.name = data.name;
-          user.email = data.email;
-          user.updatedAt = DateTime.now();
+          // if (user == null) {
+          //   continue;
+          // }
 
-          await _userRepository.update(user);
-          await _syncApiRepository.ack(event.ack);
+          // user.name = data.name;
+          // user.email = data.email;
+          // user.updatedAt = DateTime.now();
+
+          // await _userRepository.update(user);
+          // await _syncApiRepository.ack(event.ack);
         }
 
         if (event.data is SyncUserDeleteV1) {
           final data = event.data as SyncUserDeleteV1;
 
           debugPrint("User delete: $data");
-          await _syncApiRepository.ack(event.ack);
+          // await _syncApiRepository.ack(event.ack);
         }
       }
     });
--- mobile/lib/providers/infrastructure/sync_stream.provider.dart
+++ mobile/lib/providers/infrastructure/sync_stream.provider.dart
@@ -3,12 +3,10 @@ import 'package:immich_mobile/domain/services/sync_stream.service.dart';
 import 'package:immich_mobile/infrastructure/repositories/sync_api.repository.dart';
 import 'package:immich_mobile/providers/api.provider.dart';
 import 'package:immich_mobile/providers/db.provider.dart';
-import 'package:immich_mobile/repositories/user.repository.dart';
 
 final syncStreamServiceProvider = Provider(
   (ref) => SyncStreamService(
     ref.watch(syncApiRepositoryProvider),
-    ref.watch(userRepositoryProvider),
   ),
 );
 
