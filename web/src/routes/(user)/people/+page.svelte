ff06e14cea03ac501984c5e2c8dff82e90e9f4d8
Alex Tran <alex.tran1502@gmail.com>
2025-03-05 22:05:34 -0600

    feat(web): better person naming interface

--- web/src/routes/(user)/people/+page.svelte
+++ web/src/routes/(user)/people/+page.svelte
@@ -421,15 +421,17 @@
             onToggleFavorite={() => handleToggleFavorite(person)}
           />
 
-          <input
-            type="text"
-            class=" bg-white dark:bg-immich-dark-gray border-gray-100 placeholder-gray-400 text-center dark:border-gray-900 w-full rounded-2xl mt-2 py-2 text-sm text-immich-primary dark:text-immich-dark-primary"
-            value={person.name}
-            placeholder={$t('add_a_name')}
-            onfocusin={() => onNameChangeInputFocus(person)}
-            onfocusout={() => onNameChangeSubmit(newName, person)}
-            oninput={(event) => onNameChangeInputUpdate(event)}
-          />
+          <form onsubmit={() => onNameChangeSubmit(newName, person)}>
+            <input
+              type="text"
+              class=" bg-white dark:bg-immich-dark-gray border-gray-100 placeholder-gray-400 text-center dark:border-gray-900 w-full rounded-2xl mt-2 py-2 text-sm text-immich-primary dark:text-immich-dark-primary"
+              value={person.name}
+              placeholder={$t('add_a_name')}
+              onfocusin={() => onNameChangeInputFocus(person)}
+              onfocusout={() => onNameChangeSubmit(newName, person)}
+              oninput={(event) => onNameChangeInputUpdate(event)}
+            />
+          </form>
         </div>
       {/snippet}
     </PeopleInfiniteScroll>
