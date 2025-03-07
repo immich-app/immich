f82786a297f3be15163281b36ddc4670c8ee8322
Jason Rasmussen <jason@rasm.me>
2025-03-07 14:30:01 -0500

    feat: use stream for template migrations (#16700)

--- server/test/repositories/asset.repository.mock.ts
+++ server/test/repositories/asset.repository.mock.ts
@@ -44,5 +44,7 @@ export const newAssetRepositoryMock = (): Mocked<RepositoryInterface<AssetReposi
     detectOfflineExternalAssets: vitest.fn(),
     filterNewExternalAssetPaths: vitest.fn(),
     updateByLibraryId: vitest.fn(),
+    streamStorageTemplateAssets: vitest.fn(),
+    getStorageTemplateAsset: vitest.fn(),
   };
 };
