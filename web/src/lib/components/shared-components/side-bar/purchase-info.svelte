c67be39b8becb80f7485edbcab608a75db7d3303
Jason Rasmussen <jason@rasm.me>
2025-03-12 17:25:02 -0400

    refactor: use more immich ui buttons

--- web/src/lib/components/shared-components/side-bar/purchase-info.svelte
+++ web/src/lib/components/shared-components/side-bar/purchase-info.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
-  import Icon from '$lib/components/elements/icon.svelte';
-  import { mdiClose, mdiInformationOutline } from '@mdi/js';
-  import Portal from '$lib/components/shared-components/portal/portal.svelte';
-  import Button from '$lib/components/elements/buttons/button.svelte';
-  import CircleIconButton from '$lib/components/elements/buttons/circle-icon-button.svelte';
-  import LicenseModal from '$lib/components/shared-components/purchasing/purchase-modal.svelte';
-  import { purchaseStore } from '$lib/stores/purchase.store';
-  import { t } from 'svelte-i18n';
   import { goto } from '$app/navigation';
-  import { AppRoute } from '$lib/constants';
-  import { getAccountAge } from '$lib/utils/auth';
-  import { fade } from 'svelte/transition';
+  import CircleIconButton from '$lib/components/elements/buttons/circle-icon-button.svelte';
+  import Icon from '$lib/components/elements/icon.svelte';
   import ImmichLogo from '$lib/components/shared-components/immich-logo.svelte';
-  import { updateMyPreferences } from '@immich/sdk';
-  import { handleError } from '$lib/utils/handle-error';
-  import { preferences } from '$lib/stores/user.store';
-  import { getButtonVisibility } from '$lib/utils/purchase-utils';
+  import Portal from '$lib/components/shared-components/portal/portal.svelte';
+  import LicenseModal from '$lib/components/shared-components/purchasing/purchase-modal.svelte';
   import SupporterBadge from '$lib/components/shared-components/side-bar/supporter-badge.svelte';
+  import { AppRoute } from '$lib/constants';
+  import { purchaseStore } from '$lib/stores/purchase.store';
+  import { preferences } from '$lib/stores/user.store';
+  import { getAccountAge } from '$lib/utils/auth';
+  import { handleError } from '$lib/utils/handle-error';
+  import { getButtonVisibility } from '$lib/utils/purchase-utils';
+  import { updateMyPreferences } from '@immich/sdk';
+  import { Button } from '@immich/ui';
+  import { mdiClose, mdiInformationOutline } from '@mdi/js';
+  import { t } from 'svelte-i18n';
+  import { fade } from 'svelte/transition';
 
   let showMessage = $state(false);
   let isOpen = $state(false);
@@ -159,12 +159,21 @@
         </p>
       </div>
 
-      <Button class="mt-2" fullwidth onclick={openPurchaseModal}>{$t('purchase_button_buy_immich')}</Button>
+      <Button shape="round" class="mt-2" fullWidth onclick={openPurchaseModal}
+        >{$t('purchase_button_buy_immich')}</Button
+      >
       <div class="mt-3 flex gap-4">
-        <Button size="sm" fullwidth shadow={false} color="transparent-gray" onclick={() => hideButton(true)}>
+        <Button shape="round" size="small" fullWidth color="secondary" variant="ghost" onclick={() => hideButton(true)}>
           {$t('purchase_button_never_show_again')}
         </Button>
-        <Button size="sm" fullwidth shadow={false} color="transparent-gray" onclick={() => hideButton(false)}>
+        <Button
+          shape="round"
+          size="small"
+          fullWidth
+          color="secondary"
+          variant="ghost"
+          onclick={() => hideButton(false)}
+        >
           {$t('purchase_button_reminder')}
         </Button>
       </div>
