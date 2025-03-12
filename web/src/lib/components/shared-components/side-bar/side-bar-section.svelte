568a0491f2be7bc0e0917b703fa109838a298cb3
ben-basten <45583362+ben-basten@users.noreply.github.com>
2025-03-11 23:21:57 -0400

    fix: sidebar spacing and reactivity

--- web/src/lib/components/shared-components/side-bar/side-bar-section.svelte
+++ web/src/lib/components/shared-components/side-bar/side-bar-section.svelte
@@ -3,7 +3,7 @@
   import { focusTrap } from '$lib/actions/focus-trap';
   import { menuButtonId } from '$lib/components/shared-components/navigation-bar/navigation-bar.svelte';
   import { isSidebarOpen } from '$lib/stores/side-bar.svelte';
-  import { onMount, type Snippet } from 'svelte';
+  import { type Snippet } from 'svelte';
 
   interface Props {
     children?: Snippet;
@@ -15,12 +15,8 @@
 
   let innerWidth: number = $state(0);
 
-  onMount(() => {
-    closeSidebar(innerWidth);
-  });
-
   const closeSidebar = (width: number) => {
-    isSidebarOpen.value = width >= mdBreakpoint ? true : false;
+    isSidebarOpen.value = width >= mdBreakpoint;
   };
 
   $effect(() => {
@@ -45,7 +41,7 @@
 <section
   id="sidebar"
   tabindex="-1"
-  class="immich-scrollbar relative z-10 flex w-0 md:w-[16rem] flex-col gap-1 overflow-y-auto overflow-x-hidden bg-immich-bg pt-8 transition-all duration-200 dark:bg-immich-dark-bg"
+  class="immich-scrollbar relative z-10 w-0 md:w-[16rem] overflow-y-auto overflow-x-hidden bg-immich-bg pt-8 transition-all duration-200 dark:bg-immich-dark-bg"
   class:shadow-2xl={isExpanded}
   class:dark:border-r-immich-dark-gray={isExpanded}
   class:border-r={isExpanded}
@@ -54,7 +50,7 @@
   use:clickOutside={{ onOutclick: handleClickOutside, onEscape: handleClickOutside }}
   use:focusTrap={{ active: isExpanded }}
 >
-  <div class="pr-6">
+  <div class="pr-6 flex flex-col gap-1 h-max min-h-full">
     {@render children?.()}
   </div>
 </section>
