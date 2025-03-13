5b5587a6442238e6202a0e64dc4a8c81457d6723
Andreas Knecht <knecht.andreas@gmail.com>
2025-03-13 14:42:29 +1100

    16712: Cleanup

--- web/src/lib/components/memory-page/memory-viewer.svelte
+++ web/src/lib/components/memory-page/memory-viewer.svelte
@@ -620,7 +620,8 @@
 
 <style>
   .main-view {
-    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.3),
-    0 8px 12px 6px rgba(0, 0, 0, 0.15);
+    box-shadow:
+      0 4px 4px 0 rgba(0, 0, 0, 0.3),
+      0 8px 12px 6px rgba(0, 0, 0, 0.15);
   }
 </style>
