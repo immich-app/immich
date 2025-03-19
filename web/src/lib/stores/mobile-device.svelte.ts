fe2472fe499b90f22d64b4c5cf4974448498346a
Min Idzelis <min123@gmail.com>
2025-03-19 03:55:16 +0000

    test

--- web/src/lib/stores/mobile-device.svelte.ts
+++ web/src/lib/stores/mobile-device.svelte.ts
@@ -3,12 +3,11 @@ import { MediaQuery } from 'svelte/reactivity';
 const hoverNone = new MediaQuery('hover: none');
 const maxMd = new MediaQuery('max-width: 767px');
 
-
 export const mobileDevice = {
   get hoverNone() {
     return hoverNone.current;
   },
   get maxMd() {
     return maxMd.current;
-  }
+  },
 };
