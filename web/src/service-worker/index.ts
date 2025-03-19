330805b163a6edc263873dcd8c53a72b4b76afda
Min Idzelis <min123@gmail.com>
2025-03-19 00:57:30 +0000

    Only handle preload/cancel

--- web/src/service-worker/index.ts
+++ web/src/service-worker/index.ts
@@ -2,24 +2,17 @@
 /// <reference no-default-lib="true"/>
 /// <reference lib="esnext" />
 /// <reference lib="webworker" />
+import { version } from '$service-worker';
 
-const sw = globalThis as unknown as ServiceWorkerGlobalScope;
-import { build, files, version } from '$service-worker';
-
-const pendingLoads = new Map<string, AbortController>();
 const useCache = true;
+const sw = globalThis as unknown as ServiceWorkerGlobalScope;
+const pendingLoads = new Map<string, AbortController>();
+
 // Create a unique cache name for this deployment
 const CACHE = `cache-${version}`;
 
-const APP_RESOURCES = [
-  ...build, // the app itself
-  ...files, // everything in `static`
-];
-
 sw.addEventListener('install', (event) => {
   event.waitUntil(sw.skipWaiting());
-  // Create a new cache and add all files to it
-  event.waitUntil(addFilesToCache());
 });
 
 sw.addEventListener('activate', (event) => {
@@ -30,18 +23,12 @@ sw.addEventListener('activate', (event) => {
 
 sw.addEventListener('fetch', (event) => {
   // ignore POST requests etc
-  if (event.request.method !== 'GET') return;
+  if (event.request.method !== 'GET') {
+    return;
+  }
   const url = new URL(event.request.url);
-  if (APP_RESOURCES.includes(url.pathname)) {
-    event.respondWith(appResources(url, event));
-  } else if (/^\/api\/assets\/[a-f0-9-]+\/(original|thumbnail)/.test(url.pathname)) {
+  if (/^\/api\/assets\/[a-f0-9-]+\/(original|thumbnail)/.test(url.pathname)) {
     event.respondWith(immichAsset(url));
-  } else if (
-    /^(\/(link|auth|admin|albums|archive|buy|explore|favorites|folders|maps|memory|partners|people|photos|places|search|share|shared-links|sharing|tags|trash|user-settings|utilities))(\/.*)?$/.test(
-      url.pathname,
-    )
-  ) {
-    event.respondWith(ssr(new URL(event.request.url).origin));
   }
 });
 
@@ -49,28 +36,6 @@ async function deleteOldCaches() {
   for (const key of await caches.keys()) {
     if (key !== CACHE) await caches.delete(key);
   }
-}
-
-async function addFilesToCache() {
-  const cache = await caches.open(CACHE);
-  await cache.addAll(APP_RESOURCES);
-}
-
-async function ssr(origin: string) {
-  const cache = await caches.open(CACHE);
-  const url = new URL('/', origin);
-  let response = useCache ? await cache.match(url) : undefined;
-  if (response) {
-    return response;
-  }
-  response = await fetch(url);
-  if (!(response instanceof Response)) {
-    throw new TypeError('invalid response from fetch');
-  }
-  if (response.status === 200) {
-    cache.put(url, response.clone());
-  }
-  return response;
 }
 
 async function immichAsset(url: URL) {
@@ -92,49 +57,11 @@ async function immichAsset(url: URL) {
     if (response.status === 200) {
       cache.put(url, response.clone());
     }
-  } catch (error) {
-    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-    if ((error as any).name !== 'AbortError') {
-      throw error;
-    }
+    return response;
+  } catch {
+    return Response.error();
   } finally {
     pendingLoads.delete(url.toString());
-  }
-  return response as Response;
-}
-
-async function appResources(url: URL, event: FetchEvent) {
-  const cache = await caches.open(CACHE);
-  // `build`/`files` can always be served from the cache
-  if (APP_RESOURCES.includes(url.pathname)) {
-    const response = await cache.match(url.pathname);
-    if (response) {
-      return response;
-    }
-  }
-  // for everything else, try the network first, but
-  // fall back to the cache if we're offline
-  try {
-    const response = await fetch(event.request);
-    // if we're offline, fetch can return a value that is not a Response
-    // instead of throwing - and we can't pass this non-Response to respondWith
-    if (!(response instanceof Response)) {
-      throw new TypeError('invalid response from fetch');
-    }
-
-    if (response.status === 200) {
-      cache.put(event.request, response.clone());
-    }
-
-    return response;
-  } catch (error) {
-    const response = await cache.match(event.request);
-    if (response) {
-      return response;
-    }
-    // if there's no cache, then just error out
-    // as there is nothing we can do to respond to this request
-    throw error;
   }
 }
 
