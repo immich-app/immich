f2fa0c8b50325340a744cd214928d9ce0f6a1c62
Min Idzelis <min123@gmail.com>
2025-03-19 01:29:00 +0000

    feat: mobile-web improvements - scrubber

---
+++ web/src/lib/stores/mobile-device.svelte.ts
@@ -0,0 +1,9 @@
+import { MediaQuery } from 'svelte/reactivity';
+
+const hoverNone = new MediaQuery('hover: none');
+
+export const mobileDevice = {
+  get hoverNone() {
+    return hoverNone.current;
+  }
+};
