69da5dee917de11418b6bf7104bc918817e3b31b
shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2025-03-20 01:16:09 +0530

    chore: rename user api interface

--- mobile/test/infrastructure/repository.mock.dart
+++ mobile/test/infrastructure/repository.mock.dart
@@ -1,7 +1,7 @@
 import 'package:immich_mobile/domain/interfaces/log.interface.dart';
 import 'package:immich_mobile/domain/interfaces/store.interface.dart';
 import 'package:immich_mobile/domain/interfaces/user.interface.dart';
-import 'package:immich_mobile/domain/interfaces/user_api.repository.dart';
+import 'package:immich_mobile/domain/interfaces/user_api.interface.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockStoreRepository extends Mock implements IStoreRepository {}
