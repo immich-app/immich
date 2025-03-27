d1756943722049a5427545132867b1240d7ba28f
ben-basten <45583362+ben-basten@users.noreply.github.com>
2025-03-26 20:26:13 -0400

    fix(web): search bar deactivates when focus exits

--- web/src/lib/stores/search.svelte.ts
+++ web/src/lib/stores/search.svelte.ts
@@ -1,12 +1,10 @@
 class SearchStore {
   savedSearchTerms = $state<string[]>([]);
   isSearchEnabled = $state(false);
-  preventRaceConditionSearchBar = $state(false);
 
   clearCache() {
     this.savedSearchTerms = [];
     this.isSearchEnabled = false;
-    this.preventRaceConditionSearchBar = false;
   }
 }
 
