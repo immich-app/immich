8412e75a284cb0fe3cb89eb37e589d0969db2a03
Ruben Hensen <ruben.hensen@protonmail.com>
2025-04-06 13:39:27 +0200

    Fix user dto formatting

--- server/src/dtos/user.dto.ts
+++ server/src/dtos/user.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
-import { IsBoolean, IsEmail, IsNotEmpty, IsNumber, Min, IsString } from 'class-validator';
+import { IsBoolean, IsEmail, IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
 import { User, UserAdmin } from 'src/database';
 import { UserMetadataEntity, UserMetadataItem } from 'src/entities/user-metadata.entity';
 import { UserEntity } from 'src/entities/user.entity';
