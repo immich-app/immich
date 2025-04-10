e3995fb5f4171f7ea466e25453a7ae602b42fb66
Ben <45583362+ben-basten@users.noreply.github.com>
2025-04-10 13:00:30 -0400

    fix(web): increase sidebar breakpoint (#17436)

--- web/src/lib/components/shared-components/side-bar/side-bar-section.svelte
+++ web/src/lib/components/shared-components/side-bar/side-bar-section.svelte
@@ -2,52 +2,45 @@
   import { clickOutside } from '$lib/actions/click-outside';
   import { focusTrap } from '$lib/actions/focus-trap';
   import { menuButtonId } from '$lib/components/shared-components/navigation-bar/navigation-bar.svelte';
-  import { isSidebarOpen } from '$lib/stores/side-bar.svelte';
-  import { type Snippet } from 'svelte';
+  import { mobileDevice } from '$lib/stores/mobile-device.svelte';
+  import { sidebarStore } from '$lib/stores/sidebar.svelte';
+  import { onMount, type Snippet } from 'svelte';
 
   interface Props {
     children?: Snippet;
   }
 
-  const mdBreakpoint = 768;
-
   let { children }: Props = $props();
 
-  let innerWidth: number = $state(0);
+  const isHidden = $derived(!sidebarStore.isOpen && !mobileDevice.isFullSidebar);
+  const isExpanded = $derived(sidebarStore.isOpen && !mobileDevice.isFullSidebar);
 
-  const closeSidebar = (width: number) => {
-    isSidebarOpen.value = width >= mdBreakpoint;
-  };
-
-  $effect(() => {
-    closeSidebar(innerWidth);
+  onMount(() => {
+    closeSidebar();
   });
 
-  const isHidden = $derived(!isSidebarOpen.value && innerWidth < mdBreakpoint);
-  const isExpanded = $derived(isSidebarOpen.value && innerWidth < mdBreakpoint);
-
-  const handleClickOutside = () => {
-    if (!isSidebarOpen.value) {
+  const closeSidebar = () => {
+    if (!isExpanded) {
       return;
     }
-    closeSidebar(innerWidth);
+    sidebarStore.reset();
     if (isHidden) {
       document.querySelector<HTMLButtonElement>(`#${menuButtonId}`)?.focus();
     }
   };
 </script>
 
-<svelte:window bind:innerWidth />
 <section
   id="sidebar"
   tabindex="-1"
-  class="immich-scrollbar relative z-10 w-0 md:w-[16rem] overflow-y-auto overflow-x-hidden bg-immich-bg pt-8 transition-all duration-200 dark:bg-immich-dark-bg"
+  class="immich-scrollbar relative z-10 w-0 sidebar:w-[16rem] overflow-y-auto overflow-x-hidden bg-immich-bg pt-8 transition-all duration-200 dark:bg-immich-dark-bg"
   class:shadow-2xl={isExpanded}
   class:dark:border-r-immich-dark-gray={isExpanded}
   class:border-r={isExpanded}
-  class:w-[min(100vw,16rem)]={isSidebarOpen.value}
+  class:w-[min(100vw,16rem)]={sidebarStore.isOpen}
+  data-testid="sidebar-parent"
   inert={isHidden}
-  use:clickOutside={{ onOutclick: handleClickOutside, onEscape: handleClickOutside }}
+  use:clickOutside={{ onOutclick: closeSidebar, onEscape: closeSidebar }}
   use:focusTrap={{ active: isExpanded }}
 >
   <div class="pr-6 flex flex-col gap-1 h-max min-h-full">
