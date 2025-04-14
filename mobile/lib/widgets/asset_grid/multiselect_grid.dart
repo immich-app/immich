70950f1fb184d9c702da53200dd61cdb9ccc821c
Toni Einecker <51962051+EinToni@users.noreply.github.com>
2025-04-14 20:17:57 +0200

    Dont show drag scroll date in search page
    * When using the drag scroll, the date of the current image is shown. This is now made toggleable.
    * For the mobile search result page, the display of the date is now disabled because the results are not sorted by date and therefore a display of the date is not desirable.

--- mobile/lib/widgets/asset_grid/multiselect_grid.dart
+++ mobile/lib/widgets/asset_grid/multiselect_grid.dart
@@ -36,6 +36,7 @@ class MultiselectGrid extends HookConsumerWidget {
     this.onRemoveFromAlbum,
     this.topWidget,
     this.stackEnabled = false,
+    this.dragScrollLabelEnabled = true,
     this.archiveEnabled = false,
     this.deleteEnabled = true,
     this.favoriteEnabled = true,
@@ -51,6 +52,7 @@ class MultiselectGrid extends HookConsumerWidget {
   final Future<bool> Function(Iterable<Asset>)? onRemoveFromAlbum;
   final Widget? topWidget;
   final bool stackEnabled;
+  final bool dragScrollLabelEnabled;
   final bool archiveEnabled;
   final bool unarchive;
   final bool deleteEnabled;
@@ -430,6 +432,7 @@ class MultiselectGrid extends HookConsumerWidget {
                               ),
                         topWidget: topWidget,
                         showStack: stackEnabled,
+                        showDragScrollLabel: dragScrollLabelEnabled,
                       ),
                 error: (error, _) => Center(child: Text(error.toString())),
                 loading: buildLoadingIndicator ?? buildDefaultLoadingIndicator,
