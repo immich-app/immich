ed2b54527c0634e332d10fcf26c25a3499ffc59b
Jonathan Jogenfors <jonathan@jogenfors.se>
2025-04-17 00:40:08 +0200

    chore(server): don't check null dates (#17664)

--- server/src/services/library.service.ts
+++ server/src/services/library.service.ts
@@ -582,12 +582,7 @@ export class LibraryService extends BaseService {
       return AssetSyncResult.CHECK_OFFLINE;
     }
 
-    if (
-      !asset.fileCreatedAt ||
-      !asset.localDateTime ||
-      !asset.fileModifiedAt ||
-      stat.mtime.valueOf() !== asset.fileModifiedAt.valueOf()
-    ) {
+    if (stat.mtime.valueOf() !== asset.fileModifiedAt.valueOf()) {
       this.logger.verbose(`Asset ${asset.originalPath} needs metadata extraction in library ${asset.libraryId}`);
 
       return AssetSyncResult.UPDATE;
