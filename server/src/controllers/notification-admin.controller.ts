488dc4efbdcb64fd84d4cf67f8b145160313a1c4
Jason Rasmussen <jason@rasm.me>
2025-04-21 10:49:26 -0400

    refactor: notification-admin controller (#17748)

--- server/src/controllers/notification-admin.controller.ts
+++ server/src/controllers/notification-admin.controller.ts
@@ -7,22 +7,22 @@ import { Auth, Authenticated } from 'src/middleware/auth.guard';
 import { EmailTemplate } from 'src/repositories/notification.repository';
 import { NotificationService } from 'src/services/notification.service';
 
-@ApiTags('Notifications')
-@Controller('notifications')
-export class NotificationController {
+@ApiTags('Notifications (Admin)')
+@Controller('notifications/admin')
+export class NotificationAdminController {
   constructor(private service: NotificationService) {}
 
   @Post('test-email')
   @HttpCode(HttpStatus.OK)
   @Authenticated({ admin: true })
-  sendTestEmail(@Auth() auth: AuthDto, @Body() dto: SystemConfigSmtpDto): Promise<TestEmailResponseDto> {
+  sendTestEmailAdmin(@Auth() auth: AuthDto, @Body() dto: SystemConfigSmtpDto): Promise<TestEmailResponseDto> {
     return this.service.sendTestEmail(auth.user.id, dto);
   }
 
   @Post('templates/:name')
   @HttpCode(HttpStatus.OK)
   @Authenticated({ admin: true })
-  getNotificationTemplate(
+  getNotificationTemplateAdmin(
     @Auth() auth: AuthDto,
     @Param('name') name: EmailTemplate,
     @Body() dto: TemplateDto,
