e09f975ef2aab75edcf1fed84005cb873cb2111e
Toni Einecker <51962051+EinToni@users.noreply.github.com>
2025-04-23 16:30:23 +0200

    Remove import

--- mobile/lib/widgets/common/immich_thumbnail.dart
+++ mobile/lib/widgets/common/immich_thumbnail.dart
@@ -1,7 +1,6 @@
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/providers/image/immich_local_thumbnail_provider.dart';
 import 'package:immich_mobile/providers/image/immich_remote_thumbnail_provider.dart';
