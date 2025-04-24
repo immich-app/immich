1364064a81426b6a6ca1faf427a36745bd4b515a
Jason Rasmussen <jason@rasm.me>
2025-04-24 16:42:36 -0400

    refactor: event manager

---
+++ web/src/lib/stores/auth-manager.svelte.ts
@@ -0,0 +1,31 @@
+import { goto } from '$app/navigation';
+import { AppRoute } from '$lib/constants';
+import { eventManager } from '$lib/stores/event-manager.svelte';
+import { logout } from '@immich/sdk';
+
+class AuthManager {
+  async logout(redirectUri?: string) {
+    try {
+      const response = await logout();
+      if (response.redirectUri) {
+        redirectUri = response.redirectUri;
+      }
+    } catch (error) {
+      console.log('Error logging out:', error);
+    }
+
+    redirectUri = redirectUri ?? AppRoute.AUTH_LOGIN;
+
+    try {
+      if (redirectUri.startsWith('/')) {
+        await goto(redirectUri);
+      } else {
+        globalThis.location.href = redirectUri;
+      }
+    } finally {
+      eventManager.emit('auth.logout');
+    }
+  }
+}
+
+export const authManager = new AuthManager();
