41fc7fea5a0107c837c4c82fb8503be8e9df1508
Andreas Tollkötter <andreas.tollkoetter@rwth-aachen.de>
2025-04-26 22:59:59 +0200

    Remove unused types and functions in timeline-util

--- web/src/lib/utils/timeline-util.ts
+++ web/src/lib/utils/timeline-util.ts
@@ -1,72 +1,19 @@
-import type { AssetBucket } from '$lib/stores/assets-store.svelte';
 import { locale } from '$lib/stores/preferences.store';
-import { type CommonJustifiedLayout } from '$lib/utils/layout-utils';
-
-import type { AssetResponseDto } from '@immich/sdk';
 import { memoize } from 'lodash-es';
 import { DateTime, type LocaleOptions } from 'luxon';
 import { get } from 'svelte/store';
 
-export type DateGroup = {
-  bucket: AssetBucket;
-  index: number;
-  row: number;
-  col: number;
-  date: DateTime;
-  groupTitle: string;
-  assets: AssetResponseDto[];
-  assetsIntersecting: boolean[];
-  height: number;
-  intersecting: boolean;
-  geometry: CommonJustifiedLayout;
-};
 export type ScrubberListener = (
   bucketDate: string | undefined,
   overallScrollPercent: number,
   bucketScrollPercent: number,
 ) => void | Promise<void>;
-export type ScrollTargetListener = ({
-  bucket,
-  dateGroup,
-  asset,
-  offset,
-}: {
-  bucket: AssetBucket;
-  dateGroup: DateGroup;
-  asset: AssetResponseDto;
-  offset: number;
-}) => void;
 
 export const fromLocalDateTime = (localDateTime: string) =>
   DateTime.fromISO(localDateTime, { zone: 'UTC', locale: get(locale) });
 
 export const fromDateTimeOriginal = (dateTimeOriginal: string, timeZone: string) =>
   DateTime.fromISO(dateTimeOriginal, { zone: timeZone });
-
-export type LayoutBox = {
-  aspectRatio: number;
-  top: number;
-  width: number;
-  height: number;
-  left: number;
-  forcedAspectRatio?: boolean;
-};
-
-export function findTotalOffset(element: HTMLElement, stop: HTMLElement) {
-  let offset = 0;
-  while (element.offsetParent && element !== stop) {
-    offset += element.offsetTop;
-    element = element.offsetParent as HTMLElement;
-  }
-  return offset;
-}
-
-export const groupDateFormat: Intl.DateTimeFormatOptions = {
-  weekday: 'short',
-  month: 'short',
-  day: 'numeric',
-  year: 'numeric',
-};
 
 export function formatGroupTitle(_date: DateTime): string {
   if (!_date.isValid) {
