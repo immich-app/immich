dfaeb9d5fb59d98f67daab5dd89d88bb386078df
Min Idzelis <min123@gmail.com>
2025-05-01 02:16:19 +0000

    wip

--- .devcontainer/server/postCreate.sh
+++ .devcontainer/server/postCreate.sh
@@ -2,12 +2,13 @@
 # shellcheck source=common.sh
 source /immich-devcontainer/common.sh
 
+sudo find "${IMMICH_WORKSPACE}/server/upload" -not -path "${IMMICH_WORKSPACE}/server/upload/postgres/*" -not -path "${IMMICH_WORKSPACE}/server/upload/postgres" -exec chown node {} +
+
 sudo chown node -R "${IMMICH_WORKSPACE}/.vscode" \
     "${IMMICH_WORKSPACE}/cli/node_modules" \
     "${IMMICH_WORKSPACE}/e2e/node_modules" \
     "${IMMICH_WORKSPACE}/open-api/typescript-sdk/node_modules" \
     "${IMMICH_WORKSPACE}/server/node_modules" \
-    "${IMMICH_WORKSPACE}/server/upload" \
     "${IMMICH_WORKSPACE}/server/dist" \
     "${IMMICH_WORKSPACE}/web/node_modules" \
     "${IMMICH_WORKSPACE}/web/dist"
