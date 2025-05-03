fbefd9e2ec912d919d17c0f520441357c751fd2b
niwla23 <46248939+niwla23@users.noreply.github.com>
2025-05-03 13:53:45 +0200

    add translation and handle file deletes

--- web/src/routes/(user)/utilities/large-assets/[[photos=photos]]/[[assetId=id]]/+page.svelte
+++ web/src/routes/(user)/utilities/large-assets/[[photos=photos]]/[[assetId=id]]/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import UserPageLayout from '$lib/components/layouts/user-page-layout.svelte';
-  import { locale } from '$lib/stores/preferences.store';
   import { t } from 'svelte-i18n';
   import type { PageData } from './$types';
   import LargeAssetData from '$lib/components/utilities-page/large-assets/large-asset-data.svelte';
@@ -8,21 +7,23 @@
   import { assetViewingStore } from '$lib/stores/asset-viewing.store';
   import { handlePromiseError } from '$lib/utils';
   import { navigate } from '$lib/utils/navigation';
+  import type { Action } from '$lib/components/asset-viewer/actions/action';
 
   interface Props {
     data: PageData;
   }
 
   let { data = $bindable() }: Props = $props();
+  let assets = $state(data.assets);
   const { isViewing: showAssetViewer, asset: viewingAsset, setAsset } = assetViewingStore;
-  const getAssetIndex = (id: string) => data.assets.findIndex((asset) => asset.id === id);
+  const getAssetIndex = (id: string) => assets.findIndex((asset) => asset.id === id);
 
   const onNext = () => {
     const index = getAssetIndex($viewingAsset.id) + 1;
-    if (index >= data.assets.length) {
+    if (index >= assets.length) {
       return Promise.resolve(false);
     }
-    setAsset(data.assets[index]);
+    setAsset(assets[index]);
     return Promise.resolve(true);
   };
 
@@ -31,30 +32,37 @@
     if (index < 0) {
       return Promise.resolve(false);
     }
-    setAsset(data.assets[index]);
+    setAsset(assets[index]);
     return Promise.resolve(true);
   };
 
   const onRandom = () => {
-    if (data.assets.length <= 0) {
+    if (assets.length <= 0) {
       return Promise.resolve(undefined);
     }
-    const index = Math.floor(Math.random() * data.assets.length);
-    const asset = data.assets[index];
+    const index = Math.floor(Math.random() * assets.length);
+    const asset = assets[index];
     setAsset(asset);
     return Promise.resolve(asset);
+  };
+
+  const onAction = (payload: Action) => {
+    if (payload.type == 'trash') {
+      assets = assets.filter((a) => a.id != payload.asset.id);
+      $showAssetViewer = false;
+    }
   };
 </script>
 
 <UserPageLayout title={data.meta.title} scrollbar={true}>
   <div class="flex gap-2 flex-wrap">
-    {#if data.assets && data.assets.length > 0}
-      {#each data.assets as asset (asset.id)}
+    {#if assets && data.assets.length > 0}
+      {#each assets as asset (asset.id)}
         <LargeAssetData {asset} onViewAsset={(asset) => setAsset(asset)} />
       {/each}
     {:else}
       <p class="text-center text-lg dark:text-white flex place-items-center place-content-center">
-        {$t('no_assets_found')}
+        {$t('no_assets_to_show')}
       </p>
     {/if}
   </div>
@@ -65,10 +73,11 @@
     <Portal target="body">
       <AssetViewer
         asset={$viewingAsset}
-        showNavigation={data.assets.length > 1}
+        showNavigation={assets.length > 1}
         {onNext}
         {onPrevious}
         {onRandom}
+        {onAction}
         onClose={() => {
           assetViewingStore.showAssetViewer(false);
           handlePromiseError(navigate({ targetRoute: 'current', assetId: null }));
