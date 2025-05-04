4977a6c5d8fbb9fa4d38e53f301406fdb3ce8c5b
Andrei Mazniak <andreymaznyak@gmail.com>
2025-05-04 11:53:56 +0200

    feat(web): add support view 360 video in vr mode

--- web/src/lib/components/asset-viewer/video-panorama-viewer.svelte
+++ web/src/lib/components/asset-viewer/video-panorama-viewer.svelte
@@ -1,14 +1,33 @@
 <script lang="ts">
-  import { getAssetPlaybackUrl, getAssetOriginalUrl } from '$lib/utils';
+  import { getAssetOriginalUrl, getAssetPlaybackUrl } from '$lib/utils';
+  import { onMount } from 'svelte';
+  import { t } from 'svelte-i18n';
   import { fade } from 'svelte/transition';
   import LoadingSpinner from '../shared-components/loading-spinner.svelte';
-  import { t } from 'svelte-i18n';
-
   interface Props {
     assetId: string;
   }
 
   const { assetId }: Props = $props();
+
+  let isVrSupported = $state(false);
+  let showVR = $state(false);
+  let currentTime = $state(0);
+
+  onMount(async () => {
+    // Check if WebXR is supported and specifically if immersive-vr is supported
+    if ('xr' in navigator && typeof navigator?.xr?.isSessionSupported === 'function') {
+      try {
+        isVrSupported = await navigator.xr.isSessionSupported('immersive-vr');
+        showVR = isVrSupported;
+      } catch (err) {
+        console.error('Error checking VR support:', err);
+        isVrSupported = false;
+      }
+    } else {
+      console.error('WebXR not supported');
+    }
+  });
 
   const modules = Promise.all([
     import('./photo-sphere-viewer-adapter.svelte').then((module) => module.default),
@@ -16,20 +35,40 @@
     import('@photo-sphere-viewer/video-plugin').then((module) => module.VideoPlugin),
     import('@photo-sphere-viewer/video-plugin/index.css'),
   ]);
+
+  const immersiveModule = Promise.all([
+    import('./immersive-video-panorama-viewer.svelte').then((module) => module.default),
+  ]);
 </script>
 
 <div transition:fade={{ duration: 150 }} class="flex h-full select-none place-content-center place-items-center">
-  {#await modules}
-    <LoadingSpinner />
-  {:then [PhotoSphereViewer, adapter, videoPlugin]}
-    <PhotoSphereViewer
-      panorama={{ source: getAssetPlaybackUrl(assetId) }}
-      originalPanorama={{ source: getAssetOriginalUrl(assetId) }}
-      plugins={[videoPlugin]}
-      {adapter}
-      navbar
-    />
-  {:catch}
-    {$t('errors.failed_to_load_asset')}
-  {/await}
+  {#if !showVR || !isVrSupported}
+    {#await modules}
+      <LoadingSpinner />
+    {:then [PhotoSphereViewer, adapter, videoPlugin]}
+      <PhotoSphereViewer
+        panorama={{ source: getAssetPlaybackUrl(assetId) }}
+        originalPanorama={{ source: getAssetOriginalUrl(assetId) }}
+        plugins={[videoPlugin]}
+        {adapter}
+        navbar
+      />
+    {:catch}
+      {$t('errors.failed_to_load_asset')}
+    {/await}
+  {:else}
+    {#await immersiveModule}
+      <LoadingSpinner />
+    {:then [ImmersiveVideoPanoramaViewer]}
+      <ImmersiveVideoPanoramaViewer
+        videoUrl={getAssetPlaybackUrl(assetId)}
+        {currentTime}
+        onClose={() => {
+          showVR = false;
+        }}
+      />
+    {:catch}
+      {$t('errors.failed_to_load_asset')}
+    {/await}
+  {/if}
 </div>
