f75d853e9a53fce2338d67abf241f9bdeb71aac4
Robert Vollmer <dev@robv.de>
2025-05-08 15:45:11 +0200

    fix(mobile): Remote video playback and asset download on Android with mTLS (#16403)
    * Add class to apply SSL options
    
    * Apply client certificate for native Android code
    
    * Refactor self-signed check
    
    * Allow self-signed certificates
    
    * Fix Dart analysis
    
    * Add HostnameVerifier
    
    Android explicitly does NOT check the Common Name of a certificate,
    only the Subject Alt Names. Chances are that someone who self-signs a
    certificate doesn't go through the extra steps to add a SAN, and in
    that case the connection would be prevented by the HostnameVerifier
    even thought the TrustManager was fine with the certificate itself.
    
    * Rename parameter like in Dart
    
    * Fix NPE
    
    * Catch all native errors in HttpSSLOptionsPlugin
    
    * Workaround for too early onChanged() callback
    
    * Fix formatting
    
    ---------
    
    Co-authored-by: Alex <alex.tran1502@gmail.com>

--- mobile/lib/widgets/settings/advanced_settings.dart
+++ mobile/lib/widgets/settings/advanced_settings.dart
@@ -10,7 +10,7 @@ import 'package:immich_mobile/providers/user.provider.dart';
 import 'package:immich_mobile/repositories/local_files_manager.repository.dart';
 import 'package:immich_mobile/services/app_settings.service.dart';
 import 'package:immich_mobile/utils/hooks/app_settings_update_hook.dart';
-import 'package:immich_mobile/utils/http_ssl_cert_override.dart';
+import 'package:immich_mobile/utils/http_ssl_options.dart';
 import 'package:immich_mobile/widgets/settings/custom_proxy_headers_settings/custome_proxy_headers_settings.dart';
 import 'package:immich_mobile/widgets/settings/local_storage_settings.dart';
 import 'package:immich_mobile/widgets/settings/settings_slider_list_tile.dart';
@@ -104,7 +104,7 @@ class AdvancedSettings extends HookConsumerWidget {
         valueNotifier: allowSelfSignedSSLCert,
         title: "advanced_settings_self_signed_ssl_title".tr(),
         subtitle: "advanced_settings_self_signed_ssl_subtitle".tr(),
-        onChanged: (_) => HttpOverrides.global = HttpSSLCertOverride(),
+        onChanged: HttpSSLOptions.applyFromSettings,
       ),
       const CustomeProxyHeaderSettings(),
       SslClientCertSettings(isLoggedIn: ref.read(currentUserProvider) != null),
