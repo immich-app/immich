0d789617c3826b13b2fc5d969dc18de275b2d6a4
wuzihao051119 <wuzihao051119@outlook.com>
2025-05-09 22:38:33 +0800

    feat: api access control

---
+++ web/src/lib/components/user-settings-page/api-key-grid.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Checkbox from "../elements/checkbox.svelte";
+
+  interface Props {
+    title: string;
+    content: string[];
+  };
+
+  let { title, content }: Props = $props();
+</script>
+
+<div class="m-4 flex flex-col gap-2">
+  <Checkbox
+    id=""
+    label={title}
+    labelClass="text-sm dark:text-immich-dark-fg"
+  />
+  <div class="ml-4 flex flex-wrap gap-x-5 gap-y-2">
+    {#each content as item}
+      <Checkbox
+        id=""
+        label={item}
+        labelClass="text-sm dark:text-immich-dark-fg"
+      />
+    {/each}
+  </div>
+</div>
