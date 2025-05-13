668288ca20e4020afdf387c1b5d365e989b98639
Daniel Dietzler <36593685+danieldietzler@users.noreply.github.com>
2025-05-13 19:20:44 +0200

    refactor: album users modal (#18266)

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -369,7 +369,6 @@ export enum SettingInputFieldType {
 export const AlbumPageViewMode = {
   SELECT_THUMBNAIL: 'select-thumbnail',
   SELECT_ASSETS: 'select-assets',
-  VIEW_USERS: 'view-users',
   VIEW: 'view',
   OPTIONS: 'options',
 };
@@ -377,7 +376,6 @@ export const AlbumPageViewMode = {
 export type AlbumPageViewMode =
   | typeof AlbumPageViewMode.SELECT_THUMBNAIL
   | typeof AlbumPageViewMode.SELECT_ASSETS
-  | typeof AlbumPageViewMode.VIEW_USERS
   | typeof AlbumPageViewMode.VIEW
   | typeof AlbumPageViewMode.OPTIONS;
 
