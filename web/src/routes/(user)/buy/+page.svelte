3629ce6df0b92f122828de97f75722d251890af8
Jason Rasmussen <jason@rasm.me>
2025-05-14 11:52:45 -0400

    refactor: buy immich

--- web/src/routes/(user)/buy/+page.svelte
+++ web/src/routes/(user)/buy/+page.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
   import { goto } from '$app/navigation';
-  import Icon from '$lib/components/elements/icon.svelte';
   import UserPageLayout from '$lib/components/layouts/user-page-layout.svelte';
   import LicenseActivationSuccess from '$lib/components/shared-components/purchasing/purchase-activation-success.svelte';
   import LicenseContent from '$lib/components/shared-components/purchasing/purchase-content.svelte';
   import SupporterBadge from '$lib/components/shared-components/side-bar/supporter-badge.svelte';
   import { AppRoute } from '$lib/constants';
   import { purchaseStore } from '$lib/stores/purchase.store';
-  import { Container } from '@immich/ui';
+  import { Alert, Container, Stack } from '@immich/ui';
   import { mdiAlertCircleOutline } from '@mdi/js';
   import { t } from 'svelte-i18n';
   import type { PageData } from './$types';
@@ -23,28 +22,24 @@
 
 <UserPageLayout title={$t('buy')}>
   <Container size="medium" center>
-    {#if data.isActivated === false}
-      <div
-        class="bg-red-100 text-red-700 px-4 py-3 rounded-md flex place-items-center place-content-center gap-2"
-        role="alert"
-      >
-        <Icon path={mdiAlertCircleOutline} size="18" />
-        <p>{$t('purchase_failed_activation')}</p>
-      </div>
-    {/if}
+    <Stack gap={4} class="mt-4">
+      {#if data.isActivated === false}
+        <Alert icon={mdiAlertCircleOutline} color="danger" title={$t('purchase_failed_activation')} />
+      {/if}
 
-    {#if $isPurchased}
-      <SupporterBadge logoSize="lg" centered />
-    {/if}
+      {#if $isPurchased}
+        <SupporterBadge logoSize="lg" centered />
+      {/if}
 
-    {#if showLicenseActivated || data.isActivated === true}
-      <LicenseActivationSuccess onDone={() => goto(AppRoute.PHOTOS, { replaceState: false })} />
-    {:else}
-      <LicenseContent
-        onActivate={() => {
-          showLicenseActivated = true;
-        }}
-      />
-    {/if}
+      {#if showLicenseActivated || data.isActivated === true}
+        <LicenseActivationSuccess onDone={() => goto(AppRoute.PHOTOS, { replaceState: false })} />
+      {:else}
+        <LicenseContent
+          onActivate={() => {
+            showLicenseActivated = true;
+          }}
+        />
+      {/if}
+    </Stack>
   </Container>
 </UserPageLayout>
