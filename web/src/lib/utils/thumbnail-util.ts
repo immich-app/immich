b262164f972ae0194d77e1572c0f0a0a18f1fd53
mertalev <101130780+mertalev@users.noreply.github.com>
2025-05-16 17:48:33 -0400

    array access actually doesn't seem to matter

--- web/src/lib/utils/thumbnail-util.ts
+++ web/src/lib/utils/thumbnail-util.ts
@@ -50,9 +50,9 @@ export const getAltText = derived(t, ($t) => {
       date,
       city: asset.city,
       country: asset.country,
-      person1: asset.people.at(0),
-      person2: asset.people.at(1),
-      person3: asset.people.at(2),
+      person1: asset.people[0],
+      person2: asset.people[1],
+      person3: asset.people[2],
       isVideo,
       additionalCount: peopleCount > 3 ? peopleCount - 2 : 0,
     };
