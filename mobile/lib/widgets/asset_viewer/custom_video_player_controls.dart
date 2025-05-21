d7df4294bb7c82108ea635e933c80cb108ab9df4
bwees <brandonwees@gmail.com>
2025-05-21 11:08:15 -0500

    fix lintings

--- mobile/lib/widgets/asset_viewer/custom_video_player_controls.dart
+++ mobile/lib/widgets/asset_viewer/custom_video_player_controls.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/models/cast/cast_manager_state.dart';
 import 'package:immich_mobile/providers/asset_viewer/current_asset.provider.dart';
