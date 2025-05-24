f2ba3f49d13f32e7e0b1bfc1e9b649762773343b
Andreas Tollkötter <andreas.tollkoetter@rwth-aachen.de>
2025-05-24 18:59:10 +0200

    make build

--- mobile/lib/providers/backup/backup_verification.provider.g.dart
+++ mobile/lib/providers/backup/backup_verification.provider.g.dart
@@ -7,7 +7,7 @@ part of 'backup_verification.provider.dart';
 // **************************************************************************
 
 String _$backupVerificationHash() =>
-    r'7c8cedb1a5af6d0ca735f3447795a78980c3fbf8';
+    r'b204e43ab575d5fa5b2ee663297f32bcee9074f5';
 
 /// See also [BackupVerification].
 @ProviderFor(BackupVerification)
