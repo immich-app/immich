7cfdcdf449b5650a178823edf77bdaa1bc5a8ea1
bwees <brandonwees@gmail.com>
2025-05-26 13:57:40 -0500

    remove circle icon button from search history box

--- web/src/lib/components/shared-components/search-bar/search-history-box.svelte
+++ web/src/lib/components/shared-components/search-bar/search-history-box.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import CircleIconButton from '$lib/components/elements/buttons/circle-icon-button.svelte';
   import Icon from '$lib/components/elements/icon.svelte';
   import { searchStore } from '$lib/stores/search.svelte';
   import { IconButton } from '@immich/ui';
