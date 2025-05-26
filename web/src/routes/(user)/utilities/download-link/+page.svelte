4c396cebe93b1f50810d071c61790e264506c181
Nicholas Flamy <30300649+NicholasFlamy@users.noreply.github.com>
2025-05-26 17:55:37 -0400

    commenting my code

--- web/src/routes/(user)/utilities/download-link/+page.svelte
+++ web/src/routes/(user)/utilities/download-link/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import type { PageProps } from './$types';
-
+  // load data (specifically version) from `+page.ts`
   const { data }: PageProps = $props();
-
+  // initialize constants, apks and docker files are arrays so we can iterate later
+  // files should not have slashes in front (eg. `app-release.apk` not `/app-release.apk`)
   const baseURL = 'https://github.com/immich-app/immich/releases/';
   const APKs = [
     'app-arm64-v8a-release.apk',
@@ -10,20 +11,22 @@
     'app-release.apk',
     'app-x86_64-release.apk',
   ];
-  const docker_files = ['docker-compose.yml', 'example.env', 'hwaccel.ml.yml', 'hwaccel.transcoding.yml'];
+  const dockerFiles = ['docker-compose.yml', 'example.env', 'hwaccel.ml.yml', 'hwaccel.transcoding.yml'];
   const prometheus = 'prometheus.yml';
   const temp = ['', '', '', ''];
 
   const downloadBaseURL = new URL('download/' + data.version + '/', baseURL);
 </script>
 
-<h1>GitHub Releases Page:</h1>
-<a href={baseURL} class="underline text-sm immich-form-label" target="_blank" rel="noreferrer">
-  {data.version}
-  GitHub
-</a>
-<br />
-<br />
+<div>
+  <h1>GitHub Releases Page:</h1>
+  <a href={baseURL} class="underline text-sm immich-form-label" target="_blank" rel="noreferrer">
+    {data.version}
+    GitHub
+  </a>
+  <br />
+  <br />
+</div>
 <div>
   <h1>Android APKs:</h1>
   {#each APKs as file (file)}
@@ -37,7 +40,7 @@
 <br />
 <div>
   <h1>Docker Files:</h1>
-  {#each docker_files as file (file)}
+  {#each dockerFiles as file (file)}
     <a href={downloadBaseURL + file} class="underline text-sm immich-form-label" target="_blank" rel="noreferrer">
       {data.version}
       {file}
--- web/src/routes/(user)/utilities/download-link/+page.ts
+++ web/src/routes/(user)/utilities/download-link/+page.ts
@@ -2,6 +2,7 @@ import { getAboutInfo } from '@immich/sdk';
 import type { PageLoad } from './$types';
 
 export const load = (async () => {
+  // we do this before the page loads because the page is useless otherwise
   const { version } = await getAboutInfo();
   return {
     version,
