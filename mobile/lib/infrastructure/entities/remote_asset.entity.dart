3218d3d250936542670433208d4bdddcdc5a2048
shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2025-05-30 08:51:46 +0530

    add visibility and update constraints

--- mobile/lib/infrastructure/entities/remote_asset.entity.dart
+++ mobile/lib/infrastructure/entities/remote_asset.entity.dart
@@ -1,16 +1,21 @@
 import 'package:drift/drift.dart';
+import 'package:immich_mobile/domain/models/asset/base_asset.model.dart';
 import 'package:immich_mobile/infrastructure/entities/user.entity.dart';
 import 'package:immich_mobile/infrastructure/utils/asset.mixin.dart';
 import 'package:immich_mobile/infrastructure/utils/drift_default.mixin.dart';
 
-@TableIndex(name: 'remote_asset_checksum', columns: {#checksum})
+@TableIndex(
+  name: 'UQ_remote_asset_owner_checksum',
+  columns: {#checksum, #ownerId},
+  unique: true,
+)
 class RemoteAssetEntity extends Table
     with DriftDefaultsMixin, AssetEntityMixin {
   const RemoteAssetEntity();
 
   BlobColumn get remoteId => blob()();
 
-  TextColumn get checksum => text().unique()();
+  TextColumn get checksum => text()();
 
   BoolColumn get isFavorite => boolean().withDefault(const Constant(false))();
 
@@ -22,6 +27,8 @@ class RemoteAssetEntity extends Table
   TextColumn get thumbHash => text().nullable()();
 
   DateTimeColumn get deletedAt => dateTime().nullable()();
+
+  IntColumn get visibility => intEnum<AssetVisibility>()();
 
   @override
   Set<Column> get primaryKey => {remoteId};
