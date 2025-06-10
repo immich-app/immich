6f1dd05def9fc40cc229d63b93007ac5fa01763e
Andreas Tollkötter <andreas.tollkoetter@rwth-aachen.de>
2025-06-10 16:34:58 +0200

    DCM automatic fixes

--- mobile/lib/services/auth.service.dart
+++ mobile/lib/services/auth.service.dart
@@ -135,9 +135,9 @@ class AuthService {
     ]);
   }
 
-  Future<void> changePassword(String newPassword) {
+  Future<void> changePassword(String newPassword) async {
     try {
-      return _authApiRepository.changePassword(newPassword);
+      return await _authApiRepository.changePassword(newPassword);
     } catch (error, stackTrace) {
       _log.severe("Error changing password", error, stackTrace);
       rethrow;
