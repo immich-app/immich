78e6ac9accd1376769616c8d82a3a9bb946b8e4f
CJPeckover <cjpeckover@hotmail.ca>
2025-06-10 11:02:40 -0400

    lint

--- web/src/lib/components/memory-page/memory-viewer.svelte
+++ web/src/lib/components/memory-page/memory-viewer.svelte
@@ -25,10 +25,9 @@
   } from '$lib/components/shared-components/notification/notification';
   import { AppRoute, QueryParameter } from '$lib/constants';
   import { authManager } from '$lib/managers/auth-manager.svelte';
+  import type { TimelineAsset, Viewport } from '$lib/managers/timeline-manager/types';
   import { AssetInteraction } from '$lib/stores/asset-interaction.svelte';
   import { assetViewingStore } from '$lib/stores/asset-viewing.store';
-  import type { TimelineAsset } from '$lib/managers/timeline-manager/types';
-  import type { Viewport } from '$lib/managers/timeline-manager/types';
   import { type MemoryAsset, memoryStore } from '$lib/stores/memory.store.svelte';
   import { locale, videoViewerMuted, videoViewerVolume } from '$lib/stores/preferences.store';
   import { preferences } from '$lib/stores/user.store';
@@ -261,7 +260,7 @@
     playerInitialized = true;
   };
 
-  afterNavigate(({ from, to, type }) => {
+  afterNavigate(({ from, to }) => {
     memoryStore.initialize().then(
       () => {
         let target = null;
