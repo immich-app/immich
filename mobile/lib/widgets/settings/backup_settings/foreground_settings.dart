9dc80348ab2f0ad3a35a7d964fc12b0e8950e8ed
dvbthien <dvbthien@gmail.com>
2025-06-19 19:25:26 +0700

    clean up foreground backup

--- mobile/lib/widgets/settings/backup_settings/foreground_settings.dart
+++ mobile/lib/widgets/settings/backup_settings/foreground_settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/extensions/translate_extensions.dart';
 import 'package:immich_mobile/providers/backup/backup.provider.dart';
@@ -7,20 +8,21 @@ import 'package:immich_mobile/widgets/settings/core/setting_section_header.dart'
 import 'package:immich_mobile/widgets/settings/core/setting_switch_list_tile.dart';
 import 'package:immich_mobile/widgets/settings/layouts/settings_card_layout.dart';
 
-class ForegroundBackupSettings extends ConsumerWidget {
+class ForegroundBackupSettings extends HookConsumerWidget {
   const ForegroundBackupSettings({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final isEnabled = ref.watch(backupProvider.select((s) => s.autoBackup));
-    final valueNotifier = ValueNotifier<bool>(isEnabled);
+    final isAutoBackup = ref.watch(backupProvider.select((s) => s.autoBackup));
+    final isAutoBackupNotifier = useValueNotifier(isAutoBackup);
+    useValueChanged(
+      isAutoBackup,
+      (_, __) => WidgetsBinding.instance.addPostFrameCallback(
+        (_) => isAutoBackupNotifier.value = isAutoBackup,
+      ),
+    );
 
-    void toggleForegroundBackup(bool value) {
-      ref.read(backupProvider.notifier).setAutoBackup(value);
-      valueNotifier.value = value;
-    }
-
-    final subtitle = isEnabled
+    final subtitle = isAutoBackup
         ? 'backup_controller_page_status_on'.t(context: context)
         : 'backup_controller_page_status_off'.t(context: context);
 
@@ -35,8 +37,9 @@ class ForegroundBackupSettings extends ConsumerWidget {
           text: 'backup_controller_page_desc_backup',
         ),
         SettingSwitchListTile(
-          valueNotifier: valueNotifier,
-          onChanged: toggleForegroundBackup,
+          valueNotifier: isAutoBackupNotifier,
+          onChanged: (enabled) =>
+              ref.read(backupProvider.notifier).setAutoBackup(enabled),
           title: 'backup_controller_page_foreground_title'.t(context: context),
           subtitle: subtitle,
         ),
