2cf24f466591fa628b063274dc4f8aa7e89fce66
Alex Tran <alex.tran1502@gmail.com>
2025-06-19 22:26:13 -0500

    refactor(mobile): biometric.interface.dart

--- mobile/lib/interfaces/biometric.interface.dart
+++
@@ -1,6 +0,0 @@
-import 'package:immich_mobile/models/auth/biometric_status.model.dart';
-
-abstract interface class IBiometricRepository {
-  Future<BiometricStatus> getStatus();
-  Future<bool> authenticate(String? message);
-}
