2ed6a16d09177a8cbed31a3713f4e894414bda75
Daimolean <92239625+wuzihao051119@users.noreply.github.com>
2025-06-23 19:49:21 +0800

    fix(web): undefined release
    Co-authored-by: Daniel Dietzler <36593685+danieldietzler@users.noreply.github.com>

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -78,7 +78,7 @@
   const { release } = websocketStore;
 
   const handleRelease = async (release: ReleaseEvent) => {
-    if (!release || !release.isAvailable || !$user.isAdmin) {
+    if (!release?.isAvailable || !$user.isAdmin) {
       return;
     }
 
