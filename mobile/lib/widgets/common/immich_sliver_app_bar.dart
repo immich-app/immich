afdcf0299f1d5f377b9520f0e64d7a6ed8be833f
Alex <alex.tran1502@gmail.com>
2025-06-23 11:34:07 -0500

    chore: refactor to private widget

--- mobile/lib/widgets/common/immich_sliver_app_bar.dart
+++ mobile/lib/widgets/common/immich_sliver_app_bar.dart
@@ -37,126 +37,7 @@ class ImmichSliverAppBar extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final BackUpState backupState = ref.watch(backupProvider);
-    final bool isEnableAutoBackup =
-        backupState.backgroundBackup || backupState.autoBackup;
-    final ServerInfo serverInfoState = ref.watch(serverInfoProvider);
-    final user = ref.watch(currentUserProvider);
-    final isDarkTheme = context.isDarkTheme;
-    const widgetSize = 30.0;
     final isCasting = ref.watch(castProvider.select((c) => c.isCasting));
-
-    buildProfileIndicator() {
-      return InkWell(
-        onTap: () => showDialog(
-          context: context,
-          useRootNavigator: false,
-          builder: (ctx) => const ImmichAppBarDialog(),
-        ),
-        borderRadius: BorderRadius.circular(12),
-        child: Badge(
-          label: Container(
-            decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.circular(widgetSize / 2),
-            ),
-            child: const Icon(
-              Icons.info,
-              color: Color.fromARGB(255, 243, 188, 106),
-              size: widgetSize / 2,
-            ),
-          ),
-          backgroundColor: Colors.transparent,
-          alignment: Alignment.bottomRight,
-          isLabelVisible: serverInfoState.isVersionMismatch ||
-              ((user?.isAdmin ?? false) &&
-                  serverInfoState.isNewReleaseAvailable),
-          offset: const Offset(-2, -12),
-          child: user == null
-              ? const Icon(
-                  Icons.face_outlined,
-                  size: widgetSize,
-                )
-              : Semantics(
-                  label: "logged_in_as".tr(namedArgs: {"user": user.name}),
-                  child: UserCircleAvatar(
-                    radius: 17,
-                    size: 31,
-                    user: user,
-                  ),
-                ),
-        ),
-      );
-    }
-
-    getBackupBadgeIcon() {
-      final iconColor = isDarkTheme ? Colors.white : Colors.black;
-
-      if (isEnableAutoBackup) {
-        if (backupState.backupProgress == BackUpProgressEnum.inProgress) {
-          return Container(
-            padding: const EdgeInsets.all(3.5),
-            child: CircularProgressIndicator(
-              strokeWidth: 2,
-              strokeCap: StrokeCap.round,
-              valueColor: AlwaysStoppedAnimation<Color>(iconColor),
-              semanticsLabel: 'backup_controller_page_backup'.tr(),
-            ),
-          );
-        } else if (backupState.backupProgress !=
-                BackUpProgressEnum.inBackground &&
-            backupState.backupProgress != BackUpProgressEnum.manualInProgress) {
-          return Icon(
-            Icons.check_outlined,
-            size: 9,
-            color: iconColor,
-            semanticLabel: 'backup_controller_page_backup'.tr(),
-          );
-        }
-      }
-
-      if (!isEnableAutoBackup) {
-        return Icon(
-          Icons.cloud_off_rounded,
-          size: 9,
-          color: iconColor,
-          semanticLabel: 'backup_controller_page_backup'.tr(),
-        );
-      }
-    }
-
-    buildBackupIndicator() {
-      final indicatorIcon = getBackupBadgeIcon();
-      final badgeBackground = context.colorScheme.surfaceContainer;
-
-      return InkWell(
-        onTap: () => context.pushRoute(const BackupControllerRoute()),
-        borderRadius: BorderRadius.circular(12),
-        child: Badge(
-          label: Container(
-            width: widgetSize / 2,
-            height: widgetSize / 2,
-            decoration: BoxDecoration(
-              color: badgeBackground,
-              border: Border.all(
-                color: context.colorScheme.outline.withValues(alpha: .3),
-              ),
-              borderRadius: BorderRadius.circular(widgetSize / 2),
-            ),
-            child: indicatorIcon,
-          ),
-          backgroundColor: Colors.transparent,
-          alignment: Alignment.bottomRight,
-          isLabelVisible: indicatorIcon != null,
-          offset: const Offset(-2, -12),
-          child: Icon(
-            Icons.backup_rounded,
-            size: widgetSize,
-            color: context.primaryColor,
-          ),
-        ),
-      );
-    }
 
     return SliverAppBar(
       floating: floating,
@@ -171,28 +52,7 @@ class ImmichSliverAppBar extends ConsumerWidget {
       ),
       automaticallyImplyLeading: false,
       centerTitle: false,
-      title: title ??
-          Builder(
-            builder: (BuildContext context) {
-              return Row(
-                children: [
-                  Builder(
-                    builder: (context) {
-                      return Padding(
-                        padding: const EdgeInsets.only(top: 3.0),
-                        child: SvgPicture.asset(
-                          context.isDarkTheme
-                              ? 'assets/immich-logo-inline-dark.svg'
-                              : 'assets/immich-logo-inline-light.svg',
-                          height: 40,
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              );
-            },
-          ),
+      title: title ?? const _ImmichLogoWithText(),
       actions: [
         if (actions != null)
           ...actions!.map(
@@ -221,15 +81,176 @@ class ImmichSliverAppBar extends ConsumerWidget {
             ),
           ),
         if (showUploadButton)
-          Padding(
-            padding: const EdgeInsets.only(right: 20),
-            child: buildBackupIndicator(),
+          const Padding(
+            padding: EdgeInsets.only(right: 20),
+            child: _BackupIndicator(),
           ),
-        Padding(
-          padding: const EdgeInsets.only(right: 20),
-          child: buildProfileIndicator(),
+        const Padding(
+          padding: EdgeInsets.only(right: 20),
+          child: _ProfileIndicator(),
         ),
       ],
     );
+  }
+}
+
+class _ImmichLogoWithText extends StatelessWidget {
+  const _ImmichLogoWithText();
+
+  @override
+  Widget build(BuildContext context) {
+    return Builder(
+      builder: (BuildContext context) {
+        return Row(
+          children: [
+            Builder(
+              builder: (context) {
+                return Padding(
+                  padding: const EdgeInsets.only(top: 3.0),
+                  child: SvgPicture.asset(
+                    context.isDarkTheme
+                        ? 'assets/immich-logo-inline-dark.svg'
+                        : 'assets/immich-logo-inline-light.svg',
+                    height: 40,
+                  ),
+                );
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class _ProfileIndicator extends ConsumerWidget {
+  const _ProfileIndicator();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final ServerInfo serverInfoState = ref.watch(serverInfoProvider);
+    final user = ref.watch(currentUserProvider);
+    const widgetSize = 30.0;
+
+    return InkWell(
+      onTap: () => showDialog(
+        context: context,
+        useRootNavigator: false,
+        builder: (ctx) => const ImmichAppBarDialog(),
+      ),
+      borderRadius: BorderRadius.circular(12),
+      child: Badge(
+        label: Container(
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(widgetSize / 2),
+          ),
+          child: const Icon(
+            Icons.info,
+            color: Color.fromARGB(255, 243, 188, 106),
+            size: widgetSize / 2,
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        alignment: Alignment.bottomRight,
+        isLabelVisible: serverInfoState.isVersionMismatch ||
+            ((user?.isAdmin ?? false) && serverInfoState.isNewReleaseAvailable),
+        offset: const Offset(-2, -12),
+        child: user == null
+            ? const Icon(
+                Icons.face_outlined,
+                size: widgetSize,
+              )
+            : Semantics(
+                label: "logged_in_as".tr(namedArgs: {"user": user.name}),
+                child: UserCircleAvatar(
+                  radius: 17,
+                  size: 31,
+                  user: user,
+                ),
+              ),
+      ),
+    );
+  }
+}
+
+class _BackupIndicator extends ConsumerWidget {
+  const _BackupIndicator();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    const widgetSize = 30.0;
+    final indicatorIcon = _getBackupBadgeIcon(context, ref);
+    final badgeBackground = context.colorScheme.surfaceContainer;
+
+    return InkWell(
+      onTap: () => context.pushRoute(const BackupControllerRoute()),
+      borderRadius: BorderRadius.circular(12),
+      child: Badge(
+        label: Container(
+          width: widgetSize / 2,
+          height: widgetSize / 2,
+          decoration: BoxDecoration(
+            color: badgeBackground,
+            border: Border.all(
+              color: context.colorScheme.outline.withValues(alpha: .3),
+            ),
+            borderRadius: BorderRadius.circular(widgetSize / 2),
+          ),
+          child: indicatorIcon,
+        ),
+        backgroundColor: Colors.transparent,
+        alignment: Alignment.bottomRight,
+        isLabelVisible: indicatorIcon != null,
+        offset: const Offset(-2, -12),
+        child: Icon(
+          Icons.backup_rounded,
+          size: widgetSize,
+          color: context.primaryColor,
+        ),
+      ),
+    );
+  }
+
+  Widget? _getBackupBadgeIcon(BuildContext context, WidgetRef ref) {
+    final BackUpState backupState = ref.watch(backupProvider);
+    final bool isEnableAutoBackup =
+        backupState.backgroundBackup || backupState.autoBackup;
+    final isDarkTheme = context.isDarkTheme;
+    final iconColor = isDarkTheme ? Colors.white : Colors.black;
+
+    if (isEnableAutoBackup) {
+      if (backupState.backupProgress == BackUpProgressEnum.inProgress) {
+        return Container(
+          padding: const EdgeInsets.all(3.5),
+          child: CircularProgressIndicator(
+            strokeWidth: 2,
+            strokeCap: StrokeCap.round,
+            valueColor: AlwaysStoppedAnimation<Color>(iconColor),
+            semanticsLabel: 'backup_controller_page_backup'.tr(),
+          ),
+        );
+      } else if (backupState.backupProgress !=
+              BackUpProgressEnum.inBackground &&
+          backupState.backupProgress != BackUpProgressEnum.manualInProgress) {
+        return Icon(
+          Icons.check_outlined,
+          size: 9,
+          color: iconColor,
+          semanticLabel: 'backup_controller_page_backup'.tr(),
+        );
+      }
+    }
+
+    if (!isEnableAutoBackup) {
+      return Icon(
+        Icons.cloud_off_rounded,
+        size: 9,
+        color: iconColor,
+        semanticLabel: 'backup_controller_page_backup'.tr(),
+      );
+    }
+
+    return null;
   }
 }
