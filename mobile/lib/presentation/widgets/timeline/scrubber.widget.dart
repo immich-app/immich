8f5219cece323fce4529b3c2ee1fa917944402ed
Alex <alex.tran1502@gmail.com>
2025-06-22 23:52:49 -0500

    lint

--- mobile/lib/presentation/widgets/timeline/scrubber.widget.dart
+++ mobile/lib/presentation/widgets/timeline/scrubber.widget.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/domain/models/timeline.model.dart';
 import 'package:immich_mobile/extensions/build_context_extensions.dart';
