e7ba0dd04ab16da62c7621ce81df7677aaf6b377
wuzihao051119 <wuzihao051119@outlook.com>
2025-06-25 00:50:02 +0800

    fix(web): viewing asset lock

--- web/src/lib/stores/asset-viewing.store.ts
+++ web/src/lib/stores/asset-viewing.store.ts
@@ -1,5 +1,6 @@
 import { authManager } from '$lib/managers/auth-manager.svelte';
 import type { TimelineAsset } from '$lib/managers/timeline-manager/types';
+import { Mutex } from 'async-mutex';
 import { type AssetGridRouteSearchParams } from '$lib/utils/navigation';
 import { getAssetInfo, type AssetResponseDto } from '@immich/sdk';
 import { readonly, writable } from 'svelte/store';
@@ -8,6 +9,7 @@ function createAssetViewingStore() {
   const viewingAssetStoreState = writable<AssetResponseDto>();
   const preloadAssets = writable<TimelineAsset[]>([]);
   const viewState = writable<boolean>(false);
+  const viewingAssetMutex = writable<Mutex>(new Mutex());
   const gridScrollTarget = writable<AssetGridRouteSearchParams | null | undefined>();
 
   const setAsset = (asset: AssetResponseDto, assetsToPreload: TimelineAsset[] = []) => {
@@ -28,6 +30,7 @@ function createAssetViewingStore() {
 
   return {
     asset: readonly(viewingAssetStoreState),
+    mutex: viewingAssetMutex,
     preloadAssets: readonly(preloadAssets),
     isViewing: viewState,
     gridScrollTarget,
