a4dd2260d8b62ccfdd13e1169de7caab364c5c20
bwees <brandonwees@gmail.com>
2025-06-26 18:49:59 -0500

    more wip changes

--- mobile/android/app/src/main/kotlin/app/alextran/immich/widget/ImageDownloadWorker.kt
+++ mobile/android/app/src/main/kotlin/app/alextran/immich/widget/ImageDownloadWorker.kt
@@ -21,29 +21,19 @@ import android.content.Intent
 import android.content.Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION
 import android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.util.Log
-import androidx.compose.ui.unit.DpSize
 import androidx.core.content.FileProvider.getUriForFile
-import androidx.glance.GlanceId
+import androidx.glance.*
 import androidx.glance.appwidget.GlanceAppWidgetManager
-import androidx.glance.appwidget.state.updateAppWidgetState
 import androidx.glance.appwidget.updateAll
-import androidx.work.CoroutineWorker
-import androidx.work.Data
-import androidx.work.ExistingWorkPolicy
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.OutOfQuotaPolicy
-import androidx.work.WorkManager
-import androidx.work.WorkerParameters
-import coil.annotation.ExperimentalCoilApi
-import coil.imageLoader
-import coil.memory.MemoryCache
-import coil.request.ErrorResult
-import coil.request.ImageRequest
-import com.example.android.appwidget.glance.toPx
-import java.time.Duration
-import kotlin.math.roundToInt
-
+import androidx.work.*
+import com.google.gson.Gson
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
+import kotlin.random.Random
 
 class ImageDownloadWorker(
   private val context: Context,
@@ -54,39 +44,23 @@ class ImageDownloadWorker(
 
     private val uniqueWorkName = ImageDownloadWorker::class.java.simpleName
 
-    fun enqueue(context: Context, size: DpSize, glanceId: GlanceId, force: Boolean = false) {
+    fun enqueue(context: Context, glanceId: GlanceId, config: WidgetConfig) {
       val manager = WorkManager.getInstance(context)
       val requestBuilder = OneTimeWorkRequestBuilder<ImageDownloadWorker>().apply {
         addTag(glanceId.toString())
         setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
         setInputData(
           Data.Builder()
-            .putFloat("width", size.width.value.toPx)
-            .putFloat("height", size.height.value.toPx)
-            .putBoolean("force", force)
+            .putString("config", Gson().toJson(config))
+            .putInt("glanceId", glanceId.hashCode())
             .build()
         )
       }
-      val workPolicy = if (force) {
-        ExistingWorkPolicy.REPLACE
-      } else {
-        ExistingWorkPolicy.KEEP
-      }
 
       manager.enqueueUniqueWork(
-        uniqueWorkName + size.width + size.height,
-        workPolicy,
-        requestBuilder.build()
-      )
-
-      // Temporary workaround to avoid WM provider to disable itself and trigger an
-      // app widget update
-      manager.enqueueUniqueWork(
-        "$uniqueWorkName-workaround",
+        uniqueWorkName + glanceId.hashCode(),
         ExistingWorkPolicy.KEEP,
-        OneTimeWorkRequestBuilder<ImageWorker>().apply {
-          setInitialDelay(Duration.ofDays(365))
-        }.build()
+        requestBuilder.build()
       )
     }
 
@@ -100,11 +74,17 @@ class ImageDownloadWorker(
 
   override suspend fun doWork(): Result {
     return try {
-      val width = inputData.getFloat("width", 0f)
-      val height = inputData.getFloat("height", 0f)
-      val force = inputData.getBoolean("force", false)
-      val uri = getRandomImage(width, height, force)
-      updateImageWidget(width, height, uri)
+      val configString = inputData.getString("config")
+      val config = Gson().fromJson(configString, WidgetConfig::class.java)
+      val glanceId = inputData.getInt("glanceId", -1)
+
+      if (glanceId == -1) {
+        Result.failure()
+      }
+
+      fetchImage(config, glanceId)
+      updateWidget(config, glanceId)
+
       Result.success()
     } catch (e: Exception) {
       Log.e(uniqueWorkName, "Error while loading image", e)
@@ -118,72 +98,31 @@ class ImageDownloadWorker(
     }
   }
 
-  private suspend fun updateImageWidget(width: Float, height: Float, uri: String) {
+  private suspend fun updateWidget(config: WidgetConfig, glanceId: Int) {
     val manager = GlanceAppWidgetManager(context)
-    val glanceIds = manager.getGlanceIds(ImageGlanceWidget::class.java)
-    glanceIds.forEach { glanceId ->
-      updateAppWidgetState(context, glanceId) { prefs ->
-        prefs[ImageGlanceWidget.getImageKey(width, height)] = uri
-        prefs[ImageGlanceWidget.sourceKey] = "Picsum Photos"
-        prefs[ImageGlanceWidget.sourceUrlKey] = "https://picsum.photos/"
+    val glanceIds = manager.getGlanceIds(config.widgetType.widgetClass)
+
+    for (id in glanceIds) {
+      if (id.hashCode() == glanceId) {
+        config.widgetType.widgetClass.getDeclaredConstructor().newInstance().updateAll(context)
+        break
       }
     }
-    ImageGlanceWidget().updateAll(context)
   }
 
-  /**
-   * Use Coil and Picsum Photos to randomly load images into the cache based on the provided
-   * size. This method returns the path of the cached image, which you can send to the widget.
-   */
-  @OptIn(ExperimentalCoilApi::class)
-  private suspend fun getRandomImage(width: Float, height: Float, force: Boolean): String {
-    val url = "https://picsum.photos/${width.roundToInt()}/${height.roundToInt()}"
-    val request = ImageRequest.Builder(context)
-      .data(url)
-      .build()
+  private suspend fun fetchImage(config: WidgetConfig, glanceId: Int) {
+    val api = ImmichAPI(config.credentials)
 
-    // Request the image to be loaded and throw error if it failed
-    with(context.imageLoader) {
-      if (force) {
-        diskCache?.remove(url)
-        memoryCache?.remove(MemoryCache.Key(url))
-      }
-      val result = execute(request)
-      if (result is ErrorResult) {
-        throw result.throwable
-      }
-    }
+    val random = api.fetchSearchResults(SearchFilters(AssetType.IMAGE, size=1))
+    val image = api.fetchImage(random[0])
 
-    // Get the path of the loaded image from DiskCache.
-    val path = context.imageLoader.diskCache?.get(url)?.use { snapshot ->
-      val imageFile = snapshot.data.toFile()
+    saveImage(image, glanceId)
+  }
 
-      // Use the FileProvider to create a content URI
-      val contentUri = getUriForFile(
-        context,
-        "com.example.android.appwidget.fileprovider",
-        imageFile
-      )
-
-      // Find the current launcher everytime to ensure it has read permissions
-      val resolveInfo = context.packageManager.resolveActivity(
-        Intent(Intent.ACTION_MAIN).apply { addCategory(Intent.CATEGORY_HOME) },
-        PackageManager.MATCH_DEFAULT_ONLY
-      )
-      val launcherName = resolveInfo?.activityInfo?.packageName
-      if (launcherName != null) {
-        context.grantUriPermission(
-          launcherName,
-          contentUri,
-          FLAG_GRANT_READ_URI_PERMISSION or FLAG_GRANT_PERSISTABLE_URI_PERMISSION
-        )
-      }
-
-      // return the path
-      contentUri.toString()
-    }
-    return requireNotNull(path) {
-      "Couldn't find cached file"
+  private suspend fun saveImage(bitmap: Bitmap, glanceId: Int) = withContext(Dispatchers.IO) {
+    val file = File(context.cacheDir, "widget_image_$glanceId.jpg")
+    FileOutputStream(file).use { out ->
+      bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out)
     }
   }
 }
--- mobile/android/app/src/main/kotlin/app/alextran/immich/widget/WidgetEntry.kt
+++ mobile/android/app/src/main/kotlin/app/alextran/immich/widget/WidgetEntry.kt
@@ -1,8 +1,5 @@
 package app.alextran.immich.widget
 
-enum class WidgetError {
-  NO_LOGIN, FETCH_FAILED, UNKNOWN, ALBUM_NOT_FOUND
-}
 
 data class WidgetEntry(
   val imageURI: String? = null,
