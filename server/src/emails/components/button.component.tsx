09f0f157397074003e33a1b08a1d691bdf3d3589
Min Idzelis <min123@gmail.com>
2025-06-29 20:37:29 +0000

    lint

--- server/src/emails/components/button.component.tsx
+++ server/src/emails/components/button.component.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 import { Button, ButtonProps } from '@react-email/components';
 
-interface ImmichButtonProps extends ButtonProps {}
-
-export const ImmichButton = ({ children, ...props }: ImmichButtonProps) => (
+export const ImmichButton = ({ children, ...props }: ButtonProps) => (
   <Button
     {...props}
     className="py-3 px-8 border bg-immich-primary rounded-full no-underline hover:no-underline text-white hover:text-gray-50 font-bold uppercase"
