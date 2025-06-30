31824140b5f9c8f1ab02049c4847203b780e4ad6
Alex <alex.tran1502@gmail.com>
2025-06-29 21:23:11 -0500

    feat: shared link action

--- mobile/lib/presentation/widgets/bottom_app_bar/home_bottom_app_bar.widget.dart
+++ mobile/lib/presentation/widgets/bottom_app_bar/home_bottom_app_bar.widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:immich_mobile/constants/enums.dart';
 import 'package:immich_mobile/presentation/widgets/action_buttons/archive_action_button.widget.dart';
 import 'package:immich_mobile/presentation/widgets/action_buttons/delete_action_button.widget.dart';
 import 'package:immich_mobile/presentation/widgets/action_buttons/delete_local_action_button.widget.dart';
@@ -33,7 +34,7 @@ class HomeBottomAppBar extends ConsumerWidget {
       actions: [
         const ShareActionButton(),
         if (multiselect.hasRemote) ...[
-          const ShareLinkActionButton(),
+          const ShareLinkActionButton(source: ActionSource.timeline),
           const ArchiveActionButton(),
           const FavoriteActionButton(),
           const DownloadActionButton(),
