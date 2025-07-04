adf7064b2c6903632ca183242adc73d3a1ddc95b
Alex <alex.tran1502@gmail.com>
2025-07-04 15:25:34 -0500

    feat: new create album page

--- mobile/lib/providers/infrastructure/album.provider.dart
+++ mobile/lib/providers/infrastructure/album.provider.dart
@@ -4,6 +4,7 @@ import 'package:immich_mobile/infrastructure/repositories/local_album.repository
 import 'package:immich_mobile/infrastructure/repositories/remote_album.repository.dart';
 import 'package:immich_mobile/providers/infrastructure/db.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/remote_album.provider.dart';
+import 'package:immich_mobile/repositories/album_api.repository.dart';
 
 final localAlbumRepository = Provider<DriftLocalAlbumRepository>(
   (ref) => DriftLocalAlbumRepository(ref.watch(driftProvider)),
@@ -14,7 +15,10 @@ final remoteAlbumRepository = Provider<DriftRemoteAlbumRepository>(
 );
 
 final remoteAlbumServiceProvider = Provider<RemoteAlbumService>(
-  (ref) => RemoteAlbumService(ref.watch(remoteAlbumRepository)),
+  (ref) => RemoteAlbumService(
+    ref.watch(remoteAlbumRepository),
+    ref.watch(albumApiRepositoryProvider),
+  ),
   dependencies: [remoteAlbumRepository],
 );
 
--- mobile/lib/utils/remote_album.utils.dart
+++ mobile/lib/utils/remote_album.utils.dart
@@ -1,38 +1,56 @@
 import 'package:collection/collection.dart';
 import 'package:immich_mobile/domain/models/album/album.model.dart';
 
-typedef AlbumSortFn = List<Album> Function(List<Album> albums, bool isReverse);
+typedef AlbumSortFn = List<RemoteAlbum> Function(
+  List<RemoteAlbum> albums,
+  bool isReverse,
+);
 
 class _RemoteAlbumSortHandlers {
   const _RemoteAlbumSortHandlers._();
 
   static const AlbumSortFn created = _sortByCreated;
-  static List<Album> _sortByCreated(List<Album> albums, bool isReverse) {
+  static List<RemoteAlbum> _sortByCreated(
+    List<RemoteAlbum> albums,
+    bool isReverse,
+  ) {
     final sorted = albums.sortedBy((album) => album.createdAt);
     return (isReverse ? sorted.reversed : sorted).toList();
   }
 
   static const AlbumSortFn title = _sortByTitle;
-  static List<Album> _sortByTitle(List<Album> albums, bool isReverse) {
+  static List<RemoteAlbum> _sortByTitle(
+    List<RemoteAlbum> albums,
+    bool isReverse,
+  ) {
     final sorted = albums.sortedBy((album) => album.name);
     return (isReverse ? sorted.reversed : sorted).toList();
   }
 
   static const AlbumSortFn lastModified = _sortByLastModified;
-  static List<Album> _sortByLastModified(List<Album> albums, bool isReverse) {
+  static List<RemoteAlbum> _sortByLastModified(
+    List<RemoteAlbum> albums,
+    bool isReverse,
+  ) {
     final sorted = albums.sortedBy((album) => album.updatedAt);
     return (isReverse ? sorted.reversed : sorted).toList();
   }
 
   static const AlbumSortFn assetCount = _sortByAssetCount;
-  static List<Album> _sortByAssetCount(List<Album> albums, bool isReverse) {
+  static List<RemoteAlbum> _sortByAssetCount(
+    List<RemoteAlbum> albums,
+    bool isReverse,
+  ) {
     final sorted =
         albums.sorted((a, b) => a.assetCount.compareTo(b.assetCount));
     return (isReverse ? sorted.reversed : sorted).toList();
   }
 
   static const AlbumSortFn mostRecent = _sortByMostRecent;
-  static List<Album> _sortByMostRecent(List<Album> albums, bool isReverse) {
+  static List<RemoteAlbum> _sortByMostRecent(
+    List<RemoteAlbum> albums,
+    bool isReverse,
+  ) {
     final sorted = albums.sorted((a, b) {
       // For most recent, we sort by updatedAt in descending order
       return b.updatedAt.compareTo(a.updatedAt);
@@ -41,7 +59,10 @@ class _RemoteAlbumSortHandlers {
   }
 
   static const AlbumSortFn mostOldest = _sortByMostOldest;
-  static List<Album> _sortByMostOldest(List<Album> albums, bool isReverse) {
+  static List<RemoteAlbum> _sortByMostOldest(
+    List<RemoteAlbum> albums,
+    bool isReverse,
+  ) {
     final sorted = albums.sorted((a, b) {
       // For oldest, we sort by createdAt in ascending order
       return a.createdAt.compareTo(b.createdAt);
