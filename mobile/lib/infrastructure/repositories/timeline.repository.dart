dcc95a71d2190cc713e946b4a0e0dda6047c0631
alex <alex@localhost-live.localdomain>
2025-07-06 22:45:44 -0500

    remove showing local indicator in bucket

--- mobile/lib/infrastructure/repositories/timeline.repository.dart
+++ mobile/lib/infrastructure/repositories/timeline.repository.dart
@@ -254,29 +254,16 @@ class DriftTimelineRepository extends DriftDatabaseRepository {
     required int offset,
     required int count,
   }) {
-    final query = _db.remoteAssetEntity
-        .select()
-        .addColumns([_db.localAssetEntity.id]).join([
-      leftOuterJoin(
-        _db.localAssetEntity,
-        _db.remoteAssetEntity.checksum.equalsExp(_db.localAssetEntity.checksum),
-        useColumns: false,
-      ),
-    ])
+    final query = _db.remoteAssetEntity.select()
       ..where(
-        _db.remoteAssetEntity.ownerId.equals(userId) &
-            _db.remoteAssetEntity.visibility
-                .equalsValue(AssetVisibility.archive),
+        (row) =>
+            row.ownerId.equals(userId) &
+            row.visibility.equalsValue(AssetVisibility.archive),
       )
-      ..orderBy([OrderingTerm.desc(_db.remoteAssetEntity.createdAt)])
+      ..orderBy([(row) => OrderingTerm.desc(row.createdAt)])
       ..limit(count, offset: offset);
 
-    return query.map((row) {
-      final asset = row.readTable(_db.remoteAssetEntity).toDto();
-      return asset.copyWith(
-        localId: row.read(_db.localAssetEntity.id),
-      );
-    }).get();
+    return query.map((row) => row.toDto()).get();
   }
 }
 
