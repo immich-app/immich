c7e48b81bfc682d6fac8a7e82fa14809eae60b6d
wuzihao051119 <wuzihao051119@outlook.com>
2025-07-07 10:59:55 +0800

    remove join in bucket

--- mobile/lib/infrastructure/repositories/timeline.repository.dart
+++ mobile/lib/infrastructure/repositories/timeline.repository.dart
@@ -222,7 +222,7 @@ class DriftTimelineRepository extends DriftDatabaseRepository {
       return _db.remoteAssetEntity
           .count(
             where: (row) =>
-                row.visibility.equals(AssetVisibility.archive.index) &
+                row.visibility.equalsValue(AssetVisibility.archive) &
                 row.ownerId.equals(userId),
           )
           .map(_generateBuckets)
@@ -234,18 +234,10 @@ class DriftTimelineRepository extends DriftDatabaseRepository {
 
     final query = _db.remoteAssetEntity.selectOnly()
       ..addColumns([assetCountExp, dateExp])
-      ..join([
-        leftOuterJoin(
-          _db.localAssetEntity,
-          _db.remoteAssetEntity.checksum
-              .equalsExp(_db.localAssetEntity.checksum),
-          useColumns: false,
-        ),
-      ])
       ..where(
         _db.remoteAssetEntity.ownerId.equals(userId) &
             _db.remoteAssetEntity.visibility
-                .equals(AssetVisibility.archive.index),
+                .equalsValue(AssetVisibility.archive),
       )
       ..groupBy([dateExp])
       ..orderBy([OrderingTerm.desc(dateExp)]);
@@ -274,7 +266,7 @@ class DriftTimelineRepository extends DriftDatabaseRepository {
       ..where(
         _db.remoteAssetEntity.ownerId.equals(userId) &
             _db.remoteAssetEntity.visibility
-                .equals(AssetVisibility.archive.index),
+                .equalsValue(AssetVisibility.archive),
       )
       ..orderBy([OrderingTerm.desc(_db.remoteAssetEntity.createdAt)])
       ..limit(count, offset: offset);
