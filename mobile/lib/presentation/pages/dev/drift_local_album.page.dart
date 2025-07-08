2223a0f68d1d2737460ffd979d640d7ee458079c
wuzihao051119 <wuzihao051119@outlook.com>
2025-07-08 20:36:21 +0800

    fix: lint

--- mobile/lib/presentation/pages/dev/drift_local_album.page.dart
+++ mobile/lib/presentation/pages/dev/drift_local_album.page.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:immich_mobile/domain/models/album/local_album.model.dart';
 import 'package:immich_mobile/extensions/build_context_extensions.dart';
 import 'package:immich_mobile/extensions/theme_extensions.dart';
 import 'package:immich_mobile/extensions/translate_extensions.dart';
