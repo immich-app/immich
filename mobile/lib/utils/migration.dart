86a926bc058f69d4125d7a7af261cbb764cd79d2
shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2025-07-20 07:34:07 +0530

    migrate backup albums over to sqlite

--- mobile/lib/utils/migration.dart
+++ mobile/lib/utils/migration.dart
@@ -2,19 +2,23 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:collection/collection.dart';
 import 'package:drift/drift.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:immich_mobile/domain/models/album/local_album.model.dart';
 import 'package:immich_mobile/domain/models/store.model.dart';
-import 'package:immich_mobile/domain/utils/background_sync.dart';
 import 'package:immich_mobile/entities/album.entity.dart';
 import 'package:immich_mobile/entities/android_device_asset.entity.dart';
 import 'package:immich_mobile/entities/asset.entity.dart';
+import 'package:immich_mobile/entities/backup_album.entity.dart'
+    as isar_backup_album;
 import 'package:immich_mobile/entities/etag.entity.dart';
 import 'package:immich_mobile/entities/ios_device_asset.entity.dart';
 import 'package:immich_mobile/entities/store.entity.dart';
 import 'package:immich_mobile/infrastructure/entities/device_asset.entity.dart';
 import 'package:immich_mobile/infrastructure/entities/exif.entity.dart';
+import 'package:immich_mobile/infrastructure/entities/local_album.entity.drift.dart';
 import 'package:immich_mobile/infrastructure/entities/local_asset.entity.drift.dart';
 import 'package:immich_mobile/infrastructure/entities/store.entity.dart';
 import 'package:immich_mobile/infrastructure/entities/user.entity.dart';
@@ -188,6 +192,71 @@ Future<void> migrateDeviceAssetToSqlite(Isar db, Drift drift) async {
   } catch (error) {
     debugPrint(
       "[MIGRATION] Error while migrating device assets to SQLite: $error",
+    );
+  }
+}
+
+Future<void> migrateBackupAlbumsToSqlite(
+  Isar db,
+  Drift drift,
+) async {
+  try {
+    final isarBackupAlbums = await db.backupAlbums.where().findAll();
+    // Recents is a virtual album on Android, and we don't have it with the new sync
+    // If recents is selected previously, select all albums during migration except the excluded ones
+    if (Platform.isAndroid) {
+      final recentAlbum =
+          isarBackupAlbums.firstWhereOrNull((album) => album.id == 'isAll');
+      if (recentAlbum != null) {
+        await drift.localAlbumEntity.update().write(
+              const LocalAlbumEntityCompanion(
+                backupSelection: Value(BackupSelection.selected),
+              ),
+            );
+        final excluded = isarBackupAlbums
+            .where(
+              (album) =>
+                  album.selection == isar_backup_album.BackupSelection.exclude,
+            )
+            .map((album) => album.id)
+            .toList();
+        await drift.batch((batch) async {
+          for (final id in excluded) {
+            batch.update(
+              drift.localAlbumEntity,
+              const LocalAlbumEntityCompanion(
+                backupSelection: Value(BackupSelection.excluded),
+              ),
+              where: (t) => t.id.equals(id),
+            );
+          }
+        });
+      }
+      return;
+    }
+
+    await drift.batch((batch) {
+      for (final album in isarBackupAlbums) {
+        batch.update(
+          drift.localAlbumEntity,
+          LocalAlbumEntityCompanion(
+            backupSelection: Value(
+              switch (album.selection) {
+                isar_backup_album.BackupSelection.none => BackupSelection.none,
+                isar_backup_album.BackupSelection.select =>
+                  BackupSelection.selected,
+                isar_backup_album.BackupSelection.exclude =>
+                  BackupSelection.excluded,
+              },
+            ),
+          ),
+          where: (t) => t.id.equals(album.id),
+        );
+      }
+    });
+  } catch (error) {
+    debugPrint(
+      "[MIGRATION] Error while migrating backup albums to SQLite: $error",
     );
   }
 }
