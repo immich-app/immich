20b2aa3203096e0faae23a10698036bf4949ece0
andre-antunesdesa <80642274+andre-antunesdesa@users.noreply.github.com>
2025-07-20 23:39:37 -0400

    added user preference for always loading original video
    added ability to toggle between transcoded/original in the video viewer
    
    add fix to static check error

--- web/src/lib/components/asset-viewer/asset-viewer-nav-bar.svelte
+++ web/src/lib/components/asset-viewer/asset-viewer-nav-bar.svelte
@@ -50,6 +50,7 @@
     mdiMagnifyPlusOutline,
     mdiPresentationPlay,
     mdiUpload,
+    mdiVideoOutline,
   } from '@mdi/js';
   import type { Snippet } from 'svelte';
   import { t } from 'svelte-i18n';
@@ -72,6 +73,8 @@
     // export let showEditorHandler: () => void;
     onClose: () => void;
     motionPhoto?: Snippet;
+    playOriginalVideo: boolean;
+    setPlayOriginalVideo: (value: boolean) => void;
   }
 
   let {
@@ -91,6 +94,8 @@
     onShowDetail,
     onClose,
     motionPhoto,
+    playOriginalVideo = false,
+    setPlayOriginalVideo = () => {},
   }: Props = $props();
 
   const sharedLink = getSharedLink();
@@ -226,6 +231,23 @@
           {#if !asset.isTrashed}
             <SetVisibilityAction asset={toTimelineAsset(asset)} {onAction} {preAction} />
           {/if}
+
+          {#if asset.type === AssetTypeEnum.Video}
+            {#if playOriginalVideo}
+              <MenuOption
+                icon={mdiVideoOutline}
+                onClick={() => setPlayOriginalVideo(false)}
+                text={$t('play_transcoded_video')}
+              />
+            {:else}
+              <MenuOption
+                icon={mdiVideoOutline}
+                onClick={() => setPlayOriginalVideo(true)}
+                text={$t('play_original_video')}
+              />
+            {/if}
+          {/if}
+
           <hr />
           <MenuOption
             icon={mdiHeadSyncOutline}
--- web/src/lib/components/asset-viewer/asset-viewer.svelte
+++ web/src/lib/components/asset-viewer/asset-viewer.svelte
@@ -11,7 +11,7 @@
   import type { TimelineAsset } from '$lib/managers/timeline-manager/types';
   import { closeEditorCofirm } from '$lib/stores/asset-editor.store';
   import { assetViewingStore } from '$lib/stores/asset-viewing.store';
-  import { isShowDetail } from '$lib/stores/preferences.store';
+  import { alwaysLoadOriginalVideo, isShowDetail } from '$lib/stores/preferences.store';
   import { SlideshowNavigation, SlideshowState, slideshowStore } from '$lib/stores/slideshow.store';
   import { user } from '$lib/stores/user.store';
   import { websocketEvents } from '$lib/stores/websocket';
@@ -109,6 +109,11 @@
   let stack: StackResponseDto | null = $state(null);
 
   let zoomToggle = $state(() => void 0);
+  let playOriginalVideo = $state($alwaysLoadOriginalVideo);
+
+  const setPlayOriginalVideo = (value: boolean) => {
+    playOriginalVideo = value;
+  };
 
   const refreshStack = async () => {
     if (authManager.key) {
@@ -398,6 +403,8 @@
         onPlaySlideshow={() => ($slideshowState = SlideshowState.PlaySlideshow)}
         onShowDetail={toggleDetailPanel}
         onClose={closeViewer}
+        {playOriginalVideo}
+        {setPlayOriginalVideo}
       >
         {#snippet motionPhoto()}
           <MotionPhotoAction
@@ -453,6 +460,7 @@
             onClose={closeViewer}
             onVideoEnded={() => navigateAsset()}
             onVideoStarted={handleVideoStarted}
+            {playOriginalVideo}
           />
         {/if}
       {/key}
@@ -468,6 +476,7 @@
               onPreviousAsset={() => navigateAsset('previous')}
               onNextAsset={() => navigateAsset('next')}
               onVideoEnded={() => (shouldPlayMotionPhoto = false)}
+              {playOriginalVideo}
             />
           {:else if asset.exifInfo?.projectionType === ProjectionType.EQUIRECTANGULAR || (asset.originalPath && asset.originalPath
                 .toLowerCase()
@@ -498,6 +507,7 @@
             onClose={closeViewer}
             onVideoEnded={() => navigateAsset()}
             onVideoStarted={handleVideoStarted}
+            {playOriginalVideo}
           />
         {/if}
         {#if $slideshowState === SlideshowState.None && isShared && ((album && album.isActivityEnabled) || activityManager.commentCount > 0) && !activityManager.isLoading}
