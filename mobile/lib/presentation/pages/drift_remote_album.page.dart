c298b4278a7e0584ad3ddb112fcddd5590e83631
Brandon Wees <brandonwees@gmail.com>
2025-07-24 12:35:26 -0500

    Fix lint

--- mobile/lib/presentation/pages/drift_remote_album.page.dart
+++ mobile/lib/presentation/pages/drift_remote_album.page.dart
@@ -1,5 +1,4 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
