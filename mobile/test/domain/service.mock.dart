8662285698349d1ac930f60da53a494a1735a810
shenlong <139912620+shenlong-tanwen@users.noreply.github.com>
2025-07-25 01:41:52 +0530

    fix: cancel upload queue on logout (#20131)
    * fix: cancel upload on logout
    
    * fix: test
    
    ---------
    
    Co-authored-by: shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
    Co-authored-by: Alex <alex.tran1502@gmail.com>

--- mobile/test/domain/service.mock.dart
+++ mobile/test/domain/service.mock.dart
@@ -3,6 +3,7 @@ import 'package:immich_mobile/domain/services/user.service.dart';
 import 'package:immich_mobile/domain/utils/background_sync.dart';
 import 'package:immich_mobile/platform/native_sync_api.g.dart';
 import 'package:immich_mobile/services/app_settings.service.dart';
+import 'package:immich_mobile/services/upload.service.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockStoreService extends Mock implements StoreService {}
@@ -14,3 +15,5 @@ class MockBackgroundSyncManager extends Mock implements BackgroundSyncManager {}
 class MockNativeSyncApi extends Mock implements NativeSyncApi {}
 
 class MockAppSettingsService extends Mock implements AppSettingsService {}
+
+class MockUploadService extends Mock implements UploadService {}
