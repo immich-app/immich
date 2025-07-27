abf494b05b502d7e72b33cd10f498f1c17f9cc08
Aviv <51673860+aviv926@users.noreply.github.com>
2025-07-27 04:22:17 +0300

    foramt fix

--- server/src/database.ts
+++ server/src/database.ts
@@ -308,7 +308,15 @@ export const columns = {
     'asset.type',
   ],
   assetFiles: ['asset_file.id', 'asset_file.path', 'asset_file.type'],
-  authUser: ['user.id', 'user.name', 'user.email', 'user.isAdmin', 'user.quotaUsageInBytes', 'user.quotaSizeInBytes', 'user.appVersion'],
+  authUser: [
+    'user.id',
+    'user.name',
+    'user.email',
+    'user.isAdmin',
+    'user.quotaUsageInBytes',
+    'user.quotaSizeInBytes',
+    'user.appVersion',
+  ],
   authApiKey: ['api_key.id', 'api_key.permissions'],
   authSession: ['session.id', 'session.isPendingSyncReset', 'session.updatedAt', 'session.pinExpiresAt'],
   authSharedLink: [
