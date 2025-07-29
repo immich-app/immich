f28532bcf04b82d23a8b691603f0011f745f1612
bwees <brandonwees@gmail.com>
2025-07-28 21:50:01 -0500

    revert updateUsersV1 change

--- mobile/lib/infrastructure/repositories/sync_stream.repository.dart
+++ mobile/lib/infrastructure/repositories/sync_stream.repository.dart
@@ -42,12 +42,7 @@ class SyncStreamRepository extends DriftDatabaseRepository {
     try {
       await _db.batch((batch) {
         for (final user in data) {
-          final companion = UserEntityCompanion(
-            name: Value(user.name),
-            email: Value(user.email),
-            hasProfileImage: Value(user.hasProfileImage),
-            profileChangedAt: Value(user.profileChangedAt),
-          );
+          final companion = UserEntityCompanion(name: Value(user.name), email: Value(user.email));
 
           batch.insert(_db.userEntity, companion.copyWith(id: Value(user.id)), onConflict: DoUpdate((_) => companion));
         }
