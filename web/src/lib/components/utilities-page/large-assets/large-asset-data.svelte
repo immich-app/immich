3a06f944f31d371952be0d1b1315c481c7459413
Alex Tran <alex.tran1502@gmail.com>
2025-07-31 11:01:09 -0500

    chore: large file grid styles

--- web/src/lib/components/utilities-page/large-assets/large-asset-data.svelte
+++ web/src/lib/components/utilities-page/large-assets/large-asset-data.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
   import Thumbnail from '$lib/components/assets/thumbnail/thumbnail.svelte';
-  import Icon from '$lib/components/elements/icon.svelte';
   import { getFileSize } from '$lib/utils/asset-utils';
   import { toTimelineAsset } from '$lib/utils/timeline-util';
   import { type AssetResponseDto } from '@immich/sdk';
-  import { mdiHeart } from '@mdi/js';
 
   interface Props {
     asset: AssetResponseDto;
@@ -14,29 +12,20 @@
   let { asset, onViewAsset }: Props = $props();
 
   let assetData = $derived(JSON.stringify(asset, null, 2));
+
+  let boxWidth = $state(300);
 </script>
 
 <div
   class="w-full aspect-square rounded-xl border-4 transition-colors font-semibold text-xs bg-gray-200 dark:bg-gray-800 border-gray-200 dark:border-gray-800"
+  bind:clientWidth={boxWidth}
+  title={assetData}
 >
-  <div class="relative w-full">
-    <Thumbnail
-      imageClass="w-full h-full border border-red-500"
-      asset={toTimelineAsset(asset)}
-      readonly
-      onClick={() => onViewAsset(asset)}
-    />
+  <div class="relative w-full h-full overflow-hidden rounded-lg">
+    <Thumbnail asset={toTimelineAsset(asset)} readonly onClick={() => onViewAsset(asset)} thumbnailSize={boxWidth} />
 
-    <!-- OVERLAY CHIP -->
     {#if !!asset.libraryId}
       <div class="absolute bottom-1 end-3 px-4 py-1 rounded-xl text-xs transition-colors bg-red-500">External</div>
-    {/if}
-
-    <!-- FAVORITE ICON -->
-    {#if asset.isFavorite}
-      <div class="absolute bottom-2 start-2">
-        <Icon path={mdiHeart} size="24" class="text-white" />
-      </div>
     {/if}
   </div>
   <div class="text-center mt-4 px-4 text-sm font-normal truncate" title={asset.originalFileName}>
