fa3a064099d99ad36c01cc1bec55551592e2a7c5
mertalev <101130780+mertalev@users.noreply.github.com>
2025-08-01 18:35:40 -0400

    buttery hero animations

--- mobile/lib/presentation/widgets/images/local_image_provider.dart
+++ mobile/lib/presentation/widgets/images/local_image_provider.dart
@@ -3,7 +3,9 @@ import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+import 'package:immich_mobile/constants/constants.dart';
 import 'package:immich_mobile/infrastructure/repositories/asset_media.repository.dart';
+import 'package:immich_mobile/presentation/widgets/images/one_frame_multi_image_stream_completer.dart';
 
 class LocalThumbProvider extends ImageProvider<LocalThumbProvider> {
   static const _assetMediaRepository = AssetMediaRepository();
@@ -11,7 +13,7 @@ class LocalThumbProvider extends ImageProvider<LocalThumbProvider> {
   final String id;
   final Size size;
 
-  const LocalThumbProvider({required this.id, required this.size});
+  const LocalThumbProvider({required this.id, this.size = kTimelineThumbnailSize});
 
   @override
   Future<LocalThumbProvider> obtainKey(ImageConfiguration configuration) {
@@ -62,16 +64,45 @@ class LocalFullImageProvider extends ImageProvider<LocalFullImageProvider> {
 
   @override
   ImageStreamCompleter loadImage(LocalFullImageProvider key, ImageDecoderCallback decode) {
-    return OneFrameImageStreamCompleter(_codec(key));
+    ImageInfo? thumbnail;
+    final thumbnailProvider = LocalThumbProvider(id: key.id);
+
+    final ImageStreamCompleter? stream = PaintingBinding.instance.imageCache.putIfAbsent(
+      thumbnailProvider,
+      () => throw Exception(), // don't bother loading the thumbnail if it isn't cacched
+    );
+
+    if (stream != null) {
+      void listener(ImageInfo info, bool synchronousCall) {
+        thumbnail = info;
+      }
+
+      try {
+        stream.addListener(ImageStreamListener(listener));
+      } finally {
+        stream.removeListener(ImageStreamListener(listener));
+      }
+    }
+
+    return OneFramePlaceholderImageStreamCompleter(
+      _codec(key, decode),
+      initialImage: thumbnail,
+      informationCollector: () => <DiagnosticsNode>[
+        DiagnosticsProperty<ImageProvider>('Image provider', this),
+        DiagnosticsProperty<String>('Id', key.id),
+        DiagnosticsProperty<Size>('Size', key.size),
+      ],
+    );
   }
 
-  Future<ImageInfo> _codec(LocalFullImageProvider key) async {
+  Future<ImageInfo> _codec(LocalFullImageProvider key, ImageDecoderCallback decode) async {
     final devicePixelRatio = PlatformDispatcher.instance.views.first.devicePixelRatio;
     final codec = await _assetMediaRepository.getLocalThumbnail(
       key.id,
       Size(size.width * devicePixelRatio, size.height * devicePixelRatio),
     );
-    return ImageInfo(image: (await codec.getNextFrame()).image, scale: 1.0);
+    final frame = await codec.getNextFrame();
+    return ImageInfo(image: frame.image, scale: 1.0);
   }
 
   @override
