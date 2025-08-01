47b2016ea833fd8464a1585be0d634576d5b25b4
mertalev <101130780+mertalev@users.noreply.github.com>
2025-08-01 19:26:14 -0400

    buttery hero animation for remote assets

--- mobile/lib/presentation/widgets/images/remote_image_provider.dart
+++ mobile/lib/presentation/widgets/images/remote_image_provider.dart
@@ -6,7 +6,6 @@ import 'package:flutter/painting.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:immich_mobile/domain/models/setting.model.dart';
 import 'package:immich_mobile/domain/services/setting.service.dart';
-import 'package:immich_mobile/extensions/codec_extensions.dart';
 import 'package:immich_mobile/presentation/widgets/images/image_provider.dart';
 import 'package:immich_mobile/presentation/widgets/images/one_frame_multi_image_stream_completer.dart';
 import 'package:immich_mobile/providers/image/cache/image_loader.dart';
@@ -27,11 +26,9 @@ class RemoteThumbProvider extends ImageProvider<RemoteThumbProvider> {
   @override
   ImageStreamCompleter loadImage(RemoteThumbProvider key, ImageDecoderCallback decode) {
     final cache = cacheManager ?? RemoteImageCacheManager();
-    final chunkController = StreamController<ImageChunkEvent>();
     return MultiFrameImageStreamCompleter(
-      codec: _codec(key, cache, decode, chunkController),
+      codec: _codec(key, cache, decode),
       scale: 1.0,
-      chunkEvents: chunkController.stream,
       informationCollector: () => <DiagnosticsNode>[
         DiagnosticsProperty<ImageProvider>('Image provider', this),
         DiagnosticsProperty<String>('Asset Id', key.assetId),
@@ -39,20 +36,10 @@ class RemoteThumbProvider extends ImageProvider<RemoteThumbProvider> {
     );
   }
 
-  Future<Codec> _codec(
-    RemoteThumbProvider key,
-    CacheManager cache,
-    ImageDecoderCallback decode,
-    StreamController<ImageChunkEvent> chunkController,
-  ) async {
+  Future<Codec> _codec(RemoteThumbProvider key, CacheManager cache, ImageDecoderCallback decode) async {
     final preview = getThumbnailUrlForRemoteId(key.assetId);
 
-    return ImageLoader.loadImageFromCache(
-      preview,
-      cache: cache,
-      decode: decode,
-      chunkEvents: chunkController,
-    ).whenComplete(chunkController.close);
+    return ImageLoader.loadImageFromCache(preview, cache: cache, decode: decode);
   }
 
   @override
@@ -85,25 +72,36 @@ class RemoteFullImageProvider extends ImageProvider<RemoteFullImageProvider> {
     final cache = cacheManager ?? RemoteImageCacheManager();
     return OneFramePlaceholderImageStreamCompleter(
       _codec(key, cache, decode),
-      initialImage: getCachedImage(RemoteThumbProvider(assetId: key.assetId)),
+      initialImage: getCachedImage(RemoteThumbProvider(assetId: assetId)),
+      informationCollector: () => <DiagnosticsNode>[
+        DiagnosticsProperty<ImageProvider>('Image provider', this),
+        DiagnosticsProperty<String>('Asset Id', key.assetId),
+      ],
     );
   }
 
   Stream<ImageInfo> _codec(RemoteFullImageProvider key, CacheManager cache, ImageDecoderCallback decode) async* {
-    final codec = await ImageLoader.loadImageFromCache(
-      getPreviewUrlForRemoteId(key.assetId),
-      cache: cache,
-      decode: decode,
-    );
-    yield await codec.getImageInfo();
+    ImageInfo? imageInfo;
+    final originalImageFuture = AppSetting.get(Setting.loadOriginal)
+        ? ImageLoader.loadImageFromCache(
+            getOriginalUrlForRemoteId(key.assetId),
+            cache: cache,
+            decode: decode,
+          ).then((image) => image.getNextFrame()).then((frame) => imageInfo = ImageInfo(image: frame.image, scale: 1.0))
+        : null;
 
-    if (AppSetting.get(Setting.loadOriginal)) {
-      final codec = await ImageLoader.loadImageFromCache(
-        getOriginalUrlForRemoteId(key.assetId),
-        cache: cache,
-        decode: decode,
-      );
-      yield await codec.getImageInfo();
+    final previewImageFuture =
+        ImageLoader.loadImageFromCache(getPreviewUrlForRemoteId(key.assetId), cache: cache, decode: decode)
+            .then((image) async => imageInfo == null ? await image.getNextFrame() : null)
+            .then((frame) => imageInfo == null ? ImageInfo(image: frame!.image, scale: 1.0) : null);
+
+    final previewImage = await previewImageFuture;
+    if (previewImage != null) {
+      yield previewImage;
+    }
+
+    if (originalImageFuture != null) {
+      yield await originalImageFuture;
     }
   }
 
