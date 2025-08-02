8d665b357c44a52a25ea485a07af3d43338dbb05
Nicholas Flamy <30300649+NicholasFlamy@users.noreply.github.com>
2025-08-02 17:51:25 -0400

    make it work

--- web/src/lib/components/utilities-page/utilities-menu.svelte
+++ web/src/lib/components/utilities-page/utilities-menu.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import Icon from '$lib/components/elements/icon.svelte';
   import { AppRoute } from '$lib/constants';
-  import { modalManager } from '$lib/managers/modal-manager.svelte';
   import AppDownloadModal from '$lib/modals/AppDownloadModal.svelte';
+  import { modalManager } from '@immich/ui';
   import { mdiContentDuplicate, mdiImageSizeSelectLarge } from '@mdi/js';
   import { t } from 'svelte-i18n';
 </script>
@@ -28,15 +28,6 @@
     </span>
     {$t('review_large_files')}
   </a>
-</div>
-<div class="border border-gray-300 dark:border-immich-dark-gray rounded-3xl pt-1 pb-6 dark:text-white">
-  <p class="text-xs font-medium p-4">{$t('download').toUpperCase()}</p>
-  <button
-    type="button"
-    onclick={() => modalManager.show(AppDownloadModal, {})}
-    class="w-full hover:bg-gray-100 dark:hover:bg-immich-dark-gray flex items-center gap-4 p-4"
-    >test
-  </button>
 </div>
 <div class="border border-gray-300 dark:border-immich-dark-gray rounded-3xl pt-1 pb-6 dark:text-white">
   <p class="text-xs font-medium p-4">{$t('download').toUpperCase()}</p>
