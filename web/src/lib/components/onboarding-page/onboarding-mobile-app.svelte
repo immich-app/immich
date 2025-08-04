7a705b393aa99b463e0a815905b88403430490e2
Nicholas Flamy <30300649+NicholasFlamy@users.noreply.github.com>
2025-08-03 21:04:17 -0400

    add onboarding step just to try it out

---
+++ web/src/lib/components/onboarding-page/onboarding-mobile-app.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import AppDownloadLinks from '$lib/components/app-links/app-download-links.svelte';
+  import ObtainiumGenerator from '$lib/components/app-links/obtainium-generator.svelte';
+</script>
+
+<ObtainiumGenerator />
+<AppDownloadLinks />
