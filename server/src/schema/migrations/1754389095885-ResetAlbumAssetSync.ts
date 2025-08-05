94c57dc5f2e4b719e898322a9fe83930c3075539
Zack Pollard <zackpollard@ymail.com>
2025-08-05 11:35:29 +0100

    chore: handle new sync entity types in the mobile app

--- server/src/schema/migrations/1754389095885-ResetAlbumAssetSync.ts
+++ server/src/schema/migrations/1754389095885-ResetAlbumAssetSync.ts
@@ -4,4 +4,4 @@ export async function up(db: Kysely<any>): Promise<void> {
   await sql`DELETE FROM session_sync_checkpoint WHERE type IN ('AlbumAssetBackfillV1', 'AlbumAssetExifV1', 'AlbumAssetV1')`.execute(db);
 }
 
-export async function down(db: Kysely<any>): Promise<void> {}
+export async function down(): Promise<void> {}
