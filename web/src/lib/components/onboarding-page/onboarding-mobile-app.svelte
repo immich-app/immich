6604ca9cc826b4b293e9508c351edb12fa9fe938
Nicholas Flamy <30300649+NicholasFlamy@users.noreply.github.com>
2025-08-10 19:19:16 -0400

    redo onboarding page, unfinished

--- web/src/lib/components/onboarding-page/onboarding-mobile-app.svelte
+++ web/src/lib/components/onboarding-page/onboarding-mobile-app.svelte
@@ -1,7 +1,27 @@
 <script lang="ts">
-  import AppDownloadLinks from '$lib/components/app-links/app-download-links.svelte';
-  import ObtainiumGenerator from '$lib/components/app-links/obtainium-generator.svelte';
+  import Icon from '$lib/components/elements/icon.svelte';
+  import AppDownloadModal from '$lib/modals/AppDownloadModal.svelte';
+  import ObtainiumConfigModal from '$lib/modals/ObtainiumConfigModal.svelte';
+  import { modalManager } from '@immich/ui';
+  import { mdiCellphoneArrowDownVariant, mdiLinkEdit } from '@mdi/js';
+  import { t } from 'svelte-i18n';
 </script>
 
-<ObtainiumGenerator />
-<AppDownloadLinks />
+<div class="flex gap-4">
+  <button
+    type="button"
+    onclick={() => modalManager.show(ObtainiumConfigModal, {})}
+    class="w-full border-immich-primary border-[3px] hover:bg-gray-100 dark:hover:bg-immich-dark-gray flex items-center gap-4 p-4"
+    ><span><Icon path={mdiLinkEdit} class="text-immich-primary dark:text-immich-dark-primary" size="24" /> </span>
+    {$t('obtainium_configurator')}
+  </button>
+  <button
+    type="button"
+    onclick={() => modalManager.show(AppDownloadModal, {})}
+    class="w-full border-immich-primary border-[3px] hover:bg-gray-100 dark:hover:bg-immich-dark-gray flex items-center gap-4 p-4"
+    ><span
+      ><Icon path={mdiCellphoneArrowDownVariant} class="text-immich-primary dark:text-immich-dark-primary" size="24" />
+    </span>
+    {$t('app_download_links')}
+  </button>
+</div>
