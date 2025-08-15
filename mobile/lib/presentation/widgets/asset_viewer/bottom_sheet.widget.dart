43c8bc80371b0870cfa798d9d72c33cc014b13ac
Alex <alex.tran1502@gmail.com>
2025-08-15 09:44:41 -0500

    fix: incorrect archive action shown in asset viewer'

--- mobile/lib/presentation/widgets/asset_viewer/bottom_sheet.widget.dart
+++ mobile/lib/presentation/widgets/asset_viewer/bottom_sheet.widget.dart
@@ -17,6 +17,7 @@ import 'package:immich_mobile/presentation/widgets/action_buttons/remove_from_al
 import 'package:immich_mobile/presentation/widgets/action_buttons/share_action_button.widget.dart';
 import 'package:immich_mobile/presentation/widgets/action_buttons/share_link_action_button.widget.dart';
 import 'package:immich_mobile/presentation/widgets/action_buttons/trash_action_button.widget.dart';
+import 'package:immich_mobile/presentation/widgets/action_buttons/unarchive_action_button.widget.dart';
 import 'package:immich_mobile/presentation/widgets/action_buttons/upload_action_button.widget.dart';
 import 'package:immich_mobile/presentation/widgets/asset_viewer/bottom_sheet/sheet_location_details.widget.dart';
 import 'package:immich_mobile/presentation/widgets/asset_viewer/bottom_sheet/sheet_people_details.widget.dart';
@@ -26,6 +27,7 @@ import 'package:immich_mobile/providers/infrastructure/asset_viewer/current_asse
 import 'package:immich_mobile/providers/infrastructure/current_album.provider.dart';
 import 'package:immich_mobile/providers/routes.provider.dart';
 import 'package:immich_mobile/providers/server_info.provider.dart';
+import 'package:immich_mobile/providers/user.provider.dart';
 import 'package:immich_mobile/utils/bytes_units.dart';
 import 'package:immich_mobile/widgets/common/immich_toast.dart';
 
@@ -45,16 +47,26 @@ class AssetDetailBottomSheet extends ConsumerWidget {
     }
 
     final isTrashEnable = ref.watch(serverInfoProvider.select((state) => state.serverFeatures.trash));
+    final isOwner = asset is RemoteAsset && asset.ownerId == ref.watch(currentUserProvider)?.id;
     final isInLockedView = ref.watch(inLockedViewProvider);
     final currentAlbum = ref.watch(currentRemoteAlbumProvider);
+    final isArchived = asset is RemoteAsset && asset.visibility == AssetVisibility.archive;
 
-    final actions = <Widget>[
+    final localAssetActions = <Widget>[
+      const DeleteLocalActionButton(source: ActionSource.viewer),
+      const UploadActionButton(source: ActionSource.timeline),
+    ];
+
+    final baseActions = <Widget>[
       const ShareActionButton(source: ActionSource.viewer),
       if (currentAlbum != null && currentAlbum.isActivityEnabled && currentAlbum.isShared)
         const LikeActivityActionButton(),
       if (asset.hasRemote) ...[
         const ShareLinkActionButton(source: ActionSource.viewer),
-        const ArchiveActionButton(source: ActionSource.viewer),
+        if (isArchived)
+          const UnArchiveActionButton(source: ActionSource.viewer)
+        else
+          const ArchiveActionButton(source: ActionSource.viewer),
         if (!asset.hasLocal) const DownloadActionButton(source: ActionSource.viewer),
         isTrashEnable
             ? const TrashActionButton(source: ActionSource.viewer)
@@ -62,17 +74,14 @@ class AssetDetailBottomSheet extends ConsumerWidget {
         const DeleteActionButton(source: ActionSource.viewer),
         const MoveToLockFolderActionButton(source: ActionSource.viewer),
       ],
-      if (asset.storage == AssetState.local) ...[
-        const DeleteLocalActionButton(source: ActionSource.viewer),
-        const UploadActionButton(source: ActionSource.timeline),
-      ],
+      if (asset.storage == AssetState.local) ...localAssetActions,
       if (currentAlbum != null) RemoveFromAlbumActionButton(albumId: currentAlbum.id, source: ActionSource.viewer),
     ];
 
     final lockedViewActions = <Widget>[];
 
     return BaseBottomSheet(
-      actions: isInLockedView ? lockedViewActions : actions,
+      actions: isInLockedView ? lockedViewActions : baseActions,
       slivers: const [_AssetDetailBottomSheet()],
       controller: controller,
       initialChildSize: initialChildSize,
