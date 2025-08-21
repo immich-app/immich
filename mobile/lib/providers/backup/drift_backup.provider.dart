302735ac77d874fe3ca986d88fc236f6bf3a635c
Alex Tran <alex.tran1502@gmail.com>
2025-08-21 16:17:10 -0500

    remove items

--- mobile/lib/providers/backup/drift_backup.provider.dart
+++ mobile/lib/providers/backup/drift_backup.provider.dart
@@ -248,6 +248,7 @@ class DriftBackupNotifier extends StateNotifier<DriftBackupState> {
       case TaskStatus.failed:
         // Ignore retry errors to avoid confusing users
         if (update.exception?.description == 'Delayed or retried enqueue failed') {
+          _removeUploadItem(taskId);
           return;
         }
 
