b73a88308db23fb7ecc2d575bed693774783acfd
Xantin <xantin@live.be>
2025-08-26 13:01:15 +0200

    feat(web): follow IETF BCP-47

--- web/src/lib/components/i18n/lang-ietf-bcp-47.ts
+++ web/src/lib/components/i18n/lang-ietf-bcp-47.ts
@@ -1,32 +1,31 @@
-export const LANGUAGES_LIST_WEBLATE_SPECIAL = {
+export const LANGUAGES_LIST_IETF_BCP_47 = {
   nb_NO: {
     name: 'Norwegian (Bokmål)',
     nativeName: 'Norsk (Bokmål)',
-    webplateCode: 'nb_NO',
   },
   pt_BR: {
     name: 'Portuguese (Brazil)',
     nativeName: 'Português (Brasil)',
-    webplateCode: 'pt_BR',
   },
   sr_Latn: {
     name: 'Serbian (Latin)',
     nativeName: 'Srpski',
-    webplateCode: 'sr_Latn',
   },
   sr_Cyrl: {
     name: 'Serbian (Cyrillic)',
     nativeName: 'Српски',
-    webplateCode: 'sr_Cyrl',
   },
   zh_Hant: {
     name: 'Chinese (Traditional)',
     nativeName: '中文（繁體）',
-    webplateCode: 'zh_TW',
   },
-  zh_SIMPLIFIED: {
+  // zh_SIMPLIFIED: {
+  //   name: 'Chinese (Simplified)',
+  //   nativeName: '中文（简体中文）',
+  // },
+  zh_Hans: {
     name: 'Chinese (Simplified)',
-    nativeName: '中文（简体）',
-    webplateCode: 'zh_CN',
+    nativeName: '中文（简体中文）',
+    webplateCode: 'zh_Hans',
   },
 };
