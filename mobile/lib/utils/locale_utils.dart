766a403d593bd88658669b4142be1cd61def35eb
Xantin <xantin@live.be>
2025-08-27 00:41:01 +0200

    feat(mobile): display language using defined language locale

---
+++ mobile/lib/utils/locale_utils.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+bool isMaterialSupported(Locale locale) {
+  final delegates = [
+    GlobalMaterialLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+  ];
+  return delegates.any((d) => d.isSupported(locale));
+}
+
+Locale effectiveMaterialLocale(BuildContext context) {
+  final current = context.locale;
+  if (isMaterialSupported(current)) return current;
+
+  final phoneLocale = WidgetsBinding.instance.platformDispatcher.locale;
+  return isMaterialSupported(phoneLocale) ? phoneLocale : const Locale('en');
+}
