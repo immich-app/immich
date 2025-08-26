a7821a0b7938097892df1dda39f505707bb4780d
xCJPECKOVERx <cjpeckover@hotmail.ca>
2025-08-26 17:31:58 -0400

    feat(web): Album picker shortcut info (#21273)
    * - add shortcut info to album modal footer
    
    * styling
    
    * translation
    
    ---------
    
    Co-authored-by: Alex Tran <alex.tran1502@gmail.com>

--- web/src/lib/modals/AlbumPickerModal.svelte
+++ web/src/lib/modals/AlbumPickerModal.svelte
@@ -7,7 +7,8 @@
   } from '$lib/components/shared-components/album-selection/album-selection-utils';
   import { albumViewSettings } from '$lib/stores/preferences.store';
   import { createAlbum, getAllAlbums, type AlbumResponseDto } from '@immich/sdk';
-  import { Button, Modal, ModalBody } from '@immich/ui';
+  import { Button, Icon, Modal, ModalBody, ModalFooter, Text } from '@immich/ui';
+  import { mdiKeyboardReturn } from '@mdi/js';
   import { onMount } from 'svelte';
   import { t } from 'svelte-i18n';
   import AlbumListItem from '../components/asset-viewer/album-list-item.svelte';
@@ -199,4 +200,22 @@
       >
     {/if}
   </ModalBody>
+  <ModalFooter>
+    <div class="flex justify-around w-full">
+      <div class="flex gap-4">
+        <div class="flex gap-1 place-items-center">
+          <span class="bg-gray-300 dark:bg-gray-500 rounded p-1">
+            <Icon icon={mdiKeyboardReturn} size="1rem" />
+          </span>
+          <Text size="tiny">{$t('to_select')}</Text>
+        </div>
+        <div class="flex gap-1 place-items-center">
+          <span class="bg-gray-300 dark:bg-gray-500 rounded p-1">
+            <Text size="tiny">CTRL</Text>
+          </span>
+          <Text size="tiny">{$t('to_multi_select')}</Text>
+        </div>
+      </div>
+    </div>
+  </ModalFooter>
 </Modal>
