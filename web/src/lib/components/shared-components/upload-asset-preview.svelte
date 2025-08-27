75dc033a10fb7971ca7f57c3769992280df140ce
prajwal <prajwal@hopbox.in>
2025-08-28 00:54:03 +0530

    fix:make it text-primary

--- web/src/lib/components/shared-components/upload-asset-preview.svelte
+++ web/src/lib/components/shared-components/upload-asset-preview.svelte
@@ -93,7 +93,7 @@
   {#if uploadAsset.state === UploadState.STARTED}
     <div class="text-black relative mt-[5px] h-[15px] w-full rounded-md bg-gray-300 dark:bg-gray-700">
       <div class="h-[15px] rounded-md bg-immich-primary transition-all" style={`width: ${uploadAsset.progress}%`}></div>
-      <p class="absolute top-0 h-full w-full text-center text-immich-primary dark:text-immich-dark-primary text-[10px]">
+      <p class="absolute top-0 h-full w-full text-center text-primary text-[10px]">
         {#if uploadAsset.message}
           {uploadAsset.message}
         {:else}
