922830339cc7cab4d8111bb697cab1fbff0e19e5
Johannbr <johannbr@gmail.com>
2025-08-28 20:39:30 +0200

    feat(web,server): use timeline in geolocation utility
    commit 37229acadbdda4d5e5e2c5545942c16e35719a82
    Merge: 700364b3e 8853079c5
    Author: Johannbr <johannbr@gmail.com>
    Date:   Thu Aug 28 20:27:49 2025 +0200
    
        Merge branch 'main' into feat/geolocation-utility-timeline
    
    commit 700364b3e980c40fa4c4e2376693fac5dcdab7dc
    Author: Johannbr <johannbr@gmail.com>
    Date:   Thu Aug 28 16:23:09 2025 +0200
    
        feat(web): Refresh assets after updating location
    
    commit 91b3f3c44fa625c6912d04a791b02c6af1a50733
    Author: Johannbr <johannbr@gmail.com>
    Date:   Thu Aug 28 15:52:22 2025 +0200
    
        feat(web): add geo loc on bucket
    
    commit 55a1d5db11348d662c1d267191387eba92c7e951
    Author: Johannbr <johannbr@gmail.com>
    Date:   Wed Aug 20 16:01:01 2025 +0200
    
        feat(web): geolocation utility with timeline
    
    commit c3b78d49ad3f72d1279efd4370490509185da50d
    Merge: fd7fa8eed e78144ea3
    Author: Johann <Johannbr@users.noreply.github.com>
    Date:   Thu Aug 28 13:01:50 2025 +0200
    
        Merge branch 'immich-app:main' into feat/geolocation-utility
    
    commit fd7fa8eedaae14628df240964915d225168be06d
    Author: Johannbr <johannbr@gmail.com>
    Date:   Thu Aug 28 12:46:41 2025 +0200
    
        feat(web): add animation and text
    
        animation on locations change and action text on thumbnail
    
    commit 698f9f8eaeb5411381a2a28c8dceed211d28826e
    Merge: eb7152119 5fb8d651e
    Author: Alex <alex.tran1502@gmail.com>
    Date:   Mon Aug 25 10:28:16 2025 -0500
    
        Merge branch 'main' into feat/geolocation-utility
    
    commit eb71521191538061dc28217982e38bc936efa202
    Author: Jason Rasmussen <jason@rasm.me>
    Date:   Wed Aug 20 13:06:10 2025 -0400
    
        chore: feedback
    
    commit e146488cd4199461d614516b90293641fe88b284
    Merge: 85908eb9d a1beb0a87
    Author: Johann <Johannbr@users.noreply.github.com>
    Date:   Wed Aug 20 11:11:54 2025 +0200
    
        Merge branch 'immich-app:main' into feat/geolocation-utility
    
    commit 85908eb9dcff5bd1807dcfd0b5721aaa6563d1ef
    Author: Jason Rasmussen <jason@rasm.me>
    Date:   Tue Aug 19 11:33:05 2025 -0400
    
        chore: cleanup
    
    commit fd96e925bb34c4a6887ca411881264eaa0c02024
    Merge: 17c31322d 3d515f507
    Author: Johann <Johannbr@users.noreply.github.com>
    Date:   Mon Aug 18 19:13:19 2025 +0200
    
        Merge branch 'main' into feat/geolocation-utility
    
    commit 17c31322da5abca67bfff1d78b4c76d52239d4ac
    Merge: 83ae583ac 1198311d6
    Author: Johann <Johannbr@users.noreply.github.com>
    Date:   Sat Aug 16 11:12:16 2025 +0200
    
        Merge branch 'main' into feat/geolocation-utility
    
    commit 83ae583ac5570dc8abdeb6ea18068e1465b995e5
    Merge: 1764b4bcc 13563fc50
    Author: Johann <Johannbr@users.noreply.github.com>
    Date:   Sat Aug 9 17:14:03 2025 +0200
    
        Merge branch 'main' into feat/geolocation-utility
    
    commit 1764b4bcc667e7f44f5948f815b9c10a40b2050e
    Author: Johannbr <johannbr@gmail.com>
    Date:   Sat Aug 9 17:13:40 2025 +0200
    
        feat(web): geolocation utility - fix code review - 2
    
    commit 71e8d1bf6c973b84b4882c97dad3313e45f7932e
    Author: Johannbr <johannbr@gmail.com>
    Date:   Fri Aug 8 10:20:48 2025 +0200
    
        feat(web): geolocation utility - fix code review - 1
    
    commit b389e32c8a2dc4182a503e52494ae5a4a4ac8aba
    Merge: 044e39f00 b1aacfdbd
    Author: Johann <Johannbr@users.noreply.github.com>
    Date:   Fri Aug 8 10:20:05 2025 +0200
    
        Merge branch 'immich-app:main' into feat/geolocation-utility
    
    commit 044e39f006c0ddf8d25f48df4faacb85ba2b403c
    Author: Johannbr <johannbr@gmail.com>
    Date:   Tue Aug 5 18:44:29 2025 +0200
    
        feat(geolocation):  add geolocation utility

--- web/src/routes/(user)/utilities/geolocation/+page.ts
+++ web/src/routes/(user)/utilities/geolocation/+page.ts
@@ -1,15 +1,12 @@
 import { authenticate } from '$lib/utils/auth';
 import { getFormatter } from '$lib/utils/i18n';
-import { getQueryValue } from '$lib/utils/navigation';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ url }) => {
   await authenticate(url);
-  const partialDate = getQueryValue('date');
   const $t = await getFormatter();
 
   return {
-    partialDate,
     meta: {
       title: $t('manage_geolocation'),
     },
