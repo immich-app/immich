9fa52691b3ff63a2c32705896676a840a4cac0ee
aviv <51673860+aviv926@users.noreply.github.com>
2025-08-28 03:47:56 +0300

    fix sql error

--- server/src/schema/migrations/1756340577214-AddAppVersionToSession.ts
+++ server/src/schema/migrations/1756340577214-AddAppVersionToSession.ts
@@ -2,12 +2,8 @@ import { Kysely, sql } from 'kysely';
 
 export async function up(db: Kysely<any>): Promise<void> {
   await sql`ALTER TABLE "session" ADD "appVersion" character varying NOT NULL DEFAULT '';`.execute(db);
-  await sql`ALTER TABLE "session" DROP COLUMN "appVersion";`.execute(db);
-  await sql`ALTER TABLE "session" DROP COLUMN "app_version";`.execute(db);
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-  await sql`ALTER TABLE "session" ADD "appVersion" character varying(32) NOT NULL DEFAULT ''::character varying;`.execute(db);
-  await sql`ALTER TABLE "session" ADD "app_version" character varying NOT NULL DEFAULT ''::character varying;`.execute(db);
   await sql`ALTER TABLE "session" DROP COLUMN "appVersion";`.execute(db);
 }
