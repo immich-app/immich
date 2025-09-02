8d272f8abc99be3f0cdde32b997d23a06fdf9f4b
mertalev <101130780+mertalev@users.noreply.github.com>
2025-09-02 12:29:31 -0400

    init before app launch

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -115,6 +115,8 @@ Future<void> initApp() async {
       yield LicenseEntryWithLineBreaks([license.key], license.value);
     }
   });
+
+  await NetworkRepository.init();
 }
 
 class ImmichApp extends ConsumerStatefulWidget {
@@ -168,7 +170,6 @@ class ImmichAppState extends ConsumerState<ImmichApp> with WidgetsBindingObserve
     }
     SystemChrome.setSystemUIOverlayStyle(overlayStyle);
     await ref.read(localNotificationService).setup();
-    await NetworkRepository.init();
   }
 
   Future<DeepLink> _deepLinkBuilder(PlatformDeepLink deepLink) async {
