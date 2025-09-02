f34e98ab6ff2e77d6151c275a21fcc462a4e0d71
Nicholas Flamy <30300649+NicholasFlamy@users.noreply.github.com>
2025-09-02 17:06:42 -0400

    use immich-ui components on onboarding

--- web/src/lib/components/onboarding-page/onboarding-mobile-app.svelte
+++ web/src/lib/components/onboarding-page/onboarding-mobile-app.svelte
@@ -1,28 +1,27 @@
 <script lang="ts">
-  import Icon from '$lib/components/elements/icon.svelte';
   import AppDownloadModal from '$lib/modals/AppDownloadModal.svelte';
   import ObtainiumConfigModal from '$lib/modals/ObtainiumConfigModal.svelte';
-  import { modalManager } from '@immich/ui';
+  import { Button, HStack, modalManager } from '@immich/ui';
   import { mdiCellphoneArrowDownVariant, mdiLinkEdit } from '@mdi/js';
   import { t } from 'svelte-i18n';
 </script>
 
-<div class="flex gap-4">
-  <button
-    type="button"
+<HStack wrap>
+  <Button
+    size="large"
+    shape="semi-round"
     onclick={() => modalManager.show(ObtainiumConfigModal, {})}
-    class="w-full border-immich-primary border-[3px] hover:bg-gray-100 dark:hover:bg-immich-dark-gray flex items-center gap-4 p-4"
-    ><span><Icon path={mdiLinkEdit} class="text-immich-primary dark:text-immich-dark-primary" size="24" /> </span>
-    {$t('obtainium_configurator')}
-  </button>
-  <button
-    type="button"
+    leadingIcon={mdiLinkEdit}
+  >
+    {$t('obtainium_configurator')}</Button
+  >
+  <Button
+    size="large"
+    shape="semi-round"
     onclick={() => modalManager.show(AppDownloadModal, {})}
-    class="w-full border-immich-primary border-[3px] hover:bg-gray-100 dark:hover:bg-immich-dark-gray flex items-center gap-4 p-4"
-    ><span
-      ><Icon path={mdiCellphoneArrowDownVariant} class="text-immich-primary dark:text-immich-dark-primary" size="24" />
-    </span>
-    {$t('app_download_links')}
-  </button>
-</div>
+    leadingIcon={mdiCellphoneArrowDownVariant}
+  >
+    {$t('app_download_links')}</Button
+  >
+</HStack>
 <p>{$t('mobile_app_download_onboarding_note')}</p>
