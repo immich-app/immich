3108712c420b1ce2cd6460b9fda964fa370a147e
mertalev <101130780+mertalev@users.noreply.github.com>
2025-09-12 00:07:01 -0400

    fix stack widget

--- mobile/lib/presentation/widgets/asset_viewer/asset_stack.widget.dart
+++ mobile/lib/presentation/widgets/asset_viewer/asset_stack.widget.dart
@@ -11,27 +11,25 @@ class AssetStackRow extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    int opacity = ref.watch(assetViewerProvider.select((state) => state.backgroundOpacity));
-    final showControls = ref.watch(assetViewerProvider.select((s) => s.showingControls));
-
-    if (!showControls) {
-      opacity = 0;
+    final asset = ref.watch(assetViewerProvider.select((state) => state.currentAsset));
+    if (asset == null) {
+      return const SizedBox.shrink();
     }
 
-    final asset = ref.watch(assetViewerProvider.select((s) => s.currentAsset));
+    final stackChildren = ref.watch(stackChildrenNotifier(asset)).valueOrNull;
+    if (stackChildren == null || stackChildren.isEmpty) {
+      return const SizedBox.shrink();
+    }
+
+    final showControls = ref.watch(assetViewerProvider.select((s) => s.showingControls));
+    final opacity = showControls ? ref.watch(assetViewerProvider.select((state) => state.backgroundOpacity)) : 0;
 
     return IgnorePointer(
       ignoring: opacity < 255,
       child: AnimatedOpacity(
         opacity: opacity / 255,
         duration: Durations.short2,
-        child: ref
-            .watch(stackChildrenNotifier(asset))
-            .when(
-              data: (state) => SizedBox.square(dimension: 80, child: _StackList(stack: state)),
-              error: (_, __) => const SizedBox.shrink(),
-              loading: () => const SizedBox.shrink(),
-            ),
+        child: _StackList(stack: stackChildren),
       ),
     );
   }
@@ -44,58 +42,77 @@ class _StackList extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ListView.builder(
-      scrollDirection: Axis.horizontal,
-      padding: const EdgeInsets.only(left: 5, right: 5, bottom: 30),
-      itemCount: stack.length,
-      itemBuilder: (ctx, index) {
-        final asset = stack[index];
-        return Padding(
-          padding: const EdgeInsets.only(right: 5),
-          child: GestureDetector(
-            onTap: () {
-              ref.read(assetViewerProvider.notifier).setStackIndex(index);
-              ref.read(currentAssetNotifier.notifier).setAsset(asset);
-            },
-            child: Container(
-              height: 60,
-              width: 60,
-              decoration: index == ref.watch(assetViewerProvider.select((s) => s.stackIndex))
-                  ? const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(6)),
-                      border: Border.fromBorderSide(BorderSide(color: Colors.white, width: 2)),
-                    )
-                  : const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(6)),
-                      border: null,
-                    ),
-              child: ClipRRect(
-                borderRadius: const BorderRadius.all(Radius.circular(4)),
-                child: Stack(
-                  fit: StackFit.expand,
-                  children: [
-                    Image(
-                      fit: BoxFit.cover,
-                      image: getThumbnailImageProvider(remoteId: asset.id, size: const Size.square(60)),
-                    ),
-                    if (asset.isVideo)
-                      const Icon(
-                        Icons.play_circle_outline_rounded,
-                        color: Colors.white,
-                        size: 16,
-                        shadows: [
-                          Shadow(blurRadius: 5.0, color: Color.fromRGBO(0, 0, 0, 0.6), offset: Offset(0.0, 0.0)),
-                        ],
-                      ),
-                  ],
-                ),
-              ),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        spacing: 5.0,
+        children: List.generate(stack.length, (i) {
+          final asset = stack[i];
+          return _StackItem(key: ValueKey(asset.heroTag), asset: asset, index: i);
+        }),
+      ),
+    );
+  }
+}
+
+class _StackItem extends ConsumerStatefulWidget {
+  final RemoteAsset asset;
+  final int index;
+
+  const _StackItem({super.key, required this.asset, required this.index});
+
+  @override
+  ConsumerState<_StackItem> createState() => _StackItemState();
+}
+
+class _StackItemState extends ConsumerState<_StackItem> {
+  void _onTap() {
+    ref.read(currentAssetNotifier.notifier).setAsset(widget.asset);
+    ref.read(assetViewerProvider.notifier).setStackIndex(widget.index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget thumbnail = Image(
+      fit: BoxFit.cover,
+      image: getThumbnailImageProvider(remoteId: widget.asset.id, size: const Size(60, 40)),
+      width: 60,
+      height: 40,
+    );
+    if (widget.asset.isVideo) {
+      thumbnail = Stack(
+        children: [
+          thumbnail,
+          const Center(
+            child: Icon(
+              Icons.play_circle_outline_rounded,
+              color: Colors.white,
+              size: 16,
+              shadows: [Shadow(blurRadius: 5.0, color: Color.fromRGBO(0, 0, 0, 0.6), offset: Offset(0.0, 0.0))],
             ),
           ),
-        );
-      },
+        ],
+      );
+    }
+    thumbnail = ClipRRect(borderRadius: const BorderRadius.all(Radius.circular(10)), child: thumbnail);
+
+    final isSelected = ref.watch(assetViewerProvider.select((s) => s.stackIndex == widget.index));
+    if (isSelected) {
+      thumbnail = DecoratedBox(
+        decoration: const BoxDecoration(
+          border: Border.fromBorderSide(BorderSide(color: Colors.white, width: 2)),
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        position: DecorationPosition.foreground,
+        child: thumbnail,
+      );
+    }
+
+    return SizedBox(
+      width: 60,
+      height: 40,
+      child: GestureDetector(onTap: _onTap, child: thumbnail),
     );
   }
 }
