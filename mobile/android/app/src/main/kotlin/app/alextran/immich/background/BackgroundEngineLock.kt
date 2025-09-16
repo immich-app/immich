63b280ed270ffcbf3f384059064d5bf7475fb85b
shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2025-09-16 22:23:05 +0530

    use atomicints

--- mobile/android/app/src/main/kotlin/app/alextran/immich/background/BackgroundEngineLock.kt
+++ mobile/android/app/src/main/kotlin/app/alextran/immich/background/BackgroundEngineLock.kt
@@ -4,19 +4,21 @@ import android.util.Log
 import androidx.work.WorkManager
 import io.flutter.embedding.engine.FlutterEngineCache
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import java.util.concurrent.atomic.AtomicInteger
 
 private const val TAG = "BackgroundEngineLock"
 
 class BackgroundEngineLock : FlutterPlugin {
   companion object {
     const val ENGINE_CACHE_KEY = "immich::background_worker::engine"
-    var engineCount = 0
+    var engineCount = AtomicInteger(0)
   }
 
   override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    engineCount++
     // work manager task is running while the main app is opened, cancel the worker
-    if (engineCount == 2 && FlutterEngineCache.getInstance().get(ENGINE_CACHE_KEY) != null) {
+    if (engineCount.incrementAndGet() == 2 && FlutterEngineCache.getInstance()
+        .get(ENGINE_CACHE_KEY) != null
+    ) {
       WorkManager.getInstance(binding.applicationContext)
         .cancelUniqueWork(BackgroundWorkerApiImpl.BACKGROUND_WORKER_NAME)
       FlutterEngineCache.getInstance().remove(ENGINE_CACHE_KEY)
@@ -25,7 +27,7 @@ class BackgroundEngineLock : FlutterPlugin {
   }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    engineCount--
+    engineCount.decrementAndGet()
     Log.i(TAG, "Flutter engine detached. Attached Engines count: $engineCount")
   }
 }
