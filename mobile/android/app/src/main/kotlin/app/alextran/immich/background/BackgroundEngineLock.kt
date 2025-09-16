0f20eb897e8bb2546269d114f6af36cfe611589c
shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2025-09-16 21:49:12 +0530

    fix: native locks

---
+++ mobile/android/app/src/main/kotlin/app/alextran/immich/background/BackgroundEngineLock.kt
@@ -0,0 +1,31 @@
+package app.alextran.immich.background
+
+import android.util.Log
+import androidx.work.WorkManager
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+
+private const val TAG = "BackgroundEngineLock"
+
+class BackgroundEngineLock : FlutterPlugin {
+  companion object {
+    const val ENGINE_CACHE_KEY = "immich::background_worker::engine"
+    var engineCount = 0
+  }
+
+  override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    engineCount++
+    // work manager task is running while the main app is opened, cancel the worker
+    if (engineCount == 2 && FlutterEngineCache.getInstance().get(ENGINE_CACHE_KEY) != null) {
+      WorkManager.getInstance(binding.applicationContext)
+        .cancelUniqueWork(BackgroundWorkerApiImpl.BACKGROUND_WORKER_NAME)
+      FlutterEngineCache.getInstance().remove(ENGINE_CACHE_KEY)
+    }
+    Log.i(TAG, "Flutter engine attached. Attached Engines count: $engineCount")
+  }
+
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    engineCount--
+    Log.i(TAG, "Flutter engine detached. Attached Engines count: $engineCount")
+  }
+}
