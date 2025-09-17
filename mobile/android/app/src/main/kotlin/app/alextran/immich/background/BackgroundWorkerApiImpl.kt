61c3f27fdc48824524a4eb0af9b05dd732c63fa3
shenlong <139912620+shenlong-tanwen@users.noreply.github.com>
2025-09-17 19:43:49 +0530

    feat: add configurable backup on charging only and delay settings for android (#22114)
    * feat: add configurable on charging only and delay
    
    * Segmented and style the settings
    
    ---------
    
    Co-authored-by: shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
    Co-authored-by: Alex <alex.tran1502@gmail.com>

--- mobile/android/app/src/main/kotlin/app/alextran/immich/background/BackgroundWorkerApiImpl.kt
+++ mobile/android/app/src/main/kotlin/app/alextran/immich/background/BackgroundWorkerApiImpl.kt
@@ -1,6 +1,7 @@
 package app.alextran.immich.background
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.provider.MediaStore
 import android.util.Log
 import androidx.work.BackoffPolicy
@@ -10,7 +11,7 @@ import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 import java.util.concurrent.TimeUnit
 
-private const val TAG = "BackgroundUploadImpl"
+private const val TAG = "BackgroundWorkerApiImpl"
 
 class BackgroundWorkerApiImpl(context: Context) : BackgroundWorkerFgHostApi {
   private val ctx: Context = context.applicationContext
@@ -19,9 +20,16 @@ class BackgroundWorkerApiImpl(context: Context) : BackgroundWorkerFgHostApi {
     enqueueMediaObserver(ctx)
   }
 
+  override fun configure(settings: BackgroundWorkerSettings) {
+    BackgroundWorkerPreferences(ctx).updateSettings(settings)
+    enqueueMediaObserver(ctx)
+  }
+
   override fun disable() {
-    WorkManager.getInstance(ctx).cancelUniqueWork(OBSERVER_WORKER_NAME)
-    WorkManager.getInstance(ctx).cancelUniqueWork(BACKGROUND_WORKER_NAME)
+    WorkManager.getInstance(ctx).apply {
+      cancelUniqueWork(OBSERVER_WORKER_NAME)
+      cancelUniqueWork(BACKGROUND_WORKER_NAME)
+    }
     Log.i(TAG, "Cancelled background upload tasks")
   }
 
@@ -30,14 +38,16 @@ class BackgroundWorkerApiImpl(context: Context) : BackgroundWorkerFgHostApi {
     private const val OBSERVER_WORKER_NAME = "immich/MediaObserverV1"
 
     fun enqueueMediaObserver(ctx: Context) {
-      val constraints = Constraints.Builder()
-        .addContentUriTrigger(MediaStore.Images.Media.INTERNAL_CONTENT_URI, true)
-        .addContentUriTrigger(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, true)
-        .addContentUriTrigger(MediaStore.Video.Media.INTERNAL_CONTENT_URI, true)
-        .addContentUriTrigger(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, true)
-        .setTriggerContentUpdateDelay(30, TimeUnit.SECONDS)
-        .setTriggerContentMaxDelay(3, TimeUnit.MINUTES)
-        .build()
+      val settings = BackgroundWorkerPreferences(ctx).getSettings()
+      val constraints = Constraints.Builder().apply {
+        addContentUriTrigger(MediaStore.Images.Media.INTERNAL_CONTENT_URI, true)
+        addContentUriTrigger(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, true)
+        addContentUriTrigger(MediaStore.Video.Media.INTERNAL_CONTENT_URI, true)
+        addContentUriTrigger(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, true)
+        setTriggerContentUpdateDelay(settings.minimumDelaySeconds, TimeUnit.SECONDS)
+        setTriggerContentMaxDelay(settings.minimumDelaySeconds * 10, TimeUnit.MINUTES)
+        setRequiresCharging(settings.requiresCharging)
+      }.build()
 
       val work = OneTimeWorkRequest.Builder(MediaObserver::class.java)
         .setConstraints(constraints)
@@ -45,7 +55,10 @@ class BackgroundWorkerApiImpl(context: Context) : BackgroundWorkerFgHostApi {
       WorkManager.getInstance(ctx)
         .enqueueUniqueWork(OBSERVER_WORKER_NAME, ExistingWorkPolicy.REPLACE, work)
 
-      Log.i(TAG, "Enqueued media observer worker with name: $OBSERVER_WORKER_NAME")
+      Log.i(
+        TAG,
+        "Enqueued media observer worker with name: $OBSERVER_WORKER_NAME and settings: $settings"
+      )
     }
 
     fun enqueueBackgroundWorker(ctx: Context) {
@@ -60,5 +73,35 @@ class BackgroundWorkerApiImpl(context: Context) : BackgroundWorkerFgHostApi {
 
       Log.i(TAG, "Enqueued background worker with name: $BACKGROUND_WORKER_NAME")
     }
+  }
+}
+
+private class BackgroundWorkerPreferences(private val ctx: Context) {
+  companion object {
+    private const val SHARED_PREF_NAME = "Immich::BackgroundWorker"
+    private const val SHARED_PREF_MIN_DELAY_KEY = "BackgroundWorker::minDelaySeconds"
+    private const val SHARED_PREF_REQUIRE_CHARGING_KEY = "BackgroundWorker::requireCharging"
+
+    private const val DEFAULT_MIN_DELAY_SECONDS = 30L
+    private const val DEFAULT_REQUIRE_CHARGING = false
+  }
+
+  private val sp: SharedPreferences by lazy {
+    ctx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
+  }
+
+  fun updateSettings(settings: BackgroundWorkerSettings) {
+    sp.edit().apply {
+      putLong(SHARED_PREF_MIN_DELAY_KEY, settings.minimumDelaySeconds)
+      putBoolean(SHARED_PREF_REQUIRE_CHARGING_KEY, settings.requiresCharging)
+      apply()
+    }
+  }
+
+  fun getSettings(): BackgroundWorkerSettings {
+    return BackgroundWorkerSettings(
+      minimumDelaySeconds = sp.getLong(SHARED_PREF_MIN_DELAY_KEY, DEFAULT_MIN_DELAY_SECONDS),
+      requiresCharging = sp.getBoolean(SHARED_PREF_REQUIRE_CHARGING_KEY, DEFAULT_REQUIRE_CHARGING),
+    )
   }
 }
