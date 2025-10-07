8ee495b08f00fc4005d81c6aa228df97b58cd1f7
shenlong <139912620+shenlong-tanwen@users.noreply.github.com>
2025-10-07 19:19:43 +0530

    fix: promote to foreground service before starting engine (#22517)
    fix: show notification from native
    
    Co-authored-by: shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
    Co-authored-by: Alex <alex.tran1502@gmail.com>

--- mobile/pigeon/background_worker_api.dart
+++ mobile/pigeon/background_worker_api.dart
@@ -22,6 +22,8 @@ class BackgroundWorkerSettings {
 abstract class BackgroundWorkerFgHostApi {
   void enable();
 
+  void saveNotificationMessage(String title, String body);
+
   void configure(BackgroundWorkerSettings settings);
 
   void disable();
@@ -32,8 +34,6 @@ abstract class BackgroundWorkerBgHostApi {
   // Called from the background flutter engine when it has bootstrapped and established the
   // required platform channels to notify the native side to start the background upload
   void onInitialized();
-
-  void showNotification(String title, String content);
 
   // Called from the background flutter engine to request the native side to cleanup
   void close();
