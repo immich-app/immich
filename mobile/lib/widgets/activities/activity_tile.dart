06b92c780344cdbff3d59406ddb52f5f7058747d
bwees <brandonwees@gmail.com>
2025-10-11 22:57:27 -0500

    chore: migrate to new timeline

--- mobile/lib/widgets/activities/activity_tile.dart
+++ mobile/lib/widgets/activities/activity_tile.dart
@@ -1,10 +1,16 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:immich_mobile/entities/store.entity.dart';
 import 'package:immich_mobile/extensions/build_context_extensions.dart';
 import 'package:immich_mobile/extensions/datetime_extensions.dart';
 import 'package:immich_mobile/models/activities/activity.model.dart';
-import 'package:immich_mobile/providers/image/immich_remote_thumbnail_provider.dart';
+import 'package:immich_mobile/presentation/widgets/asset_viewer/asset_viewer.page.dart';
 import 'package:immich_mobile/providers/asset_viewer/current_asset.provider.dart';
+import 'package:immich_mobile/providers/image/immich_remote_thumbnail_provider.dart';
+import 'package:immich_mobile/providers/infrastructure/asset.provider.dart';
+import 'package:immich_mobile/providers/infrastructure/timeline.provider.dart';
+import 'package:immich_mobile/routing/router.dart';
 import 'package:immich_mobile/widgets/common/user_circle_avatar.dart';
 
 class ActivityTile extends HookConsumerWidget {
@@ -76,24 +82,40 @@ class _ActivityTitle extends StatelessWidget {
   }
 }
 
-class _ActivityAssetThumbnail extends StatelessWidget {
+class _ActivityAssetThumbnail extends ConsumerWidget {
   final String assetId;
 
   const _ActivityAssetThumbnail(this.assetId);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 40,
-      height: 30,
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(Radius.circular(4)),
-        image: DecorationImage(
-          image: ImmichRemoteThumbnailProvider(assetId: assetId),
-          fit: BoxFit.cover,
+  Widget build(BuildContext context, WidgetRef ref) {
+    Future<void> onAssetTapped() async {
+      final asset = await ref.read(assetServiceProvider).getRemoteAsset(assetId);
+      if (asset == null) {
+        return;
+      }
+
+      // TODO: remove this check when old timeline is removed
+      if (Store.isBetaTimelineEnabled) {
+        AssetViewer.setAsset(ref, asset);
+        final timelineService = ref.read(timelineFactoryProvider).fromAssets([asset]);
+        context.pushRoute(AssetViewerRoute(initialIndex: 0, timelineService: timelineService));
+      }
+    }
+
+    return GestureDetector(
+      onTap: onAssetTapped,
+      child: Container(
+        width: 40,
+        height: 30,
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(4)),
+          image: DecorationImage(
+            image: ImmichRemoteThumbnailProvider(assetId: assetId),
+            fit: BoxFit.cover,
+          ),
         ),
       ),
-      child: const SizedBox.shrink(),
     );
   }
 }
