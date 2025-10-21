edb4235609035cbd7b2c2dcf28c983602f57e061
Aviv <51673860+aviv926@users.noreply.github.com>
2025-10-22 01:18:22 +0300

    Better fix

--- web/src/lib/components/shared-components/upload-asset-preview.svelte
+++ web/src/lib/components/shared-components/upload-asset-preview.svelte
@@ -94,8 +94,9 @@
     <div class="text-black relative mt-[5px] h-[15px] w-full rounded-md bg-gray-300 dark:bg-gray-700">
       <div class="h-[15px] rounded-md bg-immich-primary transition-all" style={`width: ${uploadAsset.progress}%`}></div>
       <p class="absolute top-0 h-full w-full text-center text-primary text-[10px]">
-        {#if uploadAsset.message}
+        {#if uploadAsset.message === $t('asset_hashing')}
           {uploadAsset.message}
+        {:else}
           {uploadAsset.progress}% - {getByteUnitString(uploadAsset.speed || 0, $locale)}/s - {uploadAsset.eta}s
         {/if}
       </p>
