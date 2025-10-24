78fb815cdba71243f95fb3824ef1b933693e9076
Dag Stuan <dag.stuan@bekk.no>
2025-10-24 20:41:34 +0200

    feat(web): add search filter for camera lens model. (#21792)

--- open-api/typescript-sdk/src/fetch-client.ts
+++ open-api/typescript-sdk/src/fetch-client.ts
@@ -3566,9 +3566,10 @@ export function searchAssetStatistics({ statisticsSearchDto }: {
 /**
  * This endpoint requires the `asset.read` permission.
  */
-export function getSearchSuggestions({ country, includeNull, make, model, state, $type }: {
+export function getSearchSuggestions({ country, includeNull, lensModel, make, model, state, $type }: {
     country?: string;
     includeNull?: boolean;
+    lensModel?: string;
     make?: string;
     model?: string;
     state?: string;
@@ -3580,6 +3581,7 @@ export function getSearchSuggestions({ country, includeNull, make, model, state,
     }>(`/search/suggestions${QS.query(QS.explode({
         country,
         includeNull,
+        lensModel,
         make,
         model,
         state,
@@ -4919,7 +4921,8 @@ export enum SearchSuggestionType {
     State = "state",
     City = "city",
     CameraMake = "camera-make",
-    CameraModel = "camera-model"
+    CameraModel = "camera-model",
+    CameraLensModel = "camera-lens-model"
 }
 export enum SharedLinkType {
     Album = "ALBUM",
