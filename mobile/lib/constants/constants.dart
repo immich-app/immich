ceb36a304d9bfc59c868b222f46a5da45e32f009
Alex <alex.tran1502@gmail.com>
2025-10-31 12:24:41 -0500

    fix: view in timeline does not jump to the timeline correctly (#23428)

--- mobile/lib/constants/constants.dart
+++ mobile/lib/constants/constants.dart
@@ -53,3 +53,8 @@ const int kMinMonthsToEnableScrubberSnap = 12;
 const String kImmichAppStoreLink = "https://apps.apple.com/app/immich/id6449244941";
 const String kImmichPlayStoreLink = "https://play.google.com/store/apps/details?id=app.alextran.immich";
 const String kImmichLatestRelease = "https://github.com/immich-app/immich/releases/latest";
+
+const int kPhotoTabIndex = 0;
+const int kSearchTabIndex = 1;
+const int kAlbumTabIndex = 2;
+const int kLibraryTabIndex = 3;
