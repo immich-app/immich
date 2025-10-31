2b33a58448a252d5d359f52683c58616c617f30c
shenlong <139912620+shenlong-tanwen@users.noreply.github.com>
2025-11-01 01:25:28 +0530

    fix: show in timeline from search page (#23440)
    Co-authored-by: shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>

--- mobile/lib/presentation/widgets/action_buttons/similar_photos_action_button.widget.dart
+++ mobile/lib/presentation/widgets/action_buttons/similar_photos_action_button.widget.dart
@@ -3,14 +3,12 @@ import 'dart:async';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:immich_mobile/constants/constants.dart';
 import 'package:immich_mobile/entities/asset.entity.dart';
 import 'package:immich_mobile/extensions/translate_extensions.dart';
 import 'package:immich_mobile/models/search/search_filter.model.dart';
 import 'package:immich_mobile/presentation/pages/search/paginated_search.provider.dart';
 import 'package:immich_mobile/presentation/widgets/action_buttons/base_action_button.widget.dart';
 import 'package:immich_mobile/presentation/widgets/asset_viewer/asset_viewer.state.dart';
-import 'package:immich_mobile/providers/routes.provider.dart';
 import 'package:immich_mobile/routing/router.dart';
 
 class SimilarPhotosActionButton extends ConsumerWidget {
@@ -38,20 +36,7 @@ class SimilarPhotosActionButton extends ConsumerWidget {
           ),
         );
 
-    /// Using and currentTabIndex to make sure we are using the correct
-    /// navigation behavior. We want to be able to navigate back to the
-    /// main timline using View In Timeline button without the need of
-    /// waiting for the timeline to be rebuild. At the same time, we want
-    /// to refresh the search page when tapping the Similar Photos button
-    /// while already in the Search tab.
-    final currentTabIndex = (ref.read(currentTabIndexProvider.notifier).state);
-
-    if (currentTabIndex != kSearchTabIndex) {
-      unawaited(context.router.navigate(const DriftSearchRoute()));
-      ref.read(currentTabIndexProvider.notifier).state = kSearchTabIndex;
-    } else {
-      unawaited(context.router.popAndPush(const DriftSearchRoute()));
-    }
+    unawaited(context.navigateTo(const DriftSearchRoute()));
   }
 
   @override
