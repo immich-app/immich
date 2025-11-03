f8d510168efb77688c6aafff10dec37fb76656ef
izzy <me@insrt.uk>
2025-11-03 11:19:56 +0000

    fix: logic error in server redirect

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -6,11 +6,11 @@ export const handle = (async ({ event, resolve }) => {
   // try to check maintenance status and redirect accordingly ahead of time
   const redirectToMaintenance = await fetch(process.env.IMMICH_SERVER_URL + 'api/server/config')
     .then((response) => response.json())
-    .then(
-      ({ maintenanceMode }: { maintenanceMode: boolean }) =>
-        maintenanceMode !== event.url.pathname.startsWith('/maintenance'),
-    )
-    .catch((_) => undefined);
+    .then(({ maintenanceMode }: { maintenanceMode: boolean }) => maintenanceMode)
+    .catch((_) => false)
+    .then((maintenanceMode) =>
+      maintenanceMode !== event.url.pathname.startsWith('/maintenance') ? maintenanceMode : undefined,
+    );
 
   if (typeof redirectToMaintenance === 'boolean') {
     throw redirect(302, redirectToMaintenance ? '/maintenance' : '/');
