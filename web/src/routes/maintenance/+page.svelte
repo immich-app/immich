1ed2cdae1a0247927149a110ebe8474e8d1db03e
izzy <me@insrt.uk>
2025-11-05 17:03:51 +0000

    feat: add a link to maintenance mode documentation

--- web/src/routes/maintenance/+page.svelte
+++ web/src/routes/maintenance/+page.svelte
@@ -1,22 +1,35 @@
 <script lang="ts">
   import AuthPageLayout from '$lib/components/layouts/AuthPageLayout.svelte';
-  import { AppRoute } from '$lib/constants';
+  import FormatMessage from '$lib/elements/FormatMessage.svelte';
   import { maintenanceAuth } from '$lib/stores/maintenance.store';
   import { endMaintenance } from '@immich/sdk';
   import { Button, Heading } from '@immich/ui';
   import { t } from 'svelte-i18n';
 
   // strip token from URL after load
-  const query = new URLSearchParams(location.search);
-  if (query.get('token')) {
-    history.replaceState({}, document.title, AppRoute.MAINTENANCE);
+  const url = new URL(location.href);
+  if (url.searchParams.get('token')) {
+    url.searchParams.delete('token');
+    history.replaceState({}, document.title, url);
   }
 </script>
 
 <AuthPageLayout>
   <div class="flex flex-col place-items-center text-center gap-4">
     <Heading size="large" color="primary" tag="h1">{$t('maintenance_title')}</Heading>
-    <p>{$t('maintenance_description')}</p>
+    <p>
+      <FormatMessage key="maintenance_description">
+        {#snippet children({ tag, message })}
+          {#if tag === 'link'}
+            <span class="font-medium underline">
+              <a href="https://docs.immich.app/administration/maintenance-mode" target="_blank" rel="noopener">
+                {message}
+              </a>
+            </span>
+          {/if}
+        {/snippet}
+      </FormatMessage>
+    </p>
     {#if $maintenanceAuth}
       <p>
         {$t('maintenance_logged_in_as', {
