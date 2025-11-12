074cc1db737b93aa1e12b90d1619e2bdeabc1e82
shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2025-11-12 17:31:46 +0530

    feat(mobile): dynamic layout in new timeline

--- mobile/lib/presentation/widgets/timeline/fixed/segment.model.dart
+++ mobile/lib/presentation/widgets/timeline/fixed/segment.model.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:math' as math;
 
 import 'package:auto_route/auto_route.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/domain/models/asset/base_asset.model.dart';
@@ -14,6 +15,7 @@ import 'package:immich_mobile/presentation/widgets/timeline/segment.model.dart';
 import 'package:immich_mobile/presentation/widgets/timeline/segment_builder.dart';
 import 'package:immich_mobile/presentation/widgets/timeline/timeline.state.dart';
 import 'package:immich_mobile/presentation/widgets/timeline/timeline_drag_region.dart';
+import 'package:immich_mobile/providers/app_settings.provider.dart';
 import 'package:immich_mobile/providers/asset_viewer/is_motion_video_playing.provider.dart';
 import 'package:immich_mobile/providers/haptic_feedback.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/current_album.provider.dart';
@@ -21,6 +23,7 @@ import 'package:immich_mobile/providers/infrastructure/readonly_mode.provider.da
 import 'package:immich_mobile/providers/infrastructure/timeline.provider.dart';
 import 'package:immich_mobile/providers/timeline/multiselect.provider.dart';
 import 'package:immich_mobile/routing/router.dart';
+import 'package:immich_mobile/services/app_settings.service.dart';
 
 class FixedSegment extends Segment {
   final double tileHeight;
@@ -78,6 +81,7 @@ class FixedSegment extends Segment {
       assetCount: numberOfAssets,
       tileHeight: tileHeight,
       spacing: spacing,
+      columnCount: columnCount,
     );
   }
 }
@@ -87,24 +91,34 @@ class _FixedSegmentRow extends ConsumerWidget {
   final int assetCount;
   final double tileHeight;
   final double spacing;
+  final int columnCount;
 
   const _FixedSegmentRow({
     required this.assetIndex,
     required this.assetCount,
     required this.tileHeight,
     required this.spacing,
+    required this.columnCount,
   });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final isScrubbing = ref.watch(timelineStateProvider.select((s) => s.isScrubbing));
     final timelineService = ref.read(timelineServiceProvider);
+    final isDynamicLayout = ref.watch(
+      appSettingsServiceProvider.select((s) => s.getSetting(AppSettingsEnum.dynamicLayout)),
+    );
 
     if (isScrubbing) {
       return _buildPlaceholder(context);
     }
     if (timelineService.hasRange(assetIndex, assetCount)) {
-      return _buildAssetRow(context, timelineService.getAssets(assetIndex, assetCount), timelineService);
+      return _buildAssetRow(
+        context,
+        timelineService.getAssets(assetIndex, assetCount),
+        timelineService,
+        isDynamicLayout,
+      );
     }
 
     return FutureBuilder<List<BaseAsset>>(
@@ -113,7 +127,7 @@ class _FixedSegmentRow extends ConsumerWidget {
         if (snapshot.connectionState != ConnectionState.done) {
           return _buildPlaceholder(context);
         }
-        return _buildAssetRow(context, snapshot.requireData, timelineService);
+        return _buildAssetRow(context, snapshot.requireData, timelineService, isDynamicLayout);
       },
     );
   }
@@ -122,23 +136,69 @@ class _FixedSegmentRow extends ConsumerWidget {
     return SegmentBuilder.buildPlaceholder(context, assetCount, size: Size.square(tileHeight), spacing: spacing);
   }
 
-  Widget _buildAssetRow(BuildContext context, List<BaseAsset> assets, TimelineService timelineService) {
-    return FixedTimelineRow(
-      dimension: tileHeight,
-      spacing: spacing,
-      textDirection: Directionality.of(context),
-      children: [
-        for (int i = 0; i < assets.length; i++)
-          TimelineAssetIndexWrapper(
-            assetIndex: assetIndex + i,
-            segmentIndex: 0, // For simplicity, using 0 for now
-            child: _AssetTileWidget(
-              key: ValueKey(Object.hash(assets[i].heroTag, assetIndex + i, timelineService.hashCode)),
-              asset: assets[i],
+  Widget _buildAssetRow(
+    BuildContext context,
+    List<BaseAsset> assets,
+    TimelineService timelineService,
+    bool isDynamicLayout,
+  ) {
+    if (!isDynamicLayout) {
+      return TimelineRow.fixed(
+        dimension: tileHeight,
+        spacing: spacing,
+        textDirection: Directionality.of(context),
+        children: [
+          for (int i = 0; i < assets.length; i++)
+            TimelineAssetIndexWrapper(
               assetIndex: assetIndex + i,
+              segmentIndex: 0, // For simplicity, using 0 for now
+              child: _AssetTileWidget(
+                key: ValueKey(Object.hash(assets[i].heroTag, assetIndex + i, timelineService.hashCode)),
+                asset: assets[i],
+                assetIndex: assetIndex + i,
+              ),
             ),
-          ),
-      ],
+        ],
+      );
+    }
+
+    final aspectRatios = assets.map((e) => (e.width ?? 1) / (e.height ?? 1)).toList();
+    final meanAspectRatio = aspectRatios.sum / assets.length;
+
+    // 1: mean width
+    // 0.5: width < mean - threshold
+    // 1.5: width > mean + threshold
+    final arConfiguration = aspectRatios.map((e) {
+      if (e - meanAspectRatio > 0.3) return 1.5;
+      if (e - meanAspectRatio < -0.3) return 0.5;
+      return 1.0;
+    });
+
+    // Normalize to get width distribution
+    final sum = arConfiguration.sum;
+
+    // Distribute the available width proportionally based on aspect ratio configuration
+    final widths = arConfiguration.map((e) => ((e * assets.length) / sum) * tileHeight).toList();
+
+    return TimelineDragRegion(
+      child: TimelineRow(
+        height: tileHeight,
+        widths: widths,
+        spacing: spacing,
+        textDirection: Directionality.of(context),
+        children: [
+          for (int i = 0; i < assets.length; i++)
+            TimelineAssetIndexWrapper(
+              assetIndex: assetIndex + i,
+              segmentIndex: 0, // For simplicity, using 0 for now
+              child: _AssetTileWidget(
+                key: ValueKey(Object.hash(assets[i].heroTag, assetIndex + i, timelineService.hashCode)),
+                asset: assets[i],
+                assetIndex: assetIndex + i,
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
