2ccbdb3ca3d8271d0bbaaefd5e3411e9f32f2bf0
Alex <alex.tran1502@gmail.com>
2025-11-14 15:00:47 -0600

    menu styling

--- mobile/lib/presentation/widgets/action_buttons/add_action_button.widget.dart
+++ mobile/lib/presentation/widgets/action_buttons/add_action_button.widget.dart
@@ -1,5 +1,3 @@
-// dart
-// File: 'lib/presentation/widgets/action_buttons/add_action_button.widget.dart'
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -9,6 +7,7 @@ import 'package:immich_mobile/presentation/widgets/action_buttons/unarchive_acti
 import 'package:immich_mobile/providers/infrastructure/asset_viewer/current_asset.provider.dart';
 import 'package:immich_mobile/presentation/widgets/album/album_selector.widget.dart';
 import 'package:immich_mobile/providers/infrastructure/album.provider.dart';
+import 'package:immich_mobile/providers/routes.provider.dart';
 import 'package:immich_mobile/widgets/common/immich_toast.dart';
 import 'package:immich_mobile/providers/user.provider.dart';
 
@@ -20,30 +19,10 @@ import 'package:immich_mobile/presentation/widgets/action_buttons/archive_action
 import 'package:immich_mobile/presentation/widgets/action_buttons/move_to_lock_folder_action_button.widget.dart';
 import 'package:immich_mobile/presentation/widgets/bottom_sheet/base_bottom_sheet.widget.dart';
 
-import '../../../providers/routes.provider.dart';
-
-enum _AddMenu { album, archive, unarchive, lockedFolder }
+enum AddToMenuItem { album, archive, unarchive, lockedFolder }
 
 class AddActionButton extends ConsumerWidget {
   const AddActionButton({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-
-    final asset = ref.watch(currentAssetNotifier);
-    if (asset == null) {
-      return const SizedBox.shrink();
-    }
-    return Builder(
-      builder: (buttonContext) {
-        return BaseActionButton(
-          iconData: Icons.add,
-          label: "add_to_bottom_bar".tr(),
-          onPressed: () => _showAddOptions(buttonContext, ref),
-        );
-      },
-    );
-  }
 
   Future<void> _showAddOptions(BuildContext context, WidgetRef ref) async {
     final asset = ref.read(currentAssetNotifier);
@@ -56,63 +35,65 @@ class AddActionButton extends ConsumerWidget {
     final hasRemote = asset is RemoteAsset;
     final showArchive = isOwner && !isInLockedView && hasRemote && !isArchived;
     final showUnarchive = isOwner && !isInLockedView && hasRemote && isArchived;
+    final menuItemHeight = 30.0;
 
-    final List<PopupMenuEntry<_AddMenu>> items = [
+    final List<PopupMenuEntry<AddToMenuItem>> items = [
       PopupMenuItem(
-        value: _AddMenu.album,
-        child: ListTile(
-          leading: const Icon(Icons.photo_album_outlined),
-          title: Text("album".tr()),
-        ),
+        enabled: false,
+        textStyle: context.textTheme.labelMedium,
+        height: 40,
+        child: Text("add_to_bottom_bar".tr()),
       ),
+      PopupMenuItem(
+        height: menuItemHeight,
+        value: AddToMenuItem.album,
+        child: ListTile(leading: const Icon(Icons.photo_album_outlined), title: Text("album".tr())),
+      ),
+      const PopupMenuDivider(),
+      PopupMenuItem(enabled: false, textStyle: context.textTheme.labelMedium, height: 40, child: Text("move_to".tr())),
       if (isOwner) ...[
         if (showArchive)
           PopupMenuItem(
-            value: _AddMenu.archive,
-            child: ListTile(
-              leading: const Icon(Icons.archive_outlined),
-              title: Text("archive".tr()),
-            ),
+            height: menuItemHeight,
+            value: AddToMenuItem.archive,
+            child: ListTile(leading: const Icon(Icons.archive_outlined), title: Text("archive".tr())),
           ),
         if (showUnarchive)
           PopupMenuItem(
-            value: _AddMenu.unarchive,
-            child: ListTile(
-              leading: const Icon(Icons.unarchive_outlined),
-              title: Text("unarchive".tr()),
-            ),
+            height: menuItemHeight,
+            value: AddToMenuItem.unarchive,
+            child: ListTile(leading: const Icon(Icons.unarchive_outlined), title: Text("unarchive".tr())),
           ),
         PopupMenuItem(
-          value: _AddMenu.lockedFolder,
-          child: ListTile(
-            leading: const Icon(Icons.lock_outline),
-            title: Text("locked_folder".tr()),
-          ),
+          height: menuItemHeight,
+          value: AddToMenuItem.lockedFolder,
+          child: ListTile(leading: const Icon(Icons.lock_outline), title: Text("locked_folder".tr())),
         ),
       ],
     ];
 
-    final _AddMenu? selected = await showMenu<_AddMenu>(
+    final AddToMenuItem? selected = await showMenu<AddToMenuItem>(
       context: context,
       color: context.themeData.scaffoldBackgroundColor,
       position: _menuPosition(context),
       items: items,
     );
 
-    if (selected == null) return;
+    if (selected == null) {
+      return;
+    }
 
     switch (selected) {
-      case _AddMenu.album:
+      case AddToMenuItem.album:
         _openAlbumSelector(context, ref);
         break;
-      case _AddMenu.archive:
+      case AddToMenuItem.archive:
         await performArchiveAction(context, ref, source: ActionSource.viewer);
         break;
-      case _AddMenu.unarchive:
+      case AddToMenuItem.unarchive:
         await performUnArchiveAction(context, ref, source: ActionSource.viewer);
         break;
-
-      case _AddMenu.lockedFolder:
+      case AddToMenuItem.lockedFolder:
         await performMoveToLockFolderAction(context, ref, source: ActionSource.viewer);
         break;
     }
@@ -127,7 +108,7 @@ class AddActionButton extends ConsumerWidget {
     final size = renderObject.size;
     final position = renderObject.localToGlobal(Offset.zero);
 
-    return RelativeRect.fromLTRB(position.dx, position.dy - size.height - 110, position.dx + size.width, position.dy);
+    return RelativeRect.fromLTRB(position.dx, position.dy - size.height - 200, position.dx + size.width, position.dy);
   }
 
   void _openAlbumSelector(BuildContext context, WidgetRef ref) {
@@ -169,7 +150,9 @@ class AddActionButton extends ConsumerWidget {
 
     final addedCount = await ref.read(remoteAlbumProvider.notifier).addAssets(album.id, [latest.remoteId!]);
 
-    if (!context.mounted) return;
+    if (!context.mounted) {
+      return;
+    }
 
     if (addedCount == 0) {
       ImmichToast.show(
@@ -183,7 +166,26 @@ class AddActionButton extends ConsumerWidget {
       );
     }
 
-    if (!context.mounted) return;
+    if (!context.mounted) {
+      return;
+    }
     await Navigator.of(context).maybePop();
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asset = ref.watch(currentAssetNotifier);
+    if (asset == null) {
+      return const SizedBox.shrink();
+    }
+    return Builder(
+      builder: (buttonContext) {
+        return BaseActionButton(
+          iconData: Icons.add,
+          label: "add_to_bottom_bar".tr(),
+          onPressed: () => _showAddOptions(buttonContext, ref),
+        );
+      },
+    );
   }
 }
