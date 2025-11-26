3df57154ed1b5ce452825a3a7bfb2d0ac394644c
idubnori <i.dub.nori@gmail.com>
2025-11-26 23:41:24 +0900

    refactor: album options to use kebab menu for general UI/UX

--- mobile/lib/presentation/widgets/remote_album/drift_album_option.widget.dart
+++ mobile/lib/presentation/widgets/remote_album/drift_album_option.widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/extensions/build_context_extensions.dart';
 import 'package:immich_mobile/extensions/translate_extensions.dart';
+import 'package:immich_mobile/presentation/widgets/action_buttons/base_action_button.widget.dart';
 
 class DriftRemoteAlbumOption extends ConsumerWidget {
   const DriftRemoteAlbumOption({
@@ -27,70 +28,113 @@ class DriftRemoteAlbumOption extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    TextStyle textStyle = Theme.of(context).textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.w600);
+    final theme = context.themeData;
+    final menuChildren = <Widget>[];
 
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 24.0),
-        child: ListView(
-          shrinkWrap: true,
-          children: [
-            if (onEditAlbum != null)
-              ListTile(
-                leading: const Icon(Icons.edit),
-                title: Text('edit_album'.t(context: context), style: textStyle),
-                onTap: onEditAlbum,
-              ),
-            if (onAddPhotos != null)
-              ListTile(
-                leading: const Icon(Icons.add_a_photo),
-                title: Text('add_photos'.t(context: context), style: textStyle),
-                onTap: onAddPhotos,
-              ),
-            if (onAddUsers != null)
-              ListTile(
-                leading: const Icon(Icons.group_add),
-                title: Text('album_viewer_page_share_add_users'.t(context: context), style: textStyle),
-                onTap: onAddUsers,
-              ),
-            if (onLeaveAlbum != null)
-              ListTile(
-                leading: const Icon(Icons.person_remove_rounded),
-                title: Text('leave_album'.t(context: context), style: textStyle),
-                onTap: onLeaveAlbum,
-              ),
-            if (onToggleAlbumOrder != null)
-              ListTile(
-                leading: const Icon(Icons.swap_vert_rounded),
-                title: Text('change_display_order'.t(context: context), style: textStyle),
-                onTap: onToggleAlbumOrder,
-              ),
-            if (onCreateSharedLink != null)
-              ListTile(
-                leading: const Icon(Icons.link),
-                title: Text('create_shared_link'.t(context: context), style: textStyle),
-                onTap: onCreateSharedLink,
-              ),
-            if (onShowOptions != null)
-              ListTile(
-                leading: const Icon(Icons.settings),
-                title: Text('options'.t(context: context), style: textStyle),
-                onTap: onShowOptions,
-              ),
-            if (onDeleteAlbum != null) ...[
-              const Divider(indent: 16, endIndent: 16),
-              ListTile(
-                leading: Icon(Icons.delete, color: context.isDarkTheme ? Colors.red[400] : Colors.red[800]),
-                title: Text(
-                  'delete_album'.t(context: context),
-                  style: textStyle.copyWith(color: context.isDarkTheme ? Colors.red[400] : Colors.red[800]),
-                ),
-                onTap: onDeleteAlbum,
-              ),
-            ],
-          ],
+    if (onEditAlbum != null) {
+      menuChildren.add(
+        BaseActionButton(
+          label: 'edit_album'.t(context: context),
+          iconData: Icons.edit,
+          onPressed: onEditAlbum,
         ),
+      );
+    }
+
+    if (onAddPhotos != null) {
+      menuChildren.add(
+        BaseActionButton(
+          label: 'add_photos'.t(context: context),
+          iconData: Icons.add_a_photo,
+          onPressed: onAddPhotos,
+        ),
+      );
+    }
+
+    if (onAddUsers != null) {
+      menuChildren.add(
+        BaseActionButton(
+          label: 'album_viewer_page_share_add_users'.t(context: context),
+          iconData: Icons.group_add,
+          onPressed: onAddUsers,
+        ),
+      );
+    }
+
+    if (onLeaveAlbum != null) {
+      menuChildren.add(
+        BaseActionButton(
+          label: 'leave_album'.t(context: context),
+          iconData: Icons.person_remove_rounded,
+          onPressed: onLeaveAlbum,
+        ),
+      );
+    }
+
+    if (onToggleAlbumOrder != null) {
+      menuChildren.add(
+        BaseActionButton(
+          label: 'change_display_order'.t(context: context),
+          iconData: Icons.swap_vert_rounded,
+          onPressed: onToggleAlbumOrder,
+        ),
+      );
+    }
+
+    if (onCreateSharedLink != null) {
+      menuChildren.add(
+        BaseActionButton(
+          label: 'create_shared_link'.t(context: context),
+          iconData: Icons.link,
+          onPressed: onCreateSharedLink,
+        ),
+      );
+    }
+
+    if (onShowOptions != null) {
+      menuChildren.add(
+        BaseActionButton(
+          label: 'options'.t(context: context),
+          iconData: Icons.settings,
+          onPressed: onShowOptions,
+        ),
+      );
+    }
+
+    if (onDeleteAlbum != null) {
+      // final deleteColor = context.isDarkTheme ? Colors.red[400] : Colors.red[800];
+      menuChildren.add(
+        BaseActionButton(
+          label: 'delete_album'.t(context: context),
+          iconData: Icons.delete,
+          // iconColor: deleteColor,
+          onPressed: onDeleteAlbum,
+        ),
+      );
+    }
+
+    return MenuAnchor(
+      style: MenuStyle(
+        backgroundColor: WidgetStatePropertyAll(theme.scaffoldBackgroundColor),
+        elevation: const WidgetStatePropertyAll(4),
+        shape: const WidgetStatePropertyAll(
+          RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(12))),
+        ),
+        padding: const WidgetStatePropertyAll(EdgeInsets.symmetric(vertical: 2)),
       ),
+      menuChildren: menuChildren.expand((widget) => [const Divider(height: 0), widget]).toList(),
+      builder: (context, controller, child) {
+        return IconButton(
+          icon: const Icon(Icons.more_vert_rounded),
+          onPressed: () {
+            if (controller.isOpen) {
+              controller.close();
+            } else {
+              controller.open();
+            }
+          },
+        );
+      },
     );
   }
 }
