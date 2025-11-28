c81173166761f0db59205d999dda38d52ae2e716
Tijs <tijs.hendriks@outlook.com>
2025-11-28 09:50:34 +0100

    Add SyncThing folders to External library exclusion
    SyncThing is a popular library for syncing files (like pictures) between systems. It can really mess up your library if an external library, which is also used by SyncThing, is added and these folders are not excluded.

--- server/src/services/library.service.ts
+++ server/src/services/library.service.ts
@@ -223,7 +223,7 @@ export class LibraryService extends BaseService {
       ownerId: dto.ownerId,
       name: dto.name ?? 'New External Library',
       importPaths: dto.importPaths ?? [],
-      exclusionPatterns: dto.exclusionPatterns ?? ['**/@eaDir/**', '**/._*', '**/#recycle/**', '**/#snapshot/**'],
+      exclusionPatterns: dto.exclusionPatterns ?? ['**/@eaDir/**', '**/._*', '**/#recycle/**', '**/#snapshot/**', '**/.stversion/**', '**/.stfolder/**'],
     });
     return mapLibrary(library);
   }
