8b166f8c4838c0bc53d4bbd4a984ed1ad4a009f3
Aviv <51673860+aviv926@users.noreply.github.com>
2025-11-29 20:36:59 +0200

    updating

--- web/src/lib/components/admin-settings/UserSettings.svelte
+++ web/src/lib/components/admin-settings/UserSettings.svelte
@@ -30,16 +30,16 @@
           min={1}
           label={$t('admin.session_delete_delay_browser_settings')}
           description={$t('admin.session_delete_delay_browser_settings_description')}
-          bind:value={config.user.sessionDeleteDelayBrowser}
-          isEdited={config.user.sessionDeleteDelayBrowser !== savedConfig.user.sessionDeleteDelayBrowser}
+          bind:value={configToEdit.user.sessionDeleteDelayBrowser}
+          isEdited={configToEdit.user.sessionDeleteDelayBrowser !== config.user.sessionDeleteDelayBrowser}
         />
         <SettingInputField
           inputType={SettingInputFieldType.NUMBER}
           min={1}
           label={$t('admin.session_delete_delay_mobile_settings')}
           description={$t('admin.session_delete_delay_mobile_settings_description')}
-          bind:value={config.user.sessionDeleteDelayMobile}
-          isEdited={config.user.sessionDeleteDelayMobile !== savedConfig.user.sessionDeleteDelayMobile}
+          bind:value={configToEdit.user.sessionDeleteDelayMobile}
+          isEdited={configToEdit.user.sessionDeleteDelayMobile !== config.user.sessionDeleteDelayMobile}
         />
       </div>
 
