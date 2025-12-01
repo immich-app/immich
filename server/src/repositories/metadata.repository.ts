e53d43a3a96e3d505c66f25d000ea2343ff2da50
Alex Tran <alex.tran1502@gmail.com>
2025-12-01 21:44:18 +0000

    fix: metadata extraction failed on some video

--- server/src/repositories/metadata.repository.ts
+++ server/src/repositories/metadata.repository.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { BinaryField, DefaultReadTaskOptions, ExifTool, Tags } from 'exiftool-vendored';
 import geotz from 'geo-tz';
 import { LoggingRepository } from 'src/repositories/logging.repository';
-import { mimeTypes } from 'src/utils/mime-types';
 
 interface ExifDuration {
   Value: number;
@@ -104,8 +103,7 @@ export class MetadataRepository {
   }
 
   readTags(path: string): Promise<ImmichTags> {
-    const args = mimeTypes.isVideo(path) ? ['-ee'] : [];
-    return this.exiftool.read(path, args).catch((error) => {
+    return this.exiftool.read(path).catch((error) => {
       this.logger.warn(`Error reading exif data (${path}): ${error}\n${error?.stack}`);
       return {};
     }) as Promise<ImmichTags>;
