90ff404198fcc4916cf29d535a3660d588f14861
Yaros <siubaiev@gmail.com>
2025-12-08 16:48:51 +0100

    refactor: use statefulwidget instead of hook

--- mobile/lib/presentation/widgets/album/new_album_name_modal.widget.dart
+++ mobile/lib/presentation/widgets/album/new_album_name_modal.widget.dart
@@ -1,16 +1,25 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/extensions/build_context_extensions.dart';
 
-class NewAlbumNameModal extends HookConsumerWidget {
+class NewAlbumNameModal extends StatefulWidget {
   const NewAlbumNameModal({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final nameController = useTextEditingController();
+  State<NewAlbumNameModal> createState() => _NewAlbumNameModalState();
+}
 
+class _NewAlbumNameModalState extends State<NewAlbumNameModal> {
+  TextEditingController nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text("album_name", style: TextStyle(fontWeight: FontWeight.bold)).tr(),
       content: SingleChildScrollView(
