d43e77dbe3f4d54c747594f9b976c78507659f8e
jochbru <j@bruijns.org>
2025-12-12 19:27:48 +0000

    feat: add two-factor authentication messages and UI components for enhanced security

--- web/src/lib/components/shared-components/side-bar/bottom-info.svelte
+++ web/src/lib/components/shared-components/side-bar/bottom-info.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-  import PurchaseInfo from './purchase-info.svelte';
   import ServerStatus from './server-status.svelte';
   import StorageSpace from './storage-space.svelte';
+  import TwoFactorInfo from './two-factor-info.svelte';
 </script>
 
 <div class="mt-auto">
   <StorageSpace />
 </div>
 
-<PurchaseInfo />
+<TwoFactorInfo />
 
 <div class="mb-6 mt-2">
   <ServerStatus />
