d2f8ed485e3f10dfa65b9a45ac44fd570214ffff
jochbru <j@bruijns.org>
2025-12-14 14:41:23 +0100

    fix: remove systemConfigManager from admin layout to resolve loading issue for PixelUnion users

--- web/src/routes/admin/+layout.ts
+++ web/src/routes/admin/+layout.ts
@@ -1,6 +1,8 @@
-import { systemConfigManager } from '$lib/managers/system-config-manager.svelte';
+// Removed systemConfigManager from layout to fix issue with system config not being loaded in the admin page
+// This is because this data is not available for PixelUnion users
+// import { systemConfigManager } from '$lib/managers/system-config-manager.svelte';
 import type { LayoutLoad } from './$types';
 
 export const load = (async () => {
-  await systemConfigManager.init();
+  // await systemConfigManager.init();
 }) satisfies LayoutLoad;
