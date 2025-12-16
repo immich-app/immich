895c27eb0aba3cd2d859d4c8dec609ae700bfe6a
Alex <alex.tran1502@gmail.com>
2025-12-16 09:04:51 -0600

    chore: modify colors

--- mobile/lib/widgets/activities/comment_bubble.dart
+++ mobile/lib/widgets/activities/comment_bubble.dart
@@ -67,7 +67,7 @@ class CommentBubble extends ConsumerWidget {
                 bottom: 6,
                 child: Container(
                   padding: const EdgeInsets.all(4),
-                  decoration: BoxDecoration(color: Colors.white.withValues(alpha: 0.7), shape: BoxShape.circle),
+                  decoration: BoxDecoration(color: context.colorScheme.surfaceContainer, shape: BoxShape.circle),
                   child: Icon(Icons.thumb_up, color: context.primaryColor, size: 18),
                 ),
               ),
@@ -81,7 +81,7 @@ class CommentBubble extends ConsumerWidget {
     if (isLike && !showThumbnail) {
       likes = Container(
         padding: const EdgeInsets.all(8),
-        decoration: BoxDecoration(color: Colors.white.withValues(alpha: 0.7), shape: BoxShape.circle),
+        decoration: BoxDecoration(color: context.colorScheme.surfaceContainer, shape: BoxShape.circle),
         child: Icon(Icons.thumb_up, color: context.primaryColor, size: 18),
       );
     }
