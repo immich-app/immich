62cc12be3c98972f62cf6c51077e26827ab099a5
Jason Rasmussen <jason@rasm.me>
2026-01-05 11:26:58 -0500

    refactor: asset from param (#25041)

--- web/src/routes/(user)/locked/[[photos=photos]]/[[assetId=id]]/+page.ts
+++ web/src/routes/(user)/locked/[[photos=photos]]/[[assetId=id]]/+page.ts
@@ -1,12 +1,11 @@
 import { AppRoute } from '$lib/constants';
 import { authenticate } from '$lib/utils/auth';
 import { getFormatter } from '$lib/utils/i18n';
-import { getAssetInfoFromParam } from '$lib/utils/navigation';
 import { getAuthStatus } from '@immich/sdk';
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const load = (async ({ params, url }) => {
+export const load = (async ({ url }) => {
   await authenticate(url);
 
   const { isElevated, pinCode } = await getAuthStatus();
@@ -14,11 +13,9 @@ export const load = (async ({ params, url }) => {
     redirect(302, `${AppRoute.AUTH_PIN_PROMPT}?continue=${encodeURIComponent(url.pathname + url.search)}`);
   }
 
-  const asset = await getAssetInfoFromParam(params);
   const $t = await getFormatter();
 
   return {
-    asset,
     meta: {
       title: $t('locked_folder'),
     },
