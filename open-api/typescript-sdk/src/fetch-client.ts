2aca9ffc277d0e66959447e0cd52fc36052feb42
Jason Rasmussen <jason@rasm.me>
2026-01-06 11:55:52 -0500

    refactor: asset viewer navbar actions

--- open-api/typescript-sdk/src/fetch-client.ts
+++ open-api/typescript-sdk/src/fetch-client.ts
@@ -8,7 +8,7 @@ import * as Oazapfts from "@oazapfts/runtime";
 import * as QS from "@oazapfts/runtime/query";
 export const defaults: Oazapfts.Defaults<Oazapfts.CustomHeaders> = {
     headers: {},
-    baseUrl: "/api",
+    baseUrl: "/api"
 };
 const oazapfts = Oazapfts.runtime(defaults);
 export const servers = {
--- web/src/lib/services/asset.service.ts
+++ web/src/lib/services/asset.service.ts
@@ -1,10 +1,17 @@
+import { assetViewerManager } from '$lib/managers/asset-viewer-manager.svelte';
 import { eventManager } from '$lib/managers/event-manager.svelte';
 import SharedLinkCreateModal from '$lib/modals/SharedLinkCreateModal.svelte';
 import { user as authUser } from '$lib/stores/user.store';
 import { openFileUploadDialog } from '$lib/utils/file-uploader';
 import { AssetVisibility, copyAsset, deleteAssets, type AssetResponseDto } from '@immich/sdk';
 import { modalManager, type ActionItem } from '@immich/ui';
-import { mdiShareVariantOutline } from '@mdi/js';
+import {
+  mdiAlertOutline,
+  mdiInformationOutline,
+  mdiMotionPauseOutline,
+  mdiMotionPlayOutline,
+  mdiShareVariantOutline,
+} from '@mdi/js';
 import type { MessageFormatter } from 'svelte-i18n';
 import { get } from 'svelte/store';
 
@@ -16,7 +23,41 @@ export const getAssetActions = ($t: MessageFormatter, asset: AssetResponseDto) =
     onAction: () => modalManager.show(SharedLinkCreateModal, { assetIds: [asset.id] }),
   };
 
-  return { Share };
+  const PlayMotionPhoto: ActionItem = {
+    title: $t('play_motion_photo'),
+    icon: mdiMotionPlayOutline,
+    $if: () => !!asset.livePhotoVideoId && !assetViewerManager.isPlayingMotionPhoto,
+    onAction: () => {
+      assetViewerManager.isPlayingMotionPhoto = true;
+    },
+  };
+
+  const StopMotionPhoto: ActionItem = {
+    title: $t('stop_motion_photo'),
+    icon: mdiMotionPauseOutline,
+    $if: () => !!asset.livePhotoVideoId && assetViewerManager.isPlayingMotionPhoto,
+    onAction: () => {
+      assetViewerManager.isPlayingMotionPhoto = false;
+    },
+  };
+
+  const Offline: ActionItem = {
+    title: $t('asset_offline'),
+    icon: mdiAlertOutline,
+    color: 'danger',
+    $if: () => !!asset.isOffline,
+    onAction: () => assetViewerManager.toggleDetailPanel(),
+  };
+
+  const Info: ActionItem = {
+    title: $t('info'),
+    icon: mdiInformationOutline,
+    $if: () => asset.hasMetadata,
+    onAction: () => assetViewerManager.toggleDetailPanel(),
+    shortcuts: [{ key: 'i' }],
+  };
+
+  return { Share, PlayMotionPhoto, StopMotionPhoto, Offline, Info };
 };
 
 export const handleReplaceAsset = async (oldAssetId: string) => {
