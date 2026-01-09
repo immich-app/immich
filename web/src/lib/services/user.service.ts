70ce73b5c78d8f66fa9cd1b02b4edf59124796f4
Jason Rasmussen <jason@rasm.me>
2026-01-09 15:17:09 -0500

    refactor: user settings

--- web/src/lib/services/user.service.ts
+++ web/src/lib/services/user.service.ts
@@ -1,7 +1,7 @@
 import { eventManager } from '$lib/managers/event-manager.svelte';
 import { handleError } from '$lib/utils/handle-error';
 import { getFormatter } from '$lib/utils/i18n';
-import { resetPinCode, type PinCodeResetDto } from '@immich/sdk';
+import { changePassword, resetPinCode, type ChangePasswordDto, type PinCodeResetDto } from '@immich/sdk';
 import { toastManager } from '@immich/ui';
 
 export const handleResetPinCode = async (dto: PinCodeResetDto) => {
@@ -14,5 +14,17 @@ export const handleResetPinCode = async (dto: PinCodeResetDto) => {
     return true;
   } catch (error) {
     handleError(error, $t('errors.failed_to_reset_pin_code'));
+  }
+};
+
+export const handleChangePassword = async (dto: ChangePasswordDto) => {
+  const $t = await getFormatter();
+
+  try {
+    await changePassword({ changePasswordDto: dto });
+    toastManager.success($t('updated_password'));
+    return true;
+  } catch (error) {
+    handleError(error, $t('errors.unable_to_change_password'));
   }
 };
