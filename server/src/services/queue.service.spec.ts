812419deabbb8d16c1ab6e20fa3b853078ac92f5
izzy <me@insrt.uk>
2026-01-13 10:43:29 +0000

    test: update concurrency

--- server/src/services/queue.service.spec.ts
+++ server/src/services/queue.service.spec.ts
@@ -23,7 +23,7 @@ describe(QueueService.name, () => {
     it('should update concurrency', () => {
       sut.onConfigUpdate({ newConfig: defaults, oldConfig: {} as SystemConfig });
 
-      expect(mocks.job.setConcurrency).toHaveBeenCalledTimes(18);
+      expect(mocks.job.setConcurrency).toHaveBeenCalledTimes(19);
       expect(mocks.job.setConcurrency).toHaveBeenNthCalledWith(5, QueueName.FacialRecognition, 1);
       expect(mocks.job.setConcurrency).toHaveBeenNthCalledWith(7, QueueName.DuplicateDetection, 1);
       expect(mocks.job.setConcurrency).toHaveBeenNthCalledWith(8, QueueName.BackgroundTask, 5);
