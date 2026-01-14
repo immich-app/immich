12e85927a2fb22938cf6dbc3de3f71b85c2e2223
Alex Tran <alex.tran1502@gmail.com>
2026-01-14 16:05:49 +0000

    chore: use fontWeight for Text component

--- web/src/lib/components/server-statistics/ServerStatisticsCard.svelte
+++ web/src/lib/components/server-statistics/ServerStatisticsCard.svelte
@@ -23,7 +23,7 @@
 <div class="flex h-35 w-full flex-col justify-between rounded-3xl bg-subtle text-primary p-5">
   <div class="flex place-items-center gap-4">
     <Icon {icon} size="40" />
-    <Text size="giant" class="font-medium">{title}</Text>
+    <Text size="giant" fontWeight="medium">{title}</Text>
   </div>
 
   <div class="relative mx-auto font-immich-mono text-2xl font-medium">
