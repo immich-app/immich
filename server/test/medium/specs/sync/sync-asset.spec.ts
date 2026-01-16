f0e2353f3cef8f4d713d6a084d4cf794083a26dc
Daniel Dietzler <mail@ddietzler.dev>
2026-01-16 14:04:18 +0100

    feat: download original asset

--- server/test/medium/specs/sync/sync-asset.spec.ts
+++ server/test/medium/specs/sync/sync-asset.spec.ts
@@ -64,6 +64,7 @@ describe(SyncEntityType.AssetV1, () => {
           libraryId: asset.libraryId,
           width: asset.width,
           height: asset.height,
+          editCount: asset.editCount,
         },
         type: 'AssetV1',
       },
