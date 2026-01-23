205e87007a0570fb4cc683277f074e1a55fdc94a
midzelis <min123@gmail.com>
2026-01-23 15:44:20 +0000

    remove sw response handling

--- web/src/lib/utils/sw-messaging.ts
+++ web/src/lib/utils/sw-messaging.ts
@@ -1,19 +1,10 @@
 import { ServiceWorkerMessenger } from './sw-messenger';
 
 const messenger = new ServiceWorkerMessenger();
-
-let isServiceWorkerEnabled = true;
-
-messenger.onAckTimeout(() => {
-  if (!isServiceWorkerEnabled) {
-    return;
-  }
-  console.error('[ServiceWorker] No communication detected. Auto-disabled service worker.');
-  isServiceWorkerEnabled = false;
-});
+const hasServiceWorker = globalThis.isSecureContext && 'serviceWorker' in navigator;
 
 const isValidSwContext = (url: string | undefined | null): url is string => {
-  return globalThis.isSecureContext && isServiceWorkerEnabled && !!url;
+  return hasServiceWorker && !!url;
 };
 
 export function cancelImageUrl(url: string | undefined | null) {
