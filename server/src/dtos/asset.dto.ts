9112b3a500110ef39aaaf3af4ec2d38ed12084c8
timonrieger <mail@timonrieger.de>
2026-01-24 18:21:45 +0100

    lint

--- server/src/dtos/asset.dto.ts
+++ server/src/dtos/asset.dto.ts
@@ -1,4 +1,4 @@
-import { ApiPropertyOptional, ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import {
   IsArray,
