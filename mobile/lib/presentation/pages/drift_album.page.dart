be0eef405d920466a8d95da902cdca733cea06c6
Alex <alex.tran1502@gmail.com>
2026-01-25 12:43:49 -0600

    feat(mobile): scrollbar for album page (#25507)

--- mobile/lib/presentation/pages/drift_album.page.dart
+++ mobile/lib/presentation/pages/drift_album.page.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:immich_mobile/extensions/build_context_extensions.dart';
 import 'package:immich_mobile/presentation/widgets/album/album_selector.widget.dart';
 import 'package:immich_mobile/providers/infrastructure/album.provider.dart';
 import 'package:immich_mobile/routing/router.dart';
@@ -17,36 +18,63 @@ class DriftAlbumsPage extends ConsumerStatefulWidget {
 }
 
 class _DriftAlbumsPageState extends ConsumerState<DriftAlbumsPage> {
+  final ScrollController _scrollController = ScrollController();
+
   Future<void> onRefresh() async {
     await ref.read(remoteAlbumProvider.notifier).refresh();
   }
 
   @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final albumCount = ref.watch(remoteAlbumProvider.select((state) => state.albums.length));
+    final showScrollbar = albumCount > 10;
+
+    final scrollView = CustomScrollView(
+      controller: _scrollController,
+      slivers: [
+        ImmichSliverAppBar(
+          snap: false,
+          floating: false,
+          pinned: true,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.add_rounded, size: 28),
+              onPressed: () => context.pushRoute(const DriftCreateAlbumRoute()),
+            ),
+          ],
+          showUploadButton: false,
+        ),
+        AlbumSelector(
+          onAlbumSelected: (album) {
+            context.router.push(RemoteAlbumRoute(album: album));
+          },
+        ),
+      ],
+    );
+
     return RefreshIndicator(
       onRefresh: onRefresh,
       edgeOffset: 100,
-      child: CustomScrollView(
-        slivers: [
-          ImmichSliverAppBar(
-            snap: false,
-            floating: false,
-            pinned: true,
-            actions: [
-              IconButton(
-                icon: const Icon(Icons.add_rounded, size: 28),
-                onPressed: () => context.pushRoute(const DriftCreateAlbumRoute()),
-              ),
-            ],
-            showUploadButton: false,
-          ),
-          AlbumSelector(
-            onAlbumSelected: (album) {
-              context.router.push(RemoteAlbumRoute(album: album));
-            },
-          ),
-        ],
-      ),
+      child: showScrollbar
+          ? RawScrollbar(
+              controller: _scrollController,
+              interactive: true,
+              thickness: 8,
+              radius: const Radius.circular(4),
+              thumbVisibility: false,
+              thumbColor: context.colorScheme.primary,
+              crossAxisMargin: 4,
+              mainAxisMargin: 60,
+              minThumbLength: 40,
+              child: scrollView,
+            )
+          : scrollView,
     );
   }
 }
