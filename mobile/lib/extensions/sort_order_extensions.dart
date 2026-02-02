b42833984e4a439a15eb3b341ee2a7bbfb1b03de
LeLunZ <31982496+LeLunZ@users.noreply.github.com>
2026-02-02 15:51:56 +0100

    refactor: remove sort_order_extensions.dart

--- mobile/lib/extensions/sort_order_extensions.dart
+++
@@ -1,5 +0,0 @@
-import 'package:immich_mobile/constants/enums.dart';
-
-extension OppositeSortOrder on SortOrder {
-  SortOrder get opposite => this == SortOrder.asc ? SortOrder.desc : SortOrder.asc;
-}
