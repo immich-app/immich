d0501b8e50415991d13690e5ab0bb4952ca71ae3
Jason Rasmussen <jason@rasm.me>
2026-02-02 14:45:55 -0500

    fix: preference upgrade

--- web/src/lib/stores/preferences.store.ts
+++ web/src/lib/stores/preferences.store.ts
@@ -152,12 +152,16 @@ export const alwaysLoadOriginalVideo = persisted<boolean>('always-load-original-
 
 export const recentAlbumsDropdown = persisted<boolean>('recent-albums-open', true, {});
 
-export const duplicateSettings = new PersistedLocalStorage<DuplicateSettings>('duplicate-settings', {
-  syncAlbums: false,
-  syncVisibility: false,
-  syncFavorites: false,
-  syncRating: false,
-  syncDescription: false,
-  syncLocation: false,
-  syncTags: false,
-});
+export const duplicateSettings = new PersistedLocalStorage<DuplicateSettings>(
+  'duplicate-settings',
+  {
+    syncAlbums: false,
+    syncVisibility: false,
+    syncFavorites: false,
+    syncRating: false,
+    syncDescription: false,
+    syncLocation: false,
+    syncTags: false,
+  },
+  { upgrade: 'merge' },
+);
