e909b6b27509d0b769d4a3670882d3771f35c76c
Jason Rasmussen <jason@rasm.me>
2026-02-04 18:22:37 -0500

    feat: schema-check

---
+++ server/src/commands/schema-check.ts
@@ -0,0 +1,60 @@
+import { Command, CommandRunner } from 'nest-commander';
+import { ErrorMessages } from 'src/constants';
+import { CliService } from 'src/services/cli.service';
+import { asHuman } from 'src/sql-tools/schema-diff';
+
+@Command({
+  name: 'schema-check',
+  description: 'Verify database migrations and check for schema drift',
+})
+export class SchemaCheck extends CommandRunner {
+  constructor(private service: CliService) {
+    super();
+  }
+
+  async run(): Promise<void> {
+    try {
+      const { migrations, drift } = await this.service.schemaReport();
+
+      if (migrations.every((item) => item.status === 'applied')) {
+        console.log('Migrations are up to date');
+      } else {
+        console.log('Migration issues detected:');
+        for (const migration of migrations) {
+          switch (migration.status) {
+            case 'deleted': {
+              console.log(`  - ${migration.name} was applied, but the file no longer exists on disk`);
+              break;
+            }
+
+            case 'missing': {
+              console.log(`  - ${migration.name} exists, but has not been applied to the database`);
+              break;
+            }
+          }
+        }
+      }
+
+      if (drift.items.length === 0) {
+        console.log('\nNo schema drift detected');
+      } else {
+        console.log(`\n${ErrorMessages.SchemaDrift}`);
+        for (const item of drift.items) {
+          console.log(`  - ${item.type}: ` + asHuman(item));
+        }
+
+        console.log(`
+
+The below SQL is automatically generated and may be helpful for resolving drift. ** Use at your own risk! **
+
+\`\`\`sql
+${drift.asSql().join('\n')}
+\`\`\`
+`);
+      }
+    } catch (error) {
+      console.error(error);
+      console.error('Unable to debug migrations');
+    }
+  }
+}
--- server/src/services/cli.service.ts
+++ server/src/services/cli.service.ts
@@ -1,15 +1,59 @@
 import { Injectable } from '@nestjs/common';
-import { isAbsolute } from 'node:path';
+import { isAbsolute, join } from 'node:path';
 import { SALT_ROUNDS } from 'src/constants';
 import { MaintenanceAuthDto } from 'src/dtos/maintenance.dto';
 import { UserAdminResponseDto, mapUserAdmin } from 'src/dtos/user.dto';
 import { MaintenanceAction, SystemMetadataKey } from 'src/enum';
 import { BaseService } from 'src/services/base.service';
+import { schemaDiff } from 'src/sql-tools';
 import { createMaintenanceLoginUrl, generateMaintenanceSecret } from 'src/utils/maintenance';
 import { getExternalDomain } from 'src/utils/misc';
 
+export type SchemaReport = {
+  migrations: MigrationStatus[];
+  drift: ReturnType<typeof schemaDiff>;
+};
+
+type MigrationStatus = {
+  name: string;
+  status: 'applied' | 'missing' | 'deleted';
+};
+
 @Injectable()
 export class CliService extends BaseService {
+  async schemaReport(): Promise<SchemaReport> {
+    // eslint-disable-next-line unicorn/prefer-module
+    const allFiles = await this.storageRepository.readdir(join(__dirname, '../schema/migrations'));
+    const files = allFiles.filter((file) => file.endsWith('.js')).map((file) => file.slice(0, -3));
+    const rows = await this.databaseRepository.getMigrations();
+    const filesSet = new Set(files);
+    const rowsSet = new Set(rows.map((item) => item.name));
+    const combined = [...filesSet, ...rowsSet].toSorted();
+
+    const migrations: MigrationStatus[] = [];
+
+    for (const name of combined) {
+      if (filesSet.has(name) && rowsSet.has(name)) {
+        migrations.push({ name, status: 'applied' });
+        continue;
+      }
+
+      if (filesSet.has(name) && !rowsSet.has(name)) {
+        migrations.push({ name, status: 'missing' });
+        continue;
+      }
+
+      if (!filesSet.has(name) && rowsSet.has(name)) {
+        migrations.push({ name, status: 'deleted' });
+        continue;
+      }
+    }
+
+    const drift = await this.databaseRepository.getSchemaDrift();
+
+    return { migrations, drift };
+  }
+
   async listUsers(): Promise<UserAdminResponseDto[]> {
     const users = await this.userRepository.getList({ withDeleted: true });
     return users.map((user) => mapUserAdmin(user));
