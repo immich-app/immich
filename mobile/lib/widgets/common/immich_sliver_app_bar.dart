3a4456116e77bd31a85060fc5b9f1db5093d7f41
shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2026-02-12 16:37:12 +0530

    rebase over main

--- mobile/lib/widgets/common/immich_sliver_app_bar.dart
+++ mobile/lib/widgets/common/immich_sliver_app_bar.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
