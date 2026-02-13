d7cedb329e246158dcf73dc2463231bfe3651133
timonrieger <mail@timonrieger.de>
2026-02-13 23:12:51 +0100

    error handling

--- server/src/middleware/global-exception.filter.ts
+++ server/src/middleware/global-exception.filter.ts
@@ -1,7 +1,7 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
 import { Response } from 'express';
 import { ClsService } from 'nestjs-cls';
-import { ZodSerializationException } from 'nestjs-zod';
+import { ZodSerializationException, ZodValidationException } from 'nestjs-zod';
 import { LoggingRepository } from 'src/repositories/logging.repository';
 import { logGlobalError } from 'src/utils/logger';
 import { ZodError } from 'zod';
@@ -35,19 +35,26 @@ export class GlobalExceptionFilter implements ExceptionFilter<Error> {
     logGlobalError(this.logger, error);
 
     if (error instanceof HttpException) {
-      // Handle ZodSerializationException
-      if (error instanceof ZodSerializationException) {
-        const zodError = error.getZodError();
-        if (zodError instanceof ZodError) {
-          // TODO: handle zod error
-        }
-      }
       const status = error.getStatus();
       let body = error.getResponse();
 
       // unclear what circumstances would return a string
       if (typeof body === 'string') {
         body = { message: body };
+      }
+
+      // handle both request and response validation errors
+      if (error instanceof ZodValidationException || error instanceof ZodSerializationException) {
+        const zodError = error.getZodError();
+        if (zodError instanceof ZodError && zodError.issues.length > 0) {
+          body = {
+            message: zodError.issues,
+            // technically more accurate to return 422 Unprocessable Entity here
+            // nestjs-zod uses 400 Bad Request and we use 400 in v2
+            // https://github.com/BenLorantfy/nestjs-zod/issues/328
+            error: 'Bad Request',
+          };
+        }
       }
 
       return { status, body };
