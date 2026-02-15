c6279f3f0edd5ea11e3038807bad731e6522e775
Ben Nguyen <ben85629@gmail.com>
2026-02-15 12:14:38 -0800

    add bulk_tag_assets_action_button to general_bottom_sheet.widget
    include create tag tile in 'Add Tags' action modal

--- mobile/lib/presentation/pages/search/drift_search.page.dart
+++ mobile/lib/presentation/pages/search/drift_search.page.dart
@@ -209,7 +209,7 @@ class DriftSearchPage extends HookConsumerWidget {
             expanded: true,
             onSearch: search,
             onClear: handleClear,
-            child: TagPicker(onSelect: handleOnSelect, filter: (filter.value.tagIds ?? []).toSet()),
+            child: TagPicker(onSelectExistingTag: handleOnSelect, filter: (filter.value.tagIds ?? []).toSet()),
           ),
         ),
       );
