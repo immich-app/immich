a7431793afa1ac06989ec4a5ba6b5b718a48381d
Peter Ombodi <peter.ombodi@gmail.com>
2026-02-16 13:32:28 +0200

    chore(mobile): fix format

--- mobile/lib/infrastructure/entities/trash_sync.entity.dart
+++ mobile/lib/infrastructure/entities/trash_sync.entity.dart
@@ -3,7 +3,8 @@ import 'package:immich_mobile/infrastructure/utils/drift_default.mixin.dart';
 
 @TableIndex.sql('CREATE INDEX IF NOT EXISTS idx_trash_sync_is_sync_approved ON trash_sync_entity (is_sync_approved)')
 @TableIndex.sql(
-    'CREATE INDEX IF NOT EXISTS idx_trash_sync_checksum_status ON trash_sync_entity (checksum, is_sync_approved)')
+  'CREATE INDEX IF NOT EXISTS idx_trash_sync_checksum_status ON trash_sync_entity (checksum, is_sync_approved)',
+)
 class TrashSyncEntity extends Table with DriftDefaultsMixin {
   const TrashSyncEntity();
 
