4824bc53793e9c2ace1f662f64b98d56aac04b5c
Lauritz Tieste <lauritz6000000@gmail.com>
2026-02-27 22:38:52 +0100

    fix(timeline): update heroOffset to use tab index from provider

--- mobile/lib/presentation/widgets/timeline/fixed/segment.model.dart
+++ mobile/lib/presentation/widgets/timeline/fixed/segment.model.dart
@@ -5,7 +5,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:immich_mobile/constants/constants.dart';
+import 'package:immich_mobile/providers/tab.provider.dart';
 import 'package:immich_mobile/domain/models/asset/base_asset.model.dart';
 import 'package:immich_mobile/domain/services/timeline.service.dart';
 import 'package:immich_mobile/extensions/build_context_extensions.dart';
@@ -240,7 +240,7 @@ class _AssetTileWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    const heroOffset = kPhotoTabIndex;
+    final heroOffset = ref.watch(tabProvider).index;
 
     final lockSelection = _getLockSelectionStatus(ref);
     final showStorageIndicator = ref.watch(timelineArgsProvider.select((args) => args.showStorageIndicator));
