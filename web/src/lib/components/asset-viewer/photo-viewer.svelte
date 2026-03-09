6a440746a374a505115604b479e63f85fde6028d
Andreas Heinz <github@a80.net>
2026-03-09 19:44:47 +0100

    fix(web): recalculate face bounding boxes (#26737)
    Co-authored-by: Daniel Dietzler <mail@ddietzler.dev>

--- web/src/lib/components/asset-viewer/photo-viewer.svelte
+++ web/src/lib/components/asset-viewer/photo-viewer.svelte
@@ -15,7 +15,7 @@
   import { SlideshowLook, SlideshowState, slideshowLookCssMapping, slideshowStore } from '$lib/stores/slideshow.store';
   import { getAssetUrl, targetImageSize as getTargetImageSize, handlePromiseError } from '$lib/utils';
   import { canCopyImageToClipboard, copyImageToClipboard } from '$lib/utils/asset-utils';
-  import { type ContentMetrics, getContentMetrics } from '$lib/utils/container-utils';
+  import { getContentMetrics, type ContentMetrics } from '$lib/utils/container-utils';
   import { handleError } from '$lib/utils/handle-error';
   import { getOcrBoundingBoxes } from '$lib/utils/ocr-utils';
   import { getBoundingBox } from '$lib/utils/people-utils';
@@ -73,6 +73,9 @@
     if (!assetViewerManager.imgRef || !visibleImageReady) {
       return { contentWidth: 0, contentHeight: 0, offsetX: 0, offsetY: 0 };
     }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    [containerWidth, containerHeight]; // trigger recomputation on resize
 
     const { contentWidth, contentHeight, offsetX, offsetY } = getContentMetrics(assetViewerManager.imgRef);
     const { currentZoom, currentPositionX, currentPositionY } = assetViewerManager.zoomState;
