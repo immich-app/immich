96a40c3f3732402f180bfdfc7ceffe7199d34078
Jason Rasmussen <jrasm91@gmail.com>
2026-03-12 14:34:22 -0400

    refactor: clean class

--- web/src/lib/components/sharedlinks-page/covers/no-cover.svelte
+++ web/src/lib/components/sharedlinks-page/covers/no-cover.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
+  import { cleanClass } from '$lib';
+
   interface Props {
     alt?: string;
     preload?: boolean;
     class?: string;
   }
 
-  let { alt = '', preload = false, class: className = '' }: Props = $props();
+  let { alt = '', preload = false, class: className }: Props = $props();
 </script>
 
 <enhanced:img
   {alt}
-  class="size-full rounded-xl object-cover aspect-square {className}"
+  class={cleanClass('size-full rounded-xl object-cover aspect-square', className)}
   data-testid="album-image"
   draggable="false"
   loading={preload ? 'eager' : 'lazy'}
