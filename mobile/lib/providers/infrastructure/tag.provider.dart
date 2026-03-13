f054a3d270333a783431b96bff483876a2bdfed0
Ben Nguyen <ben85629@gmail.com>
2026-03-13 11:35:47 -0700

    follow provider -> svc -> repo pattern for tags

--- mobile/lib/providers/infrastructure/tag.provider.dart
+++ mobile/lib/providers/infrastructure/tag.provider.dart
@@ -1,30 +1,22 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/domain/models/tag.model.dart';
-import 'package:immich_mobile/infrastructure/repositories/tags_api.repository.dart';
+import 'package:immich_mobile/domain/services/tag.service.dart';
 
 class TagNotifier extends AsyncNotifier<Set<Tag>> {
+  late final TagService service;
+
   @override
   Future<Set<Tag>> build() async {
-    final repo = ref.read(tagsApiRepositoryProvider);
-    final allTags = await repo.getAllTags();
-    if (allTags == null) {
-      return {};
-    }
-    return allTags.map((t) => Tag.fromDto(t)).toSet();
+    final service = ref.watch(tagServiceProvider);
+    return await service.getAllTags();
   }
 
   Future<void> bulkTagAssets(List<String> assetIds, List<String> tagIds) async {
-    final repo = ref.read(tagsApiRepositoryProvider);
-    await repo.bulkTagAssets(assetIds, tagIds);
+    await service.bulkTagAssets(assetIds, tagIds);
   }
 
   Future<List<Tag>> upsertTags(List<String> tags) async {
-    final repo = ref.read(tagsApiRepositoryProvider);
-    final dtos = await repo.upsertTags(tags);
-    if (dtos == null) {
-      return [];
-    }
-    final upsertedTags = dtos.map((t) => Tag.fromDto(t)).toList();
+    final upsertedTags = await service.upsertTags(tags);
 
     state = AsyncValue.data({...?state.valueOrNull, ...upsertedTags});
     return upsertedTags;
