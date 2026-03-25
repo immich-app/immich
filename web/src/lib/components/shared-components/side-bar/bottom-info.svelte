c9f985d581ca9b2c711181da19c82b36b58361a3
jochbru <j@bruijns.org>
2026-03-25 19:26:50 +0100

    refactor(side-bar): remove TwoFactorInfo component from bottom-info.svelte

--- web/src/lib/components/shared-components/side-bar/bottom-info.svelte
+++ web/src/lib/components/shared-components/side-bar/bottom-info.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
   import ServerStatus from './server-status.svelte';
   import StorageSpace from './storage-space.svelte';
-  import TwoFactorInfo from './two-factor-info.svelte';
 </script>
 
 <div class="mt-auto">
   <StorageSpace />
 </div>
-
-<TwoFactorInfo />
 
 <div class="mb-6 mt-2">
   <ServerStatus />
