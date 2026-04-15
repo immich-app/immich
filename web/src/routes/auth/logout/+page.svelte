18b3e3c7164671f6074b64bc1b8901049c47193d
Jason Rasmussen <jason@rasm.me>
2026-04-15 16:00:05 -0400

    feat: auth logout page

---
+++ web/src/routes/auth/logout/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { authManager } from '$lib/managers/auth-manager.svelte';
+  import { LoadingSpinner } from '@immich/ui';
+
+  void authManager.logout();
+</script>
+
+<div class="h-screen w-screen overflow-hidden">
+  <div class="m-auto">
+    <LoadingSpinner />
+  </div>
+</div>
