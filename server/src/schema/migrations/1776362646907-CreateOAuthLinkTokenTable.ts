81a885c31d875f03aefe16dcf813457ed74bbfe8
bo0tzz <git@bo0tzz.me>
2026-04-16 21:54:23 +0200

    fix: migration

--- server/src/schema/migrations/1776362646907-CreateOAuthLinkTokenTable.ts
+++ server/src/schema/migrations/1776362646907-CreateOAuthLinkTokenTable.ts
@@ -1,15 +1,21 @@
 import { Kysely, sql } from 'kysely';
 
 export async function up(db: Kysely<any>): Promise<void> {
-  await sql`CREATE TABLE "oauth_link_token" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "token" bytea NOT NULL, "oauthSub" character varying NOT NULL, "userEmail" character varying NOT NULL, "expiresAt" timestamp with time zone NOT NULL, "createdAt" timestamp with time zone NOT NULL DEFAULT now());`.execute(
-    db,
-  );
-  await sql`ALTER TABLE "oauth_link_token" ADD CONSTRAINT "PK_oauth_link_token_id" PRIMARY KEY ("id");`.execute(db);
-  await sql`CREATE INDEX "IDX_oauth_link_token_token" ON "oauth_link_token" ("token")`.execute(db);
+  await sql`
+    CREATE TABLE "oauth_link_token" (
+      "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+      "token" bytea NOT NULL,
+      "oauthSub" varchar NOT NULL,
+      "userEmail" varchar NOT NULL,
+      "expiresAt" timestamp with time zone NOT NULL,
+      "createdAt" timestamp with time zone NOT NULL DEFAULT now()
+    );
+  `.execute(db);
+
+  await sql`ALTER TABLE "oauth_link_token" ADD CONSTRAINT "oauth_link_token_pkey" PRIMARY KEY ("id");`.execute(db);
+  await sql`CREATE INDEX "oauth_link_token_token_idx" ON "oauth_link_token" ("token")`.execute(db);
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-  await sql`DROP INDEX "IDX_oauth_link_token_token";`.execute(db);
-  await sql`ALTER TABLE "oauth_link_token" DROP CONSTRAINT "PK_oauth_link_token_id";`.execute(db);
-  await sql`DROP TABLE "oauth_link_token";`.execute(db);
+  await sql`DROP TABLE IF EXISTS "oauth_link_token";`.execute(db);
 }
