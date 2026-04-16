f7c20cd2a0227c44c6f713775612a4ebef65210a
shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2026-04-17 04:36:03 +0530

    make build

--- mobile/lib/routing/router.dart
+++ mobile/lib/routing/router.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/domain/models/album/album.model.dart';
