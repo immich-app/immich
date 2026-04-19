05e532d77db23f965936f47f518f12d62ba50b57
jochbru <j@bruijns.org>
2026-04-19 13:31:39 +0200

    chore(i18n): update storage calculation messages and remove two-factor authentication components
    - Added new storage calculation notice messages in multiple languages (da, de, dk, en, fr, it, nl, pl, pt, sv).
    - Introduced a new StorageCalculationInfo component to display storage updates.
    - Removed two-factor authentication related components and modals to streamline the sidebar.

---
+++ web/src/lib/components/shared-components/side-bar/storage-calculation-info.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import StorageCalculationModal from '$lib/modals/StorageCalculationModal.svelte';
+  import { Icon, modalManager } from '@immich/ui';
+  import { mdiHarddisk, mdiInformationOutline } from '@mdi/js';
+  import { t } from 'svelte-i18n';
+
+  const openStorageCalculationModal = async () => {
+    await modalManager.show(StorageCalculationModal);
+  };
+</script>
+
+<div class="ps-4 text-sm">
+  <button
+    type="button"
+    onclick={openStorageCalculationModal}
+    class="p-2 flex justify-between place-items-center place-content-center border border-immich-primary/20 dark:border-immich-dark-primary/10 mt-2 rounded-lg shadow-md dark:bg-immich-dark-primary/10 min-w-52 w-full"
+  >
+    <div class="flex justify-between w-full place-items-center place-content-center">
+      <div class="flex place-items-center place-content-center gap-1">
+        <Icon icon={mdiHarddisk} class="text-primary" size="24" />
+        <p class="flex text-primary font-medium">
+          {$t('storage_calculation_notice_button_text')}
+        </p>
+      </div>
+
+      <div>
+        <Icon icon={mdiInformationOutline} class="hidden sidebar:flex text-primary font-medium" size="18" />
+      </div>
+    </div>
+  </button>
+</div>
---
+++ web/src/lib/modals/StorageCalculationModal.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { Button, Heading, Icon, Modal, ModalBody } from '@immich/ui';
+  import { mdiHarddisk } from '@mdi/js';
+  import { t } from 'svelte-i18n';
+
+  interface Props {
+    onClose: () => void;
+  }
+
+  let { onClose }: Props = $props();
+</script>
+
+<Modal title=" " {onClose} size="medium">
+  <ModalBody>
+    <section class="text-center">
+      <div class="mb-4 flex justify-center">
+        <Icon icon={mdiHarddisk} class="text-primary" size="64" />
+      </div>
+
+      <Heading color="primary" tag="h1" class="mb-4 text-3xl font-bold tracking-wider">
+        {$t('storage_calculation_notice_title')}
+      </Heading>
+
+      <div class="my-6 text-gray-800 dark:text-white">
+        <p>
+          {$t('storage_calculation_notice_info')}
+        </p>
+      </div>
+
+      <Button shape="round" fullWidth color="secondary" variant="ghost" onclick={onClose}>
+        {$t('close')}
+      </Button>
+    </section>
+  </ModalBody>
+</Modal>
