7adc568575cd8dd4fcd300f417118676af11b3d0
Peter Ombodi <p.ombodi@gmail.com>
2026-04-20 18:20:04 +0300

    refactor(mobile): simplify code

--- mobile/lib/providers/view_intent/view_intent_handler.provider.dart
+++ mobile/lib/providers/view_intent/view_intent_handler.provider.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/platform/view_intent_api.g.dart';
-import 'package:immich_mobile/providers/view_intent/view_intent_handler_factory.dart';
+import 'package:immich_mobile/providers/view_intent/view_intent_handler_android.dart';
+import 'package:immich_mobile/providers/view_intent/view_intent_handler_stub.dart';
 
 abstract class ViewIntentHandler {
   void init();
@@ -13,5 +15,9 @@ abstract class ViewIntentHandler {
 }
 
 final viewIntentHandlerProvider = Provider<ViewIntentHandler>((ref) {
-  return createViewIntentHandler(ref);
+  if (Platform.isAndroid) {
+    return AndroidViewIntentHandler(ref);
+  }
+
+  return const StubViewIntentHandler();
 });
--- mobile/lib/providers/view_intent/view_intent_handler_factory_stub.dart
+++
@@ -1,6 +0,0 @@
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:immich_mobile/providers/view_intent/view_intent_handler_stub.dart';
-import 'package:immich_mobile/providers/view_intent/view_intent_handler.provider.dart';
-
-ViewIntentHandler createViewIntentHandler(Ref ref) => const StubViewIntentHandler();
-
