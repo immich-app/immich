02e6af4417d49cda567183f493c428dfb7ddb8ef
mertalev <101130780+mertalev@users.noreply.github.com>
2026-04-22 16:08:59 -0400

    duplicate constant

--- server/src/utils/database.ts
+++ server/src/utils/database.ts
@@ -526,5 +526,3 @@ export const updateLockedColumns = <T extends Record<string, unknown> & { locked
   exif.lockedProperties = lockableProperties.filter((property) => property in exif);
   return exif;
 };
-
-export const dummy = sql`(select 1)`.as('dummy');
