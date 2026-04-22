9b5a802ff9ae1f37aa62f11699f722b86fd333f5
Alex <alex.tran1502@gmail.com>
2026-04-22 10:51:34 -0500

    chore: update app share group

--- mobile/ios/WidgetExtension/ImmichAPI.swift
+++ mobile/ios/WidgetExtension/ImmichAPI.swift
@@ -2,7 +2,7 @@ import Foundation
 import SwiftUI
 import WidgetKit
 
-let IMMICH_SHARE_GROUP = "group.app.immich.share"
+let IMMICH_SHARE_GROUP = "group.futo.immich.share"
 
 enum WidgetError: Error, Codable {
   case noLogin
