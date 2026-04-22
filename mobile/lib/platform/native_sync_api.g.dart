5d3387040308d1fd4758bf9bad2694b23a575ba7
Peter Ombodi <p.ombodi@gmail.com>
2026-04-22 13:51:19 +0300

    refactor(mobile): resolve merge conflicts

--- mobile/lib/platform/native_sync_api.g.dart
+++ mobile/lib/platform/native_sync_api.g.dart
@@ -621,6 +621,25 @@ class NativeSyncApi {
     return (pigeonVar_replyValue! as List<Object?>).cast<HashResult>();
   }
 
+  Future<List<HashResult>> hashFiles(List<String> paths) async {
+    final pigeonVar_channelName =
+        'dev.flutter.pigeon.immich_mobile.NativeSyncApi.hashFiles$pigeonVar_messageChannelSuffix';
+    final pigeonVar_channel = BasicMessageChannel<Object?>(
+      pigeonVar_channelName,
+      pigeonChannelCodec,
+      binaryMessenger: pigeonVar_binaryMessenger,
+    );
+    final Future<Object?> pigeonVar_sendFuture = pigeonVar_channel.send(<Object?>[paths]);
+    final pigeonVar_replyList = await pigeonVar_sendFuture as List<Object?>?;
+
+    final Object? pigeonVar_replyValue = _extractReplyValueOrThrow(
+      pigeonVar_replyList,
+      pigeonVar_channelName,
+      isNullValid: false,
+    );
+    return (pigeonVar_replyValue! as List<Object?>).cast<HashResult>();
+  }
+
   Future<void> cancelHashing() async {
     final pigeonVar_channelName =
         'dev.flutter.pigeon.immich_mobile.NativeSyncApi.cancelHashing$pigeonVar_messageChannelSuffix';
