4d018f95e9f7d5b57e941dd1e1ba1f2ff9d1f1ba
Jason Rasmussen <jason@rasm.me>
2026-04-28 13:17:59 -0400

    chore: clean up header

--- server/src/repositories/config.repository.ts
+++ server/src/repositories/config.repository.ts
@@ -301,9 +301,7 @@ const getEnv = (): EnvData => {
           mount: true,
           generateId: true,
           setup: (cls, req: Request, res: Response) => {
-            const headerValues = req.headers[ImmichHeader.CorrelationId];
-            const headerValue = Array.isArray(headerValues) ? headerValues[0] : headerValues;
-            const cid = headerValue || cls.get(CLS_ID);
+            const cid = req.header(ImmichHeader.CorrelationId) || cls.get(CLS_ID);
             cls.set(CLS_ID, cid);
             res.header(ImmichHeader.CorrelationId, cid);
           },
