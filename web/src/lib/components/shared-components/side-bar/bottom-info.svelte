be9c5a385256efe2c16b25de052cd8264b28224c
jochbru <j@bruijns.org>
2026-05-03 13:15:25 +0200

    Remove StorageCalculationInfo component from bottom-info.svelte to streamline sidebar layout.

--- web/src/lib/components/shared-components/side-bar/bottom-info.svelte
+++ web/src/lib/components/shared-components/side-bar/bottom-info.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
   import ServerStatus from './server-status.svelte';
   import StorageSpace from './storage-space.svelte';
-  import StorageCalculationInfo from './storage-calculation-info.svelte';
 </script>
 
 <div class="mt-auto">
   <StorageSpace />
-  <StorageCalculationInfo />
 </div>
 
 <div class="mb-6 mt-2">
