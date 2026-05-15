6a48206e35b3c7800a00b4db75179fc58039f1ba
mertalev <101130780+mertalev@users.noreply.github.com>
2026-05-15 15:15:23 -0400

    separate group ids

--- mobile/lib/services/widget.service.dart
+++ mobile/lib/services/widget.service.dart
@@ -12,7 +12,7 @@ class WidgetService {
   const WidgetService(this._repository);
 
   Future<void> writeCredentials(String serverURL, String sessionKey, String? customHeaders) async {
-    await _repository.setAppGroupId(appShareGroupId);
+    await _repository.setAppGroupId();
     await _repository.saveData(kWidgetServerEndpoint, serverURL);
     await _repository.saveData(kWidgetAuthToken, sessionKey);
 
@@ -25,7 +25,7 @@ class WidgetService {
   }
 
   Future<void> clearCredentials() async {
-    await _repository.setAppGroupId(appShareGroupId);
+    await _repository.setAppGroupId();
     await _repository.saveData(kWidgetServerEndpoint, "");
     await _repository.saveData(kWidgetAuthToken, "");
     await _repository.saveData(kWidgetCustomHeaders, "");
