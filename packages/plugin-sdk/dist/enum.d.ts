ef752422e206284782f0e692a1f18e229925ddb7
midzelis <min123@gmail.com>
2026-05-18 01:04:28 +0000

    chore: merge main into feat/hero_view_transitions
    Change-Id: I4b00ee8b7c4201c926bf53b12900a4ea6a6a6964

---
+++ packages/plugin-sdk/dist/enum.d.ts
@@ -0,0 +1,25 @@
+export declare enum WorkflowTrigger {
+    AssetCreate = "AssetCreate",
+    PersonRecognized = "PersonRecognized"
+}
+export declare enum WorkflowType {
+    AssetV1 = "AssetV1",
+    AssetPersonV1 = "AssetPersonV1"
+}
+export declare enum AssetType {
+    Image = "IMAGE",
+    Video = "VIDEO",
+    Audio = "AUDIO",
+    Other = "OTHER"
+}
+export declare enum AssetStatus {
+    Active = "active",
+    Trashed = "trashed",
+    Deleted = "deleted"
+}
+export declare enum AssetVisibility {
+    Archive = "archive",
+    Timeline = "timeline",
+    Hidden = "hidden",
+    Locked = "locked"
+}
