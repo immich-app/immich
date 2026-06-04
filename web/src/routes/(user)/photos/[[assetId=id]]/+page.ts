209dcb38c52137de2aaef857f8a650d2514a27df
Ben Beckford <ben@benjaminbeckford.com>
2026-06-04 13:38:48 -0700

    fix(web): memories resetting

--- web/src/routes/(user)/photos/[[assetId=id]]/+page.ts
+++ web/src/routes/(user)/photos/[[assetId=id]]/+page.ts
@@ -1,17 +1,10 @@
-import { DateTime } from 'luxon';
-import { memoryManager } from '$lib/managers/memory-manager.svelte';
 import { authenticate } from '$lib/utils/auth';
-import { asLocalTimeISO } from '$lib/utils/date-time';
 import { getFormatter } from '$lib/utils/i18n';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ url }) => {
   await authenticate(url);
   const $t = await getFormatter();
-
-  if (memoryManager.filters === undefined || memoryManager.filters.$for !== asLocalTimeISO(DateTime.now())) {
-    memoryManager.filters = { $for: asLocalTimeISO(DateTime.now()) };
-  }
 
   return {
     meta: {
