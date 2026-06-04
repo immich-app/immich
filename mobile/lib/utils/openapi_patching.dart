3f07230ecfee10fe4b5ce1a1c2b159c1c7e676d6
Dell <abhijeetbonde19@gmail.com>
2026-06-03 23:25:25 -0400

    Resolve errors

--- mobile/lib/utils/openapi_patching.dart
+++ mobile/lib/utils/openapi_patching.dart
@@ -40,6 +40,14 @@ final Map<String, Map<String, Object?>> openApiPatches = {
   'SyncAssetV1': {'isEdited': false},
   'ServerFeaturesDto': {'ocr': false, 'realtimeTranscoding': false},
   'MemoriesResponse': {'duration': 5},
+  'UserUploadStatsResponseDto': {
+    'userId': '',
+    'from': '',
+    'to': '',
+    'series': [],
+    'totalCount': 0,
+},
+  
 };
 
 void upgradeDto(dynamic value, String targetType) {
