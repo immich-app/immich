a83ad3ebcd5218167f75ade405986666f145a76f
timonrieger <mail@timonrieger.de>
2026-06-09 21:28:47 +0200

    drop redundant migration again

--- server/src/schema/migrations/1780918197000-ConvertZeroRatingToNull.ts
+++
@@ -1,9 +0,0 @@
-import { Kysely, sql } from 'kysely';
-
-export async function up(db: Kysely<any>): Promise<void> {
-  await sql`UPDATE "asset_exif" SET "rating" = NULL WHERE "rating" = 0;`.execute(db);
-}
-
-export async function down(): Promise<void> {
-  // not supported
-}
