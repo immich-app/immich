a364b56b1c296a1ef747441f5533373b3aabb294
Jeevan Mohan Pawar <35501212+jeevan6996@users.noreply.github.com>
2026-06-17 14:54:20 +0100

    fix(server): skip existing users when sharing albums (#28884)
    Co-authored-by: Daniel Dietzler <mail@ddietzler.dev>

--- server/src/services/album.service.ts
+++ server/src/services/album.service.ts
@@ -290,7 +290,7 @@ export class AlbumService extends BaseService {
 
       const exists = album.albumUsers.find(({ user: { id } }) => id === userId);
       if (exists) {
-        throw new BadRequestException('User already added');
+        continue;
       }
 
       const user = await this.userRepository.get(userId, {});
