5552054c63b19479542ab30878872c194cf5f7cc
shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2026-06-26 03:17:22 +0530

    chore: cancel native operations - lifecycle change

--- mobile/lib/providers/app_life_cycle.provider.dart
+++ mobile/lib/providers/app_life_cycle.provider.dart
@@ -101,8 +101,19 @@ class AppLifeCycleNotifier extends StateNotifier<AppLifeCycleEnum> {
     }
   }
 
+  Future<void> _cancelSync() async {
+    final backgroundManager = _ref.read(backgroundSyncProvider);
+    final nativeSync = _ref.read(nativeSyncApiProvider);
+    await Future.wait([
+      nativeSync.cancelSync(),
+      nativeSync.cancelHashing(),
+      backgroundManager.cancel(),
+    ]).timeout(const Duration(seconds: 5), onTimeout: () => const <void>[]);
+  }
+
   Future<void> _handleBetaTimelineResume() async {
     unawaited(_ref.read(backgroundWorkerLockServiceProvider).lock());
+    await _cancelSync();
 
     // Give isolates time to complete any ongoing database transactions
     await Future.delayed(const Duration(milliseconds: 500));
@@ -196,6 +207,7 @@ class AppLifeCycleNotifier extends StateNotifier<AppLifeCycleEnum> {
   Future<void> _performPause() {
     if (_ref.read(authProvider).isAuthenticated) {
       _ref.read(driftBackupProvider.notifier).stopForegroundBackup();
+      unawaited(_cancelSync());
 
       _ref.read(websocketProvider.notifier).disconnect();
     }
