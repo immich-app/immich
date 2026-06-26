296704972d287c073dfafc82a5d69844797e3e88
Divyanshu Jha <divyanshuj2006@gmail.com>
2026-06-26 21:12:20 +0530

    refactor(server): update ImmichTags types and simplify string casting per review

--- server/src/services/metadata.service.ts
+++ server/src/services/metadata.service.ts
@@ -855,11 +855,11 @@ export class MetadataService extends BaseService {
     const adjustedRegionList = regionInfo.RegionList.map((region) => {
       let { X, Y, W, H } = region.Area;
 
-      // EXIF floats with >16 decimals are serialized as strings. Ensure they are numbers and limited to 16 decimal places.
-      X = Number(Number(X).toFixed(16));
-      Y = Number(Number(Y).toFixed(16));
-      W = Number(Number(W).toFixed(16));
-      H = Number(Number(H).toFixed(16));
+      // EXIF floats with >16 decimals are serialized as strings. Ensure they are numbers.
+      X = Number(X);
+      Y = Number(Y);
+      W = Number(W);
+      H = Number(H);
 
       switch (orientation) {
         case ExifOrientation.MirrorHorizontal: {
