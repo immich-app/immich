67c81a80791b2a57542e425cd2151261d7fcb4d3
Mosh <mtushar091@gmail.com>
2026-07-22 00:37:04 +0600

    Lint fix

--- web/src/lib/workers/hash-file.ts
+++ web/src/lib/workers/hash-file.ts
@@ -7,7 +7,7 @@ const HASH_CHUNK_SIZE = 5 * 1024 * 1024;
 function isWebAssemblySupported(): boolean {
   try {
     if (typeof WebAssembly === 'object' && typeof WebAssembly.instantiate === 'function') {
-      const module = new WebAssembly.Module(Uint8Array.of(0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00));
+      const module = new WebAssembly.Module(Uint8Array.of(0x00, 0x61, 0x73, 0x6D, 0x01, 0x00, 0x00, 0x00));
       if (module instanceof WebAssembly.Module) {
         return new WebAssembly.Instance(module) instanceof WebAssembly.Instance;
       }
