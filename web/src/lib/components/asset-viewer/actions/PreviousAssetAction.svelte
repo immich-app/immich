d5304857a0de54e55978a9ebcf59614e337243c2
noboike <yazeedalhimali@gmail.com>
2026-07-23 04:56:36 +0300

    fix(web): mirror asset viewer navigation icons in RTL

--- web/src/lib/components/asset-viewer/actions/PreviousAssetAction.svelte
+++ web/src/lib/components/asset-viewer/actions/PreviousAssetAction.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import { shortcuts } from '$lib/actions/shortcut';
   import { Icon } from '@immich/ui';
-  import { mdiChevronLeft } from '@mdi/js';
+  import { mdiChevronLeft, mdiChevronRight } from '@mdi/js';
   import { t } from 'svelte-i18n';
   import NavigationArea from '../NavigationArea.svelte';
+  import { languageManager } from '$lib/managers/language-manager.svelte';
 
   interface Props {
     onPreviousAsset: () => void;
@@ -20,5 +21,5 @@
 />
 
 <NavigationArea onClick={onPreviousAsset} label={$t('view_previous_asset')}>
-  <Icon icon={mdiChevronLeft} size="36" aria-hidden />
+  <Icon icon={languageManager.rtl ? mdiChevronRight : mdiChevronLeft} size="36" aria-hidden />
 </NavigationArea>
