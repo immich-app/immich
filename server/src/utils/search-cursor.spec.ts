2f83615195292127bb3fd84f184dab849d51ebcf
timonrieger <mail@timonrieger.de>
2026-07-23 16:08:27 +0200

    cursor en-/decoder

---
+++ server/src/utils/search-cursor.spec.ts
@@ -0,0 +1,34 @@
+import { BadRequestException } from '@nestjs/common';
+import { decodeSearchCursor, encodeSearchCursor } from 'src/utils/search-cursor';
+import { describe, expect, it } from 'vitest';
+
+describe('encodeSearchCursor', () => {
+  it('should produce an opaque base64url string', () => {
+    const cursor = encodeSearchCursor(250);
+    expect(cursor).toMatch(/^[\w-]+$/);
+  });
+
+  it('should round-trip an offset', () => {
+    for (const offset of [0, 1, 250, 1_000_000]) {
+      expect(decodeSearchCursor(encodeSearchCursor(offset))).toEqual({ offset });
+    }
+  });
+});
+
+describe('decodeSearchCursor', () => {
+  const encode = (value: unknown) => Buffer.from(JSON.stringify(value)).toString('base64url');
+
+  it.each([
+    ['empty string', ''],
+    ['garbage that is not base64url', '!!!not-base64!!!'],
+    ['base64 of non-JSON', Buffer.from('not json').toString('base64url')],
+    ['JSON without an offset', encode({})],
+    ['JSON with a non-object payload', encode(42)],
+    ['null payload', encode(null)],
+    ['negative offset', encode({ o: -1 })],
+    ['fractional offset', encode({ o: 1.5 })],
+    ['string offset', encode({ o: '5' })],
+  ])('should reject %s', (_, cursor) => {
+    expect(() => decodeSearchCursor(cursor)).toThrowError(new BadRequestException('Invalid cursor'));
+  });
+});
