c008f8e1cb20ec0a985d55e1dad3f2859970fc55
Adam Gastineau <adam@agg.im>
2026-07-24 11:24:07 -0700

    Version openapi-generator-cli with mise

--- open-api/bin/generate-dart-sdk.sh
+++ open-api/bin/generate-dart-sdk.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-OPENAPI_GENERATOR_VERSION=2.40.1
-
 set -euo pipefail
 
 # usage: ./bin/generate-dart-sdk.sh
@@ -8,13 +6,14 @@ set -euo pipefail
 TEMPLATE_DIR=$(mktemp -d)
 trap 'rm -rf "$TEMPLATE_DIR"' EXIT
 
-pnpm dlx --allow-build="" @openapitools/openapi-generator-cli@$OPENAPI_GENERATOR_VERSION author template -g dart -o "$TEMPLATE_DIR"
+# Installed via mise
+openapi-generator-cli author template -g dart -o "$TEMPLATE_DIR"
 patch --no-backup-if-mismatch -u "$TEMPLATE_DIR/api.mustache" <./templates/mobile/api.mustache.patch
 patch --no-backup-if-mismatch -u "$TEMPLATE_DIR/serialization/native/native_class.mustache" <./templates/mobile/serialization/native/native_class.mustache.patch
 
 rm -rf ../mobile/openapi
 
-pnpm dlx --allow-build="" @openapitools/openapi-generator-cli@$OPENAPI_GENERATOR_VERSION generate -g dart -i ./immich-openapi-specs.json -o ../mobile/openapi -t "$TEMPLATE_DIR" --additional-properties=useOptional=true
+openapi-generator-cli generate -g dart -i ./immich-openapi-specs.json -o ../mobile/openapi -t "$TEMPLATE_DIR" --additional-properties=useOptional=true
 
 # Post generate patches
 patch --no-backup-if-mismatch -u ../mobile/openapi/lib/api_client.dart <./patch/api_client.dart.patch
