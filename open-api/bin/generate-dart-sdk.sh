6c01b4f5d8ed632efa901b843ef51096adbbefb4
Adam Gastineau <adam@agg.im>
2026-07-29 13:33:30 -0700

    chore(mobile): remove OpenAPI generated code (#30287)
    * chore(mobile): remove OpenAPI generated code
    
    * Don't reference generated files for CI checks

--- open-api/bin/generate-dart-sdk.sh
+++ open-api/bin/generate-dart-sdk.sh
@@ -11,15 +11,15 @@ openapi-generator-cli author template -g dart -o "$TEMPLATE_DIR"
 patch --no-backup-if-mismatch -u "$TEMPLATE_DIR/api.mustache" <./templates/mobile/api.mustache.patch
 patch --no-backup-if-mismatch -u "$TEMPLATE_DIR/serialization/native/native_class.mustache" <./templates/mobile/serialization/native/native_class.mustache.patch
 
-rm -rf ../mobile/openapi
+rm -rf ../mobile/generated/openapi
 
-openapi-generator-cli generate -g dart -i ./immich-openapi-specs.json -o ../mobile/openapi -t "$TEMPLATE_DIR" --additional-properties=useOptional=true
+openapi-generator-cli generate -g dart -i ./immich-openapi-specs.json -o ../mobile/generated/openapi -t "$TEMPLATE_DIR" --additional-properties=useOptional=true
 
 # Post generate patches
-patch --no-backup-if-mismatch -u ../mobile/openapi/lib/api_client.dart <./patch/api_client.dart.patch
-patch --no-backup-if-mismatch -u ../mobile/openapi/lib/api.dart <./patch/api.dart.patch
-patch --no-backup-if-mismatch -u ../mobile/openapi/pubspec.yaml <./patch/pubspec_immich_mobile.yaml.patch
-patch --no-backup-if-mismatch -u ../mobile/openapi/lib/model/asset_edit_action_item_dto.dart <./patch/asset_edit_action_item_dto.dart.patch
+patch --no-backup-if-mismatch -u ../mobile/generated/openapi/lib/api_client.dart <./patch/api_client.dart.patch
+patch --no-backup-if-mismatch -u ../mobile/generated/openapi/lib/api.dart <./patch/api.dart.patch
+patch --no-backup-if-mismatch -u ../mobile/generated/openapi/pubspec.yaml <./patch/pubspec_immich_mobile.yaml.patch
+patch --no-backup-if-mismatch -u ../mobile/generated/openapi/lib/model/asset_edit_action_item_dto.dart <./patch/asset_edit_action_item_dto.dart.patch
 # Don't include analysis_options.yaml for the generated openapi files
-# so that language servers can properly exclude the mobile/openapi directory
-rm ../mobile/openapi/analysis_options.yaml
+# so that language servers can properly exclude the mobile/generated/openapi directory
+rm ../mobile/generated/openapi/analysis_options.yaml
