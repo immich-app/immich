18d4a796bb3728b6102a3cbe970f78b33b85ca94
Adam Gastineau <adam@agg.im>
2026-08-05 12:00:49 -0700

    chore(mobile): simple freezed implementation on some models (#30168)
    * chore(mobile): example freezed implementation on some models
    
    * Don't commit Freezed generated code
    
    * Freezed easy pass
    
    * Formatting fix
    
    * Generate Flutter debugging info

--- mobile/lib/domain/models/stack.model.dart
+++ mobile/lib/domain/models/stack.model.dart
@@ -1,57 +1,18 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'stack.model.freezed.dart';
+
 // Model for a stack stored in the server
-class Stack {
-  final String id;
-  final DateTime createdAt;
-  final DateTime updatedAt;
-  final String ownerId;
-  final String primaryAssetId;
-
-  const Stack({
-    required this.id,
-    required this.createdAt,
-    required this.updatedAt,
-    required this.ownerId,
-    required this.primaryAssetId,
-  });
-
-  Stack copyWith({String? id, DateTime? createdAt, DateTime? updatedAt, String? ownerId, String? primaryAssetId}) {
-    return Stack(
-      id: id ?? this.id,
-      createdAt: createdAt ?? this.createdAt,
-      updatedAt: updatedAt ?? this.updatedAt,
-      ownerId: ownerId ?? this.ownerId,
-      primaryAssetId: primaryAssetId ?? this.primaryAssetId,
-    );
-  }
-
-  @override
-  String toString() {
-    return '''Stack {
-    id: $id,
-    createdAt: $createdAt,
-    updatedAt: $updatedAt,
-    ownerId: $ownerId,
-    primaryAssetId: $primaryAssetId
-}''';
-  }
-
-  @override
-  bool operator ==(covariant Stack other) {
-    if (identical(this, other)) {
-      return true;
-    }
-
-    return other.id == id &&
-        other.createdAt == createdAt &&
-        other.updatedAt == updatedAt &&
-        other.ownerId == ownerId &&
-        other.primaryAssetId == primaryAssetId;
-  }
-
-  @override
-  int get hashCode {
-    return id.hashCode ^ createdAt.hashCode ^ updatedAt.hashCode ^ ownerId.hashCode ^ primaryAssetId.hashCode;
-  }
+@freezed
+abstract class Stack with _$Stack {
+  const factory Stack({
+    required String id,
+    required DateTime createdAt,
+    required DateTime updatedAt,
+    required String ownerId,
+    required String primaryAssetId,
+  }) = _Stack;
 }
 
 class StackResponse {
