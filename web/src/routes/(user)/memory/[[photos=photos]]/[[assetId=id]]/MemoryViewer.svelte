55860369b9d9df29eaacce8cfc08cca86d62a6a4
djadji-gueye <sennegueye@gmail.com>
2026-08-08 15:34:33 +0000

    fix(web): don't reset memory viewer to first asset on delete

--- web/src/routes/(user)/memory/[[photos=photos]]/[[assetId=id]]/MemoryViewer.svelte
+++ web/src/routes/(user)/memory/[[photos=photos]]/[[assetId=id]]/MemoryViewer.svelte
@@ -185,9 +185,6 @@
     if (!current) {
       return;
     }
-
-    // Capture where to land next *before* deleting, since the asset being
-    // removed will no longer resolve via getMemoryAsset() afterwards.
     const nextTarget = current.next?.asset ?? current.previous?.asset;
 
     await memoryManager.deleteAssetFromMemory(current.asset.id);
@@ -195,7 +192,6 @@
     if (nextTarget) {
       await handleNavigate(nextTarget);
     } else {
-      // No assets left to navigate to (memory was fully emptied and removed).
       init(page);
     }
   };
