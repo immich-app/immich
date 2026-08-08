e74d8cdcfa72f15a95d9351184a9d420da2a9c08
shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2026-08-08 16:23:30 +0530

    chore: migrate mobile translations accessor (1)

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -16,7 +16,6 @@ import 'package:immich_mobile/constants/constants.dart';
 import 'package:immich_mobile/constants/locales.dart';
 import 'package:immich_mobile/domain/services/background_worker.service.dart';
 import 'package:immich_mobile/extensions/build_context_extensions.dart';
-import 'package:immich_mobile/extensions/translate_extensions.dart';
 import 'package:immich_mobile/generated/codegen_loader.g.dart';
 import 'package:immich_mobile/generated/translations.g.dart';
 import 'package:immich_mobile/infrastructure/repositories/network.repository.dart';
@@ -273,10 +272,7 @@ class ImmichAppState extends ConsumerState<ImmichApp> with WidgetsBindingObserve
         darkTheme: getThemeData(colorScheme: immichTheme.dark, locale: context.locale),
         theme: getThemeData(colorScheme: immichTheme.light, locale: context.locale),
         builder: (context, child) => ImmichTranslationProvider(
-          translations: ImmichTranslations(
-            submit: "submit".t(context: context),
-            password: "password".t(context: context),
-          ),
+          translations: ImmichTranslations(submit: context.t.submit, password: context.t.password),
           child: ImmichThemeProvider(colorScheme: context.colorScheme, child: child!),
         ),
         routerConfig: router.config(
