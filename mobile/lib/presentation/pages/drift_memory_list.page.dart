ac92b959dfda4bde159357050c6487b534e9dc46
Ben Beckford <ben@benjaminbeckford.com>
2026-08-12 00:17:30 -0700

    fix(mobile): build errors

--- mobile/lib/presentation/pages/drift_memory_list.page.dart
+++ mobile/lib/presentation/pages/drift_memory_list.page.dart
@@ -4,6 +4,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:immich_mobile/generated/translations.g.dart';
 import 'package:immich_mobile/presentation/pages/drift_memory.page.dart';
 import 'package:immich_mobile/presentation/widgets/images/thumbnail.widget.dart';
 import 'package:immich_mobile/providers/infrastructure/memory.provider.dart';
