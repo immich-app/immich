ccb95cac5a4987e5986fe9238172cb7adf30ca1a
Daniel Dietzler <mail@ddietzler.dev>
2026-08-17 13:26:56 +0200

    fix: e2e

--- e2e/src/specs/server/api/asset.e2e-spec.ts
+++ e2e/src/specs/server/api/asset.e2e-spec.ts
@@ -130,7 +130,7 @@ describe('/asset', () => {
     });
     await utils.createFace({
       assetId: user1Assets[0].id,
-      personId: person1.id,
+      personGroupId: person1.id,
     });
   };
   beforeAll(setupTests, 30_000);
