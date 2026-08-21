37e033a09d212fca3d273990f138112abb9e0837
Adam Gastineau <adam@agg.im>
2026-08-21 10:07:03 -0700

    chore(mobile): migrate remaining simple classes to Freezed (#30907)

--- mobile/lib/presentation/actions/similar_photos.action.dart
+++ mobile/lib/presentation/actions/similar_photos.action.dart
@@ -28,11 +28,11 @@ class SimilarPhotosAction extends ActionBuilder {
         .new(
           assetId: assetId,
           people: {},
-          location: .new(),
-          camera: .new(),
-          date: .new(),
+          location: const .new(),
+          camera: const .new(),
+          date: const .new(),
           display: const .new(isNotInAlbum: false, isArchive: false, isFavorite: false),
-          rating: .new(),
+          rating: const .new(),
           mediaType: .other,
         ),
       );
