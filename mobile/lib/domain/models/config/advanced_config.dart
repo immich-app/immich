2d1e34dd609dea747efad611770532566121f91b
shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2026-08-22 14:04:00 +0530

    advance config to freezed

--- mobile/lib/domain/models/config/advanced_config.dart
+++ mobile/lib/domain/models/config/advanced_config.dart
@@ -1,33 +1,12 @@
-class AdvancedConfig {
-  final bool troubleshooting;
-  final bool enableHapticFeedback;
-  final bool readonlyModeEnabled;
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  const AdvancedConfig({
-    this.troubleshooting = false,
-    this.enableHapticFeedback = true,
-    this.readonlyModeEnabled = false,
-  });
+part 'advanced_config.freezed.dart';
 
-  AdvancedConfig copyWith({bool? troubleshooting, bool? enableHapticFeedback, bool? readonlyModeEnabled}) =>
-      AdvancedConfig(
-        troubleshooting: troubleshooting ?? this.troubleshooting,
-        enableHapticFeedback: enableHapticFeedback ?? this.enableHapticFeedback,
-        readonlyModeEnabled: readonlyModeEnabled ?? this.readonlyModeEnabled,
-      );
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      (other is AdvancedConfig &&
-          other.troubleshooting == troubleshooting &&
-          other.enableHapticFeedback == enableHapticFeedback &&
-          other.readonlyModeEnabled == readonlyModeEnabled);
-
-  @override
-  int get hashCode => Object.hash(troubleshooting, enableHapticFeedback, readonlyModeEnabled);
-
-  @override
-  String toString() =>
-      'AdvancedConfig(troubleshooting: $troubleshooting, enableHapticFeedback: $enableHapticFeedback, readonlyModeEnabled: $readonlyModeEnabled)';
+@freezed
+abstract class AdvancedConfig with _$AdvancedConfig {
+  const factory AdvancedConfig({
+    @Default(false) bool troubleshooting,
+    @Default(true) bool enableHapticFeedback,
+    @Default(false) bool readonlyModeEnabled,
+  }) = _AdvancedConfig;
 }
