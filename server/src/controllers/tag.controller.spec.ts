1b3aa9cd512a01a44ffe807ac509c456310c7750
Jordan Brock <115588091+jorbrock@users.noreply.github.com>
2026-08-24 08:57:32 -0700

    fix(server): correct tag create operations (#30877)

--- server/src/controllers/tag.controller.spec.ts
+++ server/src/controllers/tag.controller.spec.ts
@@ -24,6 +24,41 @@ describe(TagController.name, () => {
       await request(ctx.getHttpServer()).post(`/tags`).send({ name: 'tag', parentId: null });
       expect(service.create).toHaveBeenCalledWith(undefined, expect.objectContaining({ parentId: null }));
     });
+    it('should require a valid parentId', async () => {
+      const { status, body } = await request(ctx.getHttpServer())
+        .post(`/tags`)
+        .send({ name: 'tag', parentId: 'invalid-id' });
+      expect(status).toBe(400);
+      expect(body).toEqual(errorDto.validationError([{ path: ['parentId'], message: 'Invalid UUID' }]));
+    });
+    it('should throw an error if a slash is in tag name', async () => {
+      const { status, body } = await request(ctx.getHttpServer())
+        .post(`/tags`)
+        .send({ name: 'tagA/tagB', parentId: factory.uuid() });
+      expect(status).toBe(400);
+      expect(body).toEqual(
+        errorDto.validationError([{ path: ['name'], message: 'Tag name cannot contain slash characters ("/")' }]),
+      );
+    });
+    it('should accept a null color', async () => {
+      const { status } = await request(ctx.getHttpServer()).post(`/tags`).send({ name: 'tagA', color: null });
+      expect(status).toBe(201);
+    });
+    it('should require a valid color', async () => {
+      const { status, body } = await request(ctx.getHttpServer())
+        .post(`/tags`)
+        .send({ name: 'tagA', color: 'invalid-color' });
+      expect(status).toBe(400);
+      expect(body).toEqual(
+        errorDto.validationError([
+          {
+            path: ['color'],
+            message:
+              'Invalid string: must match pattern /^#?([0-9A-Fa-f]{3}|[0-9A-Fa-f]{4}|[0-9A-Fa-f]{6}|[0-9A-Fa-f]{8})$/',
+          },
+        ]),
+      );
+    });
   });
 
   describe('GET /tags/:id', () => {
