351be9576996230ed8390abfc454d1fc81a78b62
Ben Beckford <ben@benjaminbeckford.com>
2026-08-25 01:16:33 +0100

    feat: memories view (#28675)
    * wip web memories view
    
    * paginate searchMemories
    
    * improve memories ui
    
    * feat(mobile): memories view
    
    * improve memories web ui
    
    * fix(web): avoid unnecessary memory refreshes
    
    * refactor(web): move timline memory manager filter
    
    * fix(web): memories infinitely loading on 0 results
    
    * fix(web): memories resetting
    
    * feat(web): animated memory previews
    
    * feat(web): memory page dark styling
    
    * chore(mobile): fix failing memory lane tests
    
    * chore(mobile): remove old import
    
    * chore(e2e): fix failing memory lane tests
    
    * chore(server): revert searchMemories changes
    
    * chore(server): lint
    
    * chore(mobile): fix bad merge
    
    * chore(e2e): fix memory ui tests
    
    * chore(web): rename /memory to /memories
    
    * chore(mobile): linting
    
    * chore(web): linting
    
    * fix(web): prevent duplicate memory page loading
    
    * fix(web): lint
    
    * fix(web): memories page improvements
    
    * chore(mobile): lint
    
    * fix(mobile): fix translations
    
    * fix(mobile): build errors
    
    * fix(web): prevent memories from infinitely loading
    
    * chore(e2e)): fix failng memory viewer test
    
    * chore(web): clean up types in memory manager
    
    * chore(web): lint
    
    * feat: update list and detail memory page layouts
    
    * onlyFavorites through drift
    
    * rename to get all
    
    * mobile: use widgetWhen in memory list page
    
    * remove redundant check
    
    * fix: e2e
    
    ---------
    
    Co-authored-by: Timon <mail@timonrieger.de>
    Co-authored-by: Jason Rasmussen <jason@rasm.me>
    Co-authored-by: shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>

--- mobile/lib/infrastructure/repositories/memory.repository.dart
+++ mobile/lib/infrastructure/repositories/memory.repository.dart
@@ -12,10 +12,7 @@ class MemoryRepository extends DatabaseAccessor<Drift> with $MemoryRepositoryMix
 
   Drift get _db => attachedDatabase;
 
-  Future<List<DriftMemory>> getAll(String ownerId) async {
-    final now = DateTime.now();
-    final localUtc = DateTime.utc(now.year, now.month, now.day, 0, 0, 0);
-
+  Future<List<DriftMemory>> getAll(String ownerId, {bool onlyToday = true, bool onlyFavorites = false}) async {
     final query =
         _db.select(_db.memoryEntity).join([
             innerJoin(_db.memoryAssetEntity, _db.memoryAssetEntity.memoryId.equalsExp(_db.memoryEntity.id)),
@@ -27,10 +24,21 @@ class MemoryRepository extends DatabaseAccessor<Drift> with $MemoryRepositoryMix
             ),
           ])
           ..where(_db.memoryEntity.ownerId.equals(ownerId))
-          ..where(_db.memoryEntity.deletedAt.isNull())
-          ..where(_db.memoryEntity.showAt.isNull() | _db.memoryEntity.showAt.isSmallerOrEqualValue(localUtc))
-          ..where(_db.memoryEntity.hideAt.isNull() | _db.memoryEntity.hideAt.isBiggerOrEqualValue(localUtc))
-          ..orderBy([OrderingTerm.desc(_db.memoryEntity.memoryAt), OrderingTerm.asc(_db.remoteAssetEntity.createdAt)]);
+          ..where(_db.memoryEntity.deletedAt.isNull());
+
+    if (onlyFavorites) {
+      query.where(_db.memoryEntity.isSaved.equals(true));
+    }
+
+    if (onlyToday) {
+      final now = DateTime.now();
+      final localUtc = DateTime.utc(now.year, now.month, now.day, 0, 0, 0);
+
+      query.where(_db.memoryEntity.showAt.isNull() | _db.memoryEntity.showAt.isSmallerOrEqualValue(localUtc));
+      query.where(_db.memoryEntity.hideAt.isNull() | _db.memoryEntity.hideAt.isBiggerOrEqualValue(localUtc));
+    }
+
+    query.orderBy([OrderingTerm.desc(_db.memoryEntity.memoryAt), OrderingTerm.asc(_db.remoteAssetEntity.createdAt)]);
 
     final rows = await query.get();
     if (rows.isEmpty) {
