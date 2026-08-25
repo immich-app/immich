fbd5dc2c618c986c09da63adb31b4c5382eaf5ce
bo0tzz <git@bo0tzz.me>
2026-08-25 17:47:25 +0200

    fix: don't swallow fetch errors (#30496)

--- server/src/repositories/oauth.repository.ts
+++ server/src/repositories/oauth.repository.ts
@@ -123,8 +123,7 @@ export class OAuthRepository {
         );
       }
 
-      this.logger.error(`OAuth login failed: ${error.message}`);
-      this.logger.error(error);
+      this.logger.error('OAuth login failed', error);
 
       throw new Error('OAuth login failed', { cause: error });
     }
@@ -223,7 +222,7 @@ export class OAuthRepository {
         },
       );
     } catch (error: any | AggregateError) {
-      this.logger.error(`Error in OAuth discovery: ${error}`, error?.stack, error?.errors);
+      this.logger.error('Error in OAuth discovery', error);
       throw new InternalServerErrorException(`Error in OAuth discovery: ${error}`, { cause: error });
     }
   }
