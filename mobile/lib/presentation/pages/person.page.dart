c5040b94e5bfc506bb0aaee4e1278b8d14644dc5
shenlong-tanwen <139912620+shalong-tanwen@users.noreply.github.com>
2026-09-01 23:09:46 +0530

    cleanup person page

--- mobile/lib/presentation/pages/person.page.dart
+++ mobile/lib/presentation/pages/person.page.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -9,121 +7,71 @@ import 'package:immich_mobile/presentation/widgets/people/person_option_sheet.wi
 import 'package:immich_mobile/presentation/widgets/timeline/timeline.widget.dart';
 import 'package:immich_mobile/providers/infrastructure/people.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/timeline.provider.dart';
-import 'package:immich_mobile/providers/routes.provider.dart';
 import 'package:immich_mobile/providers/user.provider.dart';
 import 'package:immich_mobile/routing/router.dart';
 import 'package:immich_mobile/utils/people.utils.dart';
 import 'package:immich_mobile/widgets/common/person_sliver_app_bar.dart';
 
 @RoutePage()
-class PersonPage extends ConsumerStatefulWidget {
+class PersonPage extends ConsumerWidget {
   final Person person;
 
   const PersonPage({super.key, required this.person});
 
-  @override
-  ConsumerState<PersonPage> createState() => _PersonPageState();
-}
-
-class _PersonPageState extends ConsumerState<PersonPage> {
-  late Person _person;
-
-  @override
-  void initState() {
-    super.initState();
-    _person = widget.person;
-  }
-
-  Future<bool> handleEditName(BuildContext context) async {
-    final result = await showNameEditModal(context, _person);
-
-    // result is not null && different person => merge occurred
-    if (result != null && result.id != _person.id && mounted) {
-      setState(() => _person = result);
-
-      await this.context.replaceRoute(PersonRoute(person: result));
-      return true;
+  Future<void> handleEditName(BuildContext context, Person person) async {
+    final mergedInto = await showNameEditModal(context, person);
+    if (mergedInto != null && context.mounted) {
+      await context.replaceRoute(PersonRoute(person: mergedInto));
     }
-    return false;
   }
 
-  Future<void> handleEditBirthday(BuildContext context) async {
-    await showBirthdayEditModal(context, _person);
-  }
-
-  Future<void> showOptionSheet(BuildContext context) {
+  Future<void> showOptionSheet(BuildContext context, Person person) {
     return showModalBottomSheet(
       context: context,
       backgroundColor: context.colorScheme.surface,
       isScrollControlled: false,
-      builder: (context) {
+      builder: (sheetContext) {
         return PersonOptionSheet(
           onEditName: () async {
-            final isMerge = await handleEditName(context);
-            if (!isMerge) {
-              ContextHelper(context).pop();
-            }
+            ContextHelper(sheetContext).pop();
+            await handleEditName(context, person);
           },
           onEditBirthday: () async {
-            await handleEditBirthday(context);
-            ContextHelper(context).pop();
+            ContextHelper(sheetContext).pop();
+            await showBirthdayEditModal(context, person);
           },
-          birthdayExists: _person.birthDate != null,
+          birthdayExists: person.birthDate != null,
         );
       },
     );
   }
 
   @override
-  Widget build(BuildContext context) {
-    final personAsync = ref.watch(getPersonByIdProvider(_person.id));
-    ref.watch(currentRouteNameProvider.select((name) => name ?? PersonRoute.name));
+  Widget build(BuildContext context, WidgetRef ref) {
+    final person = ref.watch(getPersonByIdProvider(this.person.id)).valueOrNull ?? this.person;
 
-    return personAsync.when(
-      data: (personByIdProvider) {
-        if (personByIdProvider == null) {
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            if (mounted) {
-              final currentRoute = AutoRouter.of(context).current;
-              final currentArgs = currentRoute.args;
-              // Check if we are currently on the PersonRoute that corresponds to the deleted _person
-              if (currentRoute.name == PersonRoute.name &&
-                  currentArgs is PersonRouteArgs &&
-                  currentArgs.person.id == _person.id) {
-                unawaited(AutoRouter.of(context).replace(const PeopleCollectionRoute()));
-              }
-            }
-          });
-          return const Center(child: CircularProgressIndicator());
-        }
+    return ProviderScope(
+      key: ValueKey(person.id),
+      overrides: [
+        timelineServiceProvider.overrideWith((ref) {
+          final user = ref.watch(currentUserProvider);
+          if (user == null) {
+            throw Exception('User must be logged in to view person timeline');
+          }
 
-        _person = personByIdProvider;
-        return ProviderScope(
-          key: ValueKey(_person.id),
-          overrides: [
-            timelineServiceProvider.overrideWith((ref) {
-              final user = ref.watch(currentUserProvider);
-              if (user == null) {
-                throw Exception('User must be logged in to view person timeline');
-              }
-
-              final timelineService = ref.read(timelineFactoryProvider).person(user.id, _person.id);
-              ref.onDispose(timelineService.dispose);
-              return timelineService;
-            }),
-          ],
-          child: Timeline(
-            appBar: PersonSliverAppBar(
-              person: _person,
-              onNameTap: () => handleEditName(context),
-              onBirthdayTap: () => handleEditBirthday(context),
-              onShowOptions: () => showOptionSheet(context),
-            ),
-          ),
-        );
-      },
-      loading: () => const Center(child: CircularProgressIndicator()),
-      error: (e, s) => Text('Error: $e'),
+          final timelineService = ref.read(timelineFactoryProvider).person(user.id, person.id);
+          ref.onDispose(timelineService.dispose);
+          return timelineService;
+        }),
+      ],
+      child: Timeline(
+        appBar: PersonSliverAppBar(
+          person: person,
+          onNameTap: () => handleEditName(context, person),
+          onBirthdayTap: () => showBirthdayEditModal(context, person),
+          onShowOptions: () => showOptionSheet(context, person),
+        ),
+      ),
     );
   }
 }
