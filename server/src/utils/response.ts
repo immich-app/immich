b4cf3d0980ed6bdb762ed80be9112f94ac4c9300
Jason Rasmussen <jason@rasm.me>
2026-09-11 14:05:04 -0400

    fix: sync client disconnect

--- server/src/utils/response.ts
+++ server/src/utils/response.ts
@@ -1,7 +1,43 @@
 import { CookieOptions, Response } from 'express';
 import { Duration } from 'luxon';
+import { Writable } from 'node:stream';
 import { CookieResponse } from 'src/dtos/auth.dto.js';
 import { ImmichCookie } from 'src/enum.js';
+
+export class ClientDisconnectedError extends Error {}
+
+export const waitForDrain = (response: Writable) =>
+  new Promise<void>((resolve, reject) => {
+    const cleanup = () => {
+      response.off('drain', onDrain);
+      response.off('close', onClose);
+      response.off('error', onError);
+    };
+
+    const onDrain = () => {
+      cleanup();
+      resolve();
+    };
+
+    const onClose = () => {
+      cleanup();
+      reject(new ClientDisconnectedError());
+    };
+
+    const onError = (error: Error) => {
+      cleanup();
+      reject(error);
+    };
+
+    response.once('drain', onDrain);
+    response.once('close', onClose);
+    response.once('error', onError);
+
+    // 'close' may already have fired
+    if (response.destroyed) {
+      onClose();
+    }
+  });
 
 export const respondWithCookie = <T>(res: Response, body: T, { isSecure, values }: CookieResponse) => {
   const defaults: CookieOptions = {
