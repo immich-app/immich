08e6924f66282271084ea2725ba67c2a2ab8c74d
bwees <brandonwees@gmail.com>
2026-09-14 03:28:03 -0500

    feat: improved storage template onboarding

---
+++ web/src/lib/modals/StorageTemplateVariablesModal.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import SupportedDatetimePanel from '$lib/components/admin-settings/SupportedDatetimePanel.svelte';
+  import SupportedVariablesPanel from '$lib/components/admin-settings/SupportedVariablesPanel.svelte';
+  import type { SystemConfigTemplateStorageOptionDto } from '@immich/sdk';
+  import { BasicModal, Stack } from '@immich/ui';
+  import { t } from 'svelte-i18n';
+
+  type Props = {
+    options: SystemConfigTemplateStorageOptionDto;
+    onClose: () => void;
+  };
+
+  const { options, onClose }: Props = $props();
+</script>
+
+<BasicModal title={$t('variables')} size="giant" {onClose}>
+  <Stack gap={4}>
+    <SupportedDatetimePanel {options} />
+    <SupportedVariablesPanel />
+  </Stack>
+</BasicModal>
