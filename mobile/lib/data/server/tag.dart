ccf4712c197993bf9d4a2fedcb7f661d0ee0f35e
Adam Gastineau <adam@agg.im>
2026-09-21 06:54:41 -0700

    Add comments to Tag HTTP repository

--- mobile/lib/data/server/tag.dart
+++ mobile/lib/data/server/tag.dart
@@ -11,16 +11,19 @@ class TagApiRepository extends ApiRepository {
 
   const TagApiRepository(this._api);
 
+  /// Apply every tag in [tagIds] to every asset in [assetIds], returning the number of assets successfully tagged
   Future<int> bulkTagAssets(List<String> assetIds, List<String> tagIds) async {
     final response = await _api.bulkTagAssets(TagBulkAssetsDto(assetIds: assetIds, tagIds: tagIds));
     return response?.count ?? 0;
   }
 
+  /// Retrieves all known tags
   Future<List<Tag>> getAll() async {
     final response = await checkNull(_api.getAllTags());
     return response.map(_toTag).toList();
   }
 
+  /// Create the tags named [values], returning the list of successfully created (or pre-existing) tags
   Future<List<Tag>> upsert(List<String> tags) async {
     final response = await checkNull(_api.upsertTags(TagUpsertDto(tags: tags)));
     return response.map(_toTag).toList();
