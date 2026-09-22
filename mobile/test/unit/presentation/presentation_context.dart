91247f1492320522774a303c653063cd25d46c4a
Adam Gastineau <adam@agg.im>
2026-09-22 11:29:33 -0700

    chore(mobile): migrate OCR, Tag, and UserMetadata to Store (#31704)
    * chore(mobile): migrate OCR, Tag, and UserMetadata to Store
    
    * Add comments to Tag HTTP repository
    
    * Add mounted check

--- mobile/test/unit/presentation/presentation_context.dart
+++ mobile/test/unit/presentation/presentation_context.dart
@@ -7,6 +7,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/constants/locales.dart';
 import 'package:immich_mobile/data/db/main/database.dart';
+import 'package:immich_mobile/data/server/tag.dart';
 import 'package:immich_mobile/domain/models/asset/base_asset.model.dart';
 import 'package:immich_mobile/domain/models/store.model.dart';
 import 'package:immich_mobile/domain/models/user.model.dart';
@@ -63,6 +64,7 @@ class PresentationContext {
     serverInfoServiceProvider.overrideWithValue(service.serverInfo),
     inLockedViewProvider.overrideWithValue(false),
     assetMediaRepositoryProvider.overrideWithValue(repository.assetMedia.api),
+    tagApiRepositoryProvider.overrideWithValue(service.tag.repo),
   ];
 
   Drift _mockDrift() {
