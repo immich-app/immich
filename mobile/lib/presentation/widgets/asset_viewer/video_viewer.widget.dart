60cebddef5dc76f63579efea69799ff961a63b4d
Adam Gastineau <adam@agg.im>
2026-09-24 10:37:30 -0700

    Fix type

--- mobile/lib/presentation/widgets/asset_viewer/video_viewer.widget.dart
+++ mobile/lib/presentation/widgets/asset_viewer/video_viewer.widget.dart
@@ -317,7 +317,7 @@ class NativeVideoViewerState extends ConsumerState<NativeVideoViewer> with Widge
     final isCasting = ref.watch(castProvider.select((c) => c.isCasting));
     final status = ref.watch(videoPlayerProvider(widget.asset.id).select((v) => v.status));
     // https://github.com/flutter/flutter/issues/97499: iOS platform views are only disposed in frames containing platform views, or on
-    // the frist frame after a platform view disappears. Animating this view away uses those disppearing frames. Instead, forcibly remove
+    // the first frame after a platform view disappears. Animating this view away uses those disppearing frames. Instead, forcibly remove
     // the view from the tree when we start a route transition, which has the side effect of properly ordering the `dispose`
     final isRouteActive = ModalRoute.of(context)?.isActive ?? true;
     final showPlayer = !isCasting && isRouteActive;
